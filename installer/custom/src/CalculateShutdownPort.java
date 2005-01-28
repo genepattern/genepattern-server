816bef78795be2f111cda76a8afd7f945b48bffd
liefeld <liefeld@broadinstitute.org>
2005-01-28 15:56:57 +0000

    no message
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ installer/custom/src/CalculateShutdownPort.java
@@ -0,0 +1,102 @@
+/*
+
+ * RegisterGenePattern.java
+
+ *
+
+ * Created on January 14, 2004, 6:54 AM
+
+ */
+
+package custom;
+
+
+
+import com.zerog.ia.api.pub.*;
+
+import java.net.*;
+
+import java.io.*;
+
+/**
+
+ * custom class for calculating a shutdown port for the GP server
+
+ * expects
+
+ * GENEPATTERN_PORT = integer port for web server
+
+ * @author  Liefeld
+
+ */
+
+public class CalculateShutdownPort extends CustomCodeAction {
+
+    
+
+    
+
+    /** Creates a new instance of RegisterGenePattern */
+
+    public CalculateShutdownPort() {
+
+    }
+
+    
+
+    public String getInstallStatusMessage() {
+
+        return "CalculateShutdownPort";
+
+    }
+
+    
+
+    public String getUninstallStatusMessage() {
+
+        return "";
+
+    }
+
+    
+
+    public void install(com.zerog.ia.api.pub.InstallerProxy ip) throws com.zerog.ia.api.pub.InstallException {
+
+   
+
+       
+        StringBuffer escapedDirs = new StringBuffer();
+
+        String sport = (String)ip.getVariable( "GENEPATTERN_PORT" );
+
+        int port = Integer.parseInt(sport);
+        
+        int shutdownPort = port - 75;    
+        
+        String ssport = "" + shutdownPort;
+         
+
+        ip.setVariable("GENEPATTERN_SHUTDOWN_PORT",  ssport);
+
+
+
+    }
+
+    
+
+    
+
+    
+
+    public void uninstall(com.zerog.ia.api.pub.UninstallerProxy uninstallerProxy) throws com.zerog.ia.api.pub.InstallException {
+
+    // do nothing on uninstall
+
+    
+
+    }
+
+    
+
+}
+
---
+++ installer/custom/src/URLCaller.java
@@ -0,0 +1,114 @@
+/*
+ * Created on Oct 10, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package custom;
+
+import com.zerog.ia.api.pub.*;
+import java.net.*;
+import java.io.*;
+/**
+ * @author Liefeld
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class URLCaller  extends CustomCodeAction {
+
+     public URLCaller(){
+         
+     }
+     
+    
+      public String getInstallStatusMessage() {
+
+        return "CalculateShutdownPort";
+
+    }
+
+    
+
+    public String getUninstallStatusMessage() {
+
+        return "";
+
+    }
+   public void install(com.zerog.ia.api.pub.InstallerProxy ip) throws com.zerog.ia.api.pub.InstallException {
+
+       String running = "false";
+       String message = "";
+       
+       try {
+        String sport = (String)ip.getVariable( "GENEPATTERN_PORT" );
+
+        URL url = new URL("http://localhost:" + sport +"/gp/index.jsp");	
+        System.out.println("Connecting to : " + url);
+		
+	HttpURLConnection uconn = (HttpURLConnection)url.openConnection();
+        int rc = uconn.getResponseCode();
+        
+        
+        if (rc <400){
+                running = "true";     
+                message = "GenePattern appears to already be running on your system.";
+        } else if (rc >= 400){
+                running = "true";
+                message = "A web server appears to already be running on your system. ";
+        }
+        
+        
+        ip.setVariable("GENEPATTERN_RUNNING_TEST_RESPONSE",  "" + rc);
+        
+       } catch (Exception e){
+           running = "false";
+           ip.setVariable("GENEPATTERN_RUNNING_TEST_EXCEPTION",  e.toString());
+       }
+        ip.setVariable("GENEPATTERN_RUNNING_TEST",  running);
+        ip.setVariable("GENEPATTERN_RUNNING_MESSAGE",  message);
+
+    }
+    
+
+    public void uninstall(com.zerog.ia.api.pub.UninstallerProxy ip) throws com.zerog.ia.api.pub.InstallException {
+  String running = "false";
+  String message = "";
+       try {
+        String sport = (String)ip.getVariable( "GENEPATTERN_PORT" );
+
+        URL url = new URL("http://localhost:" + sport +"/gp/index.jsp");	
+        System.out.println("Connecting to : " + url);
+		
+	HttpURLConnection uconn = (HttpURLConnection)url.openConnection();
+        int rc = uconn.getResponseCode();
+         if (rc <400){
+                running = "true";     
+                message = "GenePattern appears to already be running on your system.";
+        } else if (rc >= 400){
+                running = "true";
+                message = "A web server appears to already be running on your system. ";
+        }
+        
+        
+        ip.setVariable("GENEPATTERN_RUNNING_TEST_RESPONSE",  "" + rc);
+        
+       } catch (Exception e){
+           running = "false";
+           ip.setVariable("GENEPATTERN_RUNNING_TEST_EXCEPTION",  e.toString());
+       }
+        ip.setVariable("GENEPATTERN_RUNNING_TEST",  running);
+        ip.setVariable("GENEPATTERN_RUNNING_MESSAGE",  message);
+    
+
+    }
+
+    
+/**
+ * Call a URL from the command line and write the returned html to a file
+ * passing along input parameters provided as name=value pairs
+ * @param args
+ */
+
+	
+}
