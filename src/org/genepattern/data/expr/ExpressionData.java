b100ba85abeae94cebb234f9f44743b4f0a3a8f4
jgould <jgould@broadinstitute.org>
2005-02-11 15:14:01 +0000

    Added additional accessor methods
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@82 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/data/expr/ExpressionData.java
+++ src/org/genepattern/data/expr/ExpressionData.java
@@ -1,147 +1,196 @@
 package org.genepattern.data.expr;
 import org.genepattern.data.matrix.DoubleMatrix2D;
 
-
 /**
  *  Implementation of IExpressionData interface
  *
  * @author    Joshua Gould
  */
 public class ExpressionData implements IExpressionData {
-	protected DoubleMatrix2D dataset;
-	protected String[] rowDescriptions;
-	protected String[] columnDescriptions;
+   protected DoubleMatrix2D dataset;
+   protected String[] rowDescriptions;
+   protected String[] columnDescriptions;
 
 
-	public ExpressionData(DoubleMatrix2D dataset, String[] _rowDescriptions, String[] _columnDescriptions) {
-		this.dataset = dataset;
-		if(_rowDescriptions != null) {
-			if(_rowDescriptions.length != dataset.getRowCount()) {
-				throw new IllegalArgumentException("Length of row descriptions not equal to number of rows in matrix.");
-			}
-			this.rowDescriptions = _rowDescriptions;
-		}
+   public ExpressionData(DoubleMatrix2D dataset, String[] _rowDescriptions, String[] _columnDescriptions) {
+      this.dataset = dataset;
+      if(_rowDescriptions != null) {
+         if(_rowDescriptions.length != dataset.getRowCount()) {
+            throw new IllegalArgumentException("Length of row descriptions not equal to number of rows in matrix.");
+         }
+         this.rowDescriptions = _rowDescriptions;
+      }
 
-		if(_columnDescriptions != null) {
-			if(_columnDescriptions.length != dataset.getColumnCount()) {
-				throw new IllegalArgumentException("Length of column descriptions not equal to number of columns in matrix.");
-			}
-			this.columnDescriptions = _columnDescriptions;
-		}
-	}
-
-
-	public ExpressionData slice(int[] rowIndices, int[] columnIndices) {
-		if(rowIndices == null) {
-			rowIndices = new int[dataset.getRowCount()];
-			for(int i = dataset.getRowCount(); --i >= 0; ) {
-				rowIndices[i] = i;
-			}
-		}
-		if(columnIndices == null) {
-			columnIndices = new int[dataset.getColumnCount()];
-			for(int i = dataset.getColumnCount(); --i >= 0; ) {
-				columnIndices[i] = i;
-			}
-		}
-
-		DoubleMatrix2D newDoubleMatrix2D = dataset.slice(rowIndices, columnIndices);
-		String[] newRowAnnotations = null;
-
-		if(rowDescriptions != null) {
-			newRowAnnotations = new String[rowIndices.length];
-			for(int i = 0, length = rowIndices.length; i < length; i++) {
-				newRowAnnotations[i] = rowDescriptions[rowIndices[i]];
-			}
-		}
-
-		String[] newColumnAnnotations = null;
-
-		if(columnDescriptions != null) {
-			newColumnAnnotations = new String[columnIndices.length];
-			for(int j = 0, length = columnIndices.length; j < length; j++) {
-				newColumnAnnotations[j] = columnDescriptions[columnIndices[j]];
-			}
-		}
-
-		return new ExpressionData(newDoubleMatrix2D, newRowAnnotations, newColumnAnnotations);
-	}
+      if(_columnDescriptions != null) {
+         if(_columnDescriptions.length != dataset.getColumnCount()) {
+            throw new IllegalArgumentException("Length of column descriptions not equal to number of columns in matrix.");
+         }
+         this.columnDescriptions = _columnDescriptions;
+      }
+   }
 
 
-	public void setRowDescription(int row, String description) {
-		rowDescriptions[row] = description;
-	}
+   public ExpressionData slice(int[] rowIndices, int[] columnIndices) {
+      if(rowIndices == null) {
+         rowIndices = new int[dataset.getRowCount()];
+         for(int i = dataset.getRowCount(); --i >= 0; ) {
+            rowIndices[i] = i;
+         }
+      }
+      if(columnIndices == null) {
+         columnIndices = new int[dataset.getColumnCount()];
+         for(int i = dataset.getColumnCount(); --i >= 0; ) {
+            columnIndices[i] = i;
+         }
+      }
+
+      DoubleMatrix2D newDoubleMatrix2D = dataset.slice(rowIndices, columnIndices);
+      String[] newRowAnnotations = null;
+
+      if(rowDescriptions != null) {
+         newRowAnnotations = new String[rowIndices.length];
+         for(int i = 0, length = rowIndices.length; i < length; i++) {
+            newRowAnnotations[i] = rowDescriptions[rowIndices[i]];
+         }
+      }
+
+      String[] newColumnAnnotations = null;
+
+      if(columnDescriptions != null) {
+         newColumnAnnotations = new String[columnIndices.length];
+         for(int j = 0, length = columnIndices.length; j < length; j++) {
+            newColumnAnnotations[j] = columnDescriptions[columnIndices[j]];
+         }
+      }
+
+      return new ExpressionData(newDoubleMatrix2D, newRowAnnotations, newColumnAnnotations);
+   }
 
 
-	public void setColumnDescription(int column, String description) {
-		columnDescriptions[column] = description;
-	}
+   public void setRowDescription(int row, String description) {
+      rowDescriptions[row] = description;
+   }
 
 
-	public void setColumnName(int column, String name) {
-		dataset.setColumnName(column, name);
-	}
+   public void setColumnDescription(int column, String description) {
+      columnDescriptions[column] = description;
+   }
 
 
-	public void setRowName(int row, String name) {
-		dataset.setRowName(row, name);
-	}
+   public void setColumnName(int column, String name) {
+      dataset.setColumnName(column, name);
+   }
 
 
-	public String getColumnDescription(int column) {
-		if(columnDescriptions != null) {
-			return columnDescriptions[column];
-		}
-		return null;
-	}
+   public void setRowName(int row, String name) {
+      dataset.setRowName(row, name);
+   }
 
 
-	public String getRowDescription(int row) {
-		if(rowDescriptions != null) {
-			return rowDescriptions[row];
-		}
-		return null;
-	}
+   public String getColumnDescription(int column) {
+      if(columnDescriptions != null) {
+         return columnDescriptions[column];
+      }
+      return null;
+   }
 
 
-	public int getRowCount() {
-		return dataset.getRowCount();
-	}
+   public String getRowDescription(int row) {
+      if(rowDescriptions != null) {
+         return rowDescriptions[row];
+      }
+      return null;
+   }
 
 
-	public int getColumnCount() {
-		return dataset.getColumnCount();
-	}
+   public int getRowCount() {
+      return dataset.getRowCount();
+   }
 
 
-	public double getValue(int row, int column) {
-		return dataset.get(row, column);
-	}
-   
-   
+   public int getColumnCount() {
+      return dataset.getColumnCount();
+   }
+
+
+   public double getValue(int row, int column) {
+      return dataset.get(row, column);
+   }
+
+
    public String getValueAsString(int row, int column) {
-		return String.valueOf(dataset.get(row, column));
-	}
+      return String.valueOf(dataset.get(row, column));
+   }
 
 
-	public String getColumnName(int column) {
-		return dataset.getColumnName(column);
-	}
+   public String getColumnName(int column) {
+      return dataset.getColumnName(column);
+   }
 
 
-	public String getRowName(int row) {
-		return dataset.getRowName(row);
-	}
+   public String getRowName(int row) {
+      return dataset.getRowName(row);
+   }
 
 
-	/**
-	 *  Gets a 2-dimensional matrix that holds the expression values
-	 *
-	 * @return    The expression matrix
-	 */
-	public org.genepattern.data.matrix.DoubleMatrix2D getExpressionMatrix() {
-		return dataset;
-	}
+   /**
+    *  Allocates a new array contains the row names
+    *
+    * @return    The row names.
+    */
+   public String[] getRowNames() {
+      return dataset.getRowNames();
+   }
+
+
+   /**
+    *  Allocates a new array contains the row descriptions
+    *
+    * @return    The row descriptions.
+    */
+   public String[] getRowDescriptions() {
+      return (String[]) rowDescriptions.clone();
+   }
+
+
+   /**
+    *  Allocates a new array contains the column names
+    *
+    * @return    The column names.
+    */
+   public String[] getColumnNames() {
+      return dataset.getColumnNames();
+   }
+
+
+   /**
+    *  Allocates a new array contains the column descriptions
+    *
+    * @return    The column descriptions.
+    */
+   public String[] getColumnDescriptions() {
+      return (String[]) columnDescriptions.clone();
+   }
+
+
+   /**
+    *  Gets the 2-dimensional matrix that holds the expression values
+    *
+    * @return    The expression matrix
+    */
+   public org.genepattern.data.matrix.DoubleMatrix2D getExpressionMatrix() {
+      return dataset;
+   }
+
+
+   /**
+    *  Gets the underlying double[][] array
+    *
+    * @return    the array
+    */
+   public double[][] getArray() {
+      return dataset.getArray();
+   }
 
 }
 
