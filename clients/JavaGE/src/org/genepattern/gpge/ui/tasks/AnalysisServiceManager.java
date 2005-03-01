a065bfb60bf0135518c2cf444739a055c8638164
jgould <jgould@broadinstitute.org>
2005-03-01 23:20:13 +0000

    removed getInstance method and added constructor. Added javadoc.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@181 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/tasks/AnalysisServiceManager.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/AnalysisServiceManager.java
@@ -1,75 +1,98 @@
 package org.genepattern.gpge.ui.tasks;
-import java.util.*;
-import org.genepattern.data.pipeline.JobSubmission;
-import org.genepattern.data.pipeline.PipelineModel;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import org.genepattern.util.GPConstants;
-import org.genepattern.webservice.*;
-import org.genepattern.gpge.ui.tasks.*;
+import org.genepattern.webservice.AnalysisService;
+import org.genepattern.webservice.AnalysisWebServiceProxy;
+import org.genepattern.webservice.TaskInfo;
+import org.genepattern.webservice.WebServiceException;
+
 /**
- *  Description of the Class
+ *  Mantains an internal cache of the latest analysis services. Retrieves older
+ *  versions of services when requested.
  *
  * @author    Joshua Gould
  */
 public class AnalysisServiceManager {
    private String server;
    private String username;
-   private Map lsidOrTaskName2AnalysisService;
    private String axisServletURL;
-   private static Map server2Instance = new HashMap();
+   private Map lsidOrTaskName2AnalysisService;
 
+
+   /**
+    *  Creates a new instance
+    *
+    * @param  server                   A server URL, for example
+    *      http://127.0.0.1:8080
+    * @param  username                 The username
+    */
+   public AnalysisServiceManager(String server, String username) {
+      this.server = server;
+      this.axisServletURL = server + "/gp/servlet/AxisServlet";
+      this.username = username;
+   }
+
+
+   /**
+    *  Retrieves the latest versions of all analysis services from the server
+    *  stores the analysis services internally
+    *
+    * @exception  WebServiceException  Description of the Exception
+    */
+   public void refresh() throws WebServiceException {
+      lsidOrTaskName2AnalysisService = new HashMap();
+
+      TaskInfo[] tasks = new AnalysisWebServiceProxy(axisServletURL, username, "").getTasks();
+      for(int i = 0; i < tasks.length; i++) {
+         TaskInfo task = tasks[i];
+         String lsid = (String) task.getTaskInfoAttributes().get(GPConstants.LSID);
+         String lsidOrTaskName = lsid != null ? lsid : task.getName();
+         lsidOrTaskName2AnalysisService.put(lsidOrTaskName, new AnalysisService(server, axisServletURL, task));
+      }
+
+   }
+
+
+   /**
+    *  Gets the internal cache of the latest analysis services. Invoke refresh
+    *  to update the internal cache.
+    *
+    * @return    the collections of the latest analysis services
+    */
    public Collection getLatestAnalysisServices() {
       return lsidOrTaskName2AnalysisService.values();
    }
 
-   public String getServer() {
-      return server;  
-   }
-   
-   public String getUsername() {
-      return username;  
-   }
-   
+
    /**
-    * @param  server    A server URL, for example http://127.0.0.1:8080
-    * @param  username  The username
+    *  Gets the server
+    *
+    * @return    the server
     */
-   private AnalysisServiceManager(String server, String username) {
-      this.server = server;
-      axisServletURL = server + "/gp/servlet/AxisServlet";
-      this.username = username;
-   }
-   
-   public void refresh() {
-      lsidOrTaskName2AnalysisService = new HashMap();
-      try {
-         TaskInfo[] tasks = new AnalysisWebServiceProxy(axisServletURL, username, "").getTasks();
-         for(int i = 0; i < tasks.length; i++) {
-            TaskInfo task = tasks[i];
-            String lsid = (String) task.getTaskInfoAttributes().get(GPConstants.LSID);
-            String lsidOrTaskName = lsid != null ? lsid : task.getName();
-            lsidOrTaskName2AnalysisService.put(lsidOrTaskName, new AnalysisService(server, axisServletURL, task));
-         }
-      } catch(WebServiceException wse) {
-
-      }
+   public String getServer() {
+      return server;
    }
 
 
-   public void disconnect() {
-      server2Instance.remove(this.getServer());
+   /**
+    *  Gets the username
+    *
+    * @return    the username
+    */
+   public String getUsername() {
+      return username;
    }
 
 
-   public static AnalysisServiceManager getInstance(String server, String username) {
-      AnalysisServiceManager registry = (AnalysisServiceManager) server2Instance.get(server);
-      if(registry == null) {
-         registry = new AnalysisServiceManager(server, username);
-         server2Instance.put(server, registry);
-      }
-      return registry;
-   }
-
-
+   /**
+    *  Gets the analysis service with the given task name or lsid or <code>null</code>
+    *  if no such service exists.
+    *
+    * @param  lsidOrTaskName  an LSID or task name
+    * @return                 the analysis service
+    */
    public AnalysisService getAnalysisService(String lsidOrTaskName) {
       if(lsidOrTaskName == null) {
          return null;
