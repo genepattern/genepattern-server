49662ed78d4618f5a73a2206cf5e72e6122b7cc3
jgould <jgould@broadinstitute.org>
2005-03-01 20:28:23 +0000

    Info about jobs
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@171 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/JobEvent.java
@@ -0,0 +1,15 @@
+package org.genepattern.gpge.ui.tasks;
+import org.genepattern.webservice.AnalysisJob;
+
+public class JobEvent extends java.util.EventObject {
+   private AnalysisJob job;
+   
+   public JobEvent(Object source, AnalysisJob job) {
+      super(source);
+      this.job = job;
+   }
+   
+   public AnalysisJob getJob() {
+      return job;  
+   }
+}
---
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/JobListener.java
@@ -0,0 +1,17 @@
+package org.genepattern.gpge.ui.tasks;
+
+/**
+ *  Description of the Interface
+ *
+ * @author    Joshua Gould
+ */
+public interface JobListener extends java.util.EventListener {
+
+   public void jobStatusChanged(JobEvent e);
+
+
+   public void jobCompleted(JobEvent e);
+
+
+   public void jobAdded(JobEvent e);
+}
