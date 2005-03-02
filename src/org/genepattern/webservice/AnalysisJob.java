031f1aa3fff008ff79d94a128a5b447647e3f7e0
jgould <jgould@broadinstitute.org>
2005-03-02 20:22:37 +0000

    Renamed getSiteName to getServer. Added javadoc.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@217 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/webservice/AnalysisJob.java
+++ src/org/genepattern/webservice/AnalysisJob.java
@@ -1,55 +1,118 @@
 package org.genepattern.webservice;
 
-
 import java.io.Serializable;
 
-
 /**
- * <p>Title: AnalysisJob.java</p>
- * <p>Description: Includes site name, task name and JobInfo about a analysis job. Can be serialized. </p>
- * @author Hui Gong
- * @version $Revision 1.2 $
+ *  Includes server, task name and JobInfo about a analysis job.
+ *
+ * @author     Hui Gong
+ * @version    $Revision 1.2 $
  */
 
-public class AnalysisJob implements Serializable{
-    private String _siteName;
-    private String _taskName;
-    private JobInfo _job;
-	 private String lsid;
-	 
-    public AnalysisJob(String siteName, String taskName, JobInfo job){
-        this._siteName = siteName;
-        this._taskName = taskName;
-        this._job = job;
-    }
+public class AnalysisJob implements Serializable {
+   private String server;
+   private String taskName;
+   private JobInfo job;
+   private String lsid;
 
-    public void setJobInfo(JobInfo job){
-        this._job = job;
-    }
 
-    public String getSiteName(){
-        return this._siteName;
-    }
-	 
-	 
+   /**  Constructs a new <tt>AnalysisJob</tt> .  */
+   public AnalysisJob() { }
 
-    public String getTaskName(){
-        return this._taskName;
-    }
 
-    public JobInfo getJobInfo(){
-        return this._job;
-    }
+   /**
+    *  Constructs a new <tt>AnalysisJob</tt>
+    *
+    * @param  server    The server on which this analysis job was run
+    * @param  taskName  The task name
+    * @param  job       The <tt>JobInfo</tt>
+    */
+   public AnalysisJob(String server, String taskName, JobInfo job) {
+      this.server = server;
+      this.taskName = taskName;
+      this.job = job;
+   }
 
-    public String toString(){
-        return String.valueOf(_job.getJobNumber());
-    }
-	 
-	 public String getLSID() {
-		return lsid; 
-	 }
-	 
-	 public void setLSID(String lsid) {
-		this.lsid = lsid; 
-	 }	
+
+   /**
+    *  Sets the server on which this analysis job was run
+    *
+    * @param  s  the server
+    */
+   public void setServer(String s) {
+      this.server = s;
+   }
+
+
+   /**
+    *  Sets the task name of the task that produced this analysis job.
+    *
+    * @param  s  The task name
+    */
+   public void setTaskName(String s) {
+      this.taskName = s;
+   }
+
+
+   /**
+    *  Sets the <tt>JobInfo</tt> for this analysis job
+    *
+    * @param  job  The <tt>JobInfo</tt>
+    */
+   public void setJobInfo(JobInfo job) {
+      this.job = job;
+   }
+
+
+
+   /**
+    *  Sets the LSID of the task that produced this analysis job.
+    *
+    * @param  lsid  The task LSID
+    */
+   public void setLSID(String lsid) {
+      this.lsid = lsid;
+   }
+
+
+   /**
+    *  Gets the server on which this analysis job was run
+    *
+    * @return    the server
+    */
+   public String getServer() {
+      return this.server;
+   }
+
+
+   /**
+    *  Gets the LSID of the task that produced this analysis job.
+    *
+    * @return    The task LSID
+    */
+
+   public String getLSID() {
+      return lsid;
+   }
+
+
+   /**
+    *  Gets the task name of the task that produced this analysis job.
+    *
+    * @return    The task name
+    */
+   public String getTaskName() {
+      return this.taskName;
+   }
+
+
+   /**
+    *  Gets the <tt>JobInfo</tt> for this analysis job
+    *
+    * @return    The <tt>JobInfo</tt>
+    */
+   public JobInfo getJobInfo() {
+      return this.job;
+   }
+
 }
