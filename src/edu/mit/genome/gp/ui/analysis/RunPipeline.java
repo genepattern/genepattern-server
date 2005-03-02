a8f239acdd910cd3c942618b62408b86dc58cb8e
jgould <jgould@broadinstitute.org>
2005-03-02 00:26:12 +0000

    Forwards methods to refactored class
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@185 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/edu/mit/genome/gp/ui/analysis/RunPipeline.java
@@ -0,0 +1,17 @@
+package edu.mit.genome.gp.ui.analysis;
+
+/**
+ *  Forwards main method to org.genepattern.server.webapp.RunPipeline
+ *
+ * @author    Joshua Gould
+ */
+public class RunPipeline {
+
+   private RunPipeline() { }// prevent instantiation
+
+
+   public static void main(String args[]) throws Exception {
+      org.genepattern.server.webapp.RunPipeline.main(args);
+   }
+}
+
---
+++ src/edu/mit/genome/gp/ui/analysis/RunPipelineNullDecorator.java
@@ -0,0 +1,41 @@
+package edu.mit.genome.gp.ui.analysis;
+import java.io.PrintStream;
+import org.genepattern.data.pipeline.JobSubmission;
+import org.genepattern.data.pipeline.PipelineModel;
+import org.genepattern.webservice.JobInfo;
+import org.genepattern.server.webapp.RunPipelineOutputDecoratorIF;
+
+/**
+ *  Forwards all methods to org.genepattern.server.webapp.RunPipelineNullDecorator
+ *
+ * @author    Joshua Gould
+ */
+public class RunPipelineNullDecorator implements RunPipelineOutputDecoratorIF {
+   RunPipelineNullDecorator decorator = new RunPipelineNullDecorator();
+
+
+   public void beforePipelineRuns(PipelineModel model) {
+      decorator.beforePipelineRuns(model);
+   }
+
+
+   public void recordTaskExecution(JobSubmission jobSubmission, int idx, int numSteps) {
+      decorator.recordTaskExecution(jobSubmission, idx, numSteps);
+   }
+
+
+   public void recordTaskCompletion(JobInfo jobInfo, String name) {
+      decorator.recordTaskCompletion(jobInfo, name);
+   }
+
+
+   public void afterPipelineRan(PipelineModel model) {
+      decorator.afterPipelineRan(model);
+   }
+
+
+   public void setOutputStream(PrintStream outstr) {
+      decorator.setOutputStream(outstr);
+   }
+
+}
