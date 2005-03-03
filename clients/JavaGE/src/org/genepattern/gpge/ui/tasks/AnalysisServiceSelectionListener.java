6a0c2fb1b2983cde71fe25e6e449bbe150cc50c0
jgould <jgould@broadinstitute.org>
2005-03-03 03:57:10 +0000

    importing
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@225 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/AnalysisServiceSelectionListener.java
@@ -0,0 +1,10 @@
+package org.genepattern.gpge.ui.tasks;
+
+/**
+ *  A listener for receiving analysis service selection events.
+ *
+ * @author    Joshua Gould
+ */
+public interface AnalysisServiceSelectionListener extends java.util.EventListener {
+   public void valueChanged(AnalysisServiceSelectionEvent e);
+}
