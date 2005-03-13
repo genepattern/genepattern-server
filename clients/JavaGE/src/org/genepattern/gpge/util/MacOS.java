7f0c8457f3c30d7d008bf08d6c547c0e4bab9be4
jgould <jgould@broadinstitute.org>
2005-03-13 19:35:10 +0000

    Reveals file in finder
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@481 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ clients/JavaGE/src/org/genepattern/gpge/util/MacOS.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.*;
+/**
+ *  Utilities for Mac OS X specific functions
+ *
+ * @author    Joshua Gould
+ */
+public class MacOS {
+   private MacOS() { }
+
+
+   /**
+    *  Selects the file specified by <tt>file</tt> in a new file viewer is
+    *  opened. Returns true if the file is successfully selected, false
+    *  otherwise.
+    *
+    * @param  file  the file to open
+    * @return       <tt>true</tt> if the file is opened successfully, <tt>false
+    *      </tt> otherwise
+    */
+   public static boolean showFileInFinder(File file) {
+      try {
+         if(file.exists()) {
+            Class NSWorkspaceClass = null;
+            if(new File("/System/Library/Java/com/apple/cocoa/application/NSWorkspace.class").exists()) {
+               ClassLoader classLoader = new URLClassLoader(new URL[]{new File("/System/Library/Java").toURL()});
+               NSWorkspaceClass = Class.forName("com.apple.cocoa.application.NSWorkspace", true, classLoader);
+            } else {
+               NSWorkspaceClass = Class.forName("com.apple.cocoa.application.NSWorkspace");
+            }
+
+            java.lang.reflect.Method sharedWorkspaceMethod = NSWorkspaceClass.getMethod("sharedWorkspace",
+                  null);
+            Object NSWorkspace = sharedWorkspaceMethod.invoke(null, null);
+
+            java.lang.reflect.Method selectFileMethod = NSWorkspace.getClass().getMethod("selectFile",
+                  new Class[]{String.class, String.class});
+
+            String path = file.getCanonicalPath();
+            Object opened = selectFileMethod.invoke(NSWorkspace, new Object[]{path, path});
+            if(opened instanceof Boolean) {
+               return ((Boolean) opened).booleanValue();
+            }
+         }
+         return false;
+      } catch(Throwable t) {
+         return false;
+      }
+   }
+}
