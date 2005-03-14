1eb8e96cde2f8b980c8874422b40f9d5690bd9fc
jgould <jgould@broadinstitute.org>
2005-03-14 18:25:58 +0000

    Renamed 'Not Started' to 'Pending' and removed 'Timeout'
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@498 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/webservice/JobStatus.java
+++ src/org/genepattern/webservice/JobStatus.java
@@ -21,17 +21,13 @@ public class JobStatus {
 
 	public static int JOB_ERROR = 4;
 
-	public static int JOB_TIMEOUT = 5;
-
-	public static String NOT_STARTED = "Not Started";
+	public static String NOT_STARTED = "Pending";
 
 	public static String PROCESSING = "Processing";
 
 	public static String FINISHED = "Finished";
 
 	public static String ERROR = "Error";
-
-	public static String TIMEOUT = "Time Out";
 
    /** an unmodifiable map that maps a string representation of the status to the numberic representation */
 	public static final Map STATUS_MAP;
@@ -42,7 +38,6 @@ public class JobStatus {
 		statusHash.put(PROCESSING, new Integer(JOB_PROCESSING));
 		statusHash.put(FINISHED, new Integer(JOB_FINISHED));
 		statusHash.put(ERROR, new Integer(JOB_ERROR));
-		statusHash.put(TIMEOUT, new Integer(JOB_TIMEOUT));
       STATUS_MAP = Collections.unmodifiableMap(statusHash);
 	}
 
