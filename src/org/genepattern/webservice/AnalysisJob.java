46f88034fd1db1528215f68a2a8658bc9bc0a52e
jgould <jgould@broadinstitute.org>
2005-03-15 17:49:40 +0000

    undeprecated, used to hold information on the client about a job run on the server
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@516 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/webservice/AnalysisJob.java
+++ src/org/genepattern/webservice/AnalysisJob.java
@@ -3,117 +3,112 @@ package org.genepattern.webservice;
 import java.io.Serializable;
 
 /**
- * Includes server, task name and JobInfo about a analysis job.
- * 
- * @author Hui Gong
- * @version $Revision 1.2 $
- * @deprecated use JobInfo instead
+ *  Client side job information - includes server and JobInfo.
+ *
+ * @author        Hui Gong
+ * @version       $Revision 1.2 $
  */
 
 public class AnalysisJob implements Serializable {
-	private String server;
+   private String server;
+   private JobInfo job;
 
-	private String taskName;
 
-	private JobInfo job;
 
-	private String lsid;
+   /**  Constructs a new <tt>AnalysisJob</tt> . */
+   public AnalysisJob() { }
 
-	/** Constructs a new <tt>AnalysisJob</tt>. */
-	public AnalysisJob() {
-	}
 
-	/**
-	 * Constructs a new <tt>AnalysisJob</tt>
-	 * 
-	 * @param server
-	 *            The server on which this analysis job was run
-	 * @param taskName
-	 *            The task name
-	 * @param job
-	 *            The <tt>JobInfo</tt>
-	 */
-	public AnalysisJob(String server, String taskName, JobInfo job) {
-		this.server = server;
-		this.taskName = taskName;
-		this.job = job;
-	}
+   /**
+    *  Constructs a new <tt>AnalysisJob</tt>
+    *
+    * @param  server    The server on which this analysis job was run
+    * @param  job       The <tt>JobInfo</tt>
+    */
+   public AnalysisJob(String server, JobInfo job) {
+      this.server = server;
+      this.job = job;
+   }
 
-	/**
-	 * Sets the server on which this analysis job was run
-	 * 
-	 * @param s
-	 *            the server
-	 */
-	public void setServer(String s) {
-		this.server = s;
-	}
 
-	/**
-	 * Sets the task name of the task that produced this analysis job.
-	 * 
-	 * @param s
-	 *            The task name
-	 */
-	public void setTaskName(String s) {
-		this.taskName = s;
-	}
+   /**
+    *  Sets the server on which this analysis job was run
+    *
+    * @param  s  the server
+    */
+   public void setServer(String s) {
+      this.server = s;
+   }
 
-	/**
-	 * Sets the <tt>JobInfo</tt> for this analysis job
-	 * 
-	 * @param job
-	 *            The <tt>JobInfo</tt>
-	 */
-	public void setJobInfo(JobInfo job) {
-		this.job = job;
-	}
 
-	/**
-	 * Sets the LSID of the task that produced this analysis job.
-	 * 
-	 * @param lsid
-	 *            The task LSID
-	 */
-	public void setLSID(String lsid) {
-		this.lsid = lsid;
-	}
+   /**
+    *  Sets the task name of the task that produced this analysis job.
+    *
+    * @param  s  The task name
+    */
+   public void setTaskName(String s) {
+      job.setTaskName(s);
+   }
 
-	/**
-	 * Gets the server on which this analysis job was run
-	 * 
-	 * @return the server
-	 */
-	public String getServer() {
-		return this.server;
-	}
 
-	/**
-	 * Gets the LSID of the task that produced this analysis job.
-	 * 
-	 * @return The task LSID
-	 */
+   /**
+    *  Sets the <tt>JobInfo</tt> for this analysis job
+    *
+    * @param  job  The <tt>JobInfo</tt>
+    */
+   public void setJobInfo(JobInfo job) {
+      this.job = job;
+   }
 
-	public String getLSID() {
-		return lsid;
-	}
 
-	/**
-	 * Gets the task name of the task that produced this analysis job.
-	 * 
-	 * @return The task name
-	 */
-	public String getTaskName() {
-		return this.taskName;
-	}
+   /**
+    *  Sets the LSID of the task that produced this analysis job.
+    *
+    * @param  lsid  The task LSID
+    */
+   public void setLSID(String lsid) {
+      job.setTaskLSID(lsid);
+   }
 
-	/**
-	 * Gets the <tt>JobInfo</tt> for this analysis job
-	 * 
-	 * @return The <tt>JobInfo</tt>
-	 */
-	public JobInfo getJobInfo() {
-		return this.job;
-	}
+
+   /**
+    *  Gets the server on which this analysis job was run
+    *
+    * @return    the server
+    */
+   public String getServer() {
+      return this.server;
+   }
+
+
+   /**
+    *  Gets the LSID of the task that produced this analysis job.
+    *
+    * @return    The task LSID
+    */
+
+   public String getLSID() {
+      return job.getTaskLSID();
+   }
+
+
+   /**
+    *  Gets the task name of the task that produced this analysis job.
+    *
+    * @return    The task name
+    */
+   public String getTaskName() {
+      return job.getTaskName();
+   }
+
+
+   /**
+    *  Gets the <tt>JobInfo</tt> for this analysis job
+    *
+    * @return    The <tt>JobInfo</tt>
+    */
+   public JobInfo getJobInfo() {
+      return this.job;
+   }
 
 }
