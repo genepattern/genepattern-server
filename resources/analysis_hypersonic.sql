f0909d82ae4cc9d831ef3ba7a330e272d9583163
jgould <jgould@broadinstitute.org>
2005-03-16 21:31:03 +0000

    Added deleted flag to analysis_job table
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@539 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_hypersonic.sql
+++ resources/analysis_hypersonic.sql
@@ -18,11 +18,12 @@ create index idx_lsid_version on lsids(lsid_version);
 
 --Job Info table
 
-create table analysis_job(job_no int identity primary key, task_id int, status_id int, date_submitted timestamp, date_completed timestamp, parameter_info longvarchar, user_id longvarchar, isIndexed bit, access_id int, job_name longvarchar, lsid varchar(200), task_lsid varchar(200), task_name longvarchar, parent int);
+create table analysis_job(job_no int identity primary key, task_id int, status_id int, date_submitted timestamp, date_completed timestamp, parameter_info longvarchar, user_id longvarchar, isIndexed bit, access_id int, job_name longvarchar, lsid varchar(200), task_lsid varchar(200), task_name longvarchar, parent int, deleted bit default false);
 
-create index idx_job_lsid on analysis_job(lsid);
+create index idx_analysis_job_lsid on analysis_job(lsid);
 create index idx_analysis_job_parent on analysis_job(parent);
-create index idx_analysis_job_no on analysis_job(job_no);
+create index idx_analysis_job_job_no on analysis_job(job_no);
+create index idx_analysis_job_deleted on analysis_job(deleted);
 
 --Job Status Info table
 create table Job_status(status_id int, status_name varchar(20));
