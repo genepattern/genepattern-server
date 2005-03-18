6f8cb04e9fc8cc638101c8b9e58312c2be388194
jgould <jgould@broadinstitute.org>
2005-03-18 21:26:53 +0000

    Importing
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@562 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/webservice/server/local/LocalAnalysisClient.java
@@ -0,0 +1,47 @@
+package org.genepattern.server.webservice.server.local;
+
+import org.genepattern.server.webservice.server.Analysis;
+
+import org.genepattern.webservice.*;
+
+/**
+ *  local Analysis client
+ *
+ * @author    Joshua Gould
+ */
+public class LocalAnalysisClient {
+   Analysis service;
+   String userName;
+
+
+   public LocalAnalysisClient(final String userName) {
+      this.userName = userName;
+      service =
+         new Analysis() {
+            protected String getUsernameFromContext() {
+               return userName;
+            }
+         };
+   }
+
+
+   public void deleteJob(int jobId) throws WebServiceException {
+      service.deleteJob(jobId);
+   }
+
+
+   public void deleteJobResultFile(int jobId, String value) throws WebServiceException {
+      service.deleteJobResultFile(jobId, value);
+   }
+
+
+   public void terminateJob(int jobId) throws WebServiceException {
+      service.terminateJob(jobId);
+   }
+
+
+   public JobInfo[] getJobs(String username, int maxJobNumber, int maxEntries, boolean all) throws WebServiceException {
+      return service.getJobs(username, maxJobNumber, maxEntries, all);
+   }
+
+}
