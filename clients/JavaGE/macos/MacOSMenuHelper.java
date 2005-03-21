7e571307f89523360106b3c5b84d769fd0a986eb
jgould <jgould@broadinstitute.org>
2005-03-21 20:11:17 +0000

    Added quit handler
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@594 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/macos/MacOSMenuHelper.java
+++ clients/JavaGE/macos/MacOSMenuHelper.java
@@ -13,19 +13,24 @@ public class MacOSMenuHelper {
    private MacOSMenuHelper() { }
 
 
-   public static void registerAboutHandler() {
-      Application app2 = new Application();
-      app2.addApplicationListener(new AppHandler());
-      app2.setEnabledPreferencesMenu(false);
-      app2.setEnabledAboutMenu(true);
+   public static void registerHandlers() {
+      Application app = new Application();
+      app.addApplicationListener(new AppHandler());
+      app.setEnabledPreferencesMenu(false);
+      app.setEnabledAboutMenu(true);
    }
-
 
 
    static class AppHandler extends ApplicationAdapter {
       public void handleAbout(ApplicationEvent event) {
          event.setHandled(true);
          org.genepattern.gpge.GenePattern.showAbout();
+      }
+
+
+      public void handleQuit(ApplicationEvent event) {
+         event.setHandled(true);
+         System.exit(0);
       }
    }
 }
