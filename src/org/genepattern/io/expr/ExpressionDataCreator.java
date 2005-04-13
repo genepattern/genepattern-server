381fb87965153f0b847655911843b85d0fc45bc9
jgould <jgould@broadinstitute.org>
2005-04-13 00:19:09 +0000

    Added options to discard row and column descriptions
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@870 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/io/expr/ExpressionDataCreator.java
+++ src/org/genepattern/io/expr/ExpressionDataCreator.java
@@ -7,10 +7,10 @@ import org.genepattern.data.matrix.IntMatrix2D;
 import org.genepattern.io.ParseException;
 
 /**
- * Expression data creator that creates an instance of
- * edu.mit.broad.data.expr.ExpressionData
- * 
- * @author Joshua Gould
+ *  Expression data creator that creates an instance of
+ *  edu.mit.broad.data.expr.ExpressionData
+ *
+ *@author    Joshua Gould
  */
 public class ExpressionDataCreator implements IExpressionDataCreator {
 	IntMatrix2D calls;
@@ -20,6 +20,28 @@ public class ExpressionDataCreator implements IExpressionDataCreator {
 	String[] rowDescriptions;
 
 	String[] columnDescriptions;
+	boolean keepRowDescriptions = true;
+	boolean keepColumnDescriptions = true;
+
+
+	public ExpressionDataCreator() {
+		this(true, true);
+	}
+
+
+	/**
+	 *  Creates a new <tt>ExpressionDataCreator</tt> instance
+	 *
+	 *@param  keepRowDescriptions     whether the ExpressionData returned from <tt>
+	 *      create</tt> should include row descriptions
+	 *@param  keepColumnDescriptions  whether the ExpressionData returned from <tt>
+	 *      create</tt> should include column descriptions
+	 */
+	public ExpressionDataCreator(boolean keepRowDescriptions, boolean keepColumnDescriptions) {
+		this.keepRowDescriptions = keepRowDescriptions;
+		this.keepColumnDescriptions = keepColumnDescriptions;
+	}
+
 
 	public Object create() {
 		if (calls != null) {
@@ -30,37 +52,48 @@ public class ExpressionDataCreator implements IExpressionDataCreator {
 		}
 	}
 
+
 	public void call(int row, int column, int call) throws ParseException {
 		calls.set(row, column, call);
 	}
+
 
 	public void data(int row, int column, double d) throws ParseException {
 		data.set(row, column, d);
 	}
 
+
 	public void columnName(int j, String s) throws ParseException {
 		data.setColumnName(j, s);
 	}
+
 
 	public void rowName(int i, String s) throws ParseException {
 		data.setRowName(i, s);
 	}
 
+
 	public void rowDescription(int i, String s) throws ParseException {
-		rowDescriptions[i] = s;
+		if (keepRowDescriptions) {
+			rowDescriptions[i] = s;
+		}
 	}
 
+
 	public void columnDescription(int j, String s) throws ParseException {
-		columnDescriptions[j] = s;
+		if (keepColumnDescriptions) {
+			columnDescriptions[j] = s;
+		}
 	}
+
 
 	public void init(int rows, int columns, boolean hasRowDesc,
 			boolean hasColDesc, boolean hasCalls) throws ParseException {
 		data = new DoubleMatrix2D(rows, columns);
-		if (hasRowDesc) {
+		if (hasRowDesc && keepRowDescriptions) {
 			rowDescriptions = new String[rows];
 		}
-		if (hasColDesc) {
+		if (hasColDesc && keepColumnDescriptions) {
 			columnDescriptions = new String[columns];
 		}
 		if (hasCalls) {
