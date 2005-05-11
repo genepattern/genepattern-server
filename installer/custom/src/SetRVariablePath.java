53a273d7f7bd6a2ec4515c8d36f7e65418aaa38a
liefeld <liefeld@broadinstitute.org>
2005-05-11 12:48:01 +0000

    fix for setting R var on mac due to !@#$! IA
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1192 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ installer/custom/src/SetRVariablePath.java
@@ -0,0 +1,69 @@
+/*
+ * 
+ *
+ * Created on January 14, 2004, 6:54 AM
+ */
+
+package custom;
+
+
+
+import com.zerog.ia.api.pub.*;
+
+import java.net.*;
+import java.rmi.server.UID;
+import java.io.*;
+
+/**
+
+ * custom class for adding /resources to the value of $R$
+ * @author  Liefeld
+ */
+
+public class SetRVariablePath extends CustomCodeAction {
+  
+
+    /** Creates a new instance of RegisterGenePattern */
+
+    public SetRVariablePath () {
+    }
+  
+    public String getInstallStatusMessage() {
+        return "SetRVariablePath ";
+    }
+
+    public String getUninstallStatusMessage() {
+        return "";
+    }
+
+    
+
+    public void install(com.zerog.ia.api.pub.InstallerProxy ip) throws com.zerog.ia.api.pub.InstallException {
+	try {
+		 String R_Orig =  (String)ip.getVariable("HOST_ADDRESS");
+		 String R_new = ip.substitute("$R$");
+
+ 		ip.setVariable("R",  R_new + "/resources");
+
+		
+
+        
+       } catch (Exception e){
+       	e.printStackTrace();
+       }
+    }
+    
+
+	
+	
+	
+
+	
+
+    
+    public void uninstall(com.zerog.ia.api.pub.UninstallerProxy uninstallerProxy) throws com.zerog.ia.api.pub.InstallException {
+    // do nothing on uninstall
+    
+    }
+    
+}
