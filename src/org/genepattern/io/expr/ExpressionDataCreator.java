bbd54cd21e45df4f6c40919acf5a224c3ddc2795
jgould <jgould@broadinstitute.org>
2005-07-05 21:47:40 +0000

    Reformatted code
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1557 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/io/expr/ExpressionDataCreator.java
+++ src/org/genepattern/io/expr/ExpressionDataCreator.java
@@ -8,7 +8,7 @@ import org.genepattern.io.ParseException;
 
 /**
  *  Expression data creator that creates an instance of
- *  edu.mit.broad.data.expr.ExpressionData
+ *  org.genepattern.data.expr.ExpressionData
  *
  *@author    Joshua Gould
  */
@@ -18,17 +18,20 @@ public class ExpressionDataCreator implements IExpressionDataCreator {
 	double[][] data;
 
 	String[] rowDescriptions;
-   String[] rowNames;
-   String[] columnNames;
-	String[] columnDescriptions;
-	boolean keepRowDescriptions = true;
-	boolean keepColumnDescriptions = true;
 
+	String[] rowNames;
+
+	String[] columnNames;
+
+	String[] columnDescriptions;
+
+	boolean keepRowDescriptions = true;
+
+	boolean keepColumnDescriptions = true;
 
 	public ExpressionDataCreator() {
 		this(true, true);
 	}
-
 
 	/**
 	 *  Creates a new <tt>ExpressionDataCreator</tt> instance
@@ -38,42 +41,38 @@ public class ExpressionDataCreator implements IExpressionDataCreator {
 	 *@param  keepColumnDescriptions  whether the ExpressionData returned from <tt>
 	 *      create</tt> should include column descriptions
 	 */
-	public ExpressionDataCreator(boolean keepRowDescriptions, boolean keepColumnDescriptions) {
+	public ExpressionDataCreator(boolean keepRowDescriptions,
+			boolean keepColumnDescriptions) {
 		this.keepRowDescriptions = keepRowDescriptions;
 		this.keepColumnDescriptions = keepColumnDescriptions;
 	}
 
-
 	public Object create() {
-      DoubleMatrix2D matrix = new DoubleMatrix2D(data, rowNames, columnNames);
+		DoubleMatrix2D matrix = new DoubleMatrix2D(data, rowNames, columnNames);
 		if (calls != null) {
 			return new ResExpressionData(matrix, calls, rowDescriptions,
 					columnDescriptions);
 		} else {
-			return new ExpressionData(matrix, rowDescriptions, columnDescriptions);
+			return new ExpressionData(matrix, rowDescriptions,
+					columnDescriptions);
 		}
 	}
-
 
 	public void call(int row, int column, int call) throws ParseException {
 		calls.set(row, column, call);
 	}
 
-
 	public void data(int row, int column, double d) throws ParseException {
-		data[row][column] =  d;
+		data[row][column] = d;
 	}
-
 
 	public void columnName(int j, String s) throws ParseException {
 		columnNames[j] = s;
 	}
 
-
 	public void rowName(int i, String s) throws ParseException {
 		rowNames[i] = s;
 	}
-
 
 	public void rowDescription(int i, String s) throws ParseException {
 		if (keepRowDescriptions) {
@@ -81,19 +80,17 @@ public class ExpressionDataCreator implements IExpressionDataCreator {
 		}
 	}
 
-
 	public void columnDescription(int j, String s) throws ParseException {
 		if (keepColumnDescriptions) {
 			columnDescriptions[j] = s;
 		}
 	}
 
-
 	public void init(int rows, int columns, boolean hasRowDesc,
 			boolean hasColDesc, boolean hasCalls) throws ParseException {
-      data = new double[rows][columns];
-      rowNames = new String[rows];
-      columnNames = new String[columns];
+		data = new double[rows][columns];
+		rowNames = new String[rows];
+		columnNames = new String[columns];
 		if (hasRowDesc && keepRowDescriptions) {
 			rowDescriptions = new String[rows];
 		}
