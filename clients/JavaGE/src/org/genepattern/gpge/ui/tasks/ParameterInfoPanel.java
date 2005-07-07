589a97f9ae7a49b6528007508a9c1d33a911c117
jgould <jgould@broadinstitute.org>
2005-07-07 19:59:04 +0000

    Added option to use output from previous task
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1582 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/tasks/ParameterInfoPanel.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/ParameterInfoPanel.java
@@ -6,6 +6,10 @@ package org.genepattern.gpge.ui.tasks;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,11 +21,13 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.genepattern.gpge.GenePattern;
 import org.genepattern.gpge.PropertyManager;
 import org.genepattern.gpge.ui.graphics.draggable.ObjectTextField;
 import org.genepattern.gpge.ui.maindisplay.MainFrame;
@@ -31,7 +37,9 @@ import org.genepattern.util.GPConstants;
 import org.genepattern.webservice.ParameterInfo;
 
 import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
 
 /**
  * Displays an array of <tt>ParameterInfo</tt> objects
@@ -40,12 +48,36 @@ import com.jgoodies.forms.layout.FormLayout;
  *  
  */
 public class ParameterInfoPanel extends JPanel {
-    private Map parameterName2ComponentMap;
+    
+
+	private Map parameterName2ComponentMap;
 
     private List inputFileParameters;
 
     private List parameterDescriptions;
 
+	private FormLayout formLayout;
+	private int maxLabelWidth = 0;
+	final static int PARAMETER_LABEL_COLUMN = 1;
+	final static int PARAMETER_INPUT_FIELD_COLUMN = 3;
+	
+	private static final int ROWS_PER_PARAMETER = 4;
+	
+	final static int PARAMETER_ROW_OFFSET = 0;
+	final static int USE_OUTPUT_ROW_OFFSET = 1;
+	final static int DESCRIPTION_ROW_OFFSET = 2;
+	final static int SPACE_ROW_OFFSET = 3;
+	
+	public void setLabelWidth(int labelWidth) {
+		formLayout.setColumnSpec(PARAMETER_LABEL_COLUMN, new ColumnSpec(labelWidth + "px"));
+		formLayout.invalidateLayout(this);
+		formLayout.layoutContainer(this);
+	}
+	
+	public int getLabelWidth() {
+		return maxLabelWidth;
+	}
+	
     /**
      * Gets the component that is used to display the given parameter
      * 
@@ -149,9 +181,53 @@ public class ParameterInfoPanel extends JPanel {
                 .toArray(new ParameterInfo[0]);
         return actualParameterArray;
     }
-
     
-    public ParameterInfoPanel(String taskName, ParameterInfo[] params) {
+    private int getRowIndex(int i) {
+    		return  i * ROWS_PER_PARAMETER + 1;
+    }
+
+    /**
+     * Sets the vertical spacing between the description and the next input field
+     * @param size
+     */
+    public void setVerticalSpacing(int size) {
+    	 if (parameterName2ComponentMap.size() > 0) {
+    		 for(int i = 0; i < parameterName2ComponentMap.size(); i++) {
+    			 int row = getRowIndex(i) + SPACE_ROW_OFFSET;
+    			 formLayout.setRowSpec(row, new RowSpec(size + "px"));
+    		 }
+    		 formLayout.invalidateLayout(this);
+    		 formLayout.layoutContainer(this);
+    	 }
+    }
+    
+    /**
+     * 
+     * @param parameterIndex the index of the parameter, starting at 0
+     * @param previousTaskNames, the array of task names to display
+     * @param choices the array of output type choices to display
+     */
+	public void setUseInputFromPreviousTask(int parameterIndex, String[] previousTaskNames, String[] choices) {
+		int row = getRowIndex(parameterIndex) + USE_OUTPUT_ROW_OFFSET;
+		JLabel tasksLabel = new JLabel("or use output from");
+		JComboBox tasksComboBox = new JComboBox(previousTaskNames);
+		tasksComboBox.setBackground(getBackground());
+		
+		JLabel outputFilesLabel = new JLabel("output file");
+		JComboBox outputFilesComboBox = new JComboBox(choices);
+		outputFilesComboBox.setBackground(getBackground());
+		
+		JPanel p = new JPanel();
+		p.setBackground(getBackground());
+		p.add(tasksLabel);
+		p.add(tasksComboBox);
+		p.add(outputFilesLabel);
+		p.add(outputFilesComboBox);
+		CellConstraints cc = new CellConstraints();
+		this.add(p, cc.xy(PARAMETER_INPUT_FIELD_COLUMN, row));
+	}
+    
+     public ParameterInfoPanel(String taskName, ParameterInfo[] params) {
         int numParams = params!=null?params.length:0;
         parameterName2ComponentMap = new HashMap(numParams);
         parameterDescriptions = new ArrayList(numParams);
@@ -168,24 +244,33 @@ public class ParameterInfoPanel extends JPanel {
         if (params == null || params.length == 0) {
             this.add(new JLabel(taskName + " has no input parameters"));
         } else {
-            StringBuffer rowSpec = new StringBuffer();
-            for (int i = 0; i < params.length; i++) {
-                if (i > 0) {
-                    rowSpec.append(", ");
-                }
-                rowSpec.append("pref, pref, 12px");// input, description space
-            }
-            // input label, space, input field
-            //                     description
-            FormLayout formLayout = new FormLayout(
-                    "right:pref:none, 6px, left:default:grow", rowSpec
-                            .toString());
-            this.setLayout(formLayout);
+	            StringBuffer rowSpec = new StringBuffer();
+	            for (int i = 0; i < params.length; i++) {
+	                if (i > 0) {
+	                    rowSpec.append(", ");
+	                }
+	                rowSpec.append("pref, pref, pref, 12px");
+	                // input row
+	                // use output from row
+	                // description row
+	                // space
+	            }
+	            // input label, space, input field + browse button
+	            //                     description
+	            
+	            
+	            formLayout = new FormLayout( // 
+	                    "right:pref:none, 6px, left:default:none", rowSpec
+	                            .toString());
+	            
+	            this.setLayout(formLayout);
+        		
+            
             CellConstraints cc = new CellConstraints();
 
             for (int i = 0; i < params.length; i++) {
                 cc.gridWidth = 1;
-                ParameterInfo info = params[i];
+                final ParameterInfo info = params[i];
                 final String value = info.getValue();
                 Component input = null;
                 if (value == null || value.equals("")) {
@@ -197,21 +282,51 @@ public class ParameterInfoPanel extends JPanel {
                 } else {
                     input = createComboBox(info);
                 }
-                int row = i * 3 + 1;
-                JLabel inputLabel = new JLabel(AnalysisServiceDisplay
-                        .getDisplayString(info)
-                        + ":");
+                int row = getRowIndex(i);
+                Component inputLabel = new JLabel(AnalysisServiceDisplay
+                        .getDisplayString(info) + ":");
+                maxLabelWidth = Math.max(maxLabelWidth, inputLabel.getWidth());
                 if (!isOptional(params[i])) {
                     inputLabel.setFont(inputLabel.getFont().deriveFont(
                             java.awt.Font.BOLD));
                 }
-                this.add(inputLabel, cc.xy(1, row));
+                
+                this.add(inputLabel, cc.xy(PARAMETER_LABEL_COLUMN, row));
                 JLabel description = new JLabel(info.getDescription());
-                this.add(input, cc.xy(3, row));
+               
+                if (info.isInputFile()) {
+                	JPanel p = new JPanel();
+              
+                	p.setBackground(getBackground());
+                	FormLayout f = new FormLayout("left:pref:none, left:pref:none, left:default:none","pref");
+                	p.setLayout(f);
+
+                	JButton btn = new JButton("Browse...");
+                	btn.addActionListener(new ActionListener() {
+                		public void actionPerformed(ActionEvent e) {
+                			FileDialog fc = new FileDialog(GenePattern.getDialogParent());
+                			fc.setModal(true);
+                			fc.show();
+                			String f = fc.getFile();
+                			if(f!=null) {
+                				setValue(info.getName(), new File(f));
+                			}
+                		}
+                	});
+                	btn.setBackground(getBackground());
+               // 	Dimension d = btn.getPreferredSize();
+                //	d.height = size.height - 2;
+               // 	btn.setSize(d);
+                	p.add(input, cc.xy(1, 1));
+                	p.add(btn, cc.xy(2, 1));
+                	this.add(p, cc.xy(PARAMETER_INPUT_FIELD_COLUMN, row + PARAMETER_ROW_OFFSET));
+                } else {
+                	 this.add(input, cc.xy(PARAMETER_INPUT_FIELD_COLUMN, row + PARAMETER_ROW_OFFSET));
+                }
                 cc.hAlign = CellConstraints.FILL;
                 description.setVisible(showDescriptions);
                 parameterDescriptions.add(description);
-                this.add(description, cc.xy(3, row + 1));
+                this.add(description, cc.xy(PARAMETER_INPUT_FIELD_COLUMN, row + DESCRIPTION_ROW_OFFSET));
                 parameterName2ComponentMap.put(info.getName(), input);
             }
         }
