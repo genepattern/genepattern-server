96546277fbea0a9a39ddde3b60028180c0c08fa6
jgould <jgould@broadinstitute.org>
2005-07-08 20:53:16 +0000

    Added getBoolean and getInt property methods
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1594 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/PropertyManager.java
+++ clients/JavaGE/src/org/genepattern/gpge/PropertyManager.java
@@ -9,103 +9,116 @@ import java.io.OutputStream;
 
 import java.util.Properties;
 
-
 /**
  * Manages ~/gp/gp.properties file
  * 
  * @author Joshua Gould
  */
 public class PropertyManager {
-   private static Properties properties=new Properties();
-   private static Properties defaults=new Properties();
-   private static File propertiesFile;
-   private static File propertiesFileTemp;
+	private static Properties properties = new Properties();
 
-   private PropertyManager() {
-   }
+	private static Properties defaults = new Properties();
 
-   static {
-      String fs=File.separator;
-      String path=System.getProperty("user.home") + fs + "gp" + fs;
-      propertiesFile=new File(path + "gp.properties");
-      propertiesFileTemp=new File(path + "#save#gp.properties#");
-      createPropertiesFile();
-      try {
-         FileInputStream in=new FileInputStream(propertiesFile);
-         loadProperties(properties, in);
-      } catch(IOException ioe) {
-         ioe.printStackTrace();
-      }
-   }
+	private static File propertiesFile;
 
-   private static void createPropertiesFile() {
-      if(!propertiesFile.exists()) {
-         File parent=propertiesFile.getParentFile();
-         if(!parent.exists()) {
-            parent.mkdirs();
-         }
-         try {
-            propertiesFile.createNewFile();
-         } catch(IOException ioe) {
-            ioe.printStackTrace();
-         }
-      }
-   }
+	private static File propertiesFileTemp;
 
-   public static void saveProperties()
-                              throws IOException {
-      // FIXME make backup of properties in case the following fails
-      FileOutputStream out=new FileOutputStream(propertiesFileTemp);
-      saveProperties(out);
-      propertiesFile.delete();
-      propertiesFileTemp.renameTo(propertiesFile);
-   }
+	private PropertyManager() {
+	}
 
-   private static void saveProperties(OutputStream out)
-                               throws IOException {
-      createPropertiesFile();
-      properties.store(out, "GenePattern properties");
-      out.close();
-   }
+	static {
+		String fs = File.separator;
+		String path = System.getProperty("user.home") + fs + "gp" + fs;
+		propertiesFile = new File(path + "gp.properties");
+		propertiesFileTemp = new File(path + "#save#gp.properties#");
+		createPropertiesFile();
+		try {
+			FileInputStream in = new FileInputStream(propertiesFile);
+			loadProperties(properties, in);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
 
-   public static String getProperty(String name) {
-      String value=properties.getProperty(name);
-      if(value!=null) {
-         return value;
-      } else {
-         return getDefaultProperty(name);
-      }
-   }
+	private static void createPropertiesFile() {
+		if (!propertiesFile.exists()) {
+			File parent = propertiesFile.getParentFile();
+			if (!parent.exists()) {
+				parent.mkdirs();
+			}
+			try {
+				propertiesFile.createNewFile();
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+	}
 
-   /**
-    * Sets the given property. The change is not persisted until
-    * saveProperties is called. If <tt>value</tt> is <tt>null</tt>, then the 
-    * default value for the given property is used.
-    * 
-    * @param name the property name
-    * @param value the property value
-    */
-   public static void setProperty(String name, String value) {
-      if(value==null) {
-         value = getDefaultProperty(name);
-      }
-      properties.setProperty(name, value);
-   }
+	public static void saveProperties() throws IOException {
+		// FIXME make backup of properties in case the following fails
+		FileOutputStream out = new FileOutputStream(propertiesFileTemp);
+		saveProperties(out);
+		propertiesFile.delete();
+		propertiesFileTemp.renameTo(propertiesFile);
+	}
 
-   public static String getDefaultProperty(String name) {
-      return defaults.getProperty(name);
-   }
+	private static void saveProperties(OutputStream out) throws IOException {
+		createPropertiesFile();
+		properties.store(out, "GenePattern properties");
+		out.close();
+	}
 
-   public static void setDefaultProperty(String name, String value) {
-      defaults.setProperty(name, value);
-   }
+	public static String getProperty(String name) {
+		String value = properties.getProperty(name);
+		if (value != null) {
+			return value;
+		} else {
+			return getDefaultProperty(name);
+		}
+	}
 
-   private static void loadProperties(Properties into, InputStream in)
-                               throws IOException {
-      try {
-         into.load(in);
-      } finally {
-         in.close();
-      }
-   }
+	/**
+	 * Sets the given property. The change is not persisted until
+	 * saveProperties is called. If <tt>value</tt> is <tt>null</tt>, then the 
+	 * default value for the given property is used.
+	 * 
+	 * @param name the property name
+	 * @param value the property value
+	 */
+	public static void setProperty(String name, String value) {
+		if (value == null) {
+			value = getDefaultProperty(name);
+		}
+		properties.setProperty(name, value);
+	}
+
+	public static String getDefaultProperty(String name) {
+		return defaults.getProperty(name);
+	}
+
+	public static void setDefaultProperty(String name, String value) {
+		defaults.setProperty(name, value);
+	}
+
+	private static void loadProperties(Properties into, InputStream in)
+			throws IOException {
+		try {
+			into.load(in);
+		} finally {
+			in.close();
+		}
+	}
+
+	public static boolean getBooleanProperty(String p) {
+		return Boolean.valueOf(getProperty(p)).booleanValue();
+	}
+	
+	public static int getIntProperty(String p) {
+		try {
+			return Integer.parseInt(getProperty(p));
+		} catch(Exception e) {
+			return Integer.MAX_VALUE;
+		}
+	}
+	
 }
