c326a4f4052f7a23f6f273d2bc1d0b40b9179fd7
jgould <jgould@broadinstitute.org>
2005-07-11 19:51:12 +0000

    Added delete task
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1615 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/tasks/PipelineComponent.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/PipelineComponent.java
@@ -3,11 +3,17 @@ package org.genepattern.gpge.ui.tasks;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.Vector;
 
 import javax.swing.BoxLayout;
@@ -41,29 +47,111 @@ import com.jgoodies.forms.layout.FormLayout;
 
 public class PipelineComponent extends JPanel {
 	private TaskInfoAttributes pipelineTaskInfoAttributes;
+
 	private PipelineModel pipelineModel;
+
 	private List jobSubmissions;
+
 	private String userID;
-	
+
+	private TaskInfo pipelineTaskInfo;
+
 	public PipelineComponent() {
 		setBackground(Color.white);
 	}
-	
-	public void setTaskInfo(TaskInfo pipelineTaskInfo) {
-		removeAll();
-		pipelineTaskInfoAttributes = pipelineTaskInfo.giveTaskInfoAttributes();
+
+	public void addTask(int index, TaskInfo task) {
+		
+	}
+
+	public void delete(int index) {
+		List currentTasks = pipelineModel.getTasks();
+
+		// check if other tasks inherit from this task or subsequent tasks
+		for (int i = index; i < currentTasks.size(); i++) {
+			JobSubmission js = (JobSubmission) currentTasks.get(i);
+			List parameterInfo = js.getParameters();
+			for (int j = 0; j < parameterInfo.size(); j++) {
+				ParameterInfo p = (ParameterInfo) parameterInfo.get(j);
+				Map parameterAttributes = p.getAttributes();
+				if (parameterAttributes != null) {
+					String taskNumberString = (String) parameterAttributes
+							.get(PipelineModel.INHERIT_TASKNAME);
+					System.out.println("taskNumberString " + taskNumberString
+							+ "index " + index);
+					if (taskNumberString != null) {
+						int taskNumber = Integer.parseInt(taskNumberString);
+						if (taskNumber == index) { // lost inheritance
+							parameterAttributes
+									.remove(PipelineModel.INHERIT_TASKNAME);
+							parameterAttributes
+									.remove(PipelineModel.INHERIT_FILENAME);
+							System.out
+									.println("lost inheritance " + taskNumber);
+						} else if (taskNumber > index) { // inherits from a task that comes after deleted task
+							parameterAttributes.put(
+									PipelineModel.INHERIT_TASKNAME, String
+											.valueOf(taskNumber)); // decrease task number by one
+						}
+					}
+				}
+			}
+		}
+		JobSubmission removedJob = (JobSubmission) currentTasks.remove(index);
+		String removedJobName = removedJob.getName();
+		// remove parameters that start with removed task name + removed task number + .
+		TreeMap currentInputParameters = pipelineModel.getInputParameters();
+		String removeParameter = removedJobName + (index+1) + ".";
+		for (Iterator it = currentInputParameters.keySet().iterator(); it
+				.hasNext();) {
+			String key = (String) it.next();
+			if (key.startsWith(removeParameter)) {
+				System.out.println("removed " + key);
+				currentInputParameters.remove(key);
+			}
+		}
+		if (pipelineTaskInfo.getParameterInfoArray() != null) {
+			List parameters = new ArrayList(Arrays.asList(pipelineTaskInfo
+					.getParameterInfoArray()));
+			for (int i = 0; i < parameters.size(); i++) {
+				ParameterInfo p = (ParameterInfo) parameters.get(i);
+				if (p.getName().startsWith(removeParameter)) {
+					parameters.remove(i);
+					System.out.println("removed 2" + p.getName());
+					i--;
+				}
+			}
+			pipelineTaskInfo.setParameterInfoArray((ParameterInfo[]) parameters
+					.toArray(new ParameterInfo[0]));
+		}
+
+		setPipeline(pipelineTaskInfo, pipelineModel);
+	}
+
+	public void setTaskInfo(TaskInfo info) {
+
 		try {
-			pipelineModel = PipelineModel
-					.toPipelineModel((String) pipelineTaskInfoAttributes
-							.get(GPConstants.SERIALIZED_MODEL));
+			PipelineModel pipelineModel = PipelineModel
+					.toPipelineModel((String) info.getTaskInfoAttributes().get(
+							GPConstants.SERIALIZED_MODEL));
+			setPipeline(info, pipelineModel);
 		} catch (Exception e1) {
 			e1.printStackTrace();
-			GenePattern.showErrorDialog("An error occurred while loading the pipeline");
+			GenePattern
+					.showErrorDialog("An error occurred while loading the pipeline");
 			return;
 		}
-		
-		jobSubmissions = pipelineModel.getTasks();
-		userID = pipelineTaskInfo.getUserId();
+	}
+
+	public void setPipeline(TaskInfo pipelineTaskInfo, PipelineModel model) {
+		removeAll();
+
+		this.pipelineTaskInfo = pipelineTaskInfo;
+		this.pipelineModel = model;
+		this.pipelineTaskInfoAttributes = pipelineTaskInfo
+				.giveTaskInfoAttributes();
+		this.jobSubmissions = pipelineModel.getTasks();
+		this.userID = pipelineTaskInfo.getUserId();
 		String lsidStr = pipelineTaskInfoAttributes.get("LSID");
 
 		try {
@@ -84,40 +172,39 @@ public class PipelineComponent extends JPanel {
 		String owner = pipelineTaskInfo.getUserId();
 		List tasks = pipelineModel.getTasks();
 		StringBuffer rowSpec = new StringBuffer();
-        for (int i = 0; i < tasks.size(); i++) {
-            if (i > 0) {
-                rowSpec.append(", ");
-            }
-            rowSpec.append("pref");// input, description space
+		for (int i = 0; i < tasks.size(); i++) {
+			if (i > 0) {
+				rowSpec.append(", ");
+			}
+			rowSpec.append("pref");// input, description space
 
-        }
-        
-        JPanel tasksPanel = new JPanel();
+		}
+
+		JPanel tasksPanel = new JPanel();
 		FormLayout formLayout = new FormLayout( // 
-                "left:pref:none", rowSpec
-                        .toString());
-		
+				"left:pref:none", rowSpec.toString());
+
 		tasksPanel.setLayout(formLayout);
 		CellConstraints cc = new CellConstraints();
-		for(int i = 0; i < tasks.size(); i++) {
-			tasksPanel.add(new PipelineTask(i, (JobSubmission)tasks.get(i)), cc.xy(1, (i+1)));
+		for (int i = 0; i < tasks.size(); i++) {
+			tasksPanel.add(new PipelineTask(i, (JobSubmission) tasks.get(i)),
+					cc.xy(1, (i + 1)));
 		}
 		setLayout(new BorderLayout());
 		add(new JScrollPane(tasksPanel), BorderLayout.CENTER);
-		JPanel taskNamePanel = new TaskNamePanel(pipelineTaskInfo, ChangeViewMessageRequest.SHOW_EDIT_PIPELINE_REQUEST);	
+		JPanel taskNamePanel = new TaskNamePanel(pipelineTaskInfo,
+				ChangeViewMessageRequest.SHOW_EDIT_PIPELINE_REQUEST);
 		add(taskNamePanel, BorderLayout.NORTH);
-	
-	}
 
-	protected boolean taskExists(String lsid, String userID) {
-		return true;
+		invalidate();
+		validate();
+
 	}
 
 	protected TaskInfo getTaskInfo(String lsid, String userID) {
-		System.out.println("getting task info " + lsid);
-		AnalysisService svc = AnalysisServiceManager.getInstance().getAnalysisService(lsid);
-		if(svc==null) {
-			System.out.println("Unable to find");
+		AnalysisService svc = AnalysisServiceManager.getInstance()
+				.getAnalysisService(lsid);
+		if (svc == null) {
 			return null;
 		}
 		return svc.getTaskInfo();
@@ -125,7 +212,7 @@ public class PipelineComponent extends JPanel {
 
 	private class PipelineTask extends JPanel {
 
-		public PipelineTask(int displayNumber, JobSubmission js) {
+		public PipelineTask(final int index, JobSubmission js) {
 			setBackground(Color.white);
 			TaskInfo formalTaskInfo = getTaskInfo(js.getLSID(), userID);
 			ParameterInfo[] formalParams = formalTaskInfo != null ? formalTaskInfo
@@ -135,25 +222,26 @@ public class PipelineComponent extends JPanel {
 				formalParams = new ParameterInfo[0];
 			}
 			int maxLabelWidth = 0;
-			ParameterInfoPanel parameterInfoPanel = new ParameterInfoPanel(js.getName(),
-					formalParams);
-			parameterInfoPanel.setUseInputFromPreviousTask(0, new String[]{"a"}, new String[]{"b"});
-			maxLabelWidth = Math.max(maxLabelWidth, parameterInfoPanel.getLabelWidth());
+			ParameterInfoPanel parameterInfoPanel = new ParameterInfoPanel(js
+					.getName(), formalParams);
+			parameterInfoPanel.setUseInputFromPreviousTask(0,
+					new String[] { "a" }, new String[] { "b" });
+			maxLabelWidth = Math.max(maxLabelWidth, parameterInfoPanel
+					.getLabelWidth());
 			JTextField description = new JTextField(js.getDescription(), 80);
-			
+
 			JButton docBtn = new JButton("Documentation");
 			docBtn.setBackground(getBackground());
-			
-				
-			
-			TogglePanel togglePanel = new TogglePanel((displayNumber+1) + ". " + formalTaskInfo.getName(), description, parameterInfoPanel);
+
+			TogglePanel togglePanel = new TogglePanel((index + 1) + ". "
+					+ formalTaskInfo.getName(), description, parameterInfoPanel);
 			togglePanel.setBackground(parameterInfoPanel.getBackground());
 			togglePanel.setExpanded(true);
-		     
+
 			setValues(js, formalParams, parameterInfoPanel);
 			setLayout(new BorderLayout());
 			add(togglePanel, BorderLayout.CENTER);
-			
+
 			JButton addButton = new JButton("Add Task After");
 			addButton.setBackground(getBackground());
 			JButton addBeforeButton = new JButton("Add Task Before");
@@ -164,13 +252,25 @@ public class PipelineComponent extends JPanel {
 			moveUpButton.setBackground(getBackground());
 			JButton moveDownButton = new JButton("Move Down");
 			moveDownButton.setBackground(getBackground());
+
+			ActionListener listener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					delete(index);
+				}
+			};
+			deleteButton.addActionListener(listener);
 			JPanel bottomPanel = new JPanel();
 			bottomPanel.setBackground(getBackground());
-			bottomPanel.add(addButton);
-			bottomPanel.add(addBeforeButton);
-			bottomPanel.add(deleteButton);
-			bottomPanel.add(moveUpButton);
-			bottomPanel.add(moveDownButton);
+			FormLayout formLayout = new FormLayout(
+					"left:pref, left:pref, left:pref, left:pref, left:pref",
+					"pref");
+			bottomPanel.setLayout(formLayout);
+			CellConstraints cc = new CellConstraints();
+			bottomPanel.add(addButton, cc.xy(1, 1));
+			bottomPanel.add(addBeforeButton, cc.xy(2, 1));
+			bottomPanel.add(deleteButton, cc.xy(3, 1));
+			bottomPanel.add(moveUpButton, cc.xy(4, 1));
+			bottomPanel.add(moveDownButton, cc.xy(5, 1));
 			add(bottomPanel, BorderLayout.SOUTH);
 		}
 
@@ -179,7 +279,6 @@ public class PipelineComponent extends JPanel {
 	private void setValues(JobSubmission js, ParameterInfo[] formalParams,
 			ParameterInfoPanel parameterInfoPanel) {
 
-		TaskInfo actualTaskInfo = js.getTaskInfo();
 		ParameterInfo[] actualParameters = js.giveParameterInfoArray();
 
 		boolean[] runtimePrompt = js.getRuntimePrompt();
