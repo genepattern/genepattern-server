f6066c72fb29ae113bc5aaa0fac6979535230699
jgould <jgould@broadinstitute.org>
2005-07-27 22:33:18 +0000

    Fixed drawing of grid
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1714 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/tasks/pipeline/AlternatingRowColorPanel.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/pipeline/AlternatingRowColorPanel.java
@@ -24,66 +24,123 @@ public class AlternatingRowColorPanel extends JPanel {
 
 	private Color gridColor = new Color(239, 239, 255);
 
-	private List togglePanelList;
+	private List taskList;
 
+	private boolean showGrid = true;
+
+	private static class Task {
+		GroupPanel groupPanel;
+		int parameterCount;
+		
+		Task(GroupPanel p, int count) {
+			this.groupPanel = p;
+			this.parameterCount = count;
+		}
+	}
 	public AlternatingRowColorPanel(FormLayout layout) {
 		setLayout(layout);
-		togglePanelList = new ArrayList();
+		taskList = new ArrayList();
 	}
 
-	public void addTask(GroupPanel tp) {
-		togglePanelList.add(tp);
+	public void addTask(GroupPanel tp, int parameterCount) {
+		taskList.add(new Task(tp, parameterCount));
 	}
 
+	public void setShowGrid(boolean b) {
+		showGrid  = b;
+	}
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		paintGrid(g);
 	}
 
+	public void removeAll() {
+		super.removeAll();
+		this.taskList.clear();
+	}
+	
 	private void paintGrid(Graphics g) {
 		if (!(getLayout() instanceof FormLayout)) {
 			return;
 		}
-		FormLayout.LayoutInfo layoutInfo = FormDebugUtils.getLayoutInfo(this);
+		FormLayout.LayoutInfo layoutInfo = ((FormLayout) getLayout()).getLayoutInfo(this);
+	//	FormDebugUtils.dumpRowGroups((FormLayout) getLayout());
 		int left = layoutInfo.getX();
 		int width = layoutInfo.getWidth();
 		int height = layoutInfo.getHeight();
 
 		g.setColor(gridColor);
-		int row = 0;
+		int rowIndex = 0;
 		int[] rowOrigins = layoutInfo.rowOrigins;
 
-		for (int i = 0; i < togglePanelList.size(); i++) {
-			int taskStart = rowOrigins[row];
+		for (int i = 0; i < taskList.size(); i++) {
+			int taskStartIndex = rowIndex;
 			g.setColor(gridColor);
-			GroupPanel tp = (GroupPanel) togglePanelList.get(i);
-			int numParameters = tp.getToggleComponentCount() / 2;
-			int tempRow = row + 1;
-			if (tp.isExpanded()) {
+			Task task = (Task) taskList.get(i);
+			int numParameters = task.parameterCount;
+			int parameterRowIndex = rowIndex + 1;
+			if (showGrid && task.groupPanel.isExpanded()) {
+				int tempRow = parameterRowIndex;
+				int gridHeight;
+				if(rowOrigins.length==(tempRow + 1)) {
+					gridHeight =  rowOrigins[rowOrigins.length-1] - rowOrigins[tempRow]; 
+				} else {
+					gridHeight = rowOrigins[tempRow + 1] - rowOrigins[tempRow];
+				}
+										
 				for (int j = 0; j < numParameters; j += 2, tempRow += 2) {
-					int gridHeight = 0;
-					if (rowOrigins.length == (tempRow + 1)) {
-						gridHeight = height - rowOrigins[tempRow];
-					} else {
-						gridHeight = rowOrigins[tempRow + 1]
-								- rowOrigins[tempRow];
-					}
 					g.fillRect(left, rowOrigins[tempRow], width, gridHeight);
-
 				}
 			}
-			row += numParameters + 1;
-
+			rowIndex = parameterRowIndex + numParameters;
+			int taskEndIndex = rowIndex;
+			
 			int taskHeight;
-			if (rowOrigins.length == row) {
-				taskHeight = height - taskStart;
+			if (rowOrigins.length == (taskEndIndex+1)) {
+				taskHeight = height - layoutInfo.rowOrigins[taskStartIndex]-1;
 			} else {
-				taskHeight = rowOrigins[row] - taskStart;
+				taskHeight = rowOrigins[taskEndIndex] - layoutInfo.rowOrigins[taskStartIndex];
 			}
-
+			
 			g.setColor(Color.LIGHT_GRAY);
-			g.drawRect(left, taskStart, width, taskHeight);
+			g.drawRect(left, layoutInfo.rowOrigins[taskStartIndex], width, taskHeight);
+			//g.drawString("" + taskStartIndex, left, layoutInfo.rowOrigins[taskStartIndex]);
 		}
 	}
+	
+	  /**
+     * Paints the form's grid lines and diagonals.
+     * 
+     * @param g    the Graphics object used to paint
+     */
+    private void paintDebugGrid(Graphics g) {
+        if (!(getLayout() instanceof FormLayout)) {
+            return;
+        }
+        FormLayout.LayoutInfo layoutInfo = FormDebugUtils.getLayoutInfo(this);
+        int left   = layoutInfo.getX();
+        int top    = layoutInfo.getY();
+        int width  = layoutInfo.getWidth();
+        int height = layoutInfo.getHeight();
+
+        g.setColor(Color.black);
+  
+        // Paint the column bounds.
+    //    for (int col = 0; col < layoutInfo.columnOrigins.length; col++) {
+      //      g.fillRect(layoutInfo.columnOrigins[col], top, 1, height);
+       // }
+
+        // Paint the row bounds.
+        for (int row = 0; row < layoutInfo.rowOrigins.length; row++) {
+            g.fillRect(left, layoutInfo.rowOrigins[row], width, 1);
+            g.drawString("" + row, left, layoutInfo.rowOrigins[row]);
+        }
+        
+      //  if (paintDiagonals) {
+        //    g.drawLine(left, top,          left + width, top + height);
+          //  g.drawLine(left, top + height, left + width, top);
+        //}
+    }
 
 }
