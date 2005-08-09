1d68a9fa08fa5909ddcea1364fa5d4e7acc93b7a
jgould <jgould@broadinstitute.org>
2005-08-09 20:43:10 +0000

    Initial rev
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1801 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/pipeline/MissingTasksDisplay.java
@@ -0,0 +1,128 @@
+package org.genepattern.gpge.ui.tasks.pipeline;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.border.Border;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+
+import org.genepattern.data.pipeline.JobSubmission;
+import org.genepattern.gpge.ui.table.AlternatingColorTable;
+import org.genepattern.gpge.ui.tasks.AnalysisServiceManager;
+import org.genepattern.gpge.ui.util.GUIUtil;
+import org.genepattern.gpge.util.PostData;
+import org.genepattern.util.BrowserLauncher;
+import org.genepattern.util.LSID;
+
+/**
+ * Displays missing tasks in a pipeline
+ * @author Joshua Gould
+ *
+ */
+public class MissingTasksDisplay {
+
+	private JPanel topPanel;
+	private JScrollPane sp;
+
+
+	public MissingTasksDisplay(final List missingTasks) {
+
+		final String[] columnNames = { "Name", "Version", "LSID" };
+		TableModel tableModel = new AbstractTableModel() {
+
+			public int getRowCount() {
+				return missingTasks.size();
+			}
+
+			public int getColumnCount() {
+				return columnNames.length;
+			}
+
+			public String getColumnName(int j) {
+				return columnNames[j];
+			}
+
+			public Object getValueAt(int row, int col) {
+				JobSubmission js = (JobSubmission) missingTasks.get(row);
+				if (col == 0) {
+					return js.getName();
+				} else if (col == 1) {
+					try {
+						return new LSID(js.getLSID()).getVersion();
+					} catch (MalformedURLException e) {
+						e.printStackTrace();
+					}
+					return null;
+				} else {
+					return js.getLSID();
+				}
+			}
+		};
+
+		JTable table = new AlternatingColorTable(tableModel);
+
+		JPanel buttonPanel = new JPanel();
+		JButton installFromCatalogBtn = new JButton(
+				"Install Missing Modules From Catalog");
+		JButton importZipBtn = new JButton("Import Module From Zip File");
+		importZipBtn.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+
+			}
+
+		});
+		buttonPanel.add(installFromCatalogBtn);
+		// buttonPanel.add(importZipBtn);
+		installFromCatalogBtn.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				PostData postData = new PostData(AnalysisServiceManager
+						.getInstance().getServer()
+						+ "/gp/taskCatalog.jsp");
+				try {
+					for (int i = 0; i < missingTasks.size(); i++) {
+						JobSubmission js = (JobSubmission) missingTasks.get(i);
+						postData.addPostData("LSID", js.getLSID());
+					}
+					BrowserLauncher.openURL(postData.toString());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+
+		});
+
+		sp = new JScrollPane(table);
+
+		JTextArea errorLabel = GUIUtil
+				.createWrappedLabel("The following modules do not exist on the server. Please install the missing modules from the Module Repository or import them from a zip file (File > Import Module).");
+		errorLabel.setBackground(Color.red);
+		Border b = sp.getBorder();
+		sp.setBorder(GUIUtil.createBorder(b, 0, -1, -1, -1));
+		topPanel = new JPanel(new BorderLayout());
+		topPanel.add(errorLabel, BorderLayout.NORTH);
+		topPanel.add(buttonPanel, BorderLayout.CENTER);
+	}
+	
+	public JScrollPane getScrollPane() {
+		return sp;
+	}
+	
+	public JPanel getErrorPanel() {
+		return topPanel;
+	}
+	
+
+}
