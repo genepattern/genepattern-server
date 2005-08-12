bc8d3370fbeb8f8012b857214f8b6c4709fec09e
jgould <jgould@broadinstitute.org>
2005-08-12 19:45:46 +0000

    Added replace event type
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1845 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/tasks/pipeline/PipelineEvent.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/pipeline/PipelineEvent.java
@@ -12,26 +12,40 @@ public class PipelineEvent extends EventObject {
 
 	public static final int DELETE = 0;
 	public static final int INSERT = 1;
+	public static final int MOVE = 2;
+	public static final int REPLACE = 3;
 	
 	private int type;
-	private int row;
-	
-	public PipelineEvent(Object source) {
-		super(source);
-	}
+	private int firstRow;
+	private int lastRow;
 	
 	public PipelineEvent(Object source, int type, int row) {
 		super(source);
 		this.type = type;
-		this.row = row;
+		this.firstRow = row;
+		this.lastRow = row;
+	}
+	
+	public PipelineEvent(Object source, int type, int firstRow, int lastRow) {
+		super(source);
+		this.type = type;
+		this.firstRow = firstRow;
+		this.lastRow = lastRow;
+	}
+	
+
+	public int getFirstRow() {
+		return firstRow;
+	}
+	
+	public int getLastRow() {
+		return lastRow;
 	}
 
-	public int getRow() {
-		return row;
-	}
 	
 	public int getType() {
 		return type;
 	}
 
+	
 }
