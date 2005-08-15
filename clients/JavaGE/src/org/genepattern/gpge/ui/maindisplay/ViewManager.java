a0b58a572246fff40d9c33d1402507d0911d9111
jgould <jgould@broadinstitute.org>
2005-08-15 18:09:08 +0000

    Check if pipeline is missing tasks before editing
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1863 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/maindisplay/ViewManager.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/maindisplay/ViewManager.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 
 import javax.swing.JSplitPane;
 
+import org.genepattern.data.pipeline.PipelineModel;
+import org.genepattern.gpge.GenePattern;
 import org.genepattern.gpge.message.ChangeViewMessage;
 import org.genepattern.gpge.message.ChangeViewMessageRequest;
 import org.genepattern.gpge.message.GPGEMessage;
@@ -13,56 +15,115 @@ import org.genepattern.gpge.message.MessageManager;
 import org.genepattern.gpge.ui.tasks.pipeline.PipelineEditor;
 import org.genepattern.gpge.ui.tasks.pipeline.ViewPipeline;
 import org.genepattern.gpge.ui.tasks.AnalysisServiceDisplay;
+import org.genepattern.util.GPConstants;
+import org.genepattern.webservice.AnalysisService;
+import org.genepattern.webservice.TaskInfo;
 
 public class ViewManager {
 	private AnalysisServiceDisplay analysisServiceDisplay;
+
 	private PipelineEditor pipelineComponent;
+
 	private ViewPipeline viewPipeline;
+
 	private JSplitPane splitPane;
-	
+
 	public ViewManager(JSplitPane splitPane) {
 		this.splitPane = splitPane;
 		analysisServiceDisplay = new AnalysisServiceDisplay();
 		analysisServiceDisplay.setMinimumSize(new Dimension(200, 200));
 		pipelineComponent = new PipelineEditor();
 		viewPipeline = new ViewPipeline();
-		
+
 		MessageManager.addGPGEMessageListener(new GPGEMessageListener() {
 
 			public void receiveMessage(GPGEMessage message) {
 				if (message instanceof ChangeViewMessageRequest) {
 					ChangeViewMessageRequest asm = (ChangeViewMessageRequest) message;
 					if (asm.getType() == ChangeViewMessageRequest.SHOW_RUN_TASK_REQUEST) {
-						analysisServiceDisplay.loadTask(asm.getAnalysisService());
+						analysisServiceDisplay.loadTask(asm
+								.getAnalysisService());
 						setComponent(analysisServiceDisplay);
-						MessageManager.notifyListeners(new ChangeViewMessage(message.getSource(), ChangeViewMessage.RUN_TASK_SHOWN, analysisServiceDisplay));
-					} else if(asm.getType()==ChangeViewMessageRequest.SHOW_EDIT_PIPELINE_REQUEST) {
-						pipelineComponent.display(asm.getAnalysisService());
-						setComponent(pipelineComponent);
-						MessageManager.notifyListeners(new ChangeViewMessage(message.getSource(), ChangeViewMessage.EDIT_PIPELINE_SHOWN, pipelineComponent));
-					} else if(asm.getType()==ChangeViewMessageRequest.SHOW_GETTING_STARTED_REQUEST) {
+						MessageManager.notifyListeners(new ChangeViewMessage(
+								message.getSource(),
+								ChangeViewMessage.RUN_TASK_SHOWN,
+								analysisServiceDisplay));
+					} else if (asm.getType() == ChangeViewMessageRequest.SHOW_EDIT_PIPELINE_REQUEST) {
+						AnalysisService svc = asm.getAnalysisService();
+						if (svc == null) {
+							pipelineComponent.display(null, null);
+							setComponent(pipelineComponent);
+							MessageManager
+									.notifyListeners(new ChangeViewMessage(
+											message.getSource(),
+											ChangeViewMessage.EDIT_PIPELINE_SHOWN,
+											pipelineComponent));
+						} else {
+							TaskInfo info = asm.getAnalysisService()
+									.getTaskInfo();
+							try {
+								PipelineModel pipelineModel = PipelineModel
+										.toPipelineModel((String) info
+												.getTaskInfoAttributes()
+												.get(
+														GPConstants.SERIALIZED_MODEL));
+								
+								
+								if(!pipelineComponent.display(asm
+										.getAnalysisService(), pipelineModel)) {
+									viewPipeline.display(asm.getAnalysisService());
+									setComponent(viewPipeline);
+									MessageManager.notifyListeners(new ChangeViewMessage(
+											message.getSource(),
+											ChangeViewMessage.VIEW_PIPELINE_SHOWN,
+											viewPipeline));
+									
+								} else {
+									setComponent(pipelineComponent);
+									MessageManager
+										.notifyListeners(new ChangeViewMessage(
+												message.getSource(),
+												ChangeViewMessage.EDIT_PIPELINE_SHOWN,
+												pipelineComponent));
+								}
+
+							} catch (Exception e1) {
+								e1.printStackTrace();
+								GenePattern
+										.showErrorDialog("An error occurred while loading the pipeline");
+
+							}
+						}
+
+					} else if (asm.getType() == ChangeViewMessageRequest.SHOW_GETTING_STARTED_REQUEST) {
 						analysisServiceDisplay.showGettingStarted();
 						setComponent(analysisServiceDisplay);
-						MessageManager.notifyListeners(new ChangeViewMessage(message.getSource(), ChangeViewMessage.GETTING_STARTED_SHOWN, analysisServiceDisplay));
-					} else if(asm.getType()==ChangeViewMessageRequest.SHOW_VIEW_PIPELINE_REQUEST) {
+						MessageManager.notifyListeners(new ChangeViewMessage(
+								message.getSource(),
+								ChangeViewMessage.GETTING_STARTED_SHOWN,
+								analysisServiceDisplay));
+					} else if (asm.getType() == ChangeViewMessageRequest.SHOW_VIEW_PIPELINE_REQUEST) {
 						viewPipeline.display(asm.getAnalysisService());
 						setComponent(viewPipeline);
-						MessageManager.notifyListeners(new ChangeViewMessage(message.getSource(), ChangeViewMessage.VIEW_PIPELINE_SHOWN, viewPipeline));
+						MessageManager.notifyListeners(new ChangeViewMessage(
+								message.getSource(),
+								ChangeViewMessage.VIEW_PIPELINE_SHOWN,
+								viewPipeline));
 					} else {
 						System.err.println("Unknown type:" + asm.getType());
 					}
 				}
 			}
 		});
-		
+
 	}
-	
+
 	public Component getComponent() {
 		return splitPane.getRightComponent();
 	}
-	
+
 	private void setComponent(Component c) {
-		if(c!=splitPane.getRightComponent()) {
+		if (c != splitPane.getRightComponent()) {
 			int location = splitPane.getDividerLocation();
 			splitPane.setRightComponent(c);
 			splitPane.setDividerLocation(location);
