af18725573a11c89fdb7a8c19a12b546343d8ecf
jgould <jgould@broadinstitute.org>
2005-09-11 17:09:34 +0000

    Removed right click tooltip text
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@1996 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/tasks/pipeline/PipelineEditor.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/tasks/pipeline/PipelineEditor.java
@@ -1306,7 +1306,7 @@ public class PipelineEditor extends JPanel implements TaskDisplay,
 			togglePanel = new GroupPanel((taskIndex + 1) + ". "
 					+ model.getTaskName(taskIndex), new JTextField(model
 					.getTaskDescription(taskIndex), 40));
-			togglePanel.getMajorLabel().setToolTipText("Right-click to modify pipeline");
+		//	togglePanel.getMajorLabel().setToolTipText("Right-click to modify pipeline");
 			togglePanel.setBackground(getBackground());
 			popupMenu = new JPopupMenu();
 
