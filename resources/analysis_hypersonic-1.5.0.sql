35d2dc1396cec7558193bb917d0b947b19635394
liefeld <liefeld@broadinstitute.org>
2005-09-21 18:18:25 +0000

    updated schema
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2076 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_hypersonic-1.5.0.sql
+++ resources/analysis_hypersonic-1.5.0.sql
@@ -1,1 +1,12 @@
+--Suite Table
+create table suite(lsid longvarchar, name longvarchar, author longvarchar, owner longvarchar, description longvarchar, access_id int);
+
+-- Suite Modules
+create table suite_modules(lsid longvarchar, module_lsid longvarchar);
+
+create index idx_suite_lsid on suite(lsid);
+create index idx_suite_modules_lsid on suite_modules(lsid);
+create index idx_modules_suite_lsid on suite_modules(module_lsid);
+
+
 update props set value='1.5.0' where key='schemaVersion';
