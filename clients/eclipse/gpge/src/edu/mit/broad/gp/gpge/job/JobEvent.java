fd6bf8ae099e8779d7748408c66c1ac7aa1f3130
liefeld <liefeld@broadinstitute.org>
2005-12-16 12:20:51 +0000

    added Broad copyright notice
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2620 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/eclipse/gpge/src/edu/mit/broad/gp/gpge/job/JobEvent.java
+++ clients/eclipse/gpge/src/edu/mit/broad/gp/gpge/job/JobEvent.java
@@ -1,4 +1,17 @@
 /*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2006) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+
+
+/*
  * Created on Jul 21, 2004
  *
  * TODO To change the template for this generated file go to
