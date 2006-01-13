bbaa5643346b27d3e6fbbe84135f966a5bcf57a0
jgould <jgould@broadinstitute.org>
2006-01-13 22:05:55 +0000

    implement IClassVector interface
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2710 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/data/matrix/ClassVector.java
+++ src/org/genepattern/data/matrix/ClassVector.java
@@ -1,15 +1,14 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.data.matrix;
 
@@ -23,7 +22,7 @@ import java.util.Map;
  * 
  * @author Joshua Gould
  */
-public class ClassVector {
+public class ClassVector implements IClassVector {
 	Map classNumber2IndicesMap;
 
 	int[] assignments;
