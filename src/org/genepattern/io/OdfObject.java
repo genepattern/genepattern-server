9187ef6ace8a806dc5fe26e472108e18a77a7a9e
jgould <jgould@broadinstitute.org>
2006-01-18 19:51:12 +0000

    Added OdfObejct(InputStream) constructor
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2718 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/io/OdfObject.java
+++ src/org/genepattern/io/OdfObject.java
@@ -1,15 +1,14 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.io;
 
@@ -57,6 +56,12 @@ public class OdfObject implements TableModel {
 			}
 		}
 		return -1;
+	}
+
+	public OdfObject(InputStream is) throws ParseException, IOException {
+		OdfParser parser = new OdfParser();
+		parser.setHandler(new MyHandler());
+		parser.parse(is);
 	}
 
 	public OdfObject(String fileName) throws ParseException, IOException {
