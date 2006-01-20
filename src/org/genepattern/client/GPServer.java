85f993a47e8a00b2a43459535247ced92713f5bc
jgould <jgould@broadinstitute.org>
2006-01-20 19:17:58 +0000

    Moved createParameterInfoArray to Util class
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2722 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/client/GPServer.java
+++ src/org/genepattern/client/GPServer.java
@@ -17,9 +17,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.genepattern.util.GPConstants;
@@ -204,7 +202,7 @@ public class GPServer {
 			throws WebServiceException {
 		try {
 			TaskInfo taskInfo = getTask(taskNameOrLSID);
-			ParameterInfo[] actualParameters = createParameterInfoArray(
+			ParameterInfo[] actualParameters = Util.createParameterInfoArray(
 					taskInfo, parameters);
 			AnalysisWebServiceProxy analysisProxy = null;
 			try {
@@ -244,7 +242,7 @@ public class GPServer {
 			throws WebServiceException {
 		try {
 			TaskInfo taskInfo = getTask(taskNameOrLSID);
-			ParameterInfo[] actualParameters = createParameterInfoArray(
+			ParameterInfo[] actualParameters = Util.createParameterInfoArray(
 					taskInfo, parameters);
 			AnalysisWebServiceProxy analysisProxy = null;
 			try {
@@ -299,125 +297,6 @@ public class GPServer {
 		}
 	}
 
-	private static String sub(ParameterInfo formalParam, String value)
-			throws WebServiceException {
-		// see if parameter belongs to a set of choices, e.g. 1=T-Test.
-		// If so substitute 1 for T-Test, also check to see if value is
-		// valid
-		String choicesString = formalParam.getValue();
-		if (value != null && choicesString != null && !choicesString.equals("")) {
-			String[] choices = choicesString.split(";");
-			boolean validValue = false;
-			for (int j = 0; j < choices.length && !validValue; j++) {
-				String[] choiceValueAndChoiceUIValue = choices[j].split("=");
-				if (value.equals(choiceValueAndChoiceUIValue[0])) {
-					validValue = true;
-				} else if (choiceValueAndChoiceUIValue.length == 2
-						&& value.equals(choiceValueAndChoiceUIValue[1])) {
-					value = choiceValueAndChoiceUIValue[0];
-					validValue = true;
-				}
-			}
-			if (!validValue) {
-				throw new WebServiceException("Illegal value for parameter "
-						+ formalParam.getName() + ": " + value);
-			}
-		}
-		return value;
-	}
-
-	private static void setAttributes(ParameterInfo formalParam,
-			ParameterInfo actualParam) {
-
-		if (formalParam.isInputFile()) {
-			HashMap actualAttributes = new HashMap();
-			actualParam.setAttributes(actualAttributes);
-			String value = actualParam.getValue();
-			actualAttributes.put(GPConstants.PARAM_INFO_CLIENT_FILENAME[0],
-					value);
-			if (value != null && new java.io.File(value).exists()) {
-				actualParam.setAsInputFile();
-			} else if (value != null) {
-				actualAttributes.remove("TYPE");
-				actualAttributes.put(ParameterInfo.MODE,
-						ParameterInfo.URL_INPUT_MODE);
-			}
-		}
-	}
-
-	private static ParameterInfo[] createParameterInfoArray(TaskInfo taskInfo,
-			Parameter[] parameters) throws WebServiceException {
-
-		ParameterInfo[] formalParameters = taskInfo.getParameterInfoArray();
-		List actualParameters = new ArrayList();
-
-		Map paramName2FormalParam = new HashMap();
-		if (formalParameters != null) {
-			for (int i = 0, length = formalParameters.length; i < length; i++) {
-				paramName2FormalParam.put(formalParameters[i].getName(),
-						formalParameters[i]);
-			}
-		}
-
-		if (parameters != null) {
-			for (int i = 0, length = parameters.length; i < length; i++) {
-				ParameterInfo formalParam = (ParameterInfo) paramName2FormalParam
-						.remove(parameters[i].getName());
-				if (formalParam == null) {
-					throw new WebServiceException("Unknown parameter: "
-							+ parameters[i].getName());
-				}
-				Map formalAttributes = formalParam.getAttributes();
-				if (formalAttributes == null) {
-					formalAttributes = new HashMap();
-				}
-				String value = parameters[i].getValue();
-
-				if (value == null || value.trim().equals("")) {
-					String sOptional = (String) formalAttributes
-							.get(GPConstants.PARAM_INFO_OPTIONAL[0]);
-					boolean optional = (sOptional != null && sOptional.length() > 0);
-					if (!optional) {
-						throw new WebServiceException(
-								"Missing value for required parameter "
-										+ formalParameters[i].getName());
-					}
-				}
-
-				ParameterInfo p = new ParameterInfo(formalParam.getName(),
-						value, "");
-				setAttributes(formalParam, p);
-				actualParameters.add(p);
-			}
-		}
-
-		for (Iterator it = paramName2FormalParam.keySet().iterator(); it
-				.hasNext();) {
-			String name = (String) it.next();
-			ParameterInfo p = (ParameterInfo) paramName2FormalParam.get(name);
-			String value = (String) p.getAttributes().get(
-					(String) TaskExecutor.PARAM_INFO_DEFAULT_VALUE[0]);
-			if (value == null || value.equals("")) {
-				String sOptional = (String) p.getAttributes().get(
-						GPConstants.PARAM_INFO_OPTIONAL[0]);
-				boolean optional = (sOptional != null && sOptional.length() > 0);
-				if (!optional) {
-					throw new WebServiceException(
-							"Missing value for required parameter "
-									+ p.getName());
-				}
-			} else {
-				value = sub(p, value);
-				ParameterInfo actual = new ParameterInfo(p.getName(), value, "");
-				setAttributes(p, actual);
-				actualParameters.add(actual);
-			}
-
-		}
-
-		return (ParameterInfo[]) actualParameters.toArray(new ParameterInfo[0]);
-	}
-
 	/**
 	 * Downloads the support files for the given task from the server and
 	 * executes the given task locally.
@@ -438,8 +317,8 @@ public class GPServer {
 	public void runVisualizer(String taskNameOrLSID, Parameter[] parameters)
 			throws WebServiceException {
 		TaskInfo taskInfo = getTask(taskNameOrLSID);
-		ParameterInfo[] actualParameters = createParameterInfoArray(taskInfo,
-				parameters);
+		ParameterInfo[] actualParameters = Util.createParameterInfoArray(
+				taskInfo, parameters);
 		Map paramName2ValueMap = new HashMap();
 		if (actualParameters != null) {
 			for (int i = 0; i < actualParameters.length; i++) {
