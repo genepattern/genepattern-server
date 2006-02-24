5b39cc405b8c672cb264695f8702094697797a2b
liefeld <liefeld@broadinstitute.org>
2006-02-24 10:11:25 +0000

    updates for asynch calls
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/v2_0_1@2774 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/client/MatlabGPClient.java
+++ src/org/genepattern/client/MatlabGPClient.java
@@ -1,3 +1,16 @@
+/*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2006) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+
+
 package org.genepattern.client;
 
 import java.util.HashMap;
@@ -51,6 +64,8 @@ public class MatlabGPClient extends GPServer {
 			}
 			return services;
 		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(""+System.getProperties());
 			throw new WebServiceException(e);
 		}
 	}
