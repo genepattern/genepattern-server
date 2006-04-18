dc8687cbd931e0d31c24bba6f3cf6664f839fb62
jgould <jgould@broadinstitute.org>
2006-04-18 17:54:45 +0000

    Fixed getting resource
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2868 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/src/org/genepattern/gpge/ui/maindisplay/IconManager.java
+++ clients/JavaGE/src/org/genepattern/gpge/ui/maindisplay/IconManager.java
@@ -1,48 +1,59 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.gpge.ui.maindisplay;
+
 import java.net.URL;
 import javax.swing.*;
 
 /**
- *  Manages application icons
- *
- * @author    Joshua Gould
+ * Manages application icons
+ * 
+ * @author Joshua Gould
  */
 public class IconManager {
-   public final static String DELETE_ICON = "delete.gif";
-   public final static String REFRESH_ICON = "refresh.gif";
-   public final static String SAVE_ICON = "save.gif";
-   public final static String SAVE_AS_ICON = "save_as.gif";
-   public final static String STOP_ICON = "stop.gif";
-   public final static String NEW_PROJECT_ICON = "open.gif";
-   public final static String REMOVE_ICON = "remove.gif";
-   public final static String TEXT_ICON = "text.gif";
-   public final static String ERROR_ICON = "error.gif";
-   public final static String SEND_TO_ICON = "send_to.gif";
-   public final static String IMPORT_ICON = "import.gif";
-   
-   private IconManager() { }
+    public final static String DELETE_ICON = "delete.gif";
 
+    public final static String REFRESH_ICON = "refresh.gif";
 
-   public static Icon loadIcon(String name) {
-      java.net.URL imgURL = ClassLoader.getSystemResource("/org/genepattern/gpge/resources/icons/" + name);
-      if(imgURL != null) {
-         return new ImageIcon(imgURL);
-      } else {
-         System.err.println("Unable to find " + name);
-         return null;
-      }
-   }
+    public final static String SAVE_ICON = "save.gif";
+
+    public final static String SAVE_AS_ICON = "save_as.gif";
+
+    public final static String STOP_ICON = "stop.gif";
+
+    public final static String NEW_PROJECT_ICON = "open.gif";
+
+    public final static String REMOVE_ICON = "remove.gif";
+
+    public final static String TEXT_ICON = "text.gif";
+
+    public final static String ERROR_ICON = "error.gif";
+
+    public final static String SEND_TO_ICON = "send_to.gif";
+
+    public final static String IMPORT_ICON = "import.gif";
+
+    private IconManager() {
+    }
+
+    public static Icon loadIcon(String name) {
+        java.net.URL imgURL = IconManager.class
+                .getResource("/org/genepattern/gpge/resources/icons/" + name);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Unable to find " + name);
+            return null;
+        }
+    }
 }
