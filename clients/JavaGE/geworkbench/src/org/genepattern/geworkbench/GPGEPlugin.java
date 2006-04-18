02a3e78c035e43750ec71f9271f21d1574e4436e
jgould <jgould@broadinstitute.org>
2006-04-18 16:13:46 +0000

    Display geworkbench files in project dirs
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2865 48a7a3f7-ad46-0410-b969-29950918272e

--- clients/JavaGE/geworkbench/src/org/genepattern/geworkbench/GPGEPlugin.java
+++ clients/JavaGE/geworkbench/src/org/genepattern/geworkbench/GPGEPlugin.java
@@ -15,6 +15,11 @@ package org.genepattern.geworkbench;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -30,17 +35,26 @@ import org.geworkbench.events.ProjectEvent;
 @AcceptTypes( { DSMicroarraySet.class })
 public class GPGEPlugin extends JPanel implements VisualPlugin {
 
-    private DSMicroarraySet microarraySet;
-
     private GPGE instance;
 
+    private GeWorkbenchProject workbenchProject;
+
     public GPGEPlugin() {
-        instance = GPGE.getInstance();
-        instance.setFrame(new HiddenFrame());
-        instance.startUp(false);
-        setLayout(new BorderLayout());
-        add(instance.getFrame().getContentPane());
-        add(instance.getFrame().getJMenuBar(), BorderLayout.NORTH);
+        try {
+            instance = GPGE.getInstance();
+            instance.setFrame(new HiddenFrame());
+            System.out.println("here"
+                    + System.getProperty("javax.xml.parsers.SAXParserFactory"));
+            instance.startUp(false);
+            setLayout(new BorderLayout());
+            add(instance.getFrame().getContentPane());
+            add(instance.getFrame().getJMenuBar(), BorderLayout.NORTH);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        workbenchProject = new GeWorkbenchProject();
+        instance.getProjectDirectoryModel().add(workbenchProject);
     }
 
     public Component getComponent() {
@@ -52,7 +66,9 @@ public class GPGEPlugin extends JPanel implements VisualPlugin {
         DSDataSet dataSet = event.getDataSet();
         // We will act on this object if it is a DSMicroarraySet
         if (dataSet instanceof DSMicroarraySet) {
-            microarraySet = (DSMicroarraySet) dataSet;
+            DSMicroarraySet microarraySet = (DSMicroarraySet) dataSet;
+            workbenchProject.add(microarraySet);
+            instance.getProjectDirectoryModel().refresh(workbenchProject);
         }
     }
 
@@ -61,6 +77,48 @@ public class GPGEPlugin extends JPanel implements VisualPlugin {
         }
 
         public void show() {
+        }
+    }
+
+    static class GeWorkbenchProject extends File {
+        static String name = "geWorkbench Files";
+
+        List<File> children = new ArrayList<File>();
+
+        private long lastModified;
+
+        public GeWorkbenchProject() {
+            super(name);
+            lastModified = System.currentTimeMillis();
+        }
+
+        public void add(DSMicroarraySet microarraySet) {
+            System.out.println("got data, file " + microarraySet.getFile()
+                    + " label " + microarraySet.getLabel());
+            if (!children.contains(microarraySet.getFile())) {
+                children.add(microarraySet.getFile());
+            }
+        }
+
+        public boolean isDirectory() {
+            return true;
+        }
+
+        public boolean exists() {
+            return true;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public File[] listFiles(FileFilter fileFilter) {
+            return children.toArray(new File[0]);
+        }
+
+        public long lastModified() {
+            return lastModified;
         }
     }
 
