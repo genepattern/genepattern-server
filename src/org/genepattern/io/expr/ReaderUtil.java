044ee429fd0928a22524e804779e740d6d253314
jgould <jgould@broadinstitute.org>
2006-05-03 21:14:58 +0000

    Added method
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@2889 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/io/expr/ReaderUtil.java
+++ src/org/genepattern/io/expr/ReaderUtil.java
@@ -1,20 +1,20 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.io.expr;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.genepattern.io.ParseException;
 
@@ -25,22 +25,30 @@ import org.genepattern.io.ParseException;
  */
 public class ReaderUtil {
 
-	private ReaderUtil() {
-	}
+    private ReaderUtil() {
+    }
 
-	public static Object read(IExpressionDataParser parser, String pathname,
-			IExpressionDataCreator handler) throws IOException, ParseException {
-		FileInputStream is = null;
-		try {
-			parser.setHandler(handler);
-			is = new FileInputStream(pathname);
-			parser.parse(is);
-			return handler.create();
-		} finally {
-			if (is != null) {
-				is.close();
-			}
-		}
-	}
+    public static Object read(IExpressionDataParser parser, String pathname,
+            IExpressionDataCreator handler) throws IOException, ParseException {
+        FileInputStream is = null;
+        try {
+            parser.setHandler(handler);
+            is = new FileInputStream(pathname);
+            parser.parse(is);
+            return handler.create();
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public static Object read(IExpressionDataParser parser, InputStream is,
+            IExpressionDataCreator handler) throws IOException, ParseException {
+        parser.setHandler(handler);
+        parser.parse(is);
+        return handler.create();
+
+    }
 
 }
