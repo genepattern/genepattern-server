56e907f61c3ec35ca3f342caf0245a29d0345a72
jgould <jgould@broadinstitute.org>
2006-06-21 15:19:09 +0000

    Fixed file parameter
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3065 48a7a3f7-ad46-0410-b969-29950918272e

--- build_tools/ZipUpload/src/edu/mit/genome/gp/util/ZipUpload.java
+++ build_tools/ZipUpload/src/edu/mit/genome/gp/util/ZipUpload.java
@@ -1,109 +1,96 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package edu.mit.genome.gp.util;
 
-import java.io.*;
-import java.util.*;
-import java.net.*;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.HttpState;
+import java.io.File;
+import java.io.FilenameFilter;
+
 import org.apache.commons.httpclient.Cookie;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpState;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.MultipartPostMethod;
-
-
 
 public class ZipUpload {
 
-
-
     public static void main(String[] args) throws Exception {
 
-	// get url (base only) and file or dir to upload
-	//
-	if (args.length != 2) {
-	    System.out.println ("Usage: java ZipUpload url fileOrDir");
-	    System.out.println ("To load all zips in a directory");
-	    System.out.println ("e.g. java ZipUpload http://elm:8080/gp ./gp2/modules/build  ");
-	    System.out.println ("To load just one zip");
-	    System.out.println ("e.g. java ZipUpload http://elm:8080/gp ./gp2/modules/build/TransposeDataset.zip");
-	    System.exit(999);
-	}
+        // get url (base only) and file or dir to upload
+        //
+        if (args.length != 2) {
+            System.out.println("Usage: java ZipUpload url fileOrDir");
+            System.out.println("To load all zips in a directory");
+            System.out
+                    .println("e.g. java ZipUpload http://elm:8080/gp ./gp2/modules/build  ");
+            System.out.println("To load just one zip");
+            System.out
+                    .println("e.g. java ZipUpload http://elm:8080/gp ./gp2/modules/build/TransposeDataset.zip");
+            System.exit(999);
+        }
 
+        String targetURL = args[0] + "/installZip.jsp";
 
-	String targetURL = args[0] + "/installZip.jsp";
-	//MultipartPostMethod filePost =  new MultipartPostMethod(targetURL);
+        String fileOrDirName = args[1];
+        File aFile = new File(fileOrDirName);
+        if (!aFile.exists()) {
+            System.out
+                    .println("File does not exist:" + aFile.getAbsolutePath());
+            System.exit(998);
+        }
 
-	String fileOrDirName = args[1];
-	File aFile = new File(fileOrDirName);
-	if (!aFile.exists()) {
-	    System.out.println("File does not exist:" + aFile.getAbsolutePath());
-	    System.exit(998);
-	}
-	System.out.println("Uploading zip files to: " + targetURL);
-	
-	if (aFile.isDirectory()){
-	    System.out.println("Files coming from:" + aFile.getAbsolutePath());
-	    // FilenameFilter filt =  
+        if (aFile.isDirectory()) {
+            System.out.println("Files coming from:" + aFile.getAbsolutePath());
 
-	    File[] children = aFile.listFiles(new FilenameFilter() {
-		    public boolean accept(File dir, String name){
-			return name.toLowerCase().endsWith(".zip");
-		    }
-		}
-            );
-	    for (int i=0; i < children.length; i++){
-		MultipartPostMethod filePost =  new MultipartPostMethod(targetURL);
-		File targetFile = children[i];
-		uploadFile(filePost, targetFile);
+            File[] children = aFile.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".zip");
+                }
+            });
+            for (int i = 0; i < children.length; i++) {
+                MultipartPostMethod filePost = new MultipartPostMethod(
+                        targetURL);
+                File targetFile = children[i];
+                uploadFile(filePost, targetFile);
+            }
 
-	    }
-
-	} else {
-	    MultipartPostMethod filePost =  new MultipartPostMethod(targetURL);
-	    File targetFile = aFile;
-	    uploadFile(filePost, targetFile);
-	}
-    } 
-
-    public static void uploadFile(MultipartPostMethod filePost, File targetFile) throws Exception {
-	filePost.addParameter(targetFile.getName(), targetFile);
-	HttpClient client = new HttpClient();
-	HttpState state = client.getState();
-	
-	Cookie cook = new Cookie(filePost.getHostConfiguration().getHost(), "userid", "genepattern", "/gp", Integer.MAX_VALUE, false);
-	state.addCookie(cook);
-	client.setState(state);
-
-	client.setConnectionTimeout(5000);
-	    
-	System.out.println("Uploading: " + targetFile.getName() + " to " + filePost.getHostConfiguration().getHostURL());
-	int status = client.executeMethod(filePost);
-	
-	if (status == HttpStatus.SC_OK) {
-	    //System.out.println(" : " + status);
-	    // to see the whole response uncomment the following line
-	    //System.out.println("Upload complete, response=" + filePost.getResponseBodyAsString() );
-	} else {
-	    System.out.println("\tUpload failed, response=" + HttpStatus.getStatusText(status) );
-	}
-	filePost.releaseConnection();
+        } else {
+            MultipartPostMethod filePost = new MultipartPostMethod(targetURL);
+            File targetFile = aFile;
+            uploadFile(filePost, targetFile);
+        }
     }
 
+    public static void uploadFile(MultipartPostMethod filePost, File targetFile)
+            throws Exception {
+        filePost.addParameter("file1", targetFile);
+        HttpClient client = new HttpClient();
+        HttpState state = client.getState();
 
+        Cookie cook = new Cookie(filePost.getHostConfiguration().getHost(),
+                "userid", "genepattern", "/gp", Integer.MAX_VALUE, false);
+        state.addCookie(cook);
+        client.setState(state);
 
+        client.setConnectionTimeout(5000);
 
+        System.out.println("Uploading " + targetFile.getName() + " to "
+                + filePost.getHostConfiguration().getHostURL());
+        int status = client.executeMethod(filePost);
 
+        if (status != HttpStatus.SC_OK) {
+            System.out.println("\tUpload failed, response="
+                    + HttpStatus.getStatusText(status));
+        }
+        filePost.releaseConnection();
+    }
 }
