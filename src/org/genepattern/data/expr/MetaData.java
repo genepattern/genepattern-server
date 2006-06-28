40b355f5be06f3e5de6181f3930c3f148ac3593d
jgould <jgould@broadinstitute.org>
2006-06-28 19:13:37 +0000

    Fixed slice method
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3067 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/data/expr/MetaData.java
+++ src/org/genepattern/data/expr/MetaData.java
@@ -16,40 +16,37 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * 
  * @author Joshua Gould
- * 
  */
 public class MetaData {
     private HashMap metaDataName2Depth;
 
-    private ArrayList metaData;
+    private ArrayList metaDataList;
 
-    private int size;
+    private int capacity;
 
     /**
      * Creates a new <tt>MetaData</tt> instance
-     * 
-     * @param size
-     *            the size
+     *
+     * @param capacity the capacity
      */
-    public MetaData(int size) {
-        this.size = size;
-        metaData = new ArrayList();
+    public MetaData(int capacity) {
+        this.capacity = capacity;
+        metaDataList = new ArrayList();
         metaDataName2Depth = new HashMap();
     }
 
     /**
      * Creates a new <tt>MetaData</tt> instance
-     * 
+     *
      * @param copy
      */
     protected MetaData(MetaData copy) {
-        this.size = copy.size;
-        metaData = (ArrayList) copy.metaData.clone();
-        for (int i = 0; i < metaData.size(); i++) {
-            String[] obj = (String[]) metaData.get(i);
-            metaData.set(i, obj.clone());
+        this.capacity = copy.capacity;
+        metaDataList = (ArrayList) copy.metaDataList.clone();
+        for (int i = 0; i < metaDataList.size(); i++) {
+            String[] obj = (String[]) metaDataList.get(i);
+            metaDataList.set(i, obj.clone());
         }
         metaDataName2Depth = (HashMap) copy.metaDataName2Depth.clone();
     }
@@ -57,37 +54,32 @@ public class MetaData {
     /**
      * Constructs and returns a new <tt>MetaData</tt> instance that contains
      * the indicated cells. Indices can be in arbitrary order.
-     * 
-     * @param indices
-     *            The indices
-     * 
+     *
+     * @param indices The indices
      * @return the new MetaData
-     * 
      */
     public MetaData slice(int[] indices) {
-        MetaData m = new MetaData(indices.length);
-        m.metaDataName2Depth = (HashMap) this.metaDataName2Depth.clone();
-        for (int i = 0; i < metaData.size(); i++) {
-            String[] obj = (String[]) metaData.get(i);
+        MetaData slicedMetaData = new MetaData(indices.length);
+        slicedMetaData.metaDataName2Depth = (HashMap) this.metaDataName2Depth.clone();
+        slicedMetaData.metaDataList = new ArrayList();
+        for (int i = 0, size = this.metaDataList.size(); i < size; i++) {
+            String[] obj = (String[]) this.metaDataList.get(i);
             String[] copy = new String[indices.length];
-            for (int j = 0; j < indices.length; j++) {
+            for (int j = 0, length = indices.length; j < length; j++) {
                 copy[j] = obj[indices[j]];
             }
-            m.metaData.set(i, copy);
+            slicedMetaData.metaDataList.add(copy);
         }
-        return m;
-
+        return slicedMetaData;
     }
+
 
     /**
      * Sets the meta data at the given index
-     * 
-     * @param index
-     *            The index
-     * @param name
-     *            The name of the meta data at the given index
-     * @param value
-     *            The value of the meta data at the given index
+     *
+     * @param index The index
+     * @param name  The name of the meta data at the given index
+     * @param value The value of the meta data at the given index
      */
     public void setMetaData(int index, String name, String value) {
         getArray(name)[index] = value;
@@ -95,28 +87,23 @@ public class MetaData {
 
     /**
      * Sets the meta data for the given name
-     * 
-     * 
-     * @param name
-     *            The name of the meta data
-     * @param values
-     *            The values of the meta data
+     *
+     * @param name   The name of the meta data
+     * @param values The values of the meta data
      */
     public void setMetaData(String name, String[] values) {
-        if (values.length != size) {
+        if (values.length != capacity) {
             throw new IllegalArgumentException(
-                    "Length of values must be equal to size (" + size + ")");
+                    "Length of values must be equal to capacity (" + capacity + ")");
         }
-        metaData.set(getDepth(name), values);
+        metaDataList.set(getDepth(name), values);
     }
 
     /**
      * Gets the meta data at the given index
-     * 
-     * @param index
-     *            The index
-     * @param name
-     *            The name of the meta data at the given index
+     *
+     * @param index The index
+     * @param name  The name of the meta data at the given index
      * @return The value of the meta data at the given index
      */
     public String getMetaData(int index, String name) {
@@ -125,10 +112,8 @@ public class MetaData {
 
     /**
      * Gets the meta data for the given name
-     * 
-     * 
-     * @param name
-     *            The name of the meta data
+     *
+     * @param name The name of the meta data
      * @return The values of the meta data
      */
     public String[] getMetaData(String name) {
@@ -136,40 +121,38 @@ public class MetaData {
     }
 
     /**
-     * 
-     * @param name
-     *            The name of the meta data
-     * @return index in metaData list
+     * @param name The name of the meta data
+     * @return index in metaDataList list
      */
     protected int getDepth(String name) {
         Integer depth = (Integer) metaDataName2Depth.get(name);
-
+        int _depth;
         if (depth == null) {
-            depth = new Integer(metaData.size());
-            for (int i = 0, size = depth.intValue() + 1; i < size; i++) {
-                metaData.add(null);
+            depth = new Integer(metaDataList.size());
+            _depth = depth.intValue();
+            for (int i = metaDataList.size(); i <= _depth; i++) {
+                metaDataList.add(new String[capacity]);
             }
-            metaData.set(depth.intValue(), new String[size]);
+            metaDataList.set(_depth, new String[capacity]);
             metaDataName2Depth.put(name, depth);
+        } else {
+            _depth = depth.intValue();
         }
-        return depth.intValue();
+        return _depth;
     }
 
     /**
-     * 
-     * @param name
-     *            The name of the meta data
+     * @param name The name of the meta data
      * @return the array for the given name
      */
     protected String[] getArray(String name) {
-        return (String[]) metaData.get(getDepth(name));
+        return (String[]) metaDataList.get(getDepth(name));
     }
 
     /**
      * Tests whether this instance contains the given meta data
-     * 
-     * @param name
-     *            The name of the meta data
+     *
+     * @param name The name of the meta data
      * @return <tt>true</tt> if found, <tt>false</tt> otherwise
      */
     public boolean contains(String name) {
