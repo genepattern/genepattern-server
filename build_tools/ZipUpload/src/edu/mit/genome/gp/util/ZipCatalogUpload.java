05811255317b1b35a81586792f98ea8fb72d7669
jgould <jgould@broadinstitute.org>
2006-08-04 19:33:03 +0000

    added doc and replaced strings with constants
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3180 48a7a3f7-ad46-0410-b969-29950918272e

--- build_tools/ZipUpload/src/edu/mit/genome/gp/util/ZipCatalogUpload.java
+++ build_tools/ZipUpload/src/edu/mit/genome/gp/util/ZipCatalogUpload.java
@@ -15,6 +15,7 @@ package edu.mit.genome.gp.util;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.MultipartPostMethod;
 
 import java.io.File;
@@ -25,38 +26,38 @@ import java.io.IOException;
 public class ZipCatalogUpload {
     public static final String DEV = "dev";
     public static final String PROD = "prod";
-    public static final String MODULE = "mod";
+    public static final String MODULE = "module";
     public static final String PATCH = "patch";
     public static final String BROAD_URL =
-            "http://iwww.broad.mit.edu/cgi-bin/cancer/software/genepattern/gp_publish_module.cgi";
+            "http://www.broad.mit.edu/webservices/genepatternmodulerepository/ModuleRepositoryServlet";
 
     public static void main(String[] args) throws Exception {
         if (args.length != 4) {
             System.out.println("Usage: java ZipCatalogUpload url [module|patch] [prod|dev] fileOrDir\n\n");
-            System.out.println("To load all zips in a directory");
+            System.out.println("To load all zips in a directory:");
             System.out.println(
-                    "e.g. java ZipCatalogUpload http://iwww.broad.mit.edu/cgi-bin/cancer/software/genepattern/gp_publish_module.cgi module dev ./gp2/modules/build  ");
-            System.out.println("To load just one zip");
-            System.out.println(
-                    "e.g. java ZipCatalogUpload http://iwww.broad.mit.edu/cgi-bin/cancer/software/genepattern/gp_publish_module.cgi dev ./gp2/modules/build/TransposeDataset.zip");
-            System.exit(999);
+                    "e.g. java ZipCatalogUpload " + BROAD_URL + " " + MODULE + " " + DEV + " ./gp2/modules/build  ");
+            System.out.println("To load just one zip:");
+            System.out.println("e.g. java ZipCatalogUpload " + BROAD_URL + " " + MODULE + " " + DEV +
+                    " ./gp2/modules/build/TransposeDataset.zip");
+            System.exit(1);
         }
         String targetURL = args[0];
-        String type = args[1];
-        String environment = args[2];
+        String modulePatchOrSuite = args[1];
+        String devOrProd = args[2];
         String fileOrDirName = args[3];
-        upload(targetURL, type, environment, fileOrDirName);
+        upload(targetURL, modulePatchOrSuite, devOrProd, fileOrDirName);
     }
 
 
     /**
-     * @param targetURL     The url  http://iwww.broad.mit.edu/cgi-bin/cancer/software/genepattern/gp_publish_module.cgi
-     * @param type          ZipCatalogUpload.MODULE or ZipCatalogUpload.PATCH
-     * @param environment   ZipCatalogUpload.DEV or ZipCatalogUpload.PROD
-     * @param fileOrDirName The file name or directory to upload
+     * @param targetURL          The url  http://iwww.broad.mit.edu/cgi-bin/cancer/software/genepattern/gp_publish_module.cgi
+     * @param modulePatchOrSuite ZipCatalogUpload.MODULE or ZipCatalogUpload.PATCH
+     * @param devOrProd          ZipCatalogUpload.DEV or ZipCatalogUpload.PROD
+     * @param fileOrDirName      The file name or directory to upload
      * @throws IOException If an error occurs
      */
-    public static void upload(String targetURL, String type, String environment, String fileOrDirName)
+    public static void upload(String targetURL, String modulePatchOrSuite, String devOrProd, String fileOrDirName)
             throws IOException {
         File aFile = new File(fileOrDirName);
         if (!aFile.exists()) {
@@ -64,8 +65,6 @@ public class ZipCatalogUpload {
             System.exit(1);
         }
         if (aFile.isDirectory()) {
-            System.out.println("Files coming from dir:" + aFile.getAbsolutePath());
-            // FilenameFilter filt =
             File[] children = aFile.listFiles(new FilenameFilter() {
                 public boolean accept(File dir, String name) {
                     return name.toLowerCase().endsWith(".zip");
@@ -74,32 +73,48 @@ public class ZipCatalogUpload {
             for (int i = 0; i < children.length; i++) {
                 MultipartPostMethod filePost = new MultipartPostMethod(targetURL);
                 File targetFile = children[i];
-                uploadFile(filePost, type, environment, targetFile);
+                uploadFile(filePost, modulePatchOrSuite, devOrProd, targetFile);
             }
         } else {
             MultipartPostMethod filePost = new MultipartPostMethod(targetURL);
-            File targetFile = aFile;
-            uploadFile(filePost, type, environment, targetFile);
+            uploadFile(filePost, modulePatchOrSuite, devOrProd, aFile);
         }
     }
 
-    private static void uploadFile(MultipartPostMethod filePost, String type, String environment, File targetFile)
-            throws IOException {
-        boolean DEBUG = Boolean.getBoolean("DEBUG");
-        filePost.addParameter("repos", type);
-        filePost.addParameter("location", environment);
-        filePost.addParameter("zipfilename", targetFile);
+    /**
+     * @param targetURL The url  http://iwww.broad.mit.edu/cgi-bin/cancer/software/genepattern/gp_publish_module.cgi
+     * @throws IOException If an error occurs
+     */
+
+    public static void regenerateCatalog(String targetURL) throws IOException {
         HttpClient client = new HttpClient();
-        client.setConnectionTimeout(5000);
+        GetMethod get = new GetMethod(targetURL + "?cmd=REGENERATE_CATALOG");
+        try {
+            int status = client.executeMethod(get);
+            if (status == HttpStatus.SC_OK) {
+                System.out.println("Successfully regenerated catalog");
+            } else {
+                System.out.println("Error regenerating catalog");
+            }
+        } finally {
+            get.releaseConnection();
+        }
+    }
+
+    private static void uploadFile(MultipartPostMethod filePost, String modulePatchOrSuite, String devOrProd,
+                                   File targetFile) throws IOException {
+        filePost.addParameter("repos", modulePatchOrSuite);
+        filePost.addParameter("env", devOrProd);
+        filePost.addParameter("zipfilename", targetFile);
+        filePost.addParameter("cmd", "upload");
+        HttpClient client = new HttpClient();
+        client.setConnectionTimeout(Integer.MAX_VALUE);
         int status = client.executeMethod(filePost);
         if (status == HttpStatus.SC_OK) {
-            System.out.println("\t Upload complete " + status);
-            // to see the whole response uncomment the following line
-            if (DEBUG) {
-                System.out.println("Upload complete, response=" + filePost.getResponseBodyAsString());
-            }
+            System.out.println("Successfully uploaded " + targetFile.getName());
         } else {
-            System.out.println("\t Upload failed, response=" + HttpStatus.getStatusText(status));
+            System.out.println("An error occurred while uploading " + targetFile.getName());
+            System.out.println("Response: " + HttpStatus.getStatusText(status));
         }
         filePost.releaseConnection();
     }
