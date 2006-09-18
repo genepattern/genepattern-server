7730ab0653e840d24b56869d5cf3f64d44656142
jgould <jgould@broadinstitute.org>
2006-09-18 22:13:57 +0000

    reverting back to 1.8 for backwards compatibility
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3478 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/webservice/TaskInfo.java
+++ src/org/genepattern/webservice/TaskInfo.java
@@ -1,195 +1,220 @@
 /*
- The Broad Institute
- SOFTWARE COPYRIGHT NOTICE AGREEMENT
- This software and its documentation are copyright (2003-2006) by the
- Broad Institute/Massachusetts Institute of Technology. All rights are
- reserved.
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2006) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
 
- This software is supplied without any warranty or guaranteed support
- whatsoever. Neither the Broad Institute nor MIT can be responsible for its
- use, misuse, or functionality.
- */
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+
 
 package org.genepattern.webservice;
 
 /**
  * Used to hold information about particular Task
  * 
+ * @author Rajesh Kuttan
+ * @version 1.0
  */
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.Map;
 
 public class TaskInfo implements Serializable {
 
-    private int id = 0;
+	private int taskID = 0;
 
-    private String name = "", description = "", parameterInfo = "";
+	private String taskName = "", description = "",
+			parameter_info = "";
 
-    private int accessId = 0;
+	private int accessId = 0;
 
-    private String userId;
+	private String userId = null;
 
-    private TaskInfoAttributes taskInfoAttributes;
+	// JL
+	private Map taskInfoAttributes = null;
 
-    private ParameterInfo[] parameterInfoArray;
+	private ParameterInfo[] parameterInfoArray = null;
 
-    public TaskInfo() {
-    }
+	/** Creates new TaskInfo */
+	public TaskInfo() {
+	}
 
-    public TaskInfo(int taskID, String name, String description, String parameterInfo) {
-        this(taskID, name, description, parameterInfo, null);
-    }
+	public TaskInfo(int taskID, String taskName, String description,
+			String parameter_info) {
+		this(taskID, taskName, description, parameter_info, null);
+	}
 
-    public TaskInfo(int taskID, String name, String description, String parameterInfo,
-            TaskInfoAttributes taskInfoAttributes) {
-        this(taskID, name, description, parameterInfo, taskInfoAttributes, null, 0);
-    }
+	public TaskInfo(int taskID, String taskName, String description,
+			String parameter_info, TaskInfoAttributes taskInfoAttributes) {
+		this(taskID, taskName, description, parameter_info,
+				taskInfoAttributes, null, 0);
+	}
 
-    public TaskInfo(int taskID, String name, String description, String parameterInfo,
-            TaskInfoAttributes taskInfoAttributes, String userId, int accessId) {
-        this.id = taskID;
-        this.name = name;
-        this.description = description;
-        this.parameterInfo = parameterInfo;
-        this.taskInfoAttributes = taskInfoAttributes;
-        this.userId = userId;
-        this.accessId = accessId;
-    }
+	public TaskInfo(int taskID, String taskName, String description,
+			String parameter_info,
+			TaskInfoAttributes taskInfoAttributes, String userId, int accessId) {
+		this.taskID = taskID;
+		this.taskName = taskName;
+		this.description = description;
+		this.parameter_info = parameter_info;
+		this.taskInfoAttributes = taskInfoAttributes;
+		this.userId = userId;
+		this.accessId = accessId;
+	}
 
-    /**
-     * Checks to see if the TaskInfo contains a input file parameter field
-     * 
-     * @return true if it contains a
-     *         <code>ParamterInfo<code> object with TYPE as FILE and MODE as INPUT
-     */
-    public boolean containsInputFileParam() {
-        // add convinent checking method --Hui
-        if (this.parameterInfoArray != null) {
-            for (int i = 0; i < this.parameterInfoArray.length; i++) {
-                if (this.parameterInfoArray[i].isInputFile()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
-    public boolean equals(Object otherThing) {
-        if (!(otherThing instanceof TaskInfo)) {
-            return false;
-        }
-        TaskInfo other = (TaskInfo) otherThing;
-        return getUserId().equals(other.getUserId())
-                && getAccessId() == other.getAccessId()
-                && ((getTaskInfoAttributes() == null && other.getTaskInfoAttributes() == null) || (getTaskInfoAttributes() != null
-                        && other.getTaskInfoAttributes() != null && getTaskInfoAttributes().equals(
-                        other.getTaskInfoAttributes())))
-                && getName().equals(other.getName())
-                && getId() == other.getId()
-                && getDescription().equals(other.getDescription())
-                && ((getParameterInfo() == null && other.getParameterInfo() == null) || (getParameterInfo() != null
-                        && other.getParameterInfo() != null && getParameterInfo().equals(other.getParameterInfo())));
-    }
+	public String getUserId() {
+		return this.userId;
+	}
 
-    public int hashCode() {
-        if (this.getTaskInfoAttributes() == null) {
-            return super.hashCode();
-        }
-        return this.getTaskInfoAttributes().get("LSID").hashCode();
-    }
+	public void setAccessId(int accessId) {
+		this.accessId = accessId;
+	}
 
-    /**
-     * get parameter info jaxb string
-     * 
-     * @return parameterInfo
-     */
-    public String getParameterInfo() {
-        return parameterInfo;
-    }
+	public int getAccessId() {
+		return this.accessId;
+	}
 
-    /**
-     * set parameter info jaxb string
-     * 
-     */
-    public void setParameterInfo(java.lang.String parameterInfo) {
-        this.parameterInfo = parameterInfo;
-    }
+	public Map getTaskInfoAttributes() {
+		// JL
+		return (Map) taskInfoAttributes;
+	}
 
-    public ParameterInfo[] getParameterInfoArray() {
+	public TaskInfoAttributes giveTaskInfoAttributes() {
+		// JL
+		if (taskInfoAttributes == null)
+			taskInfoAttributes = new TaskInfoAttributes();
+		return (TaskInfoAttributes) taskInfoAttributes;
+	}
 
-        if (this.parameterInfoArray == null && this.parameterInfo != null) {
-            ParameterFormatConverter converter = new ParameterFormatConverter();
-            try {
-                this.parameterInfoArray = converter.getParameterInfoArray(this.parameterInfo);
-            }
-            catch (OmnigeneException e) {
-                e.printStackTrace();
-            }
-        }
-        return this.parameterInfoArray;
-    }
+	public void setTaskInfoAttributes(Map taskInfoAttributes) {
+		try {
+			TaskInfoAttributes tia = (TaskInfoAttributes) taskInfoAttributes;
+			this.taskInfoAttributes = tia;
+		} catch (ClassCastException cce) {
+			this.taskInfoAttributes = new TaskInfoAttributes(taskInfoAttributes);
+		}
 
-    public void setParameterInfoArray(ParameterInfo[] parameterInfoArray) {
-        this.parameterInfoArray = parameterInfoArray;
-    }
+	}
 
-    public int getAccessId() {
-        return accessId;
-    }
+	public String getName() {
+		return taskName;
+	}
 
-    public void setAccessId(int accessId) {
-        this.accessId = accessId;
-    }
+	public void setName(java.lang.String taskName) {
+		this.taskName = taskName;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public int getID() {
+		return taskID;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setID(int taskID) {
+		this.taskID = taskID;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setDescription(java.lang.String description) {
+		this.description = description;
+	}
 
-    public String getName() {
-        return name;
-    }
+	/**
+	 * get parameter info jaxb string
+	 * 
+	 * @return parameter_info
+	 */
+	public String getParameterInfo() {
+		return parameter_info;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 * get <CODE>ParameterInfo</CODE> array
+	 *  
+	 */
+	public void setParameterInfo(java.lang.String parameter_info) {
+		this.parameter_info = parameter_info;
+	}
 
-    public String getUserId() {
-        return userId;
-    }
+	/**
+	 * get <CODE>ParameterInfo</CODE> array
+	 * 
+	 * @return parameterInfoArray
+	 */
+	public ParameterInfo[] getParameterInfoArray() {
+		//add checking in here --Hui
+		if (this.parameterInfoArray == null && this.parameter_info != null) {
+			ParameterFormatConverter converter = new ParameterFormatConverter();
+			try {
+				this.parameterInfoArray = converter
+						.getParameterInfoArray(this.parameter_info);
+			} catch (OmnigeneException e) {
+			}
+		}
+		return this.parameterInfoArray;
+	}
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+	/**
+	 * set <CODE>ParameterInfo</CODE> array
+	 * 
+	 * @param parameterInfoArray
+	 */
+	public void setParameterInfoArray(ParameterInfo[] parameterInfoArray) {
+		this.parameterInfoArray = parameterInfoArray;
+	}
 
-    public TaskInfoAttributes getTaskInfoAttributes() {
-        if (taskInfoAttributes == null) {
-            taskInfoAttributes = new TaskInfoAttributes();
-        }
-        return taskInfoAttributes;
-    }
+	
 
-    public void setTaskInfoAttributes(TaskInfoAttributes taskInfoAttributes) {
-        this.taskInfoAttributes = taskInfoAttributes;
-    }
+	/**
+	 * Checks to see if the TaslInfo contains a input file parameter field
+	 * 
+	 * @return true if it contains a
+	 *         <code>ParamterInfo<code> object with TYPE as FILE and MODE as INPUT
+	 */
+	public boolean containsInputFileParam() {
+		//add convinent checking method --Hui
+		if (this.parameterInfoArray != null) {
+			for (int i = 0; i < this.parameterInfoArray.length; i++) {
+				if (this.parameterInfoArray[i].isInputFile()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    public TaskInfoAttributes giveTaskInfoAttributes() {
-        return getTaskInfoAttributes();
-    }
-
-    public int getID() {
-        return getId();
-    }
+	public boolean equals(Object otherThing) {
+		if (!(otherThing instanceof TaskInfo) || otherThing == null)
+			return false;
+		TaskInfo other = (TaskInfo) otherThing;
+		return getUserId().equals(other.getUserId())
+				&& getAccessId() == other.getAccessId()
+				&& ((getTaskInfoAttributes() == null && other
+						.getTaskInfoAttributes() == null) || (getTaskInfoAttributes() != null
+						&& other.getTaskInfoAttributes() != null && getTaskInfoAttributes()
+						.equals(other.getTaskInfoAttributes())))
+				&& getName().equals(other.getName())
+				&& getID() == other.getID()
+				&& getDescription().equals(other.getDescription())
+				&& ((getParameterInfo() == null && other.getParameterInfo() == null) || (getParameterInfo() != null
+						&& other.getParameterInfo() != null && getParameterInfo()
+						.equals(other.getParameterInfo())));
+	}
+	
+	public int hashCode(){
+      if(this.getTaskInfoAttributes()==null) {
+         return super.hashCode();  
+      }
+		return this.getTaskInfoAttributes().get("LSID").hashCode();
+	}
 
 }
