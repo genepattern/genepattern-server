03a0b7de601178e015cdf4533e5a4e2a41812551
jgould <jgould@broadinstitute.org>
2006-09-22 21:34:46 +0000

    added constructor that tasks user name only
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3565 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webservice/server/local/LocalTaskIntegratorClient.java
+++ src/org/genepattern/server/webservice/server/local/LocalTaskIntegratorClient.java
@@ -1,15 +1,14 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.server.webservice.server.local;
 
@@ -34,192 +33,193 @@ import org.genepattern.webservice.WebServiceException;
  * @author Joshua Gould
  */
 public class LocalTaskIntegratorClient extends TaskIntegrator implements ITaskIntegrator {
-	String progressMessage = "";
-	JspWriter out = null;
-	String userName = null;
+    String progressMessage = "";
+    JspWriter out = null; // TODO this variable does not belong here
+    String userName = null;
 
-	public LocalTaskIntegratorClient(final String userName, final JspWriter out) {
-		this.out = out;
-		this.userName = userName;
-	}
+    public LocalTaskIntegratorClient(final String userName) {
+        this.userName = userName;
+    }
 
-	protected String getUserName() {
-		return userName;
+    public LocalTaskIntegratorClient(final String userName, final JspWriter out) {
+        this.out = out;
+        this.userName = userName;
+    }
 
-	}
+    protected String getUserName() {
+        return userName;
 
-	public String modifyTask(int accessId, String taskName, String description,
-			ParameterInfo[] parameterInfoArray, java.util.Map taskAttributes,
-			javax.activation.DataHandler[] dataHandlers, String[] fileNames)
-			throws WebServiceException {
-		return super.modifyTask(accessId, taskName, description,
-				parameterInfoArray, taskAttributes, dataHandlers, fileNames);
-	}
+    }
 
-	public String modifySuite(int access_id, String lsid, String name, String description,
-			String author, String owner, ArrayList moduleLsids, ArrayList files)
-			throws WebServiceException{
+    public String modifyTask(int accessId, String taskName, String description, ParameterInfo[] parameterInfoArray,
+            java.util.Map taskAttributes, javax.activation.DataHandler[] dataHandlers, String[] fileNames)
+            throws WebServiceException {
+        return super.modifyTask(accessId, taskName, description, parameterInfoArray, taskAttributes, dataHandlers,
+                fileNames);
+    }
 
-		return modifySuite(access_id, lsid, name, description, author, owner, moduleLsids, files);
+    public String modifySuite(int access_id, String lsid, String name, String description, String author, String owner,
+            ArrayList moduleLsids, ArrayList files) throws WebServiceException {
 
-	}
+        return modifySuite(access_id, lsid, name, description, author, owner, moduleLsids, files);
 
+    }
 
-	public String cloneTask(String lsid, String cloneName)
-			throws WebServiceException {
-		return super.cloneTask(lsid, cloneName);
-	}
+    public String cloneTask(String lsid, String cloneName) throws WebServiceException {
+        return super.cloneTask(lsid, cloneName);
+    }
 
-	public DataHandler[] getSupportFiles(String lsid)
-			throws WebServiceException {
-		return super.getSupportFiles(lsid);
-	}
+    public DataHandler[] getSupportFiles(String lsid) throws WebServiceException {
+        return super.getSupportFiles(lsid);
+    }
 
-	public String[] getSupportFileNames(String lsid) throws WebServiceException {
-		return super.getSupportFileNames(lsid);
-	}
+    public String[] getSupportFileNames(String lsid) throws WebServiceException {
+        return super.getSupportFileNames(lsid);
+    }
 
-	public File[] getAllFiles(TaskInfo task) throws WebServiceException {
-		String taskId = (String) task.getTaskInfoAttributes().get(
-				GPConstants.LSID);
-		if (taskId == null) {
-			taskId = task.getName();
-		}
-		DataHandler[] dh = super.getSupportFiles(taskId);
+    public File[] getAllFiles(TaskInfo task) throws WebServiceException {
+        String taskId = (String) task.getTaskInfoAttributes().get(GPConstants.LSID);
+        if (taskId == null) {
+            taskId = task.getName();
+        }
+        DataHandler[] dh = super.getSupportFiles(taskId);
 
-		Vector vFiles = new Vector();
-		for (int i = 0, length = dh.length; i < length; i++) {
-			FileDataSource ds = (FileDataSource) dh[i].getDataSource();
-			File file =  ds.getFile();
-			if (!file.isDirectory()) vFiles.add(file);
-		}
-		File[] files = (File[])vFiles.toArray(new File[vFiles.size()]);
-		
-		return files;
-	}
+        Vector vFiles = new Vector();
+        for (int i = 0, length = dh.length; i < length; i++) {
+            FileDataSource ds = (FileDataSource) dh[i].getDataSource();
+            File file = ds.getFile();
+            if (!file.isDirectory()) vFiles.add(file);
+        }
+        File[] files = (File[]) vFiles.toArray(new File[vFiles.size()]);
 
-	public File[] getDocFiles(TaskInfo task) throws WebServiceException {
-		String taskId = (String) task.getTaskInfoAttributes().get(
-				GPConstants.LSID);
-		if (taskId == null) {
-			taskId = task.getName();
-		}
-		DataHandler[] dh = super.getDocFiles(taskId);
-		File[] files = new File[dh.length];
-		for (int i = 0, length = dh.length; i < length; i++) {
-			FileDataSource ds = (FileDataSource) dh[i].getDataSource();
-			files[i] = ds.getFile();
-		}
-		return files;
-	}
+        return files;
+    }
 
-	
+    public File[] getDocFiles(TaskInfo task) throws WebServiceException {
+        String taskId = (String) task.getTaskInfoAttributes().get(GPConstants.LSID);
+        if (taskId == null) {
+            taskId = task.getName();
+        }
+        DataHandler[] dh = super.getDocFiles(taskId);
+        File[] files = new File[dh.length];
+        for (int i = 0, length = dh.length; i < length; i++) {
+            FileDataSource ds = (FileDataSource) dh[i].getDataSource();
+            files[i] = ds.getFile();
+        }
+        return files;
+    }
 
-	public void deleteTask(String lsid) throws WebServiceException {
-		super.deleteTask(lsid);
-	}
+    public void deleteTask(String lsid) throws WebServiceException {
+        super.deleteTask(lsid);
+    }
 
-	public String deleteFiles(String lsid, String[] fileNames)
-			throws WebServiceException {
-		return super.deleteFiles(lsid, fileNames);
-	}
+    public String deleteFiles(String lsid, String[] fileNames) throws WebServiceException {
+        return super.deleteFiles(lsid, fileNames);
+    }
 
-	public String importZipFromURL(String url, int privacy)
-			throws WebServiceException {
-		return super.importZipFromURL(url, privacy, true, this);
-	}
+    public String importZipFromURL(String url, int privacy) throws WebServiceException {
+        return super.importZipFromURL(url, privacy, true, this);
+    }
 
-	public String importZipFromURL(String url, int privacy, boolean recursive)
-			throws WebServiceException {
-		return super.importZipFromURL(url, privacy, recursive, this);
-	}
-	
-	public String importZipFromURL(String url, int privacy, boolean recursive, ITaskIntegrator taskIntegrator)
-			throws WebServiceException {
-		return super.importZipFromURL(url, privacy, recursive, taskIntegrator);
-	}
-	
-	public String importZip(DataHandler handler, int privacy) throws WebServiceException {
-		return super.importZip(handler, privacy);
-	}
-	
-	public DataHandler exportToZip(String taskName) throws WebServiceException {
-		return super.exportToZip(taskName, false);
-	}
+    public String importZipFromURL(String url, int privacy, boolean recursive) throws WebServiceException {
+        return super.importZipFromURL(url, privacy, recursive, this);
+    }
 
-	public DataHandler exportToZip(String taskName, boolean recursive) throws WebServiceException {
-		return super.exportToZip(taskName, recursive);
-	}
+    public String importZipFromURL(String url, int privacy, boolean recursive, ITaskIntegrator taskIntegrator)
+            throws WebServiceException {
+        return super.importZipFromURL(url, privacy, recursive, taskIntegrator);
+    }
 
-	public long[] getLastModificationTimes(String lsid, String[] fileNames) throws WebServiceException {
-		return super.getLastModificationTimes(lsid, fileNames);
-	}
+    public String importZip(DataHandler handler, int privacy) throws WebServiceException {
+        return super.importZip(handler, privacy);
+    }
 
-	public DataHandler[] getSupportFiles(String lsid, String[] fileNames)
-			throws WebServiceException {
-		return super.getSupportFiles(lsid, fileNames);
-	}
-	
-	public DataHandler[] getDocFiles(String lsid) throws WebServiceException {
-		return super.getDocFiles(lsid);
-	}
+    public DataHandler exportToZip(String taskName) throws WebServiceException {
+        return super.exportToZip(taskName, false);
+    }
 
+    public DataHandler exportToZip(String taskName, boolean recursive) throws WebServiceException {
+        return super.exportToZip(taskName, recursive);
+    }
 
-		
+    public long[] getLastModificationTimes(String lsid, String[] fileNames) throws WebServiceException {
+        return super.getLastModificationTimes(lsid, fileNames);
+    }
 
+    public DataHandler[] getSupportFiles(String lsid, String[] fileNames) throws WebServiceException {
+        return super.getSupportFiles(lsid, fileNames);
+    }
 
+    public DataHandler[] getDocFiles(String lsid) throws WebServiceException {
+        return super.getDocFiles(lsid);
+    }
 
-	public void statusMessage(String message) {
-		System.out.println(message);
-		try { 
-			out.println(message + "<br>");
-			flush();
-		} catch (IOException ioe) {
-			// ignore
-		}
-	}
+    public void statusMessage(String message) {
+        System.out.println(message);
+        try {
+            if (out != null) {
+                out.println(message + "<br>");
+                flush();
+            }
+        }
+        catch (IOException ioe) {
+            // ignore
+        }
+    }
 
-	public void errorMessage(String message) {
-		System.out.println(message);
-		try { 
-			out.println(message + "<br>");
-			flush();
-		} catch (IOException ioe) {
-			// ignore
-		}
-	}
+    public void errorMessage(String message) {
+        System.out.println(message);
+        try {
+            if (out != null) {
+                out.println(message + "<br>");
+                flush();
+            }
+        }
+        catch (IOException ioe) {
+            // ignore
+        }
+    }
 
-	public void beginProgress(String message) {
-		progressMessage = message;
-		try {
-			out.print(message + " <span id=\""  + message.hashCode() + "\">0</span>% complete<br>");
-			flush();
-		} catch (IOException ioe) {
-			// ignore
-		}
-	}
+    public void beginProgress(String message) {
+        progressMessage = message;
+        try {
+            if (out != null) {
+                out.print(message + " <span id=\"" + message.hashCode() + "\">0</span>% complete<br>");
+                flush();
+            }
+        }
+        catch (IOException ioe) {
+            // ignore
+        }
+    }
 
-	public void continueProgress(int percentComplete) {
-		System.out.print("\rcontinueProgress: " + progressMessage + " " + percentComplete + "% complete");
-		try { 
-			out.println("<script language=\"Javascript\">writeToLayer('" + progressMessage.hashCode() + "', " + percentComplete + ");</script>");
-			//out.println(progressMessage + " " + percentComplete + "% complete<br>");
-			flush();
-		} catch (IOException ioe) {
-			// ignore
-		}
-	}
+    public void continueProgress(int percentComplete) {
+        System.out.print("\rcontinueProgress: " + progressMessage + " " + percentComplete + "% complete");
+        try {
+            if (out != null) {
+                out.println("<script language=\"Javascript\">writeToLayer('" + progressMessage.hashCode() + "', "
+                        + percentComplete + ");</script>");
+                // out.println(progressMessage + " " + percentComplete + "%
+                // complete<br>");
+                flush();
+            }
+        }
+        catch (IOException ioe) {
+            // ignore
+        }
+    }
 
-	public void endProgress() {
-		System.out.println("\r" + progressMessage + " complete     ");
-	}
+    public void endProgress() {
+        System.out.println("\r" + progressMessage + " complete     ");
+    }
 
-	protected void flush() throws IOException {
-		for (int i = 0; i < 8*1024; i++) out.print(" ");
-		out.println();
-		out.flush();
-	}
-    
-    
-    
+    protected void flush() throws IOException {
+        if (out != null) {
+            for (int i = 0; i < 8 * 1024; i++)
+                out.print(" ");
+            out.println();
+            out.flush();
+        }
+    }
+
 }
