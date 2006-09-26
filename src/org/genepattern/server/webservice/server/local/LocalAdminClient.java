6f91e5da003e61629c43552180f5847c91bd096b
jgould <jgould@broadinstitute.org>
2006-09-26 01:56:46 +0000

    added getUserProperty
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3588 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webservice/server/local/LocalAdminClient.java
+++ src/org/genepattern/server/webservice/server/local/LocalAdminClient.java
@@ -1,134 +1,147 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.server.webservice.server.local;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
-import java.util.*;
-
+import org.genepattern.server.user.User;
+import org.genepattern.server.user.UserHome;
+import org.genepattern.server.user.UserProp;
 import org.genepattern.server.webservice.server.AdminService;
 import org.genepattern.server.webservice.server.IAdminService;
 import org.genepattern.util.GPConstants;
-import org.genepattern.webservice.TaskInfo;
 import org.genepattern.webservice.SuiteInfo;
+import org.genepattern.webservice.TaskInfo;
 import org.genepattern.webservice.WebServiceException;
 
 public class LocalAdminClient {
-	IAdminService service;
+    IAdminService service;
 
-	String userName;
+    String userName;
 
-	public LocalAdminClient(final String userName) {
-		this.userName = userName;
-		service = new AdminService() {
-			protected String getUserName() {
-				return userName;
-			}
-		};
-	}
+    public LocalAdminClient(final String userName) {
+        this.userName = userName;
+        service = new AdminService() {
+            protected String getUserName() {
+                return userName;
+            }
+        };
+    }
 
-	public TreeMap getTaskCatalogByLSID(Collection tasks)
-			throws WebServiceException {
-		TreeMap tmCatalog = new TreeMap(String.CASE_INSENSITIVE_ORDER);
-		for (Iterator it = tasks.iterator(); it.hasNext();) {
-			TaskInfo ti = (TaskInfo) it.next();
-			String lsid = ti.giveTaskInfoAttributes().get(GPConstants.LSID);
-			if (lsid != null && lsid.length() > 0) {
-				tmCatalog.put(lsid, ti);
-			}
-			tmCatalog.put(ti.getName(), ti);
-		}
-		return tmCatalog;
-	}
+    public TreeMap getTaskCatalogByLSID(Collection tasks) throws WebServiceException {
+        TreeMap tmCatalog = new TreeMap(String.CASE_INSENSITIVE_ORDER);
+        for (Iterator it = tasks.iterator(); it.hasNext();) {
+            TaskInfo ti = (TaskInfo) it.next();
+            String lsid = ti.giveTaskInfoAttributes().get(GPConstants.LSID);
+            if (lsid != null && lsid.length() > 0) {
+                tmCatalog.put(lsid, ti);
+            }
+            tmCatalog.put(ti.getName(), ti);
+        }
+        return tmCatalog;
+    }
 
-	public Map getLSIDToVersionsMap() throws WebServiceException {
-		return service.getLSIDToVersionsMap();
-	}
+    public Map getLSIDToVersionsMap() throws WebServiceException {
+        return service.getLSIDToVersionsMap();
+    }
 
-	public TreeMap getTaskCatalogByLSID() throws WebServiceException {
-		return getTaskCatalogByLSID(Arrays.asList(service.getAllTasks()));
-	}
+    public TreeMap getTaskCatalogByLSID() throws WebServiceException {
+        return getTaskCatalogByLSID(Arrays.asList(service.getAllTasks()));
+    }
 
-	public Collection getTaskCatalog() throws WebServiceException {
-		return Arrays.asList(service.getAllTasks());
-	}
-	public Collection getLatestTasks() throws WebServiceException {
-		return Arrays.asList(service.getLatestTasks());
-	}
-	
+    public Collection getTaskCatalog() throws WebServiceException {
+        return Arrays.asList(service.getAllTasks());
+    }
 
-	/**
-	 * return a map keyed by taskType with values being
-	 * alphabetically sorted list of the tasks that are
-	 * part of that type
-	 **/
-	public Map getLatestTasksByType() throws WebServiceException{
-		Collection latest = getLatestTasks();
-		TreeMap typeToTaskMap = new TreeMap(new Comparator() {
-			public int compare(Object o1, Object o2) {
-				String s1 = (String) o1;
-				String s2 = (String) o2;
-				return ((s1.toLowerCase()).compareTo(s2.toLowerCase()));
-			}
-			}
-		);
-		
-		for (Iterator iter = latest.iterator(); iter.hasNext(); ){
-			TaskInfo task = (TaskInfo)iter.next();
-			String type = (String)task.getTaskInfoAttributes().get(GPConstants.TASK_TYPE);
-			
-			if (type == null) type = "Unclassified";
-			TreeSet typeList = (TreeSet)typeToTaskMap.get(type);
-			if (typeList == null) {
-				typeList = new TreeSet( new Comparator() {
-					public int compare(Object o1, Object o2) {
-						TaskInfo t1 = (TaskInfo) o1;
-						TaskInfo t2 = (TaskInfo) o2;
-						return (((String)t1.getName().toLowerCase()).compareTo(t2.getName().toLowerCase()));
-					}
-				}
-				);
-				typeToTaskMap.put(type, typeList);
-			}
-			typeList.add(task);
-		}
-		return typeToTaskMap;
-	}
+    public Collection getLatestTasks() throws WebServiceException {
+        return Arrays.asList(service.getLatestTasks());
+    }
 
+    /**
+     * return a map keyed by taskType with values being alphabetically sorted
+     * list of the tasks that are part of that type
+     */
+    public Map getLatestTasksByType() throws WebServiceException {
+        Collection latest = getLatestTasks();
+        TreeMap typeToTaskMap = new TreeMap(new Comparator() {
+            public int compare(Object o1, Object o2) {
+                String s1 = (String) o1;
+                String s2 = (String) o2;
+                return ((s1.toLowerCase()).compareTo(s2.toLowerCase()));
+            }
+        });
 
-	public TaskInfo getTask(String lsid) throws WebServiceException {
-		return service.getTask(lsid);
-	}
+        for (Iterator iter = latest.iterator(); iter.hasNext();) {
+            TaskInfo task = (TaskInfo) iter.next();
+            String type = (String) task.getTaskInfoAttributes().get(GPConstants.TASK_TYPE);
 
-	public SuiteInfo getSuite(String lsid) throws WebServiceException{
-		return service.getSuite(lsid);
-	}
+            if (type == null) type = "Unclassified";
+            TreeSet typeList = (TreeSet) typeToTaskMap.get(type);
+            if (typeList == null) {
+                typeList = new TreeSet(new Comparator() {
+                    public int compare(Object o1, Object o2) {
+                        TaskInfo t1 = (TaskInfo) o1;
+                        TaskInfo t2 = (TaskInfo) o2;
+                        return (((String) t1.getName().toLowerCase()).compareTo(t2.getName().toLowerCase()));
+                    }
+                });
+                typeToTaskMap.put(type, typeList);
+            }
+            typeList.add(task);
+        }
+        return typeToTaskMap;
+    }
 
-	
-	public SuiteInfo[] getLatestSuites() throws WebServiceException{
-		return service.getLatestSuites();
-	}
-	
-	public SuiteInfo[] getAllSuites() throws WebServiceException{
-		return service.getAllSuites();
-	}
-	
+    public TaskInfo getTask(String lsid) throws WebServiceException {
+        return service.getTask(lsid);
+    }
 
-	public SuiteInfo[] getSuiteMembership(String lsid) throws WebServiceException{
-		return service.getSuiteMembership(lsid);
-	}
+    public SuiteInfo getSuite(String lsid) throws WebServiceException {
+        return service.getSuite(lsid);
+    }
 
+    public SuiteInfo[] getLatestSuites() throws WebServiceException {
+        return service.getLatestSuites();
+    }
 
+    public SuiteInfo[] getAllSuites() throws WebServiceException {
+        return service.getAllSuites();
+    }
 
+    public SuiteInfo[] getSuiteMembership(String lsid) throws WebServiceException {
+        return service.getSuiteMembership(lsid);
+    }
+
+    public String getUserProperty(String key) {
+        if (key == null) {
+            return null;
+        }
+        User user = (new UserHome()).findById(userName);
+        List<UserProp> props = user != null ? user.getProps() : Collections.EMPTY_LIST;
+        for (UserProp p : props) {
+            if (key.equals(p.getKey())) {
+                return p.getValue();
+            }
+        }
+        return null;
+    }
 
 }
