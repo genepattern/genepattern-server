b8bc00c26a37edbc7e379dab5eb7943f017a3697
jrobinso <jrobinso@broadinstitute.org>
2006-09-27 19:42:35 +0000

    Added check on transaction before committing,  in case app code has already committed.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3656 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/HibernateSessionRequestFilter.java
+++ src/org/genepattern/server/webapp/HibernateSessionRequestFilter.java
@@ -13,11 +13,8 @@ public class HibernateSessionRequestFilter implements Filter {
 
     private static Logger log = Logger.getLogger(HibernateSessionRequestFilter.class);
 
- 
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain chain)
-            throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
 
         try {
             log.debug("Starting a database transaction");
@@ -28,17 +25,25 @@ public class HibernateSessionRequestFilter implements Filter {
 
             // Commit and cleanup
             log.debug("Committing the database transaction");
-            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+            if (HibernateUtil.getSession().getTransaction().isActive()) {
+                HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+            }
 
-        } catch (StaleObjectStateException staleEx) {
+        }
+        catch (StaleObjectStateException staleEx) {
             log.error("This interceptor does not implement optimistic concurrency control!");
             log.error("Your application will not work until you add compensation actions!");
-            // Rollback, close everything, possibly compensate for any permanent changes
-            // during the conversation, and finally restart business conversation. Maybe
-            // give the user of the application a chance to merge some of his work with
-            // fresh data... what you do here depends on your applications design.
+            // Rollback, close everything, possibly compensate for any permanent
+            // changes
+            // during the conversation, and finally restart business
+            // conversation. Maybe
+            // give the user of the application a chance to merge some of his
+            // work with
+            // fresh data... what you do here depends on your applications
+            // design.
             throw staleEx;
-        } catch (Throwable ex) {
+        }
+        catch (Throwable ex) {
             // Rollback only
             ex.printStackTrace();
             try {
@@ -46,8 +51,12 @@ public class HibernateSessionRequestFilter implements Filter {
                     log.debug("Trying to rollback database transaction after exception");
                     HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
                 }
-            } catch (Throwable rbEx) {
+            }
+            catch (Throwable rbEx) {
                 log.error("Could not rollback transaction after exception!", rbEx);
+            }
+            finally {
+                HibernateUtil.closeCurrentSession();
             }
 
             // Let others handle it... maybe another interceptor for exceptions?
@@ -56,9 +65,10 @@ public class HibernateSessionRequestFilter implements Filter {
     }
 
     public void init(FilterConfig filterConfig) throws ServletException {
-         
+
     }
 
-    public void destroy() {}
+    public void destroy() {
+    }
 
 }
