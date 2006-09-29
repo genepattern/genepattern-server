5fba39b9b9821924feafc9cc1a50c19ea5652de5
jgould <jgould@broadinstitute.org>
2006-09-29 14:10:34 +0000

    Use UIBeanHelper instead of AbstractUIBean
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3698 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/ModuleListBean.java
+++ src/org/genepattern/server/webapp/jsf/ModuleListBean.java
@@ -12,50 +12,47 @@ import org.genepattern.server.webservice.server.local.LocalAdminClient;
 import org.genepattern.webservice.TaskInfo;
 import org.genepattern.webservice.WebServiceException;
 
-public class ModuleListBean extends AbstractUIBean {
+public class ModuleListBean {
 
-	
-	
-	
-	public List getLatestModules(){
-		
-		LocalAdminClient admin = new LocalAdminClient(getUserId());
-		
-		try {
-			return  asSelectItemList(admin.getLatestTasks());
-			
-			
-		} catch (WebServiceException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return new ArrayList();
-	}
-	
-	public List getAllModules(){
-		
-		LocalAdminClient admin = new LocalAdminClient(getUserId());
-		
-		try {
-			return asSelectItemList(admin.getTaskCatalog());
-			
-		} catch (WebServiceException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return new ArrayList();
-	}
-	
-	protected List<SelectItem> asSelectItemList(Collection in){
-    	ArrayList<SelectItem> out = new ArrayList();
-    	new SelectItem();
-    	for (Iterator iter = in.iterator(); iter.hasNext(); ){
-    		TaskInfo ti = (TaskInfo) iter.next();
-    		out.add(new SelectItem(ti.getTaskInfoAttributes().get("LSID"), ti.getName()));
-    	}
-    	
-    	
-    	return out;
+    public List getLatestModules() {
+
+        LocalAdminClient admin = new LocalAdminClient(UIBeanHelper.getUserId());
+
+        try {
+            return asSelectItemList(admin.getLatestTasks());
+
+        }
+        catch (WebServiceException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new ArrayList();
     }
-	
+
+    public List getAllModules() {
+
+        LocalAdminClient admin = new LocalAdminClient(UIBeanHelper.getUserId());
+
+        try {
+            return asSelectItemList(admin.getTaskCatalog());
+
+        }
+        catch (WebServiceException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new ArrayList();
+    }
+
+    protected List<SelectItem> asSelectItemList(Collection in) {
+        ArrayList<SelectItem> out = new ArrayList();
+        new SelectItem();
+        for (Iterator iter = in.iterator(); iter.hasNext();) {
+            TaskInfo ti = (TaskInfo) iter.next();
+            out.add(new SelectItem(ti.getTaskInfoAttributes().get("LSID"), ti.getName()));
+        }
+
+        return out;
+    }
+
 }
