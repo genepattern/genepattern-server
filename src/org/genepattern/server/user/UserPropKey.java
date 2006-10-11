f32e3c73790e7600aa3fe44de9f28612a2a5b171
jgould <jgould@broadinstitute.org>
2006-10-11 19:56:21 +0000

    Added RECENT_JOBS_TO_SHOW
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3836 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/user/UserPropKey.java
+++ src/org/genepattern/server/user/UserPropKey.java
@@ -14,7 +14,9 @@ package org.genepattern.server.user;
 
 public class UserPropKey {
     public static final String VISUALIZER_JAVA_FLAGS = "visualizer_java_flags";
+    public static final String RECENT_JOBS_TO_SHOW = "recent_jobs_to_show";
 
+    
     private UserPropKey() {
 
     }
