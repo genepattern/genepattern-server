dc133f57ef48c70e677c5f6b73126439a4a41583
jrobinso <jrobinso@broadinstitute.org>
2006-10-12 03:54:00 +0000

    Change console output to log
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3839 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/database/HibernateUtil.java
+++ src/org/genepattern/server/database/HibernateUtil.java
@@ -33,7 +33,7 @@ public class HibernateUtil {
         }
         catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            log.error("Initial SessionFactory creation failed.",ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
