dbdc0383d5da244bfffd5cd6e69b8f8c553085f3
jrobinso <jrobinso@broadinstitute.org>
2006-10-31 18:11:41 +0000

    Converted submit to javascript for changing chooser modes
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@3992 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/ModuleChooserBean.java
+++ src/org/genepattern/server/webapp/jsf/ModuleChooserBean.java
@@ -34,6 +34,7 @@ import org.genepattern.server.webservice.server.local.LocalAdminClient;
 import org.genepattern.util.LSID;
 import org.genepattern.webservice.TaskInfo;
 import org.genepattern.webservice.WebServiceException;
+import java.util.*;
 
 public class ModuleChooserBean implements java.io.Serializable {
     /**
@@ -41,38 +42,33 @@ public class ModuleChooserBean implements java.io.Serializable {
      */
     private static final long serialVersionUID = -9026970426503039995L;
     private static Logger log = Logger.getLogger(ModuleChooserBean.class);
-    List<ModuleCategory> categories = null;
 
-    private String mode = "category"; // @todo - externalize or make enum
+    private List<ModuleCategoryGroup> categories = null;
+    private String[] modes = { "category", "suite", "all" };
+    private String selectedMode;
     private String selectedModule = "";
 
-    public List<ModuleCategory> getAllTasks() {
-        if (categories == null) {
-            categories = new ArrayList<ModuleCategory>();
-            categories.add(ModuleHelper.getRecentlyUsed());
+    public List<ModuleCategoryGroup> getTasks() {
+        categories = new ArrayList<ModuleCategoryGroup>();
+        for (String mode : modes) {
+            List<ModuleCategory> tmp = new ArrayList();
+            tmp.add(ModuleHelper.getRecentlyUsed());
             if (mode.equals("all")) {
-                categories.add(ModuleHelper.getAllTasks());
+                tmp.add(ModuleHelper.getAllTasks());
             }
             else if (mode.equals("suite")) {
-                for(ModuleCategory cat : ModuleHelper.getTasksBySuite()) {
-                    categories.add(cat);
+                for (ModuleCategory cat : ModuleHelper.getTasksBySuite()) {
+                    tmp.add(cat);
                 }
             }
             else if (mode.equals("category")) {
                 for (ModuleCategory cat : ModuleHelper.getTasksByType()) {
-                    categories.add(cat);
+                    tmp.add(cat);
                 }
             }
+            categories.add(new ModuleCategoryGroup(mode, tmp));
         }
         return categories;
-    }
-
-    public String getMode() {
-        return mode;
-    }
-
-    public void setMode(String mode) {
-        this.mode = mode;
     }
 
     public String getSelectedModule() {
@@ -81,10 +77,14 @@ public class ModuleChooserBean implements java.io.Serializable {
 
     public void setSelectedModule(String selectedModule) {
         this.selectedModule = selectedModule;
+ //       RunTaskBean runTaskBean = (RunTaskBean) UIBeanHelper.getManagedBean("#{runTaskBean}");
+ //       if (runTaskBean != null) {
+//            runTaskBean.setTask(selectedModule);
+//        }
     }
 
-    public void modeChanged(ValueChangeEvent  event) {
-        categories = null;
+    public void modeChanged(ValueChangeEvent event) {
+        selectedMode = (String) event.getNewValue();
     }
 
     public void moduleClicked(ActionEvent event) {
@@ -96,7 +96,7 @@ public class ModuleChooserBean implements java.io.Serializable {
     }
 
     private String getVersion(TaskInfo ti) {
-        
+
         try {
             LSID lsid = new LSID(ti.getLsid());
             return lsid.getVersion();
@@ -105,9 +105,46 @@ public class ModuleChooserBean implements java.io.Serializable {
             log.error("Bad LSID", e);
             throw new RuntimeException(e);
         }
-        
+
     }
 
+    public String[] getModes() {
+        return modes;
+    }
 
+    public void setModes(String[] modes) {
+        this.modes = modes;
+    }
+
+    public class ModuleCategoryGroup {
+        String mode;
+        List<ModuleCategory> categories;
+
+        public ModuleCategoryGroup(String mode, List<ModuleCategory> categories) {
+            this.mode = mode;
+            this.categories = categories;
+        }
+
+        public List<ModuleCategory> getCategories() {
+            return categories;
+        }
+
+        public String getMode() {
+            return mode;
+        }
+
+    }
+
+    public String getSelectedMode() {
+        if (selectedMode == null) {
+            selectedMode = "category";
+        }
+        return selectedMode;
+    }
+
+    public void setSelectedMode(String selectedMode) {
+
+        this.selectedMode = selectedMode;
+    }
 
 }
