86b27ee938879e505f04eb24556feaa88b919e32
jgould <jgould@broadinstitute.org>
2006-11-02 17:11:24 +0000

    added method getLanguage
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4014 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/codegenerator/TaskCodeGenerator.java
+++ src/org/genepattern/codegenerator/TaskCodeGenerator.java
@@ -1,30 +1,41 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.codegenerator;
 
-import org.genepattern.webservice.*;
-
+import org.genepattern.webservice.AnalysisJob;
+import org.genepattern.webservice.ParameterInfo;
 
 /**
- *  Generates code for invocation of a single task
- *
- * @author    Joshua Gould
+ * Generates code for invocation of a single task
+ * 
+ * @author Joshua Gould
  */
 public interface TaskCodeGenerator {
 
-   public String generateTask(AnalysisJob job,
-         ParameterInfo[] params);
-     
+    /**
+     * Returns a string that contains the code to call the job with the given
+     * parameters.
+     * 
+     * @param job
+     * @param params
+     * @return the code.
+     */
+    public String generateTask(AnalysisJob job, ParameterInfo[] params);
+
+    /**
+     * Returns the language (e.g. Java, MATLAB)/
+     * @return the language.
+     */
+    public String getLanguage();
 
 }
