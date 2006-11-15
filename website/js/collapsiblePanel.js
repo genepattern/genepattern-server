e7e3fb147bd9a70667371e3fa29ba055b746e5d2
jrobinso <jrobinso@broadinstitute.org>
2006-11-15 03:55:26 +0000

    Added code to check for and toggle hidden field state when toggling visibility.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4089 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/collapsiblePanel.js
+++ website/js/collapsiblePanel.js
@@ -1,13 +1,18 @@
-                             
+
 function toggleVisibility(id) {
     var element = document.getElementById(id + "_panel");
+    var id= "expansion_state_" + id;
+    var hiddenField = document.getElementById(id);    
+    
     if(element.style.display=="none")
     {
         showCategory(id);
+        if(hiddenField != null) hiddenField.value = "true";
     }
     else
     {
         hideCategory(id);
+        if(hiddenField != null) hiddenField.value = "false";
     }
 }
 
@@ -49,4 +54,10 @@ function closeAll() {
        var catId = fullId.substring(0, index);
        hideCategory(catId);
      }
+ }
+ 
+function toggleExpansionState( categoryIdentifier) {
+   var id= "expansion_state_" + categoryIdentifier;
+   var hiddenField = document.getElementById(id);
+   hiddenField.value = (hiddenField.value == "true" ? "false" : "true");
  }
