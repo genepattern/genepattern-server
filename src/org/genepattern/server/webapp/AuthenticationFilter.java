b6e44811958b88765d66ea46709465d1014d859b
jrobinso <jrobinso@broadinstitute.org>
2006-11-20 14:37:19 +0000

    Added check for database registration.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4144 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/AuthenticationFilter.java
+++ src/org/genepattern/server/webapp/AuthenticationFilter.java
@@ -25,6 +25,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.genepattern.server.database.HibernateUtil;
+import org.genepattern.server.user.UserDAO;
+import org.genepattern.server.webapp.jsf.UIBeanHelper;
+
 /**
  * Servlet filter that requires user to log in to access certain pages
  * 
@@ -32,177 +36,207 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class AuthenticationFilter implements Filter {
 
-    private static final String[] NO_AUTH_REQUIRED_PAGES = { "getPipelineModel.jsp", "retrieveResults.jsp", "getFile.jsp", "getInputFile.jsp",
-            "login.jsp", "login.jsf", "registerUser.jsf", "forgotPassword.jsf" };
-    /** Forward to home page if logged in user requests these pages */
-    private static final String[] LOGIN_PAGES = { "login.jsf", "registerUser.jsf", "forgotPassword.jsf" };
-    private static final String HOME_PAGE = "/pages/index.jsf";
+	private static final String[] NO_AUTH_REQUIRED_PAGES = {
+			"getPipelineModel.jsp", "retrieveResults.jsp", "getFile.jsp",
+			"getInputFile.jsp", "login.jsp", "login.jsf", "registerUser.jsf",
+			"forgotPassword.jsf" };
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-            ServletException {
+	/** Forward to home page if logged in user requests these pages */
+	private static final String[] LOGIN_PAGES = { "login.jsf",
+			"registerUser.jsf", "forgotPassword.jsf" };
 
-        HttpServletRequest req = (HttpServletRequest) request;
-        String fqHostName = getFQHostName();
-        String requestedURI = req.getRequestURI();
+	private static final String HOME_PAGE = "/pages/index.jsf";
 
-        // allow jsp precompilation
-        if (isJspPrecompile(req)) {
-            chain.doFilter(request, response);
-            return;
-        }
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
 
-        // always use the fqHostName so that only one cookie needs to be written
-        String serverName = request.getServerName();
-        if (!fqHostName.equalsIgnoreCase(serverName)) {
-            redirectToFullyQualifiedHostName((HttpServletRequest) request, (HttpServletResponse) response);
-            return;
-        }
+		HttpServletRequest req = (HttpServletRequest) request;
+		String fqHostName = getFQHostName();
+		String requestedURI = req.getRequestURI();
 
-        if (isAuthenticated((HttpServletRequest) request)) {
-            for (int i = 0, length = LOGIN_PAGES.length; i < length; i++) {
-                if (requestedURI.contains(LOGIN_PAGES[i])) {
-                    ((HttpServletResponse) response).sendRedirect(req.getContextPath() + HOME_PAGE);
-                    return;
-                }
-            }
-            chain.doFilter(request, response);
-        }
-        else {
-            // escape valve for some pages that do not require authentication
-            for (int i = 0, length = NO_AUTH_REQUIRED_PAGES.length; i < length; i++) {
-                if (requestedURI.contains(NO_AUTH_REQUIRED_PAGES[i])) {
-                    chain.doFilter(request, response);
-                    return;
-                }
-            }
-            setLoginPageRedirect((HttpServletRequest) request, (HttpServletResponse) response);
-        }
-    }
+		// allow jsp precompilation
+		if (isJspPrecompile(req)) {
+			chain.doFilter(request, response);
+			return;
+		}
 
-    /**
-     * Authenticate the user by checking to see if the cookie userID is set
-     */
-    protected boolean isAuthenticated(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie c : cookies) {
-                if ("userID".equals(c.getName())) {
-                    String userId = c.getValue();
-                    request.setAttribute("userID", userId);
-                    return true;
-                }
-            }
-        }
+		// always use the fqHostName so that only one cookie needs to be written
+		String serverName = request.getServerName();
+		if (!fqHostName.equalsIgnoreCase(serverName)) {
+			redirectToFullyQualifiedHostName((HttpServletRequest) request,
+					(HttpServletResponse) response);
+			return;
+		}
 
-        return false;
-    }
+		if (isAuthenticated((HttpServletRequest) request, (HttpServletResponse) response)) {
+			for (int i = 0, length = LOGIN_PAGES.length; i < length; i++) {
+				if (requestedURI.contains(LOGIN_PAGES[i])) {
+					((HttpServletResponse) response).sendRedirect(req
+							.getContextPath()
+							+ HOME_PAGE);
+					return;
+				}
+			}
+			chain.doFilter(request, response);
+		} else {
+			// escape valve for some pages that do not require authentication
+			for (int i = 0, length = NO_AUTH_REQUIRED_PAGES.length; i < length; i++) {
+				if (requestedURI.contains(NO_AUTH_REQUIRED_PAGES[i])) {
+					chain.doFilter(request, response);
+					return;
+				}
+			}
+			setLoginPageRedirect((HttpServletRequest) request,
+					(HttpServletResponse) response);
+		}
+	}
 
-    /**
-     * check whether this is just the servlet engine precompiling jsp pages.
-     * This must be a request coming from the localhost, with only the one
-     * parameter set 'jsp_precompile'
-     */
-    protected boolean isJspPrecompile(HttpServletRequest request) {
-        String rh = request.getRemoteHost();
-        String p = request.getParameter("jsp_precompile");
+	/**
+	 * Authenticate the user by checking to see if the cookie userID is set
+	 */
+	protected boolean isAuthenticated(HttpServletRequest request, HttpServletResponse response) {
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				if ("userID".equals(c.getName())) {
+					String userId = c.getValue();
+					if (userId != null && isRegistered(userId)) {
+						request.setAttribute("userID", userId);
+						return true;
+					}
+					else {
+	                    c.setMaxAge(0);
+	                    c.setPath(request.getContextPath());
+	                    response.addCookie(c);
+					}
+				}
+			}
+		}
 
-        int numParams = request.getParameterMap().keySet().size();
+		return false;
+	}
 
-        // allow jsp precompilation
-        return ((p != null) && ("localhost".equals(rh)) && (numParams == 1));
-    }
+	/**
+	 * Check to see if user has been registerd in database.
+	 * 
+	 */
+	private boolean isRegistered(String userId) {
+		// This filter might run before the HibernateFilter. Open transaction
+		// just in case.  The transaction is closed automatically.
+		HibernateUtil.beginTransaction();
+		return (new UserDAO()).findById(userId) != null;
 
-    /**
-     * get fully qualified host name from the machine. If this was set in system
-     * properties (from the genepattern.properties file) use that since some
-     * machines have multiple aliases
-     */
-    protected String getFQHostName() throws IOException {
-        String fqHostName = System.getProperty("fullyQualifiedHostName");
-        if (fqHostName == null) {
-            fqHostName = InetAddress.getLocalHost().getCanonicalHostName();
-        }
-        if (fqHostName.equals("localhost")) {
-            fqHostName = "127.0.0.1";
-        }
-        return fqHostName;
-    }
+	}
 
-    public void setLoginPageRedirect(HttpServletRequest request, HttpServletResponse response) {
-        String currentURL = request.getRequestURI();
-        // get everything after the context root
-        int firstSlash = currentURL.indexOf("/", 1); // jump past the
-        // starting slash
-        String targetURL = null;
-        if (firstSlash != -1) {
-            targetURL = currentURL.substring(firstSlash + 1, currentURL.length());
-        }
+	/**
+	 * check whether this is just the servlet engine precompiling jsp pages.
+	 * This must be a request coming from the localhost, with only the one
+	 * parameter set 'jsp_precompile'
+	 */
+	protected boolean isJspPrecompile(HttpServletRequest request) {
+		String rh = request.getRemoteHost();
+		String p = request.getParameter("jsp_precompile");
 
-        // if (targetURL != null && request.getQueryString() != null) {
-        // targetURL = targetURL + ("?" + request.getQueryString());
-        // }
+		int numParams = request.getParameterMap().keySet().size();
 
-        // redirect to the fully-qualified host name to make sure that the
-        // cookie that we are allowed to write is useful
-        try {
-            String fqHostName = System.getProperty("fqHostName");
-            if (fqHostName == null) {
-                fqHostName = InetAddress.getLocalHost().getCanonicalHostName();
-                if (fqHostName.equals("localhost")) {
-                    fqHostName = "127.0.0.1";
-                }
-            }
+		// allow jsp precompilation
+		return ((p != null) && ("localhost".equals(rh)) && (numParams == 1));
+	}
 
-            String contextPath = request.getContextPath();
-            if (contextPath != null && contextPath.charAt(contextPath.length() - 1) != '/') {
-                contextPath += "/";
-            }
-            String basePath = request.getScheme() + "://" + fqHostName + ":" + request.getServerPort() + contextPath;
-            String fqAddress = basePath + "pages/login.jsf";
-            targetURL = basePath + targetURL;
+	/**
+	 * get fully qualified host name from the machine. If this was set in system
+	 * properties (from the genepattern.properties file) use that since some
+	 * machines have multiple aliases
+	 */
+	protected String getFQHostName() throws IOException {
+		String fqHostName = System.getProperty("fullyQualifiedHostName");
+		if (fqHostName == null) {
+			fqHostName = InetAddress.getLocalHost().getCanonicalHostName();
+		}
+		if (fqHostName.equals("localhost")) {
+			fqHostName = "127.0.0.1";
+		}
+		return fqHostName;
+	}
 
-            if (targetURL != null && !targetURL.contains("login.jsf")) { // don't
-                // redirect
-                // back to
-                // login
-                // page
-                request.getSession().setAttribute("origin", targetURL);
-            }
-            response.sendRedirect(fqAddress);
-        }
-        catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
+	public void setLoginPageRedirect(HttpServletRequest request,
+			HttpServletResponse response) {
+		String currentURL = request.getRequestURI();
+		// get everything after the context root
+		int firstSlash = currentURL.indexOf("/", 1); // jump past the
+		// starting slash
+		String targetURL = null;
+		if (firstSlash != -1) {
+			targetURL = currentURL.substring(firstSlash + 1, currentURL
+					.length());
+		}
 
-    public void redirectToFullyQualifiedHostName(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            String fqHostName = System.getProperty("fullyQualifiedHostName");
-            if (fqHostName == null) {
-                fqHostName = InetAddress.getLocalHost().getCanonicalHostName();
-            }
-            if (fqHostName.equals("localhost")) {
-                fqHostName = "127.0.0.1";
-            }
-            String queryString = request.getQueryString();
-            if (queryString == null) {
-                queryString = "";
-            }
-            else {
-                queryString = "?" + queryString;
-            }
-            String fqAddress = request.getScheme() + "://" + fqHostName + ":" + request.getServerPort()
-                    + request.getRequestURI() + queryString;
-            response.sendRedirect(fqAddress);
-        }
-        catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
+		// if (targetURL != null && request.getQueryString() != null) {
+		// targetURL = targetURL + ("?" + request.getQueryString());
+		// }
 
-    public void destroy() {
-    }
+		// redirect to the fully-qualified host name to make sure that the
+		// cookie that we are allowed to write is useful
+		try {
+			String fqHostName = System.getProperty("fqHostName");
+			if (fqHostName == null) {
+				fqHostName = InetAddress.getLocalHost().getCanonicalHostName();
+				if (fqHostName.equals("localhost")) {
+					fqHostName = "127.0.0.1";
+				}
+			}
 
-    public void init(FilterConfig filterconfig) throws ServletException {
-    }
+			String contextPath = request.getContextPath();
+			if (contextPath != null
+					&& contextPath.charAt(contextPath.length() - 1) != '/') {
+				contextPath += "/";
+			}
+			String basePath = request.getScheme() + "://" + fqHostName + ":"
+					+ request.getServerPort() + contextPath;
+			String fqAddress = basePath + "pages/login.jsf";
+			targetURL = basePath + targetURL;
+
+			if (targetURL != null && !targetURL.contains("login.jsf")) { // don't
+				// redirect
+				// back to
+				// login
+				// page
+				request.getSession().setAttribute("origin", targetURL);
+			}
+			response.sendRedirect(fqAddress);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public void redirectToFullyQualifiedHostName(HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			String fqHostName = System.getProperty("fullyQualifiedHostName");
+			if (fqHostName == null) {
+				fqHostName = InetAddress.getLocalHost().getCanonicalHostName();
+			}
+			if (fqHostName.equals("localhost")) {
+				fqHostName = "127.0.0.1";
+			}
+			String queryString = request.getQueryString();
+			if (queryString == null) {
+				queryString = "";
+			} else {
+				queryString = "?" + queryString;
+			}
+			String fqAddress = request.getScheme() + "://" + fqHostName + ":"
+					+ request.getServerPort() + request.getRequestURI()
+					+ queryString;
+			response.sendRedirect(fqAddress);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public void destroy() {
+	}
+
+	public void init(FilterConfig filterconfig) throws ServletException {
+	}
 }
