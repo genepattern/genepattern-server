6a086f21ff2c70bcb8fc92dc94aae1e6708d1de6
jrobinso <jrobinso@broadinstitute.org>
2006-11-22 04:48:51 +0000

    Changed to MM menus
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4184 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ website/js/mainMenu.js
@@ -0,0 +1,121 @@
+function mmLoadMenus() {
+  if (window.mm_menu_0925155656_0) return;
+  window.mm_menu_0925155656_0 = new Menu("root",120,18,"Verdana, Arial, Helvetica, sans-serif",10,"#000000","#000099","#EEEEEE","#FFFFFF","left","middle",3,0,500,-5,7,true,true,true,0,true,false);
+  mm_menu_0925155656_0.addMenuItem("Create Task","location=''");
+  mm_menu_0925155656_0.addMenuItem("Create Pipeline","location='/gp/pipelineDesigner.jsp'");
+  mm_menu_0925155656_0.addMenuItem("Install / Update","location='/gp/pages/taskCatalog.jsf'");
+  mm_menu_0925155656_0.addMenuItem("Delete","location=''");
+  mm_menu_0925155656_0.hideOnMouseOut=true;
+  mm_menu_0925155656_0.bgColor='#FFFFFF';
+  mm_menu_0925155656_0.menuBorder=1;
+  mm_menu_0925155656_0.menuLiteBgColor='#FFFFFF';
+  mm_menu_0925155656_0.menuBorderBgColor='#999999';
+  
+  window.mm_menu_002 = new Menu("root",100,18,"Verdana, Arial, Helvetica, sans-serif",10,"#000000","#000099","#EEEEEE","#FFFFFF","left","middle",3,0,500,-5,7,true,true,true,0,true,false);
+  mm_menu_002.addMenuItem("Create Suite","location='/gp/pages/createSuite.jsf'");
+  mm_menu_002.addMenuItem("Install / Update","location=''");
+  mm_menu_002.addMenuItem("Delete","location=''");
+  mm_menu_002.hideOnMouseOut=true;
+  mm_menu_002.bgColor='#FFFFFF';
+  mm_menu_002.menuBorder=1;
+  mm_menu_002.menuLiteBgColor='#FFFFFF';
+  mm_menu_002.menuBorderBgColor='#999999';
+  
+  window.mm_menu_003 = new Menu("root",75,18,"Verdana, Arial, Helvetica, sans-serif",10,"#000000","#000099","#EEEEEE","#FFFFFF","left","middle",3,0,500,-5,7,true,true,true,0,true,false);
+  mm_menu_003.addMenuItem("Job Results","location='/gp/pages/jobResults.jsf'");
+  mm_menu_002.hideOnMouseOut=true;
+  mm_menu_002.bgColor='#FFFFFF';
+  mm_menu_002.menuBorder=1;
+  mm_menu_002.menuLiteBgColor='#FFFFFF';
+  mm_menu_002.menuBorderBgColor='#999999';
+
+  window.mm_menu_001 = new Menu("root",150,18,"Verdana, Arial, Helvetica, sans-serif",10,"#000000","#000099","#EEEEEE","#FFFFFF","left","middle",3,0,500,-5,7,true,true,true,0,true,false);
+  mm_menu_001.addMenuItem("info","location='info.html'");
+  mm_menu_001.addMenuItem("reload","location='info.html'");
+  mm_menu_001.addMenuItem("other","location='info.html'");
+  mm_menu_001.hideOnMouseOut=true;
+  mm_menu_001.bgColor='#FFFFFF';
+  mm_menu_001.menuBorder=1;
+  mm_menu_001.menuLiteBgColor='#FFFFFF';
+  mm_menu_001.menuBorderBgColor='#999999';
+  
+  window.mm_menu_0925160045_0 = new Menu("root",114,18,"Verdana, Arial, Helvetica, sans-serif",10,"#000000","#000099","#EEEEEE","#FFFFFF","left","middle",3,0,500,-5,7,true,true,true,0,true,false);
+  mm_menu_0925160045_0.addMenuItem("modules","location='modules.html'");
+  mm_menu_0925160045_0.addMenuItem("info","location='modules.html'");
+  mm_menu_0925160045_0.addMenuItem("other","location='modules.html'");
+  mm_menu_0925160045_0.addMenuItem("create&nbsp;pipeline","location='modules.html'");
+  mm_menu_0925160045_0.hideOnMouseOut=true;
+  mm_menu_0925160045_0.bgColor='#FFFFFF';
+  mm_menu_0925160045_0.menuBorder=1;
+  mm_menu_0925160045_0.menuLiteBgColor='#FFFFFF';
+  mm_menu_0925160045_0.menuBorderBgColor='#999999';
+
+  window.mm_menu_0925165148_0 = new Menu("root",174,18,"Verdana, Arial, Helvetica, sans-serif",10,"#FFFFFF","#9999FF","#333366","#000033","left","middle",3,0,500,-5,7,true,true,true,3,true,false);
+  mm_menu_0925165148_0.addMenuItem("user's&nbsp;manual&nbsp;&&nbsp;tutorial","location='link.html'");
+  mm_menu_0925165148_0.addMenuItem("release&nbsp;notes","location='link.html'");
+  mm_menu_0925165148_0.addMenuItem("FAQ","location='link.html'");
+  mm_menu_0925165148_0.addMenuItem("public&nbsp;datasets","location='link.html'");
+  mm_menu_0925165148_0.addMenuItem("task&nbsp;documentation","location='link.html'");
+  mm_menu_0925165148_0.addMenuItem("common&nbsp;file&nbsp;formats","location='link.html'");
+  mm_menu_0925165148_0.hideOnMouseOut=true;
+  mm_menu_0925165148_0.bgColor='#CCCC66';
+  mm_menu_0925165148_0.menuBorder=0;
+  mm_menu_0925165148_0.menuLiteBgColor='#CCCC66';
+  mm_menu_0925165148_0.menuBorderBgColor='#CCCC66';
+
+  window.mm_menu_0925171310_0 = new Menu("root",93,18,"Verdana, Arial, Helvetica, sans-serif",10,"#FFFFFF","#9999FF","#333366","#000033","left","middle",3,0,500,-5,7,true,true,true,3,true,false);
+  mm_menu_0925171310_0.addMenuItem("mailing&nbsp;list","location='link.html'");
+  mm_menu_0925171310_0.addMenuItem("report&nbsp;bugs","location='link.html'");
+  mm_menu_0925171310_0.addMenuItem("user&nbsp;forum","location='link.html'");
+  mm_menu_0925171310_0.hideOnMouseOut=true;
+  mm_menu_0925171310_0.bgColor='#CCCC66';
+  mm_menu_0925171310_0.menuBorder=0;
+  mm_menu_0925171310_0.menuLiteBgColor='#CCCC66';
+  mm_menu_0925171310_0.menuBorderBgColor='#CCCC66';
+  
+  window.mm_menu_0925171536_0 = new Menu("root",170,18,"Verdana, Arial, Helvetica, sans-serif",10,"#FFFFFF","#9999FF","#333366","#000033","left","middle",3,0,500,-5,7,true,true,true,3,true,false);
+  mm_menu_0925171536_0.addMenuItem("install&nbsp;graphical&nbsp;client","location='gclient.html'");
+  mm_menu_0925171536_0.addMenuItem("Programming&nbsp;Libraries","location='programming.html'");
+  mm_menu_0925171536_0.hideOnMouseOut=true;
+  mm_menu_0925171536_0.bgColor='#CCCC66';
+  mm_menu_0925171536_0.menuBorder=0;
+  mm_menu_0925171536_0.menuLiteBgColor='#CCCC66';
+  mm_menu_0925171536_0.menuBorderBgColor='#CCCC66';
+
+  mm_menu_0925171536_0.writeMenus();
+} // mmLoadMenus()
+
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+function MM_jumpMenu(targ,selObj,restore){ //v3.0
+  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0;
+}
