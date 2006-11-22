ee2b4ed48d4bb011a1f6fc64fa009b3439a45f7a
jrobinso <jrobinso@broadinstitute.org>
2006-11-22 14:57:21 +0000

    no message
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4188 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/ServerSettingsBean.java
+++ src/org/genepattern/server/webapp/jsf/ServerSettingsBean.java
@@ -5,6 +5,8 @@ package org.genepattern.server.webapp.jsf;
 
 import static org.genepattern.server.webapp.jsf.UIBeanHelper.getRequest;
 
+import java.util.Properties;
+
 import javax.faces.event.ActionEvent;
 
 /**
@@ -19,6 +21,8 @@ public class ServerSettingsBean {
 			"Proxy Settings", "Search Engine", "Shut Down Server" };
 
 	private String currentMode = modes[0];  // Default
+	
+	private Properties settings;
 
 	public String getCurrentMode() {
 		return currentMode;
@@ -33,7 +37,37 @@ public class ServerSettingsBean {
 	}
 	
 	public void modeChanged(ActionEvent evt) {
-		setCurrentMode(getRequest().getParameter("mode"));		
+		setCurrentMode(getRequest().getParameter("mode"));	
+	}
+	
+	/**
+	 * Return the properties object with the server settings.  This should be
+	 * lazy initialized,  it might be called many times but we only need to read the
+	 * file once.
+	 * 
+	 */
+	public Properties getSettings() {
+		
+		if(settings == null) {
+			// TODO -- read this from a file.  
+			settings = new Properties();
+			settings.put("java_flags", "-Xmx512M");
+			// etc.
+		}
+		return settings;
+	}
+	
+	/**
+	 * Save the settings back to the file.  Trigger by the "submit" button on the page.
+	 * 
+	 * @return
+	 */
+	public String saveSettings() {
+		
+		// TODO -- save the settings back to the file
+		System.out.println(settings.get("java_flags"));
+		
+		return null;   // This returns us to the same page
 	}
 	
 	
