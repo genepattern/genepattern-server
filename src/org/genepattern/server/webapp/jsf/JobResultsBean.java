b05a6f140f9cb0ea4509216c85a812a1cd361017
jrobinso <jrobinso@broadinstitute.org>
2006-11-29 16:27:02 +0000

    no message
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4264 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/JobResultsBean.java
+++ src/org/genepattern/server/webapp/jsf/JobResultsBean.java
@@ -17,6 +17,7 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
@@ -24,29 +25,48 @@ import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
+import org.apache.log4j.Logger;
 import org.genepattern.server.webservice.server.local.LocalAnalysisClient;
 import org.genepattern.webservice.JobInfo;
 import org.genepattern.webservice.WebServiceException;
 
 public class JobResultsBean {
-	
-	private String jobSortField = "job id number";
-	private boolean jobSortAscending = true;
-	private String fileSortField = "name";
+	private static Logger log = Logger.getLogger(JobResultsBean.class);
+    
+    /**
+     * Specifies job column to sort on.  Possible values are
+     *   jobNumber
+     *   taskName
+     *   dateSubmitted
+     *   dateCompleted
+     *   status       
+     */
+    private String jobSortColumn = "jobNumber";
+    
+    /**
+     * Job sort direction (true for ascending, false for descending)
+     */
+  	private boolean jobSortAscending = true;
+
+    /**
+     * Specifies file column to sort on.  Possible values are
+     *   name
+     *   size
+     *   lastModified 
+     */    
+    private String fileSortColumn = "name";
+    
+    /**
+     * File sort direction (true for ascending, false for descending)
+     */
 	private boolean fileSortAscending = true;
 	
     private JobResultInfo[] jobResults;
-    private boolean ascending;
     private boolean warnBeforeDeletingJobs;
     private boolean showAllJobs = true;
     
     List checkbox = new ArrayList();
     
-    /** name of column we're sorting on */
-    private String sort;
-    /** column that was last used for sorting */
-    private String lastSort;
-
     public JobResultsBean() {
         updateJobs();
         warnBeforeDeletingJobs = true; // TODO get from property
@@ -54,6 +74,10 @@ public class JobResultsBean {
 
     }
     
+    /**
+     * Update the jobResults list
+     *
+     */
     private void updateJobs() {
         String userId = UIBeanHelper.getUserId();
         LocalAnalysisClient analysisClient = new LocalAnalysisClient(userId);
@@ -65,7 +89,7 @@ public class JobResultsBean {
             }
         }
         catch (WebServiceException wse) {
-            wse.printStackTrace();
+            log.error(wse);
         }
     }
     
@@ -74,12 +98,14 @@ public class JobResultsBean {
     	deleteJobs(selectedJobs);
     	
     	String [] selectedFiles = UIBeanHelper.getRequest().getParameterValues("selectedFiles");
+        // TODO -- deleteFiles(selectedFiles);
     	return null;
     }
     
     public String download() {
     	String [] selectedJobs = UIBeanHelper.getRequest().getParameterValues("selectedJobs");
     	String [] selectedFiles = UIBeanHelper.getRequest().getParameterValues("selectedFiles");
+        // TODO -- download selected jobs and files
     	return null;
     }
 
@@ -93,7 +119,6 @@ public class JobResultsBean {
                 int jobNumber = Integer.parseInt(job);
                 try {
                     analysisClient.deleteJob(jobNumber);
-                    System.out.println("deleted job " + jobNumber);
                 }
                 catch (NumberFormatException e) {
                     e.printStackTrace(); // ignore
@@ -116,79 +141,94 @@ public class JobResultsBean {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg));
         }
         updateJobs();
-        lastSort = null;
         sort();
 
     }
 
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-        sort();
-    }
-
-    public boolean isAscending() {
-        return ascending;
-    }
-
-    public void setAscending(boolean ascending) {
-        this.ascending = ascending;
-
-    }
 
     public String sort() {
-    	
-    	System.out.println(jobSortField);
-    	System.out.println(jobSortAscending);
-    	System.out.println(fileSortField);
-    	System.out.println(fileSortAscending);
-    	
-     
-/*        final String column = getSort();
+    	     
+        sortJobs();
+        sortFiles();
+        return null;
+    }
+
+ 
+    private void sortJobs() {
+        final String column = getJobSortColumn();
         Comparator comparator = new Comparator() {
 
             public int compare(Object o1, Object o2) {
-                JobInfo c1 = (JobInfo) o1;
-                JobInfo c2 = (JobInfo) o2;
+                JobInfo c1 = ((JobResultInfo) o1).getJobInfo();
+                JobInfo c2 = ((JobResultInfo) o2).getJobInfo();
                 if (column == null) {
                     return 0;
                 }
-                else if (column.equals("Job Number")) {
-                    return ascending ? new Integer(c1.getJobNumber()).compareTo(c2.getJobNumber()) : new Integer(c2
+                else if (column.equals("jobNumber")) {
+                    return jobSortAscending ? new Integer(c1.getJobNumber()).compareTo(c2.getJobNumber()) : new Integer(c2
                             .getJobNumber()).compareTo(c1.getJobNumber());
                 }
-                else if (column.equals("Name")) {
-                    return ascending ? c1.getTaskName().compareTo(c2.getTaskName()) : c2.getTaskName().compareTo(
+                else if (column.equals("taskName")) {
+                    return jobSortAscending ? c1.getTaskName().compareTo(c2.getTaskName()) : c2.getTaskName().compareTo(
                             c1.getTaskName());
                 }
-                else if (column.equals("Status")) {
-                    return ascending ? c1.getStatus().compareTo(c2.getStatus()) : c2.getStatus().compareTo(
+                else if (column.equals("status")) {
+                    return jobSortAscending ? c1.getStatus().compareTo(c2.getStatus()) : c2.getStatus().compareTo(
                             c1.getStatus());
                 }
-                else if (column.equals("Completed")) {
-                    return ascending ? c1.getDateCompleted().compareTo(c2.getDateCompleted()) : c2.getDateCompleted()
+                else if (column.equals("dateCompleted")) {
+                    return jobSortAscending ? c1.getDateCompleted().compareTo(c2.getDateCompleted()) : c2.getDateCompleted()
                             .compareTo(c1.getDateCompleted());
                 }
-                else if (column.equals("Submitted")) {
-                    return ascending ? c1.getDateSubmitted().compareTo(c2.getDateSubmitted()) : c2.getDateSubmitted()
+                else if (column.equals("dateSubmitted")) {
+                    return jobSortAscending ? c1.getDateSubmitted().compareTo(c2.getDateSubmitted()) : c2.getDateSubmitted()
                             .compareTo(c1.getDateSubmitted());
                 }
                 else {
-                    return 0;
+                     return 0;
                 }
             }
         };
-        if (column != null && !column.equals(lastSort)) {
-            Arrays.sort(jobResults, comparator);
-        }
-        lastSort = column;
-        */
-        
-        return null;
+        Arrays.sort(jobResults, comparator);       
     }
+    
+     
+    
+    private void sortFiles() {
+        final String column = getFileSortColumn();
+        Comparator comparator = new Comparator() {
+
+            public int compare(Object o1, Object o2) {
+                FileInfo c1 = (FileInfo) o1;
+                FileInfo c2 = (FileInfo) o2;
+                if (column == null) {
+                    return 0;
+                }
+                else if (column.equals("name")) {
+                    return fileSortAscending ? c1.getName().compareTo(c2.getName()) : c2
+                            .getName().compareTo(c1.getName());
+                }
+                else if (column.equals("size")) {
+                    return fileSortAscending ? new Long(c1.getSize()).compareTo(c2.getSize()) : new Long(c2.getSize()).compareTo(
+                            c1.getSize());
+                }
+                else if (column.equals("lastModified")) {
+                    return fileSortAscending ? c1.getLastModified().compareTo(c2.getLastModified()) : c2.getLastModified().compareTo(
+                            c1.getLastModified());
+                }
+                
+                else {
+                     return 0;
+                }
+            }
+        };
+        
+        for(JobResultInfo jobResult : jobResults) {
+            Collections.sort(jobResult.getOutputFiles(), comparator);
+        }
+        
+    }  
+    
 
     public JobResultInfo[] getJobResults() {
         return jobResults;
@@ -209,7 +249,53 @@ public class JobResultsBean {
     public void setWarnBeforeDeletingJobs(boolean warnBeforeDeletingJobs) {
         this.warnBeforeDeletingJobs = warnBeforeDeletingJobs;
     }
-    
+
+
+    public List getCheckbox() {
+        return checkbox;
+    }
+
+    public void setCheckbox(List checkboxList) {
+        this.checkbox = checkboxList;
+    }
+
+	public String getJobSortColumn() {
+		return jobSortColumn;
+	}
+
+	public void setJobSortColumn(String jobSortField) {
+		this.jobSortColumn = jobSortField;
+	}
+
+	public String getFileSortColumn() {
+		return fileSortColumn;
+	}
+
+	public void setFileSortColumn(String fileSortField) {
+		this.fileSortColumn = fileSortField;
+	}
+
+	public boolean isFileSortAscending() {
+		return fileSortAscending;
+	}
+
+	public void setFileSortAscending(boolean fileSortAscending) {
+		this.fileSortAscending = fileSortAscending;
+	}
+
+	public boolean isJobSortAscending() {
+		return jobSortAscending;
+	}
+
+	public void setJobSortAscending(boolean jobSortAscending) {
+		this.jobSortAscending = jobSortAscending;
+	}
+	
+    /**
+     * Represents a job result.  Wraps JobInfo and adds methods for getting the output files 
+     * and the expansion state of the associated UI panel
+     * 
+     */
     public static class JobResultInfo {
         JobInfo jobInfo;
         List<FileInfo> outputFiles;
@@ -237,6 +323,9 @@ public class JobResultsBean {
         }
     }
     
+    /**
+     * Misc info on a job output file
+     */
     public static class FileInfo {
         String name;
         String absolutePath;
@@ -258,6 +347,10 @@ public class JobResultsBean {
             return name;
         }
         
+        public long getSize() {
+            return size;
+        }
+        
         public String getFormattedSize() {            
             NumberFormat nf = NumberFormat.getInstance();
             return nf.format(Math.max(1, size / 1000)) + "k";
@@ -270,50 +363,7 @@ public class JobResultsBean {
         public String getAbsolutePath() {
         	return absolutePath;
         }
-        
-        
-        
-        
-        
     }
-
-    public List getCheckbox() {
-        return checkbox;
-    }
-
-    public void setCheckbox(List checkboxList) {
-        this.checkbox = checkboxList;
-    }
-
-	public String getJobSortField() {
-		return jobSortField;
-	}
-
-	public void setJobSortField(String jobSortField) {
-		this.jobSortField = jobSortField;
-	}
-
-	public String getFileSortField() {
-		return fileSortField;
-	}
-
-	public void setFileSortField(String fileSortField) {
-		this.fileSortField = fileSortField;
-	}
-
-	public boolean isFileSortAscending() {
-		return fileSortAscending;
-	}
-
-	public void setFileSortAscending(boolean fileSortAscending) {
-		this.fileSortAscending = fileSortAscending;
-	}
-
-	public boolean isJobSortAscending() {
-		return jobSortAscending;
-	}
-
-	public void setJobSortAscending(boolean jobSortAscending) {
-		this.jobSortAscending = jobSortAscending;
-	}
+        
+        
 }
