e213013c8a37d89db1bf69a7ebf2c5d6fc4d0415
jrobinso <jrobinso@broadinstitute.org>
2006-12-08 03:18:37 +0000

    no message
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4393 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/webapp/jsf/TestBean.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.genepattern.server.webapp.jsf;
+
+/**
+ * @author jrobinso
+ *
+ */
+public class TestBean {
+
+    boolean boolValue = true;
+	
+	public boolean isBoolValue() {
+		return boolValue;
+	}
+}
