9491df9cc3f1c6f40fc44b3c20da7473d2cb7121
jgould <jgould@broadinstitute.org>
2006-12-12 19:39:14 +0000

    set encoding for convering string to bytes
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4473 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/EncryptionUtil.java
+++ src/org/genepattern/server/webapp/jsf/EncryptionUtil.java
@@ -12,44 +12,54 @@
 
 package org.genepattern.server.webapp.jsf;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
+import org.apache.log4j.Logger;
+
 public class EncryptionUtil {
-	private EncryptionUtil() {
-	}
+    private static Logger log = Logger.getLogger(EncryptionUtil.class);
 
-	/**
-	 * Encrypts the clear text.  The returned string is exactly 255 bytes, the size of the password field.
-	 * 
-	 * @param clearText
-	 * @return The encrypted text
-	 * @throws NoSuchAlgorithmException
-	 */
-	public static byte[] encrypt(String clearText)
-			throws NoSuchAlgorithmException {
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		md.reset();
-		md.update(clearText.getBytes());
-		
-		byte[] encryptedString = new byte[255]; 
-		Arrays.fill(encryptedString, (byte) 0);
-		byte[] digestedString = md.digest();
-		for(int i=0; i<Math.min(encryptedString.length, digestedString.length); i++) {
-			encryptedString[i] = digestedString[i];
-		}
-		return encryptedString;
-	}
+    private EncryptionUtil() {
+    }
 
-	public static void main(String[] args) throws Exception {
+    /**
+     * Encrypts the clear text. The returned byte array is exactly 255 bytes,
+     * the size of the password field.
+     * 
+     * @param clearText
+     *            the unencrypted text
+     * @return The encrypted text as a byte array
+     * @throws NoSuchAlgorithmException
+     */
+    public static byte[] encrypt(String clearText)
+            throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.reset();
+        try {
+            md.update(clearText.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            log.error(e);
+        }
 
-		String txt = "a";
+        byte[] encryptedString = new byte[255];
+        Arrays.fill(encryptedString, (byte) 0);
+        byte[] digestedString = md.digest();
+        for (int i = 0, length = Math.min(encryptedString.length,
+                digestedString.length); i < length; i++) {
+            encryptedString[i] = digestedString[i];
+        }
+        return encryptedString;
+    }
 
-		byte[] a1 = EncryptionUtil.encrypt(txt);
-		byte[] a2 = EncryptionUtil.encrypt(txt);
-		System.out.println(a1.length);
-		System.out.println(java.util.Arrays.equals(a1, a2));
-	}
+    public static void main(String[] args) throws Exception {
+        String txt = "a";
+        byte[] a1 = EncryptionUtil.encrypt(txt);
+        byte[] a2 = EncryptionUtil.encrypt(txt);
+        System.out.println(a1.length);
+        System.out.println(java.util.Arrays.equals(a1, a2));
+    }
 
 }
