fd35cee09cb851666a7bfb8e8102d0e74a1c9f8b
jgould <jgould@broadinstitute.org>
2006-12-13 22:05:51 +0000

    moved
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4503 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/util/LSIDVersionComparator.java
@@ -0,0 +1,44 @@
+/*
+  The Broad Institute
+  SOFTWARE COPYRIGHT NOTICE AGREEMENT
+  This software and its documentation are copyright (2003-2006) by the
+  Broad Institute/Massachusetts Institute of Technology. All rights are
+  reserved.
+
+  This software is supplied without any warranty or guaranteed support
+  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+  use, misuse, or functionality.
+*/
+
+
+package org.genepattern.util;
+
+import java.util.Comparator;
+
+public class LSIDVersionComparator implements Comparator<String> {
+	public static final LSIDVersionComparator INSTANCE = new LSIDVersionComparator();
+	
+	public int compare(String s0, String s1) {	
+		String[] s0Tokens = s0.split("\\.");
+		String[] s1Tokens = s1.split("\\.");
+		int min = Math.min(s0Tokens.length, s1Tokens.length);
+		
+		for (int i = 0; i < min; i++) {
+			int s0Int = Integer.parseInt(s0Tokens[i]);
+			int s1Int = Integer.parseInt(s1Tokens[i]);
+			if (s0Int < s1Int) {
+				return -1;
+			} else if (s0Int > s1Int) {
+				return 1;
+			}
+		}
+		if (s0Tokens.length > s1Tokens.length) {
+			return 1;
+		} else if (s0Tokens.length < s1Tokens.length) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+}
