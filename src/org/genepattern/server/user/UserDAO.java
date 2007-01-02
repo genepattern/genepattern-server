6df3260c14561e87a718ba7993e77e7d4e00f2c4
jgould <jgould@broadinstitute.org>
2007-01-02 21:16:00 +0000

    save job results setting to DB
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4663 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/user/UserDAO.java
+++ src/org/genepattern/server/user/UserDAO.java
@@ -2,19 +2,14 @@ package org.genepattern.server.user;
 
 // Generated Sep 21, 2006 12:36:06 PM by Hibernate Tools 3.1.0.beta5
 
-import java.util.List;
-import javax.naming.InitialContext;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
 import org.genepattern.server.database.BaseDAO;
 import org.genepattern.server.database.HibernateUtil;
-import org.hibernate.LockMode;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Example;
+import org.hibernate.Query;
 
 /**
  * Home object for domain model class User.
+ * 
  * @see org.genepattern.server.user.User
  * @author Hibernate Tools
  */
@@ -26,13 +21,44 @@ public class UserDAO extends BaseDAO {
         log.debug("getting Props instance with id: " + id);
         try {
             return (User) HibernateUtil.getSession().get("org.genepattern.server.user.User", id);
-        }
-        catch (RuntimeException re) {
+        } catch (RuntimeException re) {
             log.error("get failed", re);
             throw re;
         }
     }
 
- 
+    public void setProperty(String userId, String key, String value) {
+        getProperty(userId, key).setValue(value);
+    }
+
+    public String getPropertyValue(String userId, String key, String defaultValue) {
+        UserProp prop = getProperty(userId, key);
+        if (prop.getValue() == null) {
+            prop.setValue(defaultValue);
+        }
+        return prop.getValue();
+    }
+
+    public UserProp getProperty(String userId, String key) {
+        try {
+            Query query = HibernateUtil.getSession().createQuery(
+                    "from org.genepattern.server.user.UserProp where gpUserId = :gpUserId AND key = :key");
+            query.setString("key", key);
+            query.setString("gpUserId", userId);
+            UserProp prop = (UserProp) query.uniqueResult();
+            if (prop == null) {
+                prop = new UserProp();
+                prop.setKey(key);
+                prop.setGpUserId(userId);
+                User user = findById(userId);
+                user.getProps().add(prop);
+            }
+            return prop;
+        } catch (RuntimeException re) {
+            log.error("find by example failed", re);
+            throw re;
+        }
+
+    }
 
 }
