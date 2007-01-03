640e60efa99284e21a592a676498bc8483f894c3
jgould <jgould@broadinstitute.org>
2007-01-03 18:48:00 +0000

    added findByIdLsid
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4682 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/domain/TaskMasterDAO.java
+++ src/org/genepattern/server/domain/TaskMasterDAO.java
@@ -3,15 +3,12 @@ package org.genepattern.server.domain;
 // Generated Sep 21, 2006 12:36:06 PM by Hibernate Tools 3.1.0.beta5
 
 import java.util.List;
-import javax.naming.InitialContext;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.genepattern.server.database.BaseDAO;
 import org.genepattern.server.database.HibernateUtil;
-import org.hibernate.LockMode;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Example;
-
+import org.hibernate.Query;
 
 /**
  * Home object for domain model class TaskMaster.
@@ -23,25 +20,28 @@ public class TaskMasterDAO extends BaseDAO {
 
     private static final Log log = LogFactory.getLog(TaskMasterDAO.class);
 
-
     public TaskMaster findById(Integer id) {
-        log.debug("getting Props instance with id: " + id);
+        log.debug("getting TaskMaster instance with id: " + id);
         try {
             return (TaskMaster) HibernateUtil.getSession().get("org.genepattern.server.domain.TaskMaster", id);
-        }
-        catch (RuntimeException re) {
+        } catch (RuntimeException re) {
             log.error("get failed", re);
             throw re;
         }
     }
 
+    public TaskMaster findByIdLsid(String lsid) {
+        String hql = "from org.genepattern.server.domain.TaskMaster where lsid = :lsid";
+        Query query = HibernateUtil.getSession().createQuery(hql);
+        query.setString("lsid", lsid);
+        return (TaskMaster) query.uniqueResult();
+    }
 
     public List<TaskMaster> findAll() {
         try {
             return HibernateUtil.getSession().createQuery("from org.genepattern.server.domain.TaskMaster").list();
-            
-        }
-        catch (RuntimeException re) {
+
+        } catch (RuntimeException re) {
             log.error("get failed", re);
             throw re;
         }
