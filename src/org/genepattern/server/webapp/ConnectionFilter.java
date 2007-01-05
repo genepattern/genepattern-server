2bac5c9de16140b7fcca17fe509b2dbc9ae8907a
qgao <qgao@broadinstitute.org>
2007-01-05 19:11:34 +0000

    bug1352: need an updated version of the notallowed page
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4729 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/ConnectionFilter.java
+++ src/org/genepattern/server/webapp/ConnectionFilter.java
@@ -19,36 +19,17 @@
  */
 package org.genepattern.server.webapp;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.Properties;
-import java.util.TreeMap;
-import java.util.StringTokenizer;
-import java.util.Vector;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.Filter;
-import javax.servlet.FilterConfig;
-import javax.servlet.FilterChain;
-import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.net.URLEncoder;
-import java.net.URLDecoder;
-
-import org.genepattern.util.IGPConstants;
 import org.genepattern.server.util.AccessManager;
 
 /**
@@ -80,7 +61,7 @@ public class ConnectionFilter implements Filter {
 			return;
 		} else {
 			if (isJspCall((HttpServletRequest)request)){
-				((HttpServletResponse)response).sendRedirect("notallowed.html");
+				((HttpServletResponse)response).sendRedirect("/pages/notallowed.html");
 			} else {
 				((HttpServletResponse)response).sendError(((HttpServletResponse)response).SC_FORBIDDEN);
 			}
