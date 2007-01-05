5ca98a87312117cf179ef9fd2f97dd67792497f0
jgould <jgould@broadinstitute.org>
2007-01-05 21:57:51 +0000

    get jobs for logged in user only
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4736 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/RecentJobsBean.java
+++ src/org/genepattern/server/webapp/jsf/RecentJobsBean.java
@@ -12,44 +12,13 @@
 
 package org.genepattern.server.webapp.jsf;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import javax.faces.event.ActionEvent;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
-import org.apache.myfaces.custom.navmenu.NavigationMenuItem;
-import org.genepattern.codegenerator.CodeGeneratorUtil;
-import org.genepattern.server.genepattern.GenePatternAnalysisTask;
 import org.genepattern.server.user.UserPropKey;
-import org.genepattern.server.webservice.server.dao.AdminDAO;
 import org.genepattern.server.webservice.server.local.LocalAnalysisClient;
-import org.genepattern.util.SemanticUtil;
-import org.genepattern.webservice.AnalysisJob;
 import org.genepattern.webservice.JobInfo;
-import org.genepattern.webservice.ParameterInfo;
-import org.genepattern.webservice.TaskInfo;
 import org.genepattern.webservice.WebServiceException;
 
 public class RecentJobsBean extends JobBean {
-
-    private List<JobResultsWrapper> jobs;
 
     private static Logger log = Logger.getLogger(RecentJobsBean.class);
 
@@ -59,9 +28,8 @@ public class RecentJobsBean extends JobBean {
         int recentJobsToShow = Integer.parseInt(UserPrefsBean.getProp(UserPropKey.RECENT_JOBS_TO_SHOW, "4").getValue());
         LocalAnalysisClient analysisClient = new LocalAnalysisClient(userId);
         try {
-            return analysisClient.getJobs(null, -1, recentJobsToShow, false);
-        }
-        catch (WebServiceException wse) {
+            return analysisClient.getJobs(userId, -1, recentJobsToShow, false);
+        } catch (WebServiceException wse) {
             log.error(wse);
             return new JobInfo[0];
         }
