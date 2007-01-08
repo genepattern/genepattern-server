685bce352217129b80de993f9b24a7cf1b6a3588
jgould <jgould@broadinstitute.org>
2007-01-08 15:39:26 +0000

    added check for createSuite permission
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4752 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/CreateSuiteBean.java
+++ src/org/genepattern/server/webapp/jsf/CreateSuiteBean.java
@@ -21,6 +21,8 @@ import org.genepattern.server.database.HibernateUtil;
 import org.genepattern.server.domain.Suite;
 import org.genepattern.server.domain.SuiteDAO;
 import org.genepattern.server.genepattern.LSIDManager;
+import org.genepattern.server.util.AuthorizationManagerFactoryImpl;
+import org.genepattern.server.util.IAuthorizationManager;
 import org.genepattern.server.webservice.server.DirectoryManager;
 import org.genepattern.server.webservice.server.TaskIntegrator;
 import org.genepattern.server.webservice.server.dao.AdminDAO;
@@ -38,28 +40,38 @@ import static org.genepattern.server.webapp.jsf.UIBeanHelper.getUserId;
  */
 public class CreateSuiteBean implements java.io.Serializable {
 
-    //private static final long serialVersionUID = 352540582209631173l;
+    // private static final long serialVersionUID = 352540582209631173l;
     private String name;
+
     private String description;
+
     private String author;
+
     private String contact;
+
     private String accessId = "1"; // Public
+
     private UploadedFile supportFile1;
+
     private UploadedFile supportFile2;
+
     private UploadedFile supportFile3;
+
     private List<ModuleCategory> categories;
+
     private boolean success = false; // Default value
+
     private Suite currentSuite = null;
 
     public CreateSuiteBean() {
-    	if (currentSuite==null) {
-			String lsid = UIBeanHelper.getRequest().getParameter("lsid");
-	    	if (lsid!=null) {
-	        	currentSuite = (new SuiteDAO()).findById(lsid);
-	    	}
-		}
+        if (currentSuite == null) {
+            String lsid = UIBeanHelper.getRequest().getParameter("lsid");
+            if (lsid != null) {
+                currentSuite = (new SuiteDAO()).findById(lsid);
+            }
+        }
     }
-    
+
     public boolean isSuccess() {
         return success;
     }
@@ -103,14 +115,14 @@ public class CreateSuiteBean implements java.io.Serializable {
     public List getCategoryColumns() {
 
         List<List> cols = new ArrayList<List>();
-        if(categories == null) {
-        	if (currentSuite!=null) {
-	        	categories = (new ModuleHelper()).getSelectedTasksByType(currentSuite);
-        	}else {
-        		categories = (new ModuleHelper()).getTasksByType();
-        	}
+        if (categories == null) {
+            if (currentSuite != null) {
+                categories = (new ModuleHelper()).getSelectedTasksByType(currentSuite);
+            } else {
+                categories = (new ModuleHelper()).getTasksByType();
+            }
         }
-        
+
         // Find the midpoint in the category list.
         int totalCount = 0;
         for (ModuleCategory cat : categories) {
@@ -124,8 +136,7 @@ public class CreateSuiteBean implements java.io.Serializable {
         for (ModuleCategory cat : categories) {
             if (cumulativeCount < midpoint) {
                 cols.get(0).add(cat);
-            }
-            else {
+            } else {
                 cols.get(1).add(cat);
             }
             cumulativeCount += cat.getModuleCount();
@@ -158,14 +169,17 @@ public class CreateSuiteBean implements java.io.Serializable {
     }
 
     public String save() {
-    	
+        IAuthorizationManager authManager = new AuthorizationManagerFactoryImpl().getAuthorizationManager();
+        if (!authManager.checkPermission("createSuite", UIBeanHelper.getUserId())) {
+            UIBeanHelper.setInfoMessage("You don't have the required permissions to perform the requested operation.");
+        }
         try {
-        	
-            LSID lsidObj = (currentSuite == null)?LSIDManager.getInstance().createNewID(org.genepattern.util.IGPConstants.SUITE_NAMESPACE):
-            	LSIDManager.getInstance().getNextIDVersion(currentSuite.getLsid());          
+
+            LSID lsidObj = (currentSuite == null) ? LSIDManager.getInstance().createNewID(
+                    org.genepattern.util.IGPConstants.SUITE_NAMESPACE) : LSIDManager.getInstance().getNextIDVersion(
+                    currentSuite.getLsid());
             String lsid = lsidObj.toString();
 
-           
             // Save database record
             Suite suite = new Suite();
             suite.setLsid(lsid);
@@ -175,20 +189,19 @@ public class CreateSuiteBean implements java.io.Serializable {
             suite.setAccessId(new Integer(accessId));
             suite.setAuthor(author);
             suite.setContact(contact);
-            
-           
+
             List<String> selectedLSIDs = new ArrayList<String>();
             for (ModuleCategory cat : categories) {
                 for (Module mod : cat.getModules()) {
                     if (mod.isSelected()) {
                         selectedLSIDs.add(mod.getSelectedVersion());
-                     }
+                    }
                 }
             }
-            if(!selectedLSIDs.isEmpty()) {
+            if (!selectedLSIDs.isEmpty()) {
                 suite.setModules(selectedLSIDs);
             }
-              
+
             (new SuiteDAO()).save(suite);
 
             // Save uploaded files, if any
@@ -202,18 +215,17 @@ public class CreateSuiteBean implements java.io.Serializable {
             if (supportFile3 != null) {
                 saveUploadedFile(supportFile3, suiteDir);
             }
-            
+
             RunTaskBean homePageBean = (RunTaskBean) UIBeanHelper.getManagedBean("#{runTaskBean}");
             homePageBean.setSplashMessage("Suite " + suite.getName() + " was successfully created.");
-            
+
             return "home";
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             HibernateUtil.rollbackTransaction(); // This shouldn't be
-                                                    // neccessary, but just in
-                                                    // case
+            // neccessary, but just in
+            // case
             throw new RuntimeException(e); // @todo -- replace with appropriate
-                                            // GP exception
+            // GP exception
         }
 
     }
@@ -246,12 +258,12 @@ public class CreateSuiteBean implements java.io.Serializable {
         this.categories = categories;
     }
 
-	public String getContact() {
-		return (currentSuite == null) ? contact : currentSuite.getContact();
-	}
+    public String getContact() {
+        return (currentSuite == null) ? contact : currentSuite.getContact();
+    }
 
-	public void setContact(String contact) {
-		this.contact = contact;
-	}
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 
 }
