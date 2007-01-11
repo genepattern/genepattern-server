94d471cda1cc93f03fc7d95a471e318f841a275a
qgao <qgao@broadinstitute.org>
2007-01-11 14:46:47 +0000

    *** empty log message ***
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4855 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/domain/SuiteDAO.java
+++ src/org/genepattern/server/domain/SuiteDAO.java
@@ -3,16 +3,11 @@ package org.genepattern.server.domain;
 // Generated Sep 21, 2006 12:36:06 PM by Hibernate Tools 3.1.0.beta5
 
 import java.util.List;
-import javax.naming.InitialContext;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
 import org.apache.log4j.Logger;
 import org.genepattern.server.database.BaseDAO;
 import org.genepattern.server.database.HibernateUtil;
-import org.hibernate.LockMode;
 import org.hibernate.Query;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Example;
 
 /**
  * Home object for domain model class Suite.
