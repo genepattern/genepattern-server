706b7cdfafc5c99a4111f041ddf87efd0f74e6f6
jgould <jgould@broadinstitute.org>
2007-01-12 04:40:06 +0000

    added getFormattedSize
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4875 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/JobHelper.java
+++ src/org/genepattern/server/webapp/jsf/JobHelper.java
@@ -1,29 +1,49 @@
 package org.genepattern.server.webapp.jsf;
 
-import java.util.*;
+import java.io.File;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.genepattern.webservice.JobInfo;
 import org.genepattern.webservice.ParameterInfo;
 
-import java.io.File;
-
 public class JobHelper {
+    private static NumberFormat numberFormat;
 
-	public static List<File> getOutputFiles(JobInfo jobInfo) {
+    static {
+        numberFormat = NumberFormat.getInstance();
+        numberFormat.setMaximumFractionDigits(1);
+    }
 
-		List<File> outputFiles = new ArrayList<File>();
-		ParameterInfo[] parameterInfoArray = jobInfo.getParameterInfoArray();
-		if (parameterInfoArray != null) {
-			String dir = System.getProperty("jobs", "./temp") + "/" + jobInfo.getJobNumber();
-			for (int i = 0; i < parameterInfoArray.length; i++) {
-				if (parameterInfoArray[i].isOutputFile()) {
-					String fn = parameterInfoArray[i].getName();
-					outputFiles.add(new File(dir, fn));
-					// get modules for output file
-				}
-			}
-		}
+    public static List<File> getOutputFiles(JobInfo jobInfo) {
 
-		return outputFiles;
-	}
+        List<File> outputFiles = new ArrayList<File>();
+        ParameterInfo[] parameterInfoArray = jobInfo.getParameterInfoArray();
+        if (parameterInfoArray != null) {
+            String dir = System.getProperty("jobs", "./temp") + "/" + jobInfo.getJobNumber();
+            for (int i = 0; i < parameterInfoArray.length; i++) {
+                if (parameterInfoArray[i].isOutputFile()) {
+                    String fn = parameterInfoArray[i].getName();
+                    outputFiles.add(new File(dir, fn));
+                    // get modules for output file
+                }
+            }
+        }
+
+        return outputFiles;
+    }
+
+    public static String getFormattedSize(long size) {
+        if (size >= 1073741824) {
+            double gigabytes = size / 1073741824.0;
+            return numberFormat.format(gigabytes) + " GB";
+        } else if (size >= 1048576) {
+            double megabytes = size / 1048576.0;
+            return numberFormat.format(megabytes) + " MB";
+        } else {
+            return Math.max(1, Math.ceil(size / 1024.0)) + " KB";
+        }
+    }
 
 }
