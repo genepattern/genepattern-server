c244ee9e9278e827e85c5a18df78ad69d7c571b5
jgould <jgould@broadinstitute.org>
2007-01-13 01:26:58 +0000

    added method getUpdateShowParameterDescriptions
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4896 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/RunTaskBean.java
+++ src/org/genepattern/server/webapp/jsf/RunTaskBean.java
@@ -14,7 +14,6 @@ package org.genepattern.server.webapp.jsf;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,13 +22,14 @@ import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 
 import org.apache.log4j.Logger;
-import org.apache.myfaces.custom.navmenu.NavigationMenuItem;
-import org.apache.myfaces.custom.navmenu.jscookmenu.HtmlCommandJSCookMenu;
+import org.genepattern.server.genepattern.GenePatternAnalysisTask;
+import org.genepattern.server.user.UserDAO;
 import org.genepattern.server.webservice.server.local.LocalAdminClient;
 import org.genepattern.server.webservice.server.local.LocalAnalysisClient;
 import org.genepattern.server.webservice.server.local.LocalTaskIntegratorClient;
 import org.genepattern.util.GPConstants;
 import org.genepattern.util.LSID;
+import org.genepattern.util.SemanticUtil;
 import org.genepattern.webservice.JobInfo;
 import org.genepattern.webservice.ParameterInfo;
 import org.genepattern.webservice.TaskInfo;
@@ -61,6 +61,8 @@ public class RunTaskBean {
     // for the home page.
     private String splashMessage;
 
+    private boolean showParameterDescriptions;
+
     /**
      * Initialize the task lsid. This page needs to support redirects from older
      * .jsp pages as well as jsf navigation. JSP pages will pass the lsid in as
@@ -71,12 +73,32 @@ public class RunTaskBean {
     public RunTaskBean() {
         String taskToRun = UIBeanHelper.getRequest().getParameter("lsid");
         if (taskToRun == null || taskToRun.length() == 0) {
-            ModuleChooserBean chooser = (ModuleChooserBean) UIBeanHelper
-                    .getManagedBean("#{moduleChooserBean}");
+            ModuleChooserBean chooser = (ModuleChooserBean) UIBeanHelper.getManagedBean("#{moduleChooserBean}");
             assert chooser != null;
             taskToRun = chooser.getSelectedModule();
         }
         setTask(taskToRun);
+        this.showParameterDescriptions = Boolean.parseBoolean(new UserDAO().getPropertyValue(UIBeanHelper.getUserId(),
+                "show.parameter.descriptions", "true"));
+    }
+
+    public boolean isShowParameterDescriptions() {
+        return showParameterDescriptions;
+    }
+
+    /**
+     * Update the show parameter descriptions property using AJAX
+     * 
+     * @return
+     */
+    public String getUpdateShowParameterDescriptions() {
+        setShowParameterDescriptions(Boolean.parseBoolean(UIBeanHelper.getRequest().getParameter("value")));
+        return "";
+    }
+
+    public void setShowParameterDescriptions(boolean b) {
+        showParameterDescriptions = b;
+        new UserDAO().setProperty(UIBeanHelper.getUserId(), "show.parameter.descriptions", String.valueOf(b));
     }
 
     public String getFormAction() {
@@ -96,7 +118,7 @@ public class RunTaskBean {
     public String getLsid() {
         return lsid;
     }
-    
+
     public String getLsidNoVersion() {
         try {
             return new LSID(lsid).toStringNoVersion();
@@ -129,8 +151,7 @@ public class RunTaskBean {
     public static class DefaultValueSelectItem extends SelectItem {
         private boolean defaultOption;
 
-        public DefaultValueSelectItem(String value, String label,
-                boolean defaultOption) {
+        public DefaultValueSelectItem(String value, String label, boolean defaultOption) {
             super(value, label);
             this.defaultOption = defaultOption;
         }
@@ -158,20 +179,18 @@ public class RunTaskBean {
 
         private Parameter(ParameterInfo pi, String passedDefaultValue) {
             HashMap pia = pi.getAttributes();
-            this.optional = ((String) pia
-                    .get(GPConstants.PARAM_INFO_OPTIONAL[GPConstants.PARAM_INFO_NAME_OFFSET]))
+            this.optional = ((String) pia.get(GPConstants.PARAM_INFO_OPTIONAL[GPConstants.PARAM_INFO_NAME_OFFSET]))
                     .length() > 0;
 
-            this.defaultValue = passedDefaultValue != null ? passedDefaultValue
-                    : (String) pia.get(GPConstants.PARAM_INFO_DEFAULT_VALUE[0]);
+            this.defaultValue = passedDefaultValue != null ? passedDefaultValue : (String) pia
+                    .get(GPConstants.PARAM_INFO_DEFAULT_VALUE[0]);
 
             if (defaultValue == null) {
                 defaultValue = "";
             }
             defaultValue = defaultValue.trim();
 
-            String[] choicesArray = pi
-                    .hasChoices(GPConstants.PARAM_INFO_CHOICE_DELIMITER) ? pi
+            String[] choicesArray = pi.hasChoices(GPConstants.PARAM_INFO_CHOICE_DELIMITER) ? pi
                     .getChoices(GPConstants.PARAM_INFO_CHOICE_DELIMITER) : null;
             if (choicesArray != null) {
                 choices = new DefaultValueSelectItem[choicesArray.length];
@@ -179,8 +198,7 @@ public class RunTaskBean {
                     String choice = choicesArray[i];
                     String display, option;
 
-                    int equalsCharIndex = choice
-                            .indexOf(GPConstants.PARAM_INFO_TYPE_SEPARATOR);
+                    int equalsCharIndex = choice.indexOf(GPConstants.PARAM_INFO_TYPE_SEPARATOR);
                     if (equalsCharIndex == -1) {
                         display = choice;
                         option = choice;
@@ -190,10 +208,8 @@ public class RunTaskBean {
                     }
                     display = display.trim();
                     option = option.trim();
-                    boolean defaultOption = defaultValue.equals(display)
-                            || defaultValue.equals(option);
-                    choices[i] = new DefaultValueSelectItem(option, display,
-                            defaultOption);
+                    boolean defaultOption = defaultValue.equals(display) || defaultValue.equals(option);
+                    choices[i] = new DefaultValueSelectItem(option, display, defaultOption);
                 }
             }
             if (pi.isPassword()) {
@@ -263,8 +279,7 @@ public class RunTaskBean {
     public void setTask(String taskNameOrLsid) {
         TaskInfo taskInfo = null;
         try {
-            taskInfo = new LocalAdminClient(UIBeanHelper.getUserId())
-                    .getTask(taskNameOrLsid);
+            taskInfo = new LocalAdminClient(UIBeanHelper.getUserId()).getTask(taskNameOrLsid);
         } catch (WebServiceException e) {
             log.error(e);
         }
@@ -272,30 +287,61 @@ public class RunTaskBean {
             lsid = null;
             return;
         }
+        ParameterInfo[] pi = taskInfo.getParameterInfoArray();
+
+        String matchJob = (String) UIBeanHelper.getRequest().getAttribute("matchJob");
+        Map outputFilesToKind = new HashMap();
+        if (matchJob != null) {
+            if (pi != null) {
+                for (ParameterInfo p : pi) {
+                    if (p.isInputFile()) {
+
+                    }
+                }
+            }
+            try {
+                int reloadJobNumber = Integer.parseInt(matchJob);
+                LocalAnalysisClient ac = new LocalAnalysisClient(UIBeanHelper.getUserId());
+                JobInfo matchJobInfo = ac.getJob(reloadJobNumber);
+                // can only reload own jobs
+                File outputDir = new File(GenePatternAnalysisTask.getJobDir("" + matchJobInfo.getJobNumber()));
+                if (UIBeanHelper.getUserId().equals(matchJobInfo.getUserId())) {
+                    ParameterInfo[] params = matchJobInfo.getParameterInfoArray();
+                    if (params != null) {
+                        for (ParameterInfo p : params) {
+                            if (p.isOutputFile()) {
+                                File file = new File(outputDir, p.getName());
+                                String kind = SemanticUtil.getKind(file);
+                                // SemanticUtil.isCorrectKind(inputTypes, kind);
+                                // outputFilesToKind.put(key, value);
+                            }
+
+                        }
+                    }
+                }
+            } catch (NumberFormatException nfe) {
+                log.error(nfe);
+            } catch (WebServiceException e) {
+                log.error(e);
+            }
+        }
 
         Map<String, String> reloadValues = new HashMap<String, String>();
-        String reloadJobNumberString = UIBeanHelper.getRequest().getParameter(
-                "reloadJob");
+        String reloadJobNumberString = UIBeanHelper.getRequest().getParameter("reloadJob");
         if (reloadJobNumberString == null) {
-            reloadJobNumberString = (String) UIBeanHelper.getRequest()
-                    .getAttribute("reloadJob");
+            reloadJobNumberString = (String) UIBeanHelper.getRequest().getAttribute("reloadJob");
         }
         if (reloadJobNumberString != null) {
             try {
                 int reloadJobNumber = Integer.parseInt(reloadJobNumberString);
-                LocalAnalysisClient ac = new LocalAnalysisClient(UIBeanHelper
-                        .getUserId());
+                LocalAnalysisClient ac = new LocalAnalysisClient(UIBeanHelper.getUserId());
                 JobInfo reloadJob = ac.getJob(reloadJobNumber);
                 // can only reload own jobs
                 if (UIBeanHelper.getUserId().equals(reloadJob.getUserId())) {
-                    ParameterInfo[] reloadParams = reloadJob
-                            .getParameterInfoArray();
+                    ParameterInfo[] reloadParams = reloadJob.getParameterInfoArray();
                     if (reloadParams != null) {
                         for (int i = 0; i < reloadParams.length; i++) {
                             String value = reloadParams[i].getValue();
-                            // if(reloadParams[i].isInputFile()) {
-                            // value = reloadParams[i].getAttributes().get();
-                            // }
                             reloadValues.put(reloadParams[i].getName(), value);
                         }
                     }
@@ -306,15 +352,16 @@ public class RunTaskBean {
                 log.error(e);
             }
         }
-        ParameterInfo[] pi = taskInfo.getParameterInfoArray();
+
         this.parameters = new Parameter[pi != null ? pi.length : 0];
         if (pi != null) {
             for (int i = 0; i < pi.length; i++) {
                 String defaultValue = reloadValues.get(pi[i].getName());
                 if (defaultValue == null) {
-                    defaultValue = UIBeanHelper.getRequest().getParameter(
-                            pi[i].getName());
+                    defaultValue = UIBeanHelper.getRequest().getParameter(pi[i].getName());
                 }
+                // if defaultValue is null default value will be set in
+                // Parameter constructor
                 parameters[i] = new Parameter(pi[i], defaultValue);
             }
         }
@@ -329,8 +376,7 @@ public class RunTaskBean {
         try {
             LSID l = new LSID(lsid);
             this.version = l.getVersion();
-            versions = new LocalAdminClient(UIBeanHelper.getUserId())
-                    .getVersions(l);
+            versions = new LocalAdminClient(UIBeanHelper.getUserId()).getVersions(l);
             versions.remove(version);
 
         } catch (MalformedURLException e) {
@@ -340,14 +386,12 @@ public class RunTaskBean {
         }
         File[] docFiles = null;
         try {
-            LocalTaskIntegratorClient taskIntegratorClient = new LocalTaskIntegratorClient(
-                    UIBeanHelper.getUserId());
+            LocalTaskIntegratorClient taskIntegratorClient = new LocalTaskIntegratorClient(UIBeanHelper.getUserId());
             docFiles = taskIntegratorClient.getDocFiles(taskInfo);
         } catch (WebServiceException e) {
             log.error(e);
         }
-        this.documentationFilenames = new String[docFiles != null ? docFiles.length
-                : 0];
+        this.documentationFilenames = new String[docFiles != null ? docFiles.length : 0];
         if (docFiles != null) {
             for (int i = 0; i < docFiles.length; i++) {
                 documentationFilenames[i] = docFiles[i].getName();
@@ -358,15 +402,12 @@ public class RunTaskBean {
 
     public void changeVersion(ActionEvent event) {
         System.out.println("changeVersion");
-        ModuleChooserBean chooser = (ModuleChooserBean) UIBeanHelper
-                .getManagedBean("#{moduleChooserBean}");
+        ModuleChooserBean chooser = (ModuleChooserBean) UIBeanHelper.getManagedBean("#{moduleChooserBean}");
         assert chooser != null;
-        String version = UIBeanHelper.decode(UIBeanHelper.getRequest()
-                .getParameter("version"));
+        String version = UIBeanHelper.decode(UIBeanHelper.getRequest().getParameter("version"));
 
         try {
-            chooser.setSelectedModule(new LSID(lsid).toStringNoVersion() + ":"
-                    + version);
+            chooser.setSelectedModule(new LSID(lsid).toStringNoVersion() + ":" + version);
             setTask(chooser.getSelectedModule());
         } catch (MalformedURLException e) {
             log.error(e);
