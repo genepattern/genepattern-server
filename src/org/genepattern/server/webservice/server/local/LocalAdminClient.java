74194e6ace63daeb6f082e71bcefa27ff0d227c5
jgould <jgould@broadinstitute.org>
2007-01-17 23:13:24 +0000

    updated to use generics
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4948 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webservice/server/local/LocalAdminClient.java
+++ src/org/genepattern/server/webservice/server/local/LocalAdminClient.java
@@ -52,10 +52,10 @@ public class LocalAdminClient {
         return new AdminDAO().getVersions(lsid, userName);
     }
 
-    public TreeMap getTaskCatalogByLSID(Collection tasks) throws WebServiceException {
-        TreeMap tmCatalog = new TreeMap(String.CASE_INSENSITIVE_ORDER);
-        for (Iterator it = tasks.iterator(); it.hasNext();) {
-            TaskInfo ti = (TaskInfo) it.next();
+    public TreeMap<String, TaskInfo> getTaskCatalogByLSID(Collection<TaskInfo> tasks) {
+        TreeMap<String, TaskInfo> tmCatalog = new TreeMap<String, TaskInfo>(String.CASE_INSENSITIVE_ORDER);
+        for (Iterator<TaskInfo> it = tasks.iterator(); it.hasNext();) {
+            TaskInfo ti = it.next();
             String lsid = ti.giveTaskInfoAttributes().get(GPConstants.LSID);
             if (lsid != null && lsid.length() > 0) {
                 tmCatalog.put(lsid, ti);
@@ -65,19 +65,19 @@ public class LocalAdminClient {
         return tmCatalog;
     }
 
-    public Map getLSIDToVersionsMap() throws WebServiceException {
+    public Map<String, List<String>> getLSIDToVersionsMap() throws WebServiceException {
         return service.getLSIDToVersionsMap();
     }
 
-    public TreeMap getTaskCatalogByLSID() throws WebServiceException {
+    public TreeMap<String, TaskInfo> getTaskCatalogByLSID() throws WebServiceException {
         return getTaskCatalogByLSID(Arrays.asList(service.getAllTasks()));
     }
 
-    public Collection getTaskCatalog() throws WebServiceException {
+    public Collection<TaskInfo> getTaskCatalog() throws WebServiceException {
         return Arrays.asList(service.getAllTasks());
     }
 
-    public Collection getLatestTasks() throws WebServiceException {
+    public Collection<TaskInfo> getLatestTasks() throws WebServiceException {
         return Arrays.asList(service.getLatestTasks());
     }
 
@@ -85,27 +85,26 @@ public class LocalAdminClient {
      * return a map keyed by taskType with values being alphabetically sorted
      * list of the tasks that are part of that type
      */
-    public Map getLatestTasksByType() throws WebServiceException {
-        Collection latest = getLatestTasks();
-        TreeMap typeToTaskMap = new TreeMap(new Comparator() {
-            public int compare(Object o1, Object o2) {
-                String s1 = (String) o1;
-                String s2 = (String) o2;
-                return ((s1.toLowerCase()).compareTo(s2.toLowerCase()));
-            }
-        });
+    public Map<String, Collection<TaskInfo>> getLatestTasksByType() throws WebServiceException {
+        Collection<TaskInfo> latest = getLatestTasks();
+        TreeMap<String, Collection<TaskInfo>> typeToTaskMap = new TreeMap<String, Collection<TaskInfo>>(
+                new Comparator<String>() {
+                    public int compare(String s1, String s2) {
+                        return ((s1.toLowerCase()).compareTo(s2.toLowerCase()));
+                    }
+                });
 
-        for (Iterator iter = latest.iterator(); iter.hasNext();) {
-            TaskInfo task = (TaskInfo) iter.next();
+        for (Iterator<TaskInfo> iter = latest.iterator(); iter.hasNext();) {
+            TaskInfo task = iter.next();
             String type = (String) task.getTaskInfoAttributes().get(GPConstants.TASK_TYPE);
 
-            if (type == null) type = "Unclassified";
-            TreeSet typeList = (TreeSet) typeToTaskMap.get(type);
+            if (type == null) {
+                type = "Unclassified";
+            }
+            Collection<TaskInfo> typeList = typeToTaskMap.get(type);
             if (typeList == null) {
-                typeList = new TreeSet(new Comparator() {
-                    public int compare(Object o1, Object o2) {
-                        TaskInfo t1 = (TaskInfo) o1;
-                        TaskInfo t2 = (TaskInfo) o2;
+                typeList = new TreeSet<TaskInfo>(new Comparator<TaskInfo>() {
+                    public int compare(TaskInfo t1, TaskInfo t2) {
                         return (((String) t1.getName().toLowerCase()).compareTo(t2.getName().toLowerCase()));
                     }
                 });
@@ -140,14 +139,9 @@ public class LocalAdminClient {
         if (key == null) {
             return null;
         }
-        User user = (new UserDAO()).findById(userName);
-        List<UserProp> props = user != null ? user.getProps() : Collections.EMPTY_LIST;
-        for (UserProp p : props) {
-            if (key.equals(p.getKey())) {
-                return p.getValue();
-            }
-        }
-        return null;
+        UserProp prop = new UserDAO().getProperty(userName, key);
+        return prop != null ? prop.getValue() : null;
+
     }
 
 }
