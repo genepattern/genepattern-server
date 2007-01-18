2990250fd000d0bbab3bdc8d1f891cc9dcfe61c9
jgould <jgould@broadinstitute.org>
2007-01-18 16:16:08 +0000

    updated to use generics
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4960 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/genepattern/TaskInstallationException.java
+++ src/org/genepattern/server/genepattern/TaskInstallationException.java
@@ -1,42 +1,53 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.server.genepattern;
 
 import java.util.Vector;
 
 public class TaskInstallationException extends Exception {
-	Vector errors;
+    private Vector<String> errors;
 
-	public TaskInstallationException(Vector errors) {
-		this.errors = errors;
-	}
+    /**
+     * Creates a new <tt>TaskInstallationException</tt> instance
+     * 
+     * @param errors
+     *            a vector of error messages
+     */
+    public TaskInstallationException(Vector<String> errors) {
+        this.errors = errors;
+    }
 
-	public Vector getErrors() {
-		return errors;
-	}
+    /**
+     * Returns the vector of error messages.
+     * 
+     * @return the error messages.
+     */
+    public Vector<String> getErrors() {
+        return errors;
+    }
 
-	public String getMessage() {
-		StringBuffer buf = new StringBuffer();
-		buf.append("Errors:");
-		for (int i = 0, size = errors.size(); i < size; i++) {
-			if (i > 0) {
-				buf.append(" ");
-			}
-			buf.append(String.valueOf(i + 1));
-			buf.append(".");
-			buf.append(errors.get(i));
-		}
-		return buf.toString();
-	}
+    @Override
+    public String getMessage() {
+        StringBuffer buf = new StringBuffer();
+        buf.append("Errors: ");
+        for (int i = 0, size = errors.size(); i < size; i++) {
+            if (i > 0) {
+                buf.append(" ");
+            }
+            buf.append(String.valueOf(i + 1));
+            buf.append(".");
+            buf.append(errors.get(i));
+        }
+        return buf.toString();
+    }
 }
