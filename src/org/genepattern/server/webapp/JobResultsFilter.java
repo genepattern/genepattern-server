1fbe58d61b82f1d0368d360d3f70a1003372e1b4
jgould <jgould@broadinstitute.org>
2007-01-18 13:47:18 +0000

    return all files directly
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@4954 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/JobResultsFilter.java
+++ src/org/genepattern/server/webapp/JobResultsFilter.java
@@ -1,35 +1,25 @@
-/*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+/*******************************************************************************
+ * The Broad Institute
+ * SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ * This software and its documentation are copyright (2003-2006) by the
+ * Broad Institute/Massachusetts Institute of Technology. All rights are
+ * reserved.
+ *  
+ * This software is supplied without any warranty or guaranteed support
+ * whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ * use, misuse, or functionality.
+ *  
+ *******************************************************************************/
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
-
-/*
- * Created on Feb 16, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.genepattern.server.webapp;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.util.Properties;
 import java.util.StringTokenizer;
 
 import javax.servlet.Filter;
@@ -41,7 +31,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.genepattern.server.util.AccessManager;
+import org.apache.log4j.Logger;
 import org.genepattern.server.util.AuthorizationManagerFactoryImpl;
 import org.genepattern.server.util.IAuthorizationManager;
 import org.genepattern.server.webservice.server.dao.AnalysisDAO;
@@ -49,150 +39,155 @@ import org.genepattern.util.GPConstants;
 import org.genepattern.webservice.JobInfo;
 
 /**
- * @author Liefeld
  * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
+ * Security filter on job results pages which are now accessible via normal http
+ * without using the retrieveResults.jsp page
+ * 
+ * In 3.0, you are allowed access to the directory of your own jobs only but not
+ * the parent dir of your jobs or anyone elses jobs
+ * 
+ * This is determined by looking at the URL, taking the job # as whatever
+ * follows the "jobResults" part in the url
+ * 
+ * 
+ * reqURL: http://gp21e-789.broad.mit.edu:8080/gp/jobResults/92/foo.out
+ * contextPath: /gp reqURI: /gp/jobResults/92/foo.out ServletPath:
+ * /jobResults/92/foo.out
+ * 
+ * and in genepattern.properties we typically have jobs=./webapps/gp/jobResults
+ * which defines the directory the files are in
+ * 
+ * In this first implementation, we'll assume the dir name 'jobResults' cannot
+ * be changed in any GP install
+ * 
+ * @author Ted Liefeld
+ * @author Joshua Gould
+ * 
  */
 public class JobResultsFilter implements Filter {
-	private FilterConfig filterConfig = null;
-   
-	public void init(FilterConfig filterConfig) throws ServletException {
-      	this.filterConfig = filterConfig;
-   	}
 
-   	public void destroy() {
-      	this.filterConfig = null;
-   	}
+    private String jobsDirectory;
 
-   	/**
-   	 * security filter on job results pages which are now accessible via normal
-   	 * http without using the retrieveResults.jsp page
-   	 * 
-   	 * In 3.0, you are allowed access to the directory of your own jobs only
-   	 * but not the parent dir of your jobs or anyone elses jobs
-   	 * 
-   	 * This is determined by looking at the URL, taking the job # as whatever follows
-   	 * the "jobResults" part in the url
-   	 * 
-   	 * 
-	 *	reqURL: http://gp21e-789.broad.mit.edu:8080/gp/jobResults/92/foo.out
-   	 *  contextPath: 	/gp
-	 *	reqURI: 		/gp/jobResults/92/foo.out
-	 *	ServletPath: 	/jobResults/92/foo.out
-	 *
-	 * and in genepattern.properties we typically have
-	 *  	jobs=./webapps/gp/jobResults
-	 * which defines the directory the files are in
-	 * 
-	 *  In this first implementation, we'll assume the dir name 'jobResults'
-	 *  cannot be changed in any GP install
-   	 */
+    private static Logger log = Logger.getLogger(JobResultsFilter.class);
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		boolean allowed = true;
-		
-		IAuthorizationManager authManager  = (new AuthorizationManagerFactoryImpl()).getAuthorizationManager();
-		
-		String userid = (String)request.getAttribute(GPConstants.USERID);
-		String jobDirName = System.getProperty("jobs"); // from genepattern.properties
-		boolean isAdmin = authManager.checkPermission("administrateServer", userid);
-		
-			
-		// since this is a dir name with a path, we want to get the path in the application context
-		// ie "gp" so...
-		// given http://aserver:aport/gp/jobResults/92/foo.txt
-		// we want to get the "92" as the job #
-		
-		HttpServletRequest hsr = (HttpServletRequest)request;
-		String servletPath = hsr.getServletPath();
-		int idx = servletPath.indexOf("jobResults");
-		if (idx == -1)((HttpServletResponse)response).sendError(((HttpServletResponse)response).SC_FORBIDDEN);
-		
-		String resultsPath = servletPath.substring(idx + 1 + ("jobResults".length()));
-		
-		
-		StringTokenizer strtok = new StringTokenizer(resultsPath,"/");
-		String job = null;
-		String file = null;
-		if (strtok.hasMoreTokens())
-			job = strtok.nextToken();
-		if (strtok.hasMoreTokens())
-			file = strtok.nextToken();
+    private void loadProperties(Properties props, File propFile) {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(propFile);
+            props.load(fis);
 
-		if (isAdmin){
-			allowed = true;
-		} else if (job == null){
-			// should admin be allowed here? or anyone if no passwords?
-			allowed = false;
-		} else if (isJobOwner(userid, job)){
-			allowed=true;
-		}
-		
-		if (allowed){
-			// if it is a jsp, jsf or xhtml extension, we return the contents directly and
-			// don't pass it on as it could be interpreted into something malicious
-			if (resultsPath.endsWith(".jsp") || resultsPath.endsWith(".jsf") || resultsPath.endsWith(".xhtml")){
-				returnFile(resultsPath, (HttpServletResponse)response);
-				
-			} else {
-				chain.doFilter(request, response);
-			}
-			return;
-		} else {
-			((HttpServletResponse)response).sendError(((HttpServletResponse)response).SC_FORBIDDEN);
-		}
-   }
+        } catch (IOException e) {
+            log.error(e);
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
 
-	  private void returnFile(String resultsPath, HttpServletResponse response) {
-		  try {
-			  // TODO Auto-generated method stub
-			  File f = new File(resultsPath);
-		
-			  response.setHeader("Content-disposition", "inline; filename=\""
-		                + f.getName() + "\"");
-		    
-		    
-		    response.setHeader("Cache-Control", "no-store"); // HTTP 1.1 cache control
-		    response.setHeader("Pragma", "no-cache");         // HTTP 1.0 cache control
-		    response.setDateHeader("Expires", 0);
-		    response.setDateHeader("X-lastModified", f.lastModified());
+                }
+            }
+        }
 
-		    OutputStream os = response.getOutputStream();
-		    Writer out  = new BufferedWriter(new OutputStreamWriter(os));
-		    BufferedReader is = null;
-		   try {
-		        is = new BufferedReader(new FileReader(f));
-		        char[] b = new char[10000];
-		        int bytesRead;
-		        while ((bytesRead = is.read(b)) != -1) {
-		            out.write(b);         
-		        }
-		    } finally {
-		        if (is != null) {
-		            is.close();
-		        }
-		    }
+    }
 
-			  
-		  } catch (Exception e){
-			  e.printStackTrace();
-		  }
-	}
+    public void init(FilterConfig filterconfig) throws ServletException {
+        String dir = filterconfig.getInitParameter("genepattern.properties");
+        File propFile = new File(dir, "genepattern.properties");
+        File customPropFile = new File(dir, "custom.properties");
+        Properties props = new Properties();
 
-	private boolean isJobOwner(String user, String jobId){
-		  try {
-			  int jobID = Integer.parseInt(jobId);
-	
-			  AnalysisDAO ds = new AnalysisDAO();
-			  JobInfo jobInfo = ds.getJobInfo(jobID);
-			  return user.equals(jobInfo.getUserId());
-		  } catch (NumberFormatException nfe) {
-			  return false;
-		  }
-		 
-	  }
-	    
+        if (propFile.exists()) {
+            loadProperties(props, propFile);
+        }
 
+        if (customPropFile.exists()) {
+            loadProperties(props, customPropFile);
+        }
+        jobsDirectory = props.getProperty("jobs");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        boolean allowed = true;
+        IAuthorizationManager authManager = (new AuthorizationManagerFactoryImpl()).getAuthorizationManager();
+        String userid = (String) request.getAttribute(GPConstants.USERID);
+        boolean isAdmin = authManager.checkPermission("administrateServer", userid);
+
+        // since this is a dir name with a path, we want to get the path in the
+        // application context
+        // ie "gp" so...
+        // given http://aserver:aport/gp/jobResults/92/foo.txt
+        // we want to get the "92" as the job #
+
+        HttpServletRequest hsr = (HttpServletRequest) request;
+        String servletPath = hsr.getServletPath();
+        int idx = servletPath.indexOf("jobResults");
+        if (idx == -1) {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
+        String resultsPath = servletPath.substring(idx + 1 + ("jobResults".length()));
+
+        StringTokenizer strtok = new StringTokenizer(resultsPath, "/");
+        String job = null;
+        String file = null;
+        if (strtok.hasMoreTokens()) {
+            job = strtok.nextToken();
+        }
+        if (strtok.hasMoreTokens()) {
+            file = strtok.nextToken();
+        }
+
+        if (isAdmin) {
+            allowed = true;
+        } else if (job == null || file == null) {
+            // if file is null, request is for a directory-prohibit directory
+            // listings
+            // should admin be allowed here?
+            allowed = false;
+        } else if (isJobOwner(userid, job)) {
+            allowed = true;
+        }
+
+        if (allowed) {
+            BufferedInputStream is = null;
+            try {
+                OutputStream os = response.getOutputStream();
+                is = new BufferedInputStream(new FileInputStream(jobsDirectory + File.separator + job + File.separator
+                        + file));
+                byte[] b = new byte[10000];
+                int bytesRead;
+                while ((bytesRead = is.read(b)) != -1) {
+                    os.write(b, 0, bytesRead);
+                }
+            } catch (FileNotFoundException e) {
+                ((HttpServletResponse) response).sendError(HttpServletResponse.SC_NOT_FOUND);
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (IOException x) {
+                    }
+                }
+            }
+        } else {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+
+    private boolean isJobOwner(String user, String jobId) {
+        try {
+            int jobID = Integer.parseInt(jobId);
+            AnalysisDAO ds = new AnalysisDAO();
+            JobInfo jobInfo = ds.getJobInfo(jobID);
+            return user.equals(jobInfo.getUserId());
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
+    }
+
+    public void destroy() {
+
+    }
 
 }
