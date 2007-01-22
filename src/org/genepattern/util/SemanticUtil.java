e5143948d09b6f3308aa16307bcde6910ee3397f
jgould <jgould@broadinstitute.org>
2007-01-22 20:09:44 +0000

    Added method getFileFormats
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5033 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/util/SemanticUtil.java
+++ src/org/genepattern/util/SemanticUtil.java
@@ -18,12 +18,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+
 import org.genepattern.io.IOdfHandler;
 import org.genepattern.io.OdfParser;
 import org.genepattern.io.ParseException;
@@ -113,16 +115,14 @@ public class SemanticUtil {
      * services that take that input type as an input parameter
      * 
      */
-    public static Map<String, List<AnalysisService>> getKindToModulesMap(
-            Collection<AnalysisService> analysisServices) {
+    public static Map<String, List<AnalysisService>> getKindToModulesMap(Collection<AnalysisService> analysisServices) {
         Iterator<AnalysisService> temp = analysisServices.iterator();
         String server = null;
         if (temp.hasNext()) {
             server = temp.next().getServer();
         }
         Map<String, Collection<TaskInfo>> map = new HashMap<String, Collection<TaskInfo>>();
-        for (Iterator<AnalysisService> it = analysisServices.iterator(); it
-                .hasNext();) {
+        for (Iterator<AnalysisService> it = analysisServices.iterator(); it.hasNext();) {
             AnalysisService svc = it.next();
             addToInputTypeToModulesMap(map, svc.getTaskInfo());
         }
@@ -132,8 +132,7 @@ public class SemanticUtil {
             Collection<TaskInfo> tasks = map.get(kind);
             if (tasks != null) {
                 List<AnalysisService> modules = new ArrayList<AnalysisService>();
-                for (Iterator<TaskInfo> taskIt = tasks.iterator(); taskIt
-                        .hasNext();) {
+                for (Iterator<TaskInfo> taskIt = tasks.iterator(); taskIt.hasNext();) {
                     modules.add(new AnalysisService(server, taskIt.next()));
                 }
                 kindToServices.put(kind, modules);
@@ -142,8 +141,7 @@ public class SemanticUtil {
         return kindToServices;
     }
 
-    public static Map<String, Collection<TaskInfo>> getKindToModulesMap(
-            TaskInfo[] tasks) {
+    public static Map<String, Collection<TaskInfo>> getKindToModulesMap(TaskInfo[] tasks) {
         Map<String, Collection<TaskInfo>> map = new HashMap<String, Collection<TaskInfo>>();
         for (TaskInfo task : tasks) {
             addToInputTypeToModulesMap(map, task);
@@ -151,21 +149,32 @@ public class SemanticUtil {
         return map;
     }
 
-    private static void addToInputTypeToModulesMap(
-            Map<String, Collection<TaskInfo>> map, TaskInfo taskInfo) {
+    public static List<String> getFileFormats(ParameterInfo p) {
+        String fileFormatsString = (String) p.getAttributes().get(GPConstants.FILE_FORMAT);
+        if (fileFormatsString == null || fileFormatsString.equals("")) {
+            return Collections.emptyList();
+        }
+        List<String> fileFormats = new ArrayList<String>();
+        StringTokenizer st = new StringTokenizer(fileFormatsString, GPConstants.PARAM_INFO_CHOICE_DELIMITER);
+        while (st.hasMoreTokens()) {
+            String type = st.nextToken();
+            fileFormats.add(type);
+
+        }
+        return fileFormats;
+    }
+
+    private static void addToInputTypeToModulesMap(Map<String, Collection<TaskInfo>> map, TaskInfo taskInfo) {
         ParameterInfo[] p = taskInfo.getParameterInfoArray();
         if (p != null) {
             for (int i = 0; i < p.length; i++) {
                 if (p[i].isInputFile()) {
                     ParameterInfo info = p[i];
-                    String fileFormatsString = (String) info.getAttributes()
-                            .get(GPConstants.FILE_FORMAT);
-                    if (fileFormatsString == null
-                            || fileFormatsString.equals("")) {
+                    String fileFormatsString = (String) info.getAttributes().get(GPConstants.FILE_FORMAT);
+                    if (fileFormatsString == null || fileFormatsString.equals("")) {
                         continue;
                     }
-                    StringTokenizer st = new StringTokenizer(fileFormatsString,
-                            GPConstants.PARAM_INFO_CHOICE_DELIMITER);
+                    StringTokenizer st = new StringTokenizer(fileFormatsString, GPConstants.PARAM_INFO_CHOICE_DELIMITER);
                     while (st.hasMoreTokens()) {
                         String type = st.nextToken();
                         Collection<TaskInfo> modules = map.get(type);
