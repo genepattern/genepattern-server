b6ba4e835cd65d5b95c186e7d2eb00a86c50f97c
jgould <jgould@broadinstitute.org>
2007-01-23 16:51:29 +0000

    replaced implementing IGPConstants interface with static imports
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5055 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/util/LSIDUtil.java
+++ src/org/genepattern/util/LSIDUtil.java
@@ -1,155 +1,145 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
-
-/*
- * Created on Jan 31, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
  */
+
 package org.genepattern.util;
+
+import static org.genepattern.util.GPConstants.TASK_NAMESPACE;
+import static org.genepattern.util.GPConstants.SUITE_NAMESPACE;
 
 /**
  * @author Liefeld
  * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
+ * 
  */
-public class LSIDUtil implements IGPConstants {
+public class LSIDUtil {
 
-	public static String AUTHORITY_MINE = "mine";
+    public static String AUTHORITY_MINE = "mine";
 
-	public static String AUTHORITY_BROAD = "broad";
+    public static String AUTHORITY_BROAD = "broad";
 
-	public static String AUTHORITY_FOREIGN = "foreign";
+    public static String AUTHORITY_FOREIGN = "foreign";
 
-	public static String BROAD_AUTHORITY = "broad.mit.edu";
+    public static String BROAD_AUTHORITY = "broad.mit.edu";
 
-	private static LSIDUtil inst = null;
+    private static LSIDUtil inst = null;
 
-	private static String authority = "broad-cancer-genomics";
-	
-	private static String SUITE_NAMESPACE_INCLUDE = "suite";
+    private static String authority = "broad-cancer-genomics";
 
+    private static String SUITE_NAMESPACE_INCLUDE = "suite";
 
+    private LSIDUtil() {
+        String auth = System.getProperty("lsid.authority");
+        if (auth != null) {
+            authority = auth;
+        }
+    }
 
-	private LSIDUtil() {
-		String auth = System.getProperty("lsid.authority");
-		if (auth != null) {
-			authority = auth;
-		}
-	}
+    public static LSIDUtil getInstance() {
+        if (inst == null) {
+            inst = new LSIDUtil();
+        }
+        return inst;
+    }
 
-	public static LSIDUtil getInstance() {
-		if (inst == null) {
-			inst = new LSIDUtil();
-		}
-		return inst;
-	}
+    public String getAuthority() {
+        return authority;
+    }
 
-	public String getAuthority() {
-		return authority;
-	}
+    public String getTaskNamespace() {
+        return TASK_NAMESPACE;
+    }
 
-	public String getTaskNamespace() {
-		return TASK_NAMESPACE;
-	}
-	public String getSuiteNamespace() {
-		return SUITE_NAMESPACE;
-	}
+    public String getSuiteNamespace() {
+        return SUITE_NAMESPACE;
+    }
 
+    public String getAuthorityType(LSID lsid) {
+        String authorityType;
+        if (lsid == null) {
+            authorityType = AUTHORITY_MINE;
+        } else {
+            String lsidAuthority = lsid.getAuthority();
+            if (lsidAuthority.equals(authority)) {
+                authorityType = AUTHORITY_MINE;
+            } else if (lsidAuthority.equals(BROAD_AUTHORITY)) {
+                authorityType = AUTHORITY_BROAD;
+            } else {
+                authorityType = AUTHORITY_FOREIGN;
+            }
+        }
+        return authorityType;
+    }
 
-	public String getAuthorityType(LSID lsid) {
-		String authorityType;
-		if (lsid == null) {
-			authorityType = AUTHORITY_MINE;
-		} else {
-			String lsidAuthority = lsid.getAuthority();
-			if (lsidAuthority.equals(authority)) {
-				authorityType = AUTHORITY_MINE;
-			} else if (lsidAuthority.equals(BROAD_AUTHORITY)) {
-				authorityType = AUTHORITY_BROAD;
-			} else {
-				authorityType = AUTHORITY_FOREIGN;
-			}
-		}
-		return authorityType;
-	}
+    // compare authority types: 1=lsid1 is closer, 0=equal, -1=lsid2 is closer
+    // closer is defined as mine > Broad > foreign
+    public int compareAuthorities(LSID lsid1, LSID lsid2) {
+        String at1 = getAuthorityType(lsid1);
+        String at2 = getAuthorityType(lsid2);
+        if (!at1.equals(at2)) {
+            if (at1.equals(AUTHORITY_MINE))
+                return 1;
+            if (at2.equals(AUTHORITY_MINE))
+                return -1;
+            if (at1.equals(AUTHORITY_BROAD))
+                return 1;
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 
-	// compare authority types: 1=lsid1 is closer, 0=equal, -1=lsid2 is closer
-	// closer is defined as mine > Broad > foreign
-	public int compareAuthorities(LSID lsid1, LSID lsid2) {
-		String at1 = getAuthorityType(lsid1);
-		String at2 = getAuthorityType(lsid2);
-		if (!at1.equals(at2)) {
-			if (at1.equals(AUTHORITY_MINE))
-				return 1;
-			if (at2.equals(AUTHORITY_MINE))
-				return -1;
-			if (at1.equals(AUTHORITY_BROAD))
-				return 1;
-			return -1;
-		} else {
-			return 0;
-		}
-	}
+    public LSID getNearerLSID(LSID lsid1, LSID lsid2) {
+        int authorityComparison = compareAuthorities(lsid1, lsid2);
+        if (authorityComparison < 0)
+            return lsid2;
+        if (authorityComparison > 0) {
+            // closer authority than lsid2.getAuthority()
+            return lsid1;
+        }
+        // same authority, check identifier
+        int identifierComparison = lsid1.getIdentifier().compareTo(lsid2.getIdentifier());
+        if (identifierComparison < 0)
+            return lsid2;
+        if (identifierComparison > 0) {
+            // greater identifier than lsid2.getIdentifier()
+            return lsid1;
+        }
+        // same authority and identifier, check version
+        int versionComparison = lsid1.compareTo(lsid2);
+        if (versionComparison < 0)
+            return lsid2;
+        if (versionComparison > 0) {
+            // later version than lsid2.getVersion()
+            return lsid1;
+        }
+        return lsid1; // equal???
+    }
 
-	public LSID getNearerLSID(LSID lsid1, LSID lsid2) {
-		int authorityComparison = compareAuthorities(lsid1, lsid2);
-		if (authorityComparison < 0)
-			return lsid2;
-		if (authorityComparison > 0) {
-			// closer authority than lsid2.getAuthority()
-			return lsid1;
-		}
-		// same authority, check identifier
-		int identifierComparison = lsid1.getIdentifier().compareTo(
-				lsid2.getIdentifier());
-		if (identifierComparison < 0)
-			return lsid2;
-		if (identifierComparison > 0) {
-			// greater identifier than lsid2.getIdentifier()
-			return lsid1;
-		}
-		// same authority and identifier, check version
-		int versionComparison = lsid1.compareTo(lsid2);
-		if (versionComparison < 0)
-			return lsid2;
-		if (versionComparison > 0) {
-			// later version than lsid2.getVersion()
-			return lsid1;
-		}
-		return lsid1; // equal???
-	}
+    public static boolean isSuiteLSID(String lsid) {
 
-	public static  boolean isSuiteLSID(String lsid){
-		
+        try {
+            LSID anLsid = new LSID(lsid);
+            String nom = anLsid.getNamespace();
+            return isSuiteLSID(anLsid);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-		try {
-			LSID anLsid = new LSID(lsid);
-			String nom = anLsid.getNamespace();
-			return isSuiteLSID(anLsid);
-		} catch (Exception e){
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public static boolean isSuiteLSID(LSID lsid) {
+        String nom = lsid.getNamespace();
 
-	public static boolean isSuiteLSID(LSID lsid){
-		String nom = lsid.getNamespace();
-
-				return (nom.indexOf(SUITE_NAMESPACE_INCLUDE)) >= 0;
-	}
+        return (nom.indexOf(SUITE_NAMESPACE_INCLUDE)) >= 0;
+    }
 
 }
-
