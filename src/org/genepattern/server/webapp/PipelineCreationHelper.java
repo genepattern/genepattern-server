d4e1cf60f6f9c4ec137bb7178a78b978af76c6bb
liefeld <liefeld@broadinstitute.org>
2007-01-26 15:01:42 +0000

    more refactoring. PipelineController now called PipelineCreationHelper and reduced responsibilities to make pipelineDesigner.jsp slightly more tractable
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5191 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/webapp/PipelineCreationHelper.java
@@ -0,0 +1,94 @@
+package org.genepattern.server.webapp;
+
+import java.util.Vector;
+
+import org.genepattern.codegenerator.AbstractPipelineCodeGenerator;
+import org.genepattern.data.pipeline.PipelineModel;
+import org.genepattern.server.genepattern.GenePatternAnalysisTask;
+import org.genepattern.server.genepattern.TaskInstallationException;
+import org.genepattern.util.GPConstants;
+import org.genepattern.util.LSID;
+import org.genepattern.webservice.ParameterInfo;
+import org.genepattern.webservice.TaskInfoAttributes;
+
+public class PipelineCreationHelper {
+    PipelineModel model = null;
+    boolean isLsidSet = false;
+
+    public PipelineCreationHelper(PipelineModel model) {
+        this.model = model;
+    }
+
+    
+
+   public String generateTask() throws TaskInstallationException {
+        String lsid = generateTask(AbstractPipelineCodeGenerator
+                .giveParameterInfoArray(model));
+        model.setLsid(lsid);
+        return lsid;
+    }
+   
+    public String generateLSID(){
+        try {
+            LSID taskLSID = GenePatternAnalysisTask.getNextTaskLsid(model.getLsid());
+            model.setLsid( taskLSID.toString());        
+            isLsidSet = true;
+            return taskLSID.toString();
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+
+    public String generateTask(ParameterInfo[] params)
+            throws TaskInstallationException {
+        try {
+            // set the LSID before using the code generator
+            if (!isLsidSet){
+                generateLSID(); 
+            }
+
+            TaskInfoAttributes tia = AbstractPipelineCodeGenerator.getTaskInfoAttributes(model);
+            
+            tia.put(GPConstants.CPU_TYPE, GPConstants.ANY);
+            tia.put(GPConstants.OS, GPConstants.ANY);
+            tia.put(GPConstants.LANGUAGE, "Java");
+            tia.put(GPConstants.SERIALIZED_MODEL, model.toXML());
+            tia.put(GPConstants.USERID, model.getUserID());
+
+            Vector probs = GenePatternAnalysisTask.installTask(model.getName() + "."+ GPConstants.TASK_TYPE_PIPELINE, 
+                    ""+model.getDescription(), 
+                    params, 
+                    tia, 
+                    model.getUserID(), 
+                    model.isPrivate() ? GPConstants.ACCESS_PRIVATE
+                        : GPConstants.ACCESS_PUBLIC, 
+                    null);
+                if ((probs != null) && (probs.size() > 0)) {
+                    throw new TaskInstallationException(probs);
+                }
+   
+            String newLsid = tia.get("LSID");   
+
+            return newLsid;
+        } catch (TaskInstallationException tie) {
+            throw tie;
+        } catch (Exception e) {
+            Vector vProblems = new Vector();
+            vProblems.add(e.getMessage() + " while generating task "
+                    + model.getName());
+            throw new TaskInstallationException(vProblems);
+        }
+    } 
+
+    public ParameterInfo[] giveParameterInfoArray() {
+        return AbstractPipelineCodeGenerator.giveParameterInfoArray(model);
+    }
+
+    public TaskInfoAttributes giveTaskInfoAttributes() {
+        return AbstractPipelineCodeGenerator.getTaskInfoAttributes(model);
+    }
+
+
+    
+}
--- src/org/genepattern/server/webservice/server/ProvenanceFinder.java
+++ src/org/genepattern/server/webservice/server/ProvenanceFinder.java
@@ -32,6 +32,7 @@ import org.genepattern.data.pipeline.JobSubmission;
 import org.genepattern.data.pipeline.PipelineModel;
 import org.genepattern.server.genepattern.GenePatternAnalysisTask;
 import org.genepattern.server.genepattern.TaskInstallationException;
+import org.genepattern.server.webapp.PipelineCreationHelper;
 import org.genepattern.server.webservice.server.local.LocalAdminClient;
 import org.genepattern.server.webservice.server.local.LocalAnalysisClient;
 import org.genepattern.util.GPConstants;
@@ -74,7 +75,7 @@ public class ProvenanceFinder {
 
         try {
             PipelineModel model = this.createPipelineModel(jobs, pipelineName);
-            PipelineTaskCreationHelper helper = new PipelineTaskCreationHelper(model);
+            PipelineCreationHelper helper = new PipelineCreationHelper(model);
             lsid = helper.generateTask();
             model.setLsid(lsid);
             copyFilesToPipelineDir(lsid, pipelineName);
@@ -430,76 +431,3 @@ public class ProvenanceFinder {
 }
 
 
-class  PipelineTaskCreationHelper {
-
-    PipelineModel model = null;
-    boolean isLsidSet = false;
-
-    PipelineTaskCreationHelper(PipelineModel model) {
-        this.model = model;
-    }
-
-    
-
-   public String generateTask() throws TaskInstallationException {
-        String lsid = generateTask(AbstractPipelineCodeGenerator
-                .giveParameterInfoArray(model));
-        model.setLsid(lsid);
-        return lsid;
-    }
-   
-    public String generateLSID(){
-        try {
-            LSID taskLSID = GenePatternAnalysisTask.getNextTaskLsid(model.getLsid());
-            model.setLsid( taskLSID.toString());        
-            isLsidSet = true;
-            return taskLSID.toString();
-        } catch (Exception e){
-            return null;
-        }
-    }
-
-
-    public String generateTask(ParameterInfo[] params)
-            throws TaskInstallationException {
-        try {
-            // set the LSID before using the code generator
-            if (!isLsidSet){
-                generateLSID(); 
-            }
-
-            TaskInfoAttributes tia = AbstractPipelineCodeGenerator.getTaskInfoAttributes(model);
-            
-            tia.put(GPConstants.CPU_TYPE, GPConstants.ANY);
-            tia.put(GPConstants.OS, GPConstants.ANY);
-            tia.put(GPConstants.LANGUAGE, "Java");
-            tia.put(GPConstants.SERIALIZED_MODEL, model.toXML());
-            tia.put(GPConstants.USERID, model.getUserID());
-
-            Vector probs = GenePatternAnalysisTask.installTask(model.getName() + "."+ GPConstants.TASK_TYPE_PIPELINE, 
-                    ""+model.getDescription(), 
-                    params, 
-                    tia, 
-                    model.getUserID(), 
-                    model.isPrivate() ? GPConstants.ACCESS_PRIVATE
-                        : GPConstants.ACCESS_PUBLIC, 
-                    null);
-                if ((probs != null) && (probs.size() > 0)) {
-                    throw new TaskInstallationException(probs);
-                }
-   
-            String newLsid = tia.get("LSID");   
-
-            return newLsid;
-        } catch (TaskInstallationException tie) {
-            throw tie;
-        } catch (Exception e) {
-            Vector vProblems = new Vector();
-            vProblems.add(e.getMessage() + " while generating task "
-                    + model.getName());
-            throw new TaskInstallationException(vProblems);
-        }
-    } 
-
-    
-}
