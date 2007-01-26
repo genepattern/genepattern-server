169affedb96c59ebced97132ea188784c11a9bcc
jgould <jgould@broadinstitute.org>
2007-01-26 01:54:34 +0000

    organized imports
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5175 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/visualizer/RunVisualizer.java
+++ src/org/genepattern/visualizer/RunVisualizer.java
@@ -16,14 +16,11 @@ import java.applet.Applet;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.CookieHandler;
 import java.net.InetAddress;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLDecoder;
@@ -31,9 +28,6 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class RunVisualizer {
