b7bc9cbb21e726f9b78099b30a8d568e1082bad0
jgould <jgould@broadinstitute.org>
2007-01-26 05:20:15 +0000

    fixed redirect page
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5183 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/ConnectionFilter.java
+++ src/org/genepattern/server/webapp/ConnectionFilter.java
@@ -1,22 +1,15 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
-
-/*
- * Created on Feb 16, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
  */
+
 package org.genepattern.server.webapp;
 
 import java.io.IOException;
@@ -35,44 +28,37 @@ import org.genepattern.server.util.AccessManager;
 /**
  * @author Liefeld
  * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
  */
 public class ConnectionFilter implements Filter {
-	private FilterConfig filterConfig = null;
-   
-	public void init(FilterConfig filterConfig) throws ServletException {
-      	this.filterConfig = filterConfig;
-   	}
 
-   	public void destroy() {
-      	this.filterConfig = null;
-   	}
+    public void init(FilterConfig filterConfig) throws ServletException {
 
+    }
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void destroy() {
 
-      	boolean allowed = AccessManager.isAllowed(request.getRemoteHost(), request.getRemoteAddr());
+    }
 
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
 
+        boolean allowed = AccessManager.isAllowed(request.getRemoteHost(), request.getRemoteAddr());
 
-		if (allowed){
-			chain.doFilter(request, response);
-			return;
-		} else {
-			if (isJspCall((HttpServletRequest)request)){
-				((HttpServletResponse)response).sendRedirect("/pages/notallowed.html");
-			} else {
-				((HttpServletResponse)response).sendError(((HttpServletResponse)response).SC_FORBIDDEN);
-			}
+        if (allowed) {
+            chain.doFilter(request, response);
+        } else {
+            if (isJspCall((HttpServletRequest) request)) {
+                ((HttpServletResponse) response).sendRedirect("/pages/notallowed.jsf");
+            } else {
+                ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN);
+            }
 
-		}
-   }
+        }
+    }
 
-   	public boolean isJspCall(HttpServletRequest request){
-		return (request.getRequestURI().indexOf(".jsp") >= 0);
+    public boolean isJspCall(HttpServletRequest request) {
+        return (request.getRequestURI().indexOf(".jsp") >= 0);
 
-	}
-
+    }
 
 }
