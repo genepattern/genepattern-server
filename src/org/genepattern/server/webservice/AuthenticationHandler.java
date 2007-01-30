aea815737e9931939dbf018a842d455c7b086be1
jgould <jgould@broadinstitute.org>
2007-01-30 22:29:00 +0000

    return false if user not found in db or if password is null
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5307 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webservice/AuthenticationHandler.java
+++ src/org/genepattern/server/webservice/AuthenticationHandler.java
@@ -1,20 +1,18 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.server.webservice;
 
 import org.apache.axis.AxisFault;
-import org.apache.axis.Message;
 import org.apache.axis.MessageContext;
 import org.apache.log4j.Logger;
 import org.genepattern.server.user.User;
@@ -23,54 +21,49 @@ import org.genepattern.server.webapp.jsf.EncryptionUtil;
 
 public class AuthenticationHandler extends org.apache.axis.handlers.BasicHandler {
     boolean passwordRequired = false;
-    
+
     private static Logger log = Logger.getLogger(AuthenticationHandler.class);
-	
-    public void init(){
+
+    public void init() {
         super.init();
-        
         String prop = System.getProperty("require.password", "false").toLowerCase();
         passwordRequired = (prop.equals("true") || prop.equals("y") || prop.equals("yes"));
+
+    }
+
+    public void invoke(MessageContext msgContext) throws AxisFault {
         
-		
-	}
-
-
-	public void invoke(MessageContext msgContext) throws AxisFault {
-		Message requestMessage = msgContext.getCurrentMessage();
-		
-		String username = msgContext.getUsername();
+        String username = msgContext.getUsername();
         String password = msgContext.getPassword();
-        
-		
-	  	//System.out.println("\n\t AH   handler: " + methodSig + " called by " + username + " ok==>" + allowed);
 
-		if (!validateUserPassword(username, password)){
-			throw new AxisFault("Error: Unknown user or invalid password.");
-			
-		}
-	}
-    
+        if (!validateUserPassword(username, password)) {
+            throw new AxisFault("Error: Unknown user or invalid password.");
+
+        }
+    }
+
     /**
-     * @todo - implementation.  Currently accepts any non-null user
+     * @todo - implementation. Currently accepts any non-null user
      * @param user
      * @param password
      * @return
      */
     private boolean validateUserPassword(String user, String password) {
-        if (!passwordRequired) return true;
-        
+        if (!passwordRequired) {
+            return true;
+        }
+
         User up = (new UserDAO()).findById(user);
+        if (up == null || password == null) {
+            return false;
+        }
+
         try {
-        return (java.util.Arrays.equals(
-                    EncryptionUtil.encrypt(password), 
-                    up.getPassword()
-                    ));
-        } catch (Exception e){
+            return (java.util.Arrays.equals(EncryptionUtil.encrypt(password), up.getPassword()));
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
-       
-        
+
     }
 }
