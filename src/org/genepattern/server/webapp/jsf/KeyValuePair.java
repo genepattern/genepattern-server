7432bf19d4bd4f817e901944134cb1635d3f5543
jgould <jgould@broadinstitute.org>
2007-02-01 20:33:08 +0000

    added default constructor
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5381 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/KeyValuePair.java
+++ src/org/genepattern/server/webapp/jsf/KeyValuePair.java
@@ -1,48 +1,50 @@
 package org.genepattern.server.webapp.jsf;
 
-import java.util.Map;
-
 public class KeyValuePair {
-	public String key;
-	public String value;
-	public String altKey;
-	
-	public String getAltKey() {
-		return altKey;
-	}
+    public String altKey;
 
-	public void setAltKey(String altKey) {
-		this.altKey = altKey;
-	}
+    public String key;
 
-	public KeyValuePair(String n, String v){
-		key = n;
-		value = v;
-		altKey = n;
-	}
-	public KeyValuePair(String n, String a, String v){
-		key = n;
-		value = v;
-		altKey = a;
-	}
-	
-	public String getKey(){
-		return key;
-	}
-	
-	public void setKey(String n){
-		 key = n;
-	}
+    public String value;
 
-	public String getValue(){
-		return value;
-	}
-	public void setValue(String n){
-		 value = n;
-	}
-/*	public Object setValue(Object n){
-		Object old = getValue();
-		value = (String)n;
-		return old;
-	} */
+    public KeyValuePair() {
+
+    }
+
+    public KeyValuePair(String key, String value) {
+        this.key = key;
+        this.value = value;
+        this.altKey = key;
+    }
+
+    public KeyValuePair(String key, String altKey, String value) {
+        this.key = key;
+        this.value = value;
+        this.altKey = altKey;
+    }
+
+    public String getAltKey() {
+        return altKey;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setAltKey(String altKey) {
+        this.altKey = altKey;
+    }
+
+    public void setKey(String n) {
+        key = n;
+    }
+
+    public void setValue(String n) {
+        value = n;
+    }
+
 }
