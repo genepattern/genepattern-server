2ee61c476132c3b2555d5089c87af92e7ce50364
jgould <jgould@broadinstitute.org>
2007-02-05 18:50:41 +0000

    Fixed sub method
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5444 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/client/Util.java
+++ src/org/genepattern/client/Util.java
@@ -36,184 +36,167 @@ import org.genepattern.webservice.WebServiceException;
  * @author Joshua Gould
  */
 public class Util {
-	private static java.io.BufferedReader br = new java.io.BufferedReader(
-			new java.io.InputStreamReader(System.in));
+    private static java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
 
-	private Util() {
-	}
+    private Util() {
+    }
 
-	/**
-	 * Prompts the user for input from the console.
-	 * 
-	 * @param prompt
-	 *            The user prompt.
-	 * @return The user entered text.
-	 * @exception java.io.IOException
-	 *                If an error occurs while reading the input from the
-	 *                console.
-	 */
-	public static String prompt(String prompt) throws java.io.IOException {
-		System.out.print(prompt);
-		return br.readLine();
-	}
+    /**
+     * Prompts the user for input from the console.
+     * 
+     * @param prompt
+     *            The user prompt.
+     * @return The user entered text.
+     * @exception java.io.IOException
+     *                If an error occurs while reading the input from the
+     *                console.
+     */
+    public static String prompt(String prompt) throws java.io.IOException {
+        System.out.print(prompt);
+        return br.readLine();
+    }
 
-	public static ParameterInfo[] createParameterInfoArray(TaskInfo taskInfo,
-			Parameter[] parameters) throws WebServiceException {
+    public static ParameterInfo[] createParameterInfoArray(TaskInfo taskInfo, Parameter[] parameters)
+            throws WebServiceException {
 
-		ParameterInfo[] formalParameters = taskInfo.getParameterInfoArray();
-		List actualParameters = new ArrayList();
+        ParameterInfo[] formalParameters = taskInfo.getParameterInfoArray();
+        List<ParameterInfo> actualParameters = new ArrayList<ParameterInfo>();
 
-		Map paramName2FormalParam = new HashMap();
-		if (formalParameters != null) {
-			for (int i = 0, length = formalParameters.length; i < length; i++) {
-				paramName2FormalParam.put(formalParameters[i].getName(),
-						formalParameters[i]);
-			}
-		}
+        Map<String, ParameterInfo> paramName2FormalParam = new HashMap<String, ParameterInfo>();
+        if (formalParameters != null) {
+            for (int i = 0, length = formalParameters.length; i < length; i++) {
+                paramName2FormalParam.put(formalParameters[i].getName(), formalParameters[i]);
+            }
+        }
 
-		if (parameters != null) {
-			for (int i = 0, length = parameters.length; i < length; i++) {
-				ParameterInfo formalParam = (ParameterInfo) paramName2FormalParam
-						.remove(parameters[i].getName());
-				if (formalParam == null) {
-					if (parameters[i].getName().equals(GPConstants.PIPELINE_ARG_STOP_AFTER_TASK_NUM)){
-						formalParam = new ParameterInfo(parameters[i].getName(), parameters[i].getValue(), "");
+        if (parameters != null) {
+            for (int i = 0, length = parameters.length; i < length; i++) {
+                ParameterInfo formalParam = paramName2FormalParam.remove(parameters[i].getName());
+                if (formalParam == null) {
+                    if (parameters[i].getName().equals(GPConstants.PIPELINE_ARG_STOP_AFTER_TASK_NUM)) {
+                        formalParam = new ParameterInfo(parameters[i].getName(), parameters[i].getValue(), "");
+                    } else {
+                        throw new WebServiceException("Unknown parameter: " + parameters[i].getName());
+                    }
+                }
+                Map formalAttributes = formalParam.getAttributes();
+                if (formalAttributes == null) {
+                    formalAttributes = new HashMap();
+                }
+                String value = parameters[i].getValue();
 
-					} else {
+                if (value == null) {
+                    value = (String) formalAttributes.get(GPConstants.PARAM_INFO_DEFAULT_VALUE[0]);
+                }
+                if (value == null && !isOptional(formalParam)) {
+                    throw new WebServiceException("Missing value for required parameter "
+                            + formalParameters[i].getName());
 
-						throw new WebServiceException("Unknown parameter: "
-							+ parameters[i].getName());
-					}
-				}
-				Map formalAttributes = formalParam.getAttributes();
-				if (formalAttributes == null) {
-					formalAttributes = new HashMap();
-				}
-				String value = parameters[i].getValue();
+                }
+                value = sub(formalParam, value);
+                ParameterInfo p = new ParameterInfo(formalParam.getName(), value, "");
+                setAttributes(formalParam, p);
+                actualParameters.add(p);
+            }
+        }
 
-				if (value == null || value.trim().equals("")) {
-					String sOptional = (String) formalAttributes
-							.get(GPConstants.PARAM_INFO_OPTIONAL[0]);
-					boolean optional = (sOptional != null && sOptional.length() > 0);
-					if (!optional) {
-						throw new WebServiceException(
-								"Missing value for required parameter "
-										+ formalParameters[i].getName());
-					}
-				}
+        // go through parameters that were not provided by user
+        for (Iterator<String> it = paramName2FormalParam.keySet().iterator(); it.hasNext();) {
+            String name = it.next();
+            ParameterInfo formalParam = paramName2FormalParam.get(name);
+            String value = (String) formalParam.getAttributes().get((String) TaskExecutor.PARAM_INFO_DEFAULT_VALUE[0]);
+            if (value == null && !isOptional(formalParam)) {
+                throw new WebServiceException("Missing value for required parameter " + formalParam.getName());
 
-				ParameterInfo p = new ParameterInfo(formalParam.getName(),
-						value, "");
-				setAttributes(formalParam, p);
-				actualParameters.add(p);
-			}
-		}
+            }
+            value = sub(formalParam, value);
+            ParameterInfo actual = new ParameterInfo(formalParam.getName(), value, "");
+            setAttributes(formalParam, actual);
+            actualParameters.add(actual);
 
-		for (Iterator it = paramName2FormalParam.keySet().iterator(); it
-				.hasNext();) {
-			String name = (String) it.next();
-			ParameterInfo p = (ParameterInfo) paramName2FormalParam.get(name);
-			String value = (String) p.getAttributes().get(
-					(String) TaskExecutor.PARAM_INFO_DEFAULT_VALUE[0]);
-			if (value == null || value.equals("")) {
-				String sOptional = (String) p.getAttributes().get(
-						GPConstants.PARAM_INFO_OPTIONAL[0]);
-				boolean optional = (sOptional != null && sOptional.length() > 0);
-				if (!optional) {
-					throw new WebServiceException(
-							"Missing value for required parameter "
-									+ p.getName());
-				}
-			} else {
-				value = sub(p, value);
-				ParameterInfo actual = new ParameterInfo(p.getName(), value, "");
-				setAttributes(p, actual);
-				actualParameters.add(actual);
-			}
+        }
 
-		}
+        return (ParameterInfo[]) actualParameters.toArray(new ParameterInfo[0]);
+    }
 
-		return (ParameterInfo[]) actualParameters.toArray(new ParameterInfo[0]);
-	}
+    private static boolean isOptional(ParameterInfo formalParameter) {
+        String sOptional = (String) formalParameter.getAttributes().get(GPConstants.PARAM_INFO_OPTIONAL[0]);
+        return (sOptional != null && sOptional.length() > 0);
 
-	private static void setAttributes(ParameterInfo formalParam,
-			ParameterInfo actualParam) {
+    }
 
-		if (formalParam.isInputFile()) {
-			HashMap actualAttributes = new HashMap();
-			actualParam.setAttributes(actualAttributes);
-			String value = actualParam.getValue();
-			actualAttributes.put(GPConstants.PARAM_INFO_CLIENT_FILENAME[0],
-					value);
-			if (value != null && new java.io.File(value).exists()) {
-				actualParam.setAsInputFile();
-			} else if (value != null) {
-				actualAttributes.remove("TYPE");
-				actualAttributes.put(ParameterInfo.MODE,
-						ParameterInfo.URL_INPUT_MODE);
-			}
-		}
-	}
+    private static void setAttributes(ParameterInfo formalParam, ParameterInfo actualParam) {
 
-	private static String sub(ParameterInfo formalParam, String value)
-			throws WebServiceException {
-		// see if parameter belongs to a set of choices, e.g. 1=T-Test.
-		// If so substitute 1 for T-Test, also check to see if value is
-		// valid
-		String choicesString = formalParam.getValue();
-		if (value != null && choicesString != null && !choicesString.equals("")) {
-			String[] choices = choicesString.split(";");
-			boolean validValue = false;
-			for (int j = 0; j < choices.length && !validValue; j++) {
-				String[] choiceValueAndChoiceUIValue = choices[j].split("=");
-				if (value.equals(choiceValueAndChoiceUIValue[0])) {
-					validValue = true;
-				} else if (choiceValueAndChoiceUIValue.length == 2
-						&& value.equals(choiceValueAndChoiceUIValue[1])) {
-					value = choiceValueAndChoiceUIValue[0];
-					validValue = true;
-				}
-			}
-			if (!validValue) {
-				throw new WebServiceException("Illegal value for parameter "
-						+ formalParam.getName() + ": " + value);
-			}
-		}
-		return value;
-	}
+        if (formalParam.isInputFile()) {
+            HashMap actualAttributes = new HashMap();
+            actualParam.setAttributes(actualAttributes);
+            String value = actualParam.getValue();
+            actualAttributes.put(GPConstants.PARAM_INFO_CLIENT_FILENAME[0], value);
+            if (value != null && new java.io.File(value).exists()) {
+                actualParam.setAsInputFile();
+            } else if (value != null) {
+                actualAttributes.remove("TYPE");
+                actualAttributes.put(ParameterInfo.MODE, ParameterInfo.URL_INPUT_MODE);
+            }
+        }
+    }
 
-	/**
-	 * Returns the model type of an odf file. The input stream is closed.
-	 * 
-	 * @param is
-	 *            An input stream to an odf file
-	 * @return the odf module type or the empty string if unable to determine
-	 *         the model
-	 */
-	public static String getOdfModelType(InputStream is) {
-		String model = "";
-		BufferedReader inputB = null;
-		try {
-			inputB = new BufferedReader(new InputStreamReader(is));
-			String modelLine = inputB.readLine();
-			while (modelLine != null && !modelLine.startsWith("Model")) {
-				modelLine = inputB.readLine();
-			}
+    private static String sub(ParameterInfo formalParam, String value) throws WebServiceException {
+        // see if parameter belongs to a set of choices, e.g. 1=T-Test.
+        // If so substitute 1 for T-Test, also check to see if value is
+        // valid
+        String choicesString = formalParam.getValue();
+        if (value != null && choicesString != null && !choicesString.equals("")) {
+            String[] choices = choicesString.split(";");
+            boolean validValue = false;
+            for (int j = 0; j < choices.length && !validValue; j++) {
+                String[] choiceValueAndChoiceUIValue = choices[j].split("=");
+                if (value.equals(choiceValueAndChoiceUIValue[0])) {
+                    validValue = true;
+                } else if (choiceValueAndChoiceUIValue.length == 2 && value.equals(choiceValueAndChoiceUIValue[1])) {
+                    value = choiceValueAndChoiceUIValue[0];
+                    validValue = true;
+                }
+            }
+            if (!validValue) {
+                throw new WebServiceException("Illegal value for parameter " + formalParam.getName() + ": " + value);
+            }
+        }
+        return value;
+    }
 
-			if (modelLine != null) {
-				model = modelLine.substring(modelLine.indexOf("=") + 1).trim();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (inputB != null) {
-				try {
-					inputB.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return model;
-	}
+    /**
+     * Returns the model type of an odf file. The input stream is closed.
+     * 
+     * @param is
+     *            An input stream to an odf file
+     * @return the odf module type or the empty string if unable to determine
+     *         the model
+     */
+    public static String getOdfModelType(InputStream is) {
+        String model = "";
+        BufferedReader inputB = null;
+        try {
+            inputB = new BufferedReader(new InputStreamReader(is));
+            String modelLine = inputB.readLine();
+            while (modelLine != null && !modelLine.startsWith("Model")) {
+                modelLine = inputB.readLine();
+            }
+
+            if (modelLine != null) {
+                model = modelLine.substring(modelLine.indexOf("=") + 1).trim();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputB != null) {
+                try {
+                    inputB.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return model;
+    }
 }
