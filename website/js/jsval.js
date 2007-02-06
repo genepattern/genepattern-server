862391c48fbfc620bb6f1a3aff13d1d39b58bf64
jgould <jgould@broadinstitute.org>
2007-02-06 16:03:26 +0000

    bug 1508 - use email validation code from commons-validator
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5463 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/jsval.js
+++ website/js/jsval.js
@@ -394,10 +394,72 @@ function _parseBoolean(value){
    return !(!value || value == 0 || value == "0" || value == "false");
 };
 
+function jcv_checkEmail(emailStr) {
+        if (emailStr.length == 0) {
+            return true;
+        }
+        // TLD checking turned off by default
+        var checkTLD=0;
+        var knownDomsPat=/^(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum)$/;
+        var emailPat=/^(.+)@(.+)$/;
+        var specialChars="\\(\\)><@,;:\\\\\\\"\\.\\[\\]";
+        var validChars="\[^\\s" + specialChars + "\]";
+        var quotedUser="(\"[^\"]*\")";
+        var ipDomainPat=/^\[(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\]$/;
+        var atom=validChars + '+';
+        var word="(" + atom + "|" + quotedUser + ")";
+        var userPat=new RegExp("^" + word + "(\\." + word + ")*$");
+        var domainPat=new RegExp("^" + atom + "(\\." + atom +")*$");
+        var matchArray=emailStr.match(emailPat);
+        if (matchArray==null) {
+            return false;
+        }
+        var user=matchArray[1];
+        var domain=matchArray[2];
+        for (i=0; i<user.length; i++) {
+            if (user.charCodeAt(i)>127) {
+                return false;
+            }
+        }
+        for (i=0; i<domain.length; i++) {
+            if (domain.charCodeAt(i)>127) {
+                return false;
+            }
+        }
+        if (user.match(userPat)==null) {
+            return false;
+        }
+        var IPArray=domain.match(ipDomainPat);
+        if (IPArray!=null) {
+            for (var i=1;i<=4;i++) {
+                if (IPArray[i]>255) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        var atomPat=new RegExp("^" + atom + "$");
+        var domArr=domain.split(".");
+        var len=domArr.length;
+        for (i=0;i<len;i++) {
+            if (domArr[i].search(atomPat)==-1) {
+                return false;
+            }
+        }
+        if (checkTLD && domArr[domArr.length-1].length!=2 && 
+            domArr[domArr.length-1].search(knownDomsPat)==-1) {
+            return false;
+        }
+        if (len<2) {
+            return false;
+        }
+        return true;
+    }
+    
 function _checkRegExp(regx, value){
   switch (regx){
   case "JSVAL_RX_EMAIL":
-    return ((/^[a-zA-ZüöäßÄÖÜ]+([\.-]?[a-zA-ZüöäßÄÖÜ]+)*@[a-zA-ZüöäßÄÖÜ]+([\.-]?[a-zA-ZüöäßÄÖÜ]+)*(\.\w{2,5})+$/).test(value));
+    return jcv_checkEmail(value);
   case "JSVAL_RX_TEL":
     return ((/^1?[\- ]?\(?\d{3}\)?[\- ]?\d{3}[\- ]?\d{4}$/).test(value));
   case "JSVAL_RX_PC":
