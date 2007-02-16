0c70512e7eea45333c7ae2287d4ecd0a940aa4ca
jrobinso <jrobinso@broadinstitute.org>
2007-02-16 16:38:07 +0000

    Changed window width function,  IE doesn't support "window.clientWidth"
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5680 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/popupMenu.js
+++ website/js/popupMenu.js
@@ -21,22 +21,25 @@ function pm_showMenu(id, pos, horizOffset, vertOffset) {
    pm_currentId = id;
    pm_showing = true;
    var style = $(id).style;
+   var isIe = navigator.appVersion.match(/\bMSIE\b/);
+   var width = document.documentElement.clientWidth;
+   var height = document.documentElement.clientHeight;
    if(pos) {
-      if(pos[0] < (window.innerWidth / 2)) {
+      if(pos[0] < (width / 2)) {
         // Menu is on left side of page, use left align
         style.left = Math.max(0, pos[0] - horizOffset) + "px";
       }
       else {
         // Menu is on right side of page, user right align. 
-        style.right = Math.max(0, window.innerWidth - pos[0] - horizOffset) + "px"; 
+        style.right = Math.max(0, width - pos[0] - horizOffset) + "px"; 
       }
-      if(pos[1] < (window.innerHeight / 2)) {
+      if(pos[1] < (height / 2)) {
         // Menu is on top half of page, use top align
         style.top = Math.max(0, pos[1] - vertOffset) + "px";
       }
       else {
         // Menu is on bottom half of page, user bottom align. 
-        style.bottom = Math.max(0, window.innerHeight - pos[1] - vertOffset) + "px"; 
+        style.bottom = Math.max(0, height - pos[1] - vertOffset) + "px"; 
       } 
     }
     style.display = "";
