79b29be684a5d84bfa69f5ee518e7c57844c20b4
nazaire <nazaire@broadinstitute.org>
2007-02-16 17:31:58 +0000

    R client test script update
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5682 48a7a3f7-ad46-0410-b969-29950918272e

--- test/R/RClientTest.R
+++ test/R/RClientTest.R
@@ -1,23 +1,50 @@
-test.valid.gp.login <- 
-function(servername, username, password)
+test.invalid.task.run.analysis <-
+function()
 {
-	gp.server <- gp.login(servername, username, password)
-	assert(!is.null(gp.server), "Error retrieving a GPServer object")	
+    servername <- "http://localhost:8080"
+    username <- "GenePattern"
+    password <- NULL
+    gp.server <- gp.login(servername, username)
+    module.or.lsid <- "InvalidTask"
+    filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
+    # Verify exception is thrown
+	checkException(run.analysis(gp.server, module.or.lsid, input.filename=filename), "Exception not found as expected")
 }
 
-
-test.invalid.run.analysis <-
-function(gp.server, module.or.lsid, ...)
+test.valid.task.run.analysis <-
+function()
 {
-	checkException(run.analysis(gp.server, module.or.lsid, ...), "Exception not found as expected") 
-}
+    servername <- "http://localhost:8080"
+    username <- "GenePattern"
+    password <- NULL
+    gp.server <- gp.login(servername, username)
+    module.or.lsid <- "PreprocessDataset"
+    filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
 
-test.valid.run.analysis <-
-function(gp.server, module.or.lsid, ...)
-{ 
-	error <- get_error(run.analysis(gp.server, module.or.lsid, ...)) 
+    #Verify that no exception is thrown
+	error <- get_error(run.analysis(gp.server, module.or.lsid, input.filename=filename))
 	if(is.logical(error))
 		return(checkEquals(error, NA)) 
 	
+	return(FALSE)
+}
+
+test.optional.params.run.analysis <-
+function()
+{
+    servername <- "http://localhost:8080"
+    username <- "GenePattern"
+    password <- NULL
+    gp.server <- gp.login(servername, username)
+    module.or.lsid <- "PreprocessDataset"
+    filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
+    out.file.format <- 2
+    use.log.base.two <- 0
+
+    #Verify that no exception is thrown
+	error <- get_error(run.analysis(gp.server, module.or.lsid, input.filename=file, output.file.format=out.file.format, use.log.base.two=0)) 
+	if(is.logical(error))
+		return(checkEquals(error, NA))
+
 	return(FALSE)
 }
