0976c1c0546cd835cd2dd2185e0b1fb88db6493e
jgould <jgould@broadinstitute.org>
2007-02-21 23:52:05 +0000

    use butler package
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@5720 48a7a3f7-ad46-0410-b969-29950918272e

--- test/R/RClientTest.R
+++ test/R/RClientTest.R
@@ -1,50 +1,62 @@
-test.invalid.task.run.analysis <-
-function()
-{
-    servername <- "http://localhost:8080"
+library(butler)
+library(GenePattern)
+
+gp.server <- setup()
+setup <-
+function() {
+	servername <- "http://localhost:8080"
     username <- "GenePattern"
-    password <- NULL
-    gp.server <- gp.login(servername, username)
-    module.or.lsid <- "InvalidTask"
-    filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
-    # Verify exception is thrown
-	checkException(run.analysis(gp.server, module.or.lsid, input.filename=filename), "Exception not found as expected")
+    password <- "GenePattern"
+    gp.server <- gp.login(servername, username, password=password)
+    return(gp.server)
 }
 
-test.valid.task.run.analysis <-
+
+test.invalid.module.run.analysis <-
 function()
 {
-    servername <- "http://localhost:8080"
-    username <- "GenePattern"
-    password <- NULL
-    gp.server <- gp.login(servername, username)
+    module.or.lsid <- "XXXDataset"
+    filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
+    # Verify exception is thrown
+	assert.error(run.analysis(gp.server, module.or.lsid, input.filename=filename))
+}
+
+test.valid.module.run.analysis <-
+function()
+{
     module.or.lsid <- "PreprocessDataset"
     filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
-
-    #Verify that no exception is thrown
-	error <- get_error(run.analysis(gp.server, module.or.lsid, input.filename=filename))
-	if(is.logical(error))
-		return(checkEquals(error, NA)) 
-	
-	return(FALSE)
+	e <- get_error(run.analysis(gp.server, module.or.lsid, input.filename=filename))
+	assert.equal(e, NA)
 }
 
 test.optional.params.run.analysis <-
 function()
 {
-    servername <- "http://localhost:8080"
-    username <- "GenePattern"
-    password <- NULL
-    gp.server <- gp.login(servername, username)
     module.or.lsid <- "PreprocessDataset"
     filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
-    out.file.format <- 2
-    use.log.base.two <- 0
-
-    #Verify that no exception is thrown
-	error <- get_error(run.analysis(gp.server, module.or.lsid, input.filename=file, output.file.format=out.file.format, use.log.base.two=0)) 
-	if(is.logical(error))
-		return(checkEquals(error, NA))
-
-	return(FALSE)
+    out.file.format <- "gct"
+	e <- get_error(run.analysis(gp.server, module.or.lsid, input.filename=filename, output.file.format=out.file.format))
+	assert.equal(e, NA)
 }
+
+test.run.visualizer <-
+function()
+{
+    module.or.lsid <- "HeatMapViewer"
+    filename <- "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res"
+	e <- get_error(run.visualizer(gp.server, module.or.lsid, dataset.filename=filename))
+	assert.equal(e, NA)
+}
+
+test.get.parameters <-
+function()
+{
+    module.or.lsid <- "PreprocessDataset"
+    e <- get_error(get.parameters(gp.server, module.or.lsid))
+    assert.equal(e, NA)
+}
+
+path <- '/Users/jgould/Documents/workspace/gp/test/R/RClientTest.R'
+test(path)
+
