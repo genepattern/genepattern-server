9250df41337fa45b2e7aeda01241f56399846052
jgould <jgould@broadinstitute.org>
2007-07-09 14:18:47 +0000

    changed logging message from error to info
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6107 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/process/JobPurger.java
+++ src/org/genepattern/server/process/JobPurger.java
@@ -1,15 +1,14 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.server.process;
 
@@ -26,115 +25,106 @@ public class JobPurger implements Runnable {
 
     private static Logger log = Logger.getLogger(JobPurger.class);
 
-	final static int DEFAULT_PURGE_JOBS_INTERVAL = 7;
+    final static int DEFAULT_PURGE_JOBS_INTERVAL = 7;
 
-	final static int DEFAULT_PURGE_HOUR = 23;
+    final static int DEFAULT_PURGE_HOUR = 23;
 
-	final static int DEFAULT_PURGE_MINUTE = 0;
+    final static int DEFAULT_PURGE_MINUTE = 0;
 
-	protected int purgeInterval = -1;
+    protected int purgeInterval = -1;
 
-	public Timer timer = new Timer(true);
-		Purger purger = null;
+    public Timer timer = new Timer(true);
 
-	boolean DEBUG = false;
+    Purger purger = null;
 
-	static JobPurger instance = null; 
-	static Thread purgerThread = null;
+    boolean DEBUG = false;
 
+    static JobPurger instance = null;
 
-	
-	public  static Thread startJobPurger(Properties props) {
+    static Thread purgerThread = null;
 
-		if (instance != null) instance.timer.cancel();
+    public static Thread startJobPurger(Properties props) {
 
-	
-		String purgeJobsAfter = props.getProperty("purgeJobsAfter", "-1");
-		String purgeTime = props.getProperty("purgeTime", "23:00");
+        if (instance != null)
+            instance.timer.cancel();
 
-		String daemonName = "JobPurger";
-		log.info("starting " + daemonName + " to purge jobs older than "
-				+ purgeJobsAfter + " days at " + purgeTime);
-		purgerThread= new Thread(new JobPurger(purgeJobsAfter,
-				purgeTime), daemonName);
-		purgerThread.setPriority(Thread.MIN_PRIORITY);
-		purgerThread.setDaemon(true);
-		purgerThread.start();
-		return purgerThread;
-	}
+        String purgeJobsAfter = props.getProperty("purgeJobsAfter", "-1");
+        String purgeTime = props.getProperty("purgeTime", "23:00");
 
+        String daemonName = "JobPurger";
+        log.info("starting " + daemonName + " to purge jobs older than " + purgeJobsAfter + " days at " + purgeTime);
+        purgerThread = new Thread(new JobPurger(purgeJobsAfter, purgeTime), daemonName);
+        purgerThread.setPriority(Thread.MIN_PRIORITY);
+        purgerThread.setDaemon(true);
+        purgerThread.start();
+        return purgerThread;
+    }
 
-	public JobPurger(){
-		String pi = System.getProperty("purgeJobsAfter", "-1");
-		String pt = System.getProperty("purgeTime", "23:00");
-		init(pi, pt); 
-	}
+    public JobPurger() {
+        String pi = System.getProperty("purgeJobsAfter", "-1");
+        String pt = System.getProperty("purgeTime", "23:00");
+        init(pi, pt);
+    }
 
-	public JobPurger(String purgeInterval, String purgeTime) {
-		init(purgeInterval, purgeTime);
-	}
+    public JobPurger(String purgeInterval, String purgeTime) {
+        init(purgeInterval, purgeTime);
+    }
 
-	public void init(String purgeInterval, String purgeTime) {
-		GregorianCalendar nextPurgeTime = new GregorianCalendar();
-		GregorianCalendar purgeTOD = new GregorianCalendar();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
-		try {
-			this.purgeInterval = Integer.parseInt(purgeInterval);
-		} catch (NumberFormatException nfe) {
-			this.purgeInterval = DEFAULT_PURGE_JOBS_INTERVAL;
-		}
-		try {
-			purgeTOD.setTime(dateFormat.parse(purgeTime));
-		} catch (ParseException pe) {
-			purgeTOD.set(GregorianCalendar.HOUR_OF_DAY, DEFAULT_PURGE_HOUR);
-			purgeTOD.set(GregorianCalendar.MINUTE, DEFAULT_PURGE_MINUTE);
-		}
-		nextPurgeTime.set(GregorianCalendar.HOUR_OF_DAY, purgeTOD
-				.get(GregorianCalendar.HOUR_OF_DAY));
-		nextPurgeTime.set(GregorianCalendar.MINUTE, purgeTOD
-				.get(GregorianCalendar.MINUTE));
-		nextPurgeTime.set(GregorianCalendar.SECOND, 0);
-		nextPurgeTime.set(GregorianCalendar.MILLISECOND, 0);
+    public void init(String purgeInterval, String purgeTime) {
+        GregorianCalendar nextPurgeTime = new GregorianCalendar();
+        GregorianCalendar purgeTOD = new GregorianCalendar();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        try {
+            this.purgeInterval = Integer.parseInt(purgeInterval);
+        } catch (NumberFormatException nfe) {
+            this.purgeInterval = DEFAULT_PURGE_JOBS_INTERVAL;
+        }
+        try {
+            purgeTOD.setTime(dateFormat.parse(purgeTime));
+        } catch (ParseException pe) {
+            purgeTOD.set(GregorianCalendar.HOUR_OF_DAY, DEFAULT_PURGE_HOUR);
+            purgeTOD.set(GregorianCalendar.MINUTE, DEFAULT_PURGE_MINUTE);
+        }
+        nextPurgeTime.set(GregorianCalendar.HOUR_OF_DAY, purgeTOD.get(GregorianCalendar.HOUR_OF_DAY));
+        nextPurgeTime.set(GregorianCalendar.MINUTE, purgeTOD.get(GregorianCalendar.MINUTE));
+        nextPurgeTime.set(GregorianCalendar.SECOND, 0);
+        nextPurgeTime.set(GregorianCalendar.MILLISECOND, 0);
 
-		if (nextPurgeTime.getTime().before(new Date())) {
-			// it's already after today's purge time, wait until tomorrow's
-			nextPurgeTime.add(GregorianCalendar.DATE, 1);
-		}
-		if (DEBUG)
-			log.info("next purge will be at "
-					+ nextPurgeTime.getTime());
-		long MILLISECONDS_IN_A_DAY = 24 * 60 * 60 * 1000;
-		purger = new Purger(this.purgeInterval);
-		timer.scheduleAtFixedRate(purger, nextPurgeTime.getTime(),
-				MILLISECONDS_IN_A_DAY);
-		instance = this;
+        if (nextPurgeTime.getTime().before(new Date())) {
+            // it's already after today's purge time, wait until tomorrow's
+            nextPurgeTime.add(GregorianCalendar.DATE, 1);
+        }
+        if (DEBUG)
+            log.info("next purge will be at " + nextPurgeTime.getTime());
+        long MILLISECONDS_IN_A_DAY = 24 * 60 * 60 * 1000;
+        purger = new Purger(this.purgeInterval);
+        timer.scheduleAtFixedRate(purger, nextPurgeTime.getTime(), MILLISECONDS_IN_A_DAY);
+        instance = this;
 
-	}
+    }
 
+    public void run() {
+        try {
+            synchronized (this) {
+                this.wait();
+            }
+        } catch (InterruptedException ie) {
+            log.info("JobPurger shutting down.");
+            timer.cancel();
+        }
+    }
 
+    public void runPurger() {
+        purger.run();
+    }
 
-	public void run() {
-		try {
-			synchronized (this) {
-				this.wait();
-			}
-		} catch (InterruptedException ie) {
-			log.error("JobPurger shutting down");
-			timer.cancel();
-		}
-	}
-
-	public void runPurger() {
-		purger.run();
-	}
-
-	public static void main(String[] args) {
-		log.info(new Date() + ": Starting JobPurger");
-		try {
-			JobPurger jobPurger = new JobPurger("7", "23:00");
-			jobPurger.runPurger();
-		} catch (Exception e) {
-			log.error(e);
-		}
-	}
+    public static void main(String[] args) {
+        log.info(new Date() + ": Starting JobPurger");
+        try {
+            JobPurger jobPurger = new JobPurger("7", "23:00");
+            jobPurger.runPurger();
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
 }
