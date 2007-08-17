df99bf7c8aab15f304a3c5683d4a0c828fd7c3e8
pcarr <pcarr@broadinstitute.org>
2007-08-17 14:04:02 +0000

    Extract interface from LocalAdminClient
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6299 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/webservice/server/local/IAdminClient.java
@@ -0,0 +1,59 @@
+package org.genepattern.server.webservice.server.local;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.genepattern.util.LSID;
+import org.genepattern.webservice.SuiteInfo;
+import org.genepattern.webservice.TaskInfo;
+import org.genepattern.webservice.WebServiceException;
+
+public interface IAdminClient {
+
+    public abstract List<String> getVersions(LSID lsid);
+
+    /**
+     * Gets all tasks in the database regardless of privacy or owner. Should only be called for a user with moduleAdmin
+     * priveleges.
+     * 
+     * @return All tasks in the database.
+     */
+    public abstract TaskInfo[] getAllTasksForModuleAdmin();
+
+    /**
+     * Gets all tasks in the database owned by the username passed to the constructor.
+     * 
+     * @return The tasks.
+     */
+    public abstract TaskInfo[] getTasksOwnedBy();
+
+    public abstract TreeMap<String, TaskInfo> getTaskCatalogByLSID(
+            Collection<TaskInfo> tasks);
+
+    public abstract TreeMap<String, TaskInfo> getTaskCatalogByLSID()
+            throws WebServiceException;
+
+    public abstract Collection<TaskInfo> getTaskCatalog()
+            throws WebServiceException;
+
+    public abstract Collection<TaskInfo> getLatestTasks()
+            throws WebServiceException;
+
+    /**
+     * return a map keyed by taskType with values being alphabetically sorted list of the tasks that are part of that
+     * type
+     */
+    public abstract Map<String, Collection<TaskInfo>> getLatestTasksByType()
+            throws WebServiceException;
+
+    public abstract TaskInfo getTask(String lsid) throws WebServiceException;
+
+    public abstract SuiteInfo getSuite(String lsid) throws WebServiceException;
+
+    public abstract SuiteInfo[] getAllSuites() throws WebServiceException;
+
+    public abstract String getUserProperty(String key);
+
+}
