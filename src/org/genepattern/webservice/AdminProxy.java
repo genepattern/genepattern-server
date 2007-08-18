0a8ac579fcead666a1745f5a8ca3f09d5a45fef5
pcarr <pcarr@broadinstitute.org>
2007-08-18 00:25:19 +0000

    Converted this class to implement IAdminClient.    There are a bunch of no-op methods which should be implemented.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6314 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/webservice/AdminProxy.java
+++ src/org/genepattern/webservice/AdminProxy.java
@@ -14,17 +14,24 @@ package org.genepattern.webservice;
 
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javax.activation.DataHandler;
 
 import org.apache.axis.client.Service;
 import org.apache.axis.configuration.BasicClientConfig;
+import org.genepattern.server.webservice.server.local.IAdminClient;
+import org.genepattern.util.LSID;
 
 /**
  * @author Joshua Gould
  */
-public class AdminProxy {
+public class AdminProxy implements IAdminClient {
     String endpoint = null;
 
     org.apache.axis.client.Service service = null;
@@ -83,9 +90,10 @@ public class AdminProxy {
         }
     }
 
-    public TaskInfo[] getLatestTasks() throws WebServiceException {
+    public Collection<TaskInfo> getLatestTasks() throws WebServiceException {
         try {
-            return proxy.getLatestTasks();
+            TaskInfo[] array = proxy.getLatestTasks();
+            return Arrays.asList(array);
         } catch (RemoteException re) {
             throw new WebServiceException(re);
         }
@@ -145,5 +153,53 @@ public class AdminProxy {
         } catch (RemoteException e) {
             throw new WebServiceException(e);
         }
+    }
+
+    public SuiteInfo[] getAllSuites() throws WebServiceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TaskInfo[] getAllTasksForModuleAdmin() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Map<String, Collection<TaskInfo>> getLatestTasksByType()
+            throws WebServiceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Collection<TaskInfo> getTaskCatalog() throws WebServiceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TreeMap<String, TaskInfo> getTaskCatalogByLSID(
+            Collection<TaskInfo> tasks) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TreeMap<String, TaskInfo> getTaskCatalogByLSID()
+            throws WebServiceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TaskInfo[] getTasksOwnedBy() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getUserProperty(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<String> getVersions(LSID lsid) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
