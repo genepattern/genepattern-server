3a62b7b51270ddab9eb974344abe6aa8855b60a7
liefeld <liefeld@broadinstitute.org>
2007-09-05 17:03:19 +0000

    updated to bring everything in house
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6398 48a7a3f7-ad46-0410-b969-29950918272e

--- src/CreateOracleLock.java
+++ src/CreateOracleLock.java
@@ -2,19 +2,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 import org.apache.log4j.Logger;
 import org.genepattern.server.AnalysisManager;
 import org.genepattern.server.AnalysisTask;
 import org.genepattern.server.database.HibernateUtil;
+import org.genepattern.server.domain.AnalysisJob;
+import org.genepattern.server.domain.AnalysisJobDAO;
+import org.genepattern.server.domain.JobStatus;
 import org.genepattern.server.genepattern.GenePatternAnalysisTask;
+import org.genepattern.webservice.JobInfo;
+import org.genepattern.webservice.OmnigeneException;
+import org.hibernate.Query;
 
 import java.util.Vector;
 
 
 public class CreateOracleLock {
 	static Logger log = Logger.getLogger(CreateOracleLock.class);
-
+	private Object jobQueueWaitObject = new Object();
+	private Vector jobQueue = new Vector();
+	   
+	
 	/**
 	 * @param args
 	 */
@@ -32,14 +42,86 @@ public class CreateOracleLock {
 //         HibernateUtil.commitTransaction();
 //         log.debug("After commit");
 		
-		AnalysisTask t = new AnalysisTask(20);
-		t.run();
+		AnalysisTask at = new AnalysisTask(20);
+		at.run();
+		
+		
+		CreateOracleLock t = new CreateOracleLock();
+		t.generateLock();
 		
 		System.out.println("Done");
 	}
 
 	
+	 private AnalysisJobDAO dao = new AnalysisJobDAO();
+		
+	    public void updateJobStatusToProcessing(Vector<JobInfo> jobs){
+	    	int maxJobCount = 20;
+	    	int i=0;
+	    	
+	    	JobStatus newStatus = (JobStatus) HibernateUtil.getSession().get(JobStatus.class, JobStatus.JOB_PROCESSING);
+	    	Iterator iter = jobs.iterator();
+	        while (iter.hasNext() && i++ <= maxJobCount) {
+	            JobInfo jobby = (JobInfo) iter.next();
+	            
+	            AnalysisJob aJob = dao.findById(jobby.getJobNumber());
+	            aJob.setStatus(newStatus);
+	        }
+	    }
 	
+	    public Vector getWaitingJobs() throws OmnigeneException {
+	        Vector jobVector = new Vector();
+
+	        String hql = "from org.genepattern.server.domain.AnalysisJob "
+	                + " where jobStatus.statusId = :statusId order by submittedDate ";
+	        Query query = HibernateUtil.getSession().createQuery(hql);
+	        query.setInteger("statusId", JobStatus.JOB_PENDING);
+
+	        List results = query.list();
+	        Iterator iter = results.iterator();
+	        
+	        while (iter.hasNext()) {
+	            AnalysisJob aJob = (AnalysisJob) iter.next();
+	            jobVector.add(new JobInfo(aJob));
+	        }
+
+	        return jobVector;
+
+	    }
+	    
+	   public void generateLock() {
+	        log.debug("Starting AnalysisTask thread");
+	        int waitTime = Integer.parseInt(System.getProperty(
+	                "AnalysisTaskQueuePollingFrequency", "0"));
+	        
+	        // Load input data to input queue
+	        synchronized (jobQueueWaitObject) {
+
+	        	// Fetch another batch of jobs.
+	        	try {
+	        		HibernateUtil.beginTransaction();
+
+	        		jobQueue =  this.getWaitingJobs();
+	        		log.debug("Here " + jobQueue.size());
+	        		
+	        		
+	        		this.updateJobStatusToProcessing(jobQueue);
+	        		
+	        		//   this.getWaitingJobs_LOCKSUP(NUM_THREADS);
+	        		
+	        		HibernateUtil.commitTransaction();
+
+	        		
+	        	} catch(RuntimeException e) {
+	        		HibernateUtil.rollbackTransaction();
+	        		log.error("Error getting waiting jobs" , e);
+	        	}
+	            
+	            
+	        }    
+	    }
+	   
+	   
     protected static void loadProperties() throws Exception {
     	File propFile = null;
     	FileInputStream fis = null;
