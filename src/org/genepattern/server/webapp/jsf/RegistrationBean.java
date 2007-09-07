4d94b1176ed2218e95d394e9731e8561c895f0e5
liefeld <liefeld@broadinstitute.org>
2007-09-07 13:45:04 +0000

    bug 1851, added register users checkbox on user registration page
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6412 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/RegistrationBean.java
+++ src/org/genepattern/server/webapp/jsf/RegistrationBean.java
@@ -4,6 +4,8 @@
 package org.genepattern.server.webapp.jsf;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.faces.FacesException;
 import javax.faces.application.FacesMessage;
@@ -33,7 +35,8 @@ public class RegistrationBean {
     private String emailConfirm;
     private UIInput passwordConfirmComponent;
     private UIInput emailConfirmComponent;
-
+    private boolean joinMailingList;
+    
     public RegistrationBean() {
 	String createAccountAllowedProp = System.getProperty("create.account.allowed", "true").toLowerCase();
 	boolean createAccountAllowed = (createAccountAllowedProp.equals("true") || createAccountAllowedProp.equals("y") || createAccountAllowedProp
@@ -73,7 +76,15 @@ public class RegistrationBean {
 	return username;
     }
 
-    /**
+    public boolean isJoinMailingList() {
+		return joinMailingList;
+	}
+
+	public void setJoinMailingList(boolean joinMailingList) {
+		this.joinMailingList = joinMailingList;
+	}
+
+	/**
      * Register a new user. For now this uses an action listener since we are redirecting to a page outside of the JSF
      * framework. This should be changed to an action to use jsf navigation in the future.
      * 
@@ -92,6 +103,11 @@ public class RegistrationBean {
 	    newUser.setPassword(EncryptionUtil.encrypt(password));
 	    (new UserDAO()).save(newUser);
 	    UIBeanHelper.login(username, true);
+	    
+	    if (this.isJoinMailingList()){
+	    	sendJoinMailingListRequest();
+	    }
+	    
 	} catch (Exception e) {
 	    log.error(e);
 	    throw new RuntimeException(e);
@@ -99,6 +115,34 @@ public class RegistrationBean {
 
     }
 
+    public void sendJoinMailingListRequest(){
+    	String mailingListURL = System.getProperty("gp.mailinglist.registration.url","http://www.broad.mit.edu/cgi-bin/cancer/software/genepattern/gp_mail_list.cgi");
+    	StringBuffer buff = new StringBuffer(mailingListURL);
+        buff.append("?choice=Add&email="+ this.getEmail());	
+    	
+        try {
+            URL regUrl = new URL(buff.toString());
+            regUrl.openConnection();
+            
+            HttpURLConnection conn = (HttpURLConnection)regUrl.openConnection();
+            conn.getContent();
+            int response = conn.getResponseCode();
+            if (response < 300) {
+            	UIBeanHelper.setInfoMessage("You have successfully been registered in the GenePattern users mailing list.");
+            } else{
+            	UIBeanHelper.setInfoMessage("Automatic registration in the GenePattern mailing list failed. You can do this manually from the Resources/Mailing list menu.");
+            }
+            
+        } catch (Exception e){
+         // do nothing.  we loose one registration   
+            e.printStackTrace();
+          
+        }
+    	
+    	
+    }
+    
+    
     public void setEmail(String email) {
 	this.email = email;
     }
