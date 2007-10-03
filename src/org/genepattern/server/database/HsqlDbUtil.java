31f8e2b9d7afff5b6e01df352fcf07d5506bd913
liefeld <liefeld@broadinstitute.org>
2007-10-03 10:33:29 +0000

    updated copyright notice to 2008 and added it if missing
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6563 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/database/HsqlDbUtil.java
+++ src/org/genepattern/server/database/HsqlDbUtil.java
@@ -1,3 +1,15 @@
+/*
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2008) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
+ 
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
+
 package org.genepattern.server.database;
 
 import java.io.File;
