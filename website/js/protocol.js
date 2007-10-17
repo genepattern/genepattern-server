ffe11f392a660242a9eedb1587d4047406c18706
pcarr <pcarr@broadinstitute.org>
2007-10-17 16:52:29 +0000

    Finished adding links from the protocals page to the genepattern server.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6651 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/protocol.js
+++ website/js/protocol.js
@@ -1,26 +1,36 @@
-var modulePrefix='urn:lsid:broad.mit.edu:cancer.software.genepattern.module.';
+var lsidPrefix='urn:lsid:broad.mit.edu:cancer.software.genepattern.module.';
 
+//link to a protocal from the splash page
 function openProtocolWindow(theURL) {
   w = window.open(theURL,'protocol','scrollbars=yes,resizable=yes,width=500,height=350');
   w.focus();
 }
 
-//function openGenePatternLink(theURL) {
-//  w = window.open(theURL,'genepattern','scrollbars=yes,resizable=yes,width=500,height=350');
-//  w.focus();
-//}
-
+//link to a module from the protocol page
 function openModuleById(id) {
-  //alert('opening: '+theURL);
-  theURL = '/gp/pages/index.jsf?lsid='+modulePrefix+''+id;
+  openModuleByLsid(lsidPrefix+''+id);
   window.opener.location = theURL;
   window.opener.focus();
 }
 
+function openModuleByLsid(lsid) {
+  theURL = '/gp/pages/index.jsf?lsid='+lsid;
+  window.opener.location = theURL;
+  window.opener.focus();
+}
+
+//link to module documentation from the protocol page
 function openModuleDocById(id) {
-  theURL = '/gp/getTaskDoc.jsp?name='+modulePrefix+''+id;
+  openModuleDocByLsid(lsidPrefix+''+id);
+}
+
+function openModuleDocByLsid(lsid) {
+  theURL = '/gp/getTaskDoc.jsp?name='+lsid;
   window.location = theURL;
 }
+
+
+
 
 
 
