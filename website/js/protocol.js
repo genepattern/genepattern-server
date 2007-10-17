66869caea09742bc226da2ed2035d5a78bc37d84
pcarr <pcarr@broadinstitute.org>
2007-10-17 23:24:22 +0000

    1. Fixed rendering problem with IE on WinXP 2. Replaced javascript generated links to the documentation files (PDFs) with hard links.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6654 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/protocol.js
+++ website/js/protocol.js
@@ -1,16 +1,12 @@
 var lsidPrefix='urn:lsid:broad.mit.edu:cancer.software.genepattern.module.';
 
-//link to a protocal from the splash page
 function openProtocolWindow(theURL) {
   w = window.open(theURL,'protocol','scrollbars=yes,resizable=yes,width=500,height=350');
   w.focus();
 }
 
-//link to a module from the protocol page
-function openModuleById(id) {
-  openModuleByLsid(lsidPrefix+''+id);
-  window.opener.location = theURL;
-  window.opener.focus();
+function openModuleById(theId) {
+  openModuleByLsid(lsidPrefix+''+theId);
 }
 
 function openModuleByLsid(lsid) {
@@ -18,20 +14,3 @@ function openModuleByLsid(lsid) {
   window.opener.location = theURL;
   window.opener.focus();
 }
-
-//link to module documentation from the protocol page
-function openModuleDocById(id) {
-  openModuleDocByLsid(lsidPrefix+''+id);
-}
-
-function openModuleDocByLsid(lsid) {
-  theURL = '/gp/getTaskDoc.jsp?name='+lsid;
-  window.location = theURL;
-}
-
-
-
-
-
-
-
