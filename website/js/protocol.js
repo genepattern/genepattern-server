a81f24be9a5e3789ecb6d16b39c9a9075e065cc1
pcarr <pcarr@broadinstitute.org>
2007-10-25 00:28:01 +0000

    replace openModuleById with openModuleByName
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6753 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/protocol.js
+++ website/js/protocol.js
@@ -1,18 +1,25 @@
-var lsidPrefix='urn:lsid:broad.mit.edu:cancer.software.genepattern.module.';
+//var lsidPrefix='urn:lsid:broad.mit.edu:cancer.software.genepattern.module.';
 
 function openProtocolWindow(theURL) {
   w = window.open(theURL,'protocol','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=500,height=350');
   w.focus();
 }
 
-function openModuleById(theId) {
-  openModuleByLsid(lsidPrefix+''+theId);
+function openModuleByName(theName) {
+  theURL = '/gp/pages/index.jsf?lsid='+theName;
+  openURL(theURL);
 }
 
-function openModuleByLsid(lsid) {
-  theURL = '/gp/pages/index.jsf?lsid='+lsid;
-  window.opener.location = theURL;
-  window.opener.focus();
+function openURL(theURL) {
+  w = window.opener;
+  if (w != null) {
+    w.location = theURL;
+  }
+  else {
+    w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600');
+  }
+  w.focus();
+  w.select();
 }
 
 function setInputFile(cb_name, filename_name, url) {
