9b6ebd2f34527c54ece9a7fd793e61c2fd88a157
pcarr <pcarr@broadinstitute.org>
2007-10-30 21:25:15 +0000

    Create link from protocol to genepattern window which opens a module with example data.     (uses url rather than javascript)
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6817 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/protocol.js
+++ website/js/protocol.js
@@ -2,6 +2,17 @@
 //    'genepattern' - the main GenePattern window
 //    'protocol' - the new window which gets created when linking to a protocol from the main GenePattern window
 
+//--------------------
+function examplePreprocessDataset() {
+  theURL = '/gp/pages/index.jsf?lsid='+'PreprocessDataset';
+  w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600');
+  w.setTimeout(0);
+  w.document.forms['taskForm'].elements['input.filename_cb_url'].click();
+  w.document.forms['taskForm'].elements['input.filename_url'].value = 'ftp://ftp.broad.mit.edu/pub/genepattern/datasets/all_aml/all_aml_train.gct';
+}
+//--------------------
+
+
 function openProtocolWindow(theURL) {
   if (window.name != 'protocol') {
     window.name = 'genepattern';
@@ -10,9 +21,25 @@ function openProtocolWindow(theURL) {
   w.focus();
 }
 
+function openModuleWindow(theURL) {
+	w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600');
+	return w;
+}
+
+
 function openModuleByName(theName) {
   theURL = '/gp/pages/index.jsf?lsid='+theName;
   openURL(theURL);
+}
+
+function openModuleByNameParams(theName, key, url) {
+  theURL = '/gp/pages/index.jsf?lsid='+theName;
+  w = openURL(theURL);
+  //setInputFile(key+'_cb_url', key+'_url', url);
+  var cb_name = key+'_cb_url';
+  var filename_name = key+'_url';
+  w.document.forms['taskForm'].elements[cb_name].click();
+  w.document.forms['taskForm'].elements[filename_name].value = url;
 }
 
 function openURL(theURL) {
@@ -25,6 +52,7 @@ function openURL(theURL) {
   }
   w.focus();
   w.select();
+  return w;
 }
 
 function setInputFile(cb_name, filename_name, url) {
