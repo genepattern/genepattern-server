517fd08e6d40042cd16508944861ed6e92e0e092
pcarr <pcarr@broadinstitute.org>
2007-11-01 22:28:58 +0000

    For IE compatibility.    set event.returnValue = false; when opening links into a new window.
       This has the same effect as 'return false;' from the onclick handler.
    
    
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6849 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/protocol.js
+++ website/js/protocol.js
@@ -3,6 +3,7 @@
 //    'protocol' - the new window which gets created when linking to a protocol from the main GenePattern window
 
 function openProtocolWindow(theURL) {
+  event.returnValue = false; //for IE
   if (window.name != 'protocol') {
     window.name = 'genepattern';
   }
@@ -11,7 +12,8 @@ function openProtocolWindow(theURL) {
 }
 
 function openModuleWindow(theURL) {
-	w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600');
-	w.focus();
-	return w;
+  event.returnValue = false; //for IE
+  w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600');
+  w.focus();
+  return w;
 }
