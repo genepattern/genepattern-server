2692c37ff7084675c854ef972f8025e6400e64f9
pcarr <pcarr@broadinstitute.org>
2007-11-02 16:32:46 +0000

    Fixed openProtocolWindow link.    Make sure links to the protocols page open in a new window.    Tested on Mac OS X (Safari, Firefox 2) and Vista (IE7, Firefox 2)
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6868 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/protocol.js
+++ website/js/protocol.js
@@ -2,17 +2,21 @@
 //    'genepattern' - the main GenePattern window
 //    'protocol' - the new window which gets created when linking to a protocol from the main GenePattern window
 
-function openProtocolWindow(theURL) {
-  event.returnValue = false; //for IE
+function openProtocolWindow(theTarget, theEvent) {
+  var theURL = theTarget.href;
+  if (theEvent != null) { //for IE
+    theEvent.returnValue = false;
+  }
   if (window.name != 'protocol') {
     window.name = 'genepattern';
   }
-  w = window.open(theURL,'protocol','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=500,height=350');
+  w = window.open(theURL,'protocol','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=500,height=400');
   w.focus();
+  return false;
 }
 
 function openModuleWindow(theURL) {
-  event.returnValue = false; //for IE
+  //event.returnValue = false; //for IE
   w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600');
   w.focus();
   return w;
