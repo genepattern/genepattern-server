52e1e910cdcb5c2d8f41ecb2250e6629cb2032f9
pcarr <pcarr@broadinstitute.org>
2007-11-06 00:46:19 +0000

    added a table of contents page (protocols.html) for the protocols.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@6880 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/protocol.js
+++ website/js/protocol.js
@@ -10,7 +10,7 @@ function openProtocolWindow(theTarget, theEvent) {
   if (window.name != 'protocol') {
     window.name = 'genepattern';
   }
-  w = window.open(theURL,'protocol','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=500,height=400');
+  w = window.open(theURL,'protocol','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=500,height=400',false);
   w.focus();
   return false;
 }
@@ -20,7 +20,7 @@ function openModuleWindow(theTarget, theEvent) {
   if (theEvent != null) { //for IE
     theEvent.returnValue = false;
   }
-  w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600');
+  w = window.open(theURL,'genepattern','toolbar=1,menubar=1,scrollbars=1,resizable=1,width=800,height=600',false);
   w.focus();
   return false;
 }
