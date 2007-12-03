51391ca978c7db95face9f47adcff2aeadae2496
jgould <jgould@broadinstitute.org>
2007-12-03 18:03:20 +0000

    added comment to panelStates variable
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@7078 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/CollapsiblePanelState.java
+++ src/org/genepattern/server/webapp/jsf/CollapsiblePanelState.java
@@ -20,58 +20,57 @@ import java.util.Map;
 
 /**
  * @author jrobinso
- *
+ * 
  */
 public class CollapsiblePanelState {
+    /** Maps panel id to state [closed, open] */
     private Map<String, String> panelStates = new HashMap<String, String>();
 
     private String selectedMode;
 
     public String getSelectedMode() {
-        return selectedMode;
+	return selectedMode;
     }
 
     /**
-     * This really should just be an action method, but the ajax servlet is
-     * configured to get properties from beans, not call action methods. So the
-     * method has to follow java bean property conventions (get...).
-     *
+     * This really should just be an action method, but the ajax servlet is configured to get properties from beans, not
+     * call action methods. So the method has to follow java bean property conventions (get...).
+     * 
      */
     public String getUpdateChooserMode() {
-        Map parameters = UIBeanHelper.getRequest().getParameterMap();
-        String[] mode = (String[]) parameters.get("mode");
+	Map parameters = UIBeanHelper.getRequest().getParameterMap();
+	String[] mode = (String[]) parameters.get("mode");
 
-        if (mode != null) {
-            setSelectedMode(mode[0]);
-        }
-        return "";
+	if (mode != null) {
+	    setSelectedMode(mode[0]);
+	}
+	return "";
     }
 
     /**
-     * This really should just be an action method, but the ajax servlet is
-     * configured to get properties from beans, not call action methods. So the
-     * method has to follow java bean property conventions (get...).
-     *
+     * This really should just be an action method, but the ajax servlet is configured to get properties from beans, not
+     * call action methods. So the method has to follow java bean property conventions (get...).
+     * 
      */
     public String getUpdatePanelState() {
-        Map parameters = UIBeanHelper.getRequest().getParameterMap();
-        String[] panelId = (String[]) parameters.get("id");
-        String[] state = (String[]) parameters.get("state");
+	Map parameters = UIBeanHelper.getRequest().getParameterMap();
+	String[] panelId = (String[]) parameters.get("id");
+	String[] state = (String[]) parameters.get("state");
 
-        if (panelId != null && state != null) {
-            panelStates.put(panelId[0], state[0]);
-        }
-        return "";
+	if (panelId != null && state != null) {
+	    panelStates.put(panelId[0], state[0]);
+	}
+	return "";
     }
 
     public boolean isClosed(String panelId) {
-        String panelState = (String) panelStates.get(panelId);
-        return panelState != null && panelState.equals("closed");
+	String panelState = panelStates.get(panelId);
+	return panelState != null && panelState.equals("closed");
 
     }
 
     public void setSelectedMode(String selectedMode) {
-        this.selectedMode = selectedMode;
+	this.selectedMode = selectedMode;
     }
 
 }
