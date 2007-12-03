f65cbc00f9369a22805da7b9018a9db3c148a5bc
jgould <jgould@broadinstitute.org>
2007-12-03 22:44:35 +0000

    Fixed deleting suites via delete link
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@7081 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/ManageSuiteBean.java
+++ src/org/genepattern/server/webapp/jsf/ManageSuiteBean.java
@@ -27,6 +27,8 @@ import javax.faces.event.ActionEvent;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Delete;
 import org.genepattern.server.database.HibernateUtil;
 import org.genepattern.server.domain.Suite;
 import org.genepattern.server.domain.SuiteDAO;
@@ -56,230 +58,234 @@ public class ManageSuiteBean {
     public ManageSuiteBean() {
     }
 
-    public List<Suite> getSuites() {
-        if (suites == null) {
-            resetSuites();
-        }
-        return suites;
-    }
-
     /**
-     * Query for suites the current user is authorized to see.
-     *
+     * Delete the selected suites.
+     * 
+     * @return
      */
-    private void resetSuites() {
-        if (AuthorizationHelper.adminSuites()) {
-            suites = (new SuiteDAO()).findAll();
-        } else {
-            suites = (new SuiteDAO()).findByOwnerOrPublic(UIBeanHelper.getUserId());
-        }
-
-        editPermissions = new HashMap<String, Boolean>();
-        for (Suite s : suites) {
-            boolean canEdit = s.getUserId().equals(UIBeanHelper.getUserId())
-                    && LSIDUtil.getInstance().isAuthorityMine(s.getLsid());
-            editPermissions.put(s.getLsid(), canEdit);
-
-        }
+    public String delete() {
+	String[] selectedSuites = UIBeanHelper.getRequest().getParameterValues("selectedSuites");
+	deleteSuites(selectedSuites);
+	return "delete suite";
     }
 
-    public Map getEditPermissions() {
-        return editPermissions;
+    public void delete(ActionEvent event) {
+	String[] lsids = UIBeanHelper.getRequest().getParameterValues("selectedVersions");
+	deleteSuites(lsids);
     }
 
-    public Suite getCurrentSuite() {
-        return currentSuite;
+    public void deleteSupportFile(ActionEvent event) {
+	if (currentSuite != null) {
+	    String key = UIBeanHelper.getRequest().getParameter("supportFileKey");
+	    String[] supportFiles = getSupportFiles();
+	    if (supportFiles != null) {
+		for (String f : supportFiles) {
+		    if (f.equals(key)) {
+			try {
+			    String suiteDirPath = DirectoryManager.getSuiteLibDir(currentSuite.getName(), currentSuite
+				    .getLsid(), currentSuite.getUserId());
+			    new File(suiteDirPath, f).delete();
+
+			} catch (Exception e) {
+			    log.error(e);
+			}
+			break;
+
+		    }
+		}
+	    }
+	}
     }
 
-    public boolean isCurrentSuiteSet() {
-        return (currentSuite != null);
-    }
-
-    public void setCurrentSuite(Suite currentSuite) {
-        this.currentSuite = currentSuite;
-    }
-
-    public List<List<ModuleCategory>> getCategoryColumnsForSuite() {
-        if (currentSuite != null) {
-            if (categories == null) {
-                categories = (new ModuleHelper()).getTasksByTypeForSuite(currentSuite);
-            }
-        }
-        return CreateSuiteBean.layoutSuiteCategories(categories);
-    }
-
-    public String[] getSupportFiles() {
-        if (currentSuite != null) {
-            try {
-                String suiteDirPath = DirectoryManager.getSuiteLibDir(currentSuite.getName(), currentSuite.getLsid(),
-                        currentSuite.getUserId());
-                File suiteDir = new File(suiteDirPath);
-                return suiteDir.list();
-            } catch (Exception e) {
-                log.error(e);
-                HibernateUtil.rollbackTransaction();
-            }
-        }
-        return null;
-    }
-
-    public String view() {
-        String lsid = UIBeanHelper.getRequest().getParameter("lsid");
-        currentSuite = (new SuiteDAO()).findById(lsid);
-        return (currentSuite != null) ? "view suite" : "failure";
+    public String edit() {
+	loadCurrentSuite();
+	return "edit suite";
     }
 
     /**
      * @param event
      */
     public void exportExDependents(ActionEvent event) {
-        ZipSuite zs = new ZipSuite();
-        export(zs);
+	ZipSuite zs = new ZipSuite();
+	export(zs);
     }
 
     public String exportInDependents() {
-        ZipSuite zs = new ZipSuiteWithDependents();
-        return export(zs);
+	ZipSuite zs = new ZipSuiteWithDependents();
+	return export(zs);
     }
 
-    private String export(ZipSuite zs) {
-        String lsid = UIBeanHelper.getRequest().getParameter("lsid");
-        if (lsid == null || lsid.equals("null") || lsid.length() == 0)
-            return "";
-
-        String userID = UIBeanHelper.getUserId();
-        if (userID == null)
-            return ""; // come back after login
-
-        FacesContext facesContext = UIBeanHelper.getFacesContext();
-        try {
-            File zipFile = zs.packageSuite(lsid, userID);
-
-            HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
-
-            IAdminClient adminClient = new LocalAdminClient(userID);
-            SuiteInfo si = adminClient.getSuite(lsid);
-            String contentType = "application/x-zip-compressed" + "; name=\"" + si.getName() + ".zip" + "\";";
-            response.addHeader("Content-Disposition", "attachment; filename=\"" + si.getName() + ".zip" + "\";");
-            response.setContentLength((int) zipFile.length());
-            response.setContentType(contentType);
-            OutputStream out = response.getOutputStream();
-
-            // Copy the contents of the file to the output stream
-            byte[] buf = new byte[1024];
-            int count = 0;
-            FileInputStream in = new FileInputStream(zipFile);
-            while ((count = in.read(buf)) >= 0) {
-                out.write(buf, 0, count);
-            }
-            in.close();
-            out.flush();
-            out.close();
-
-            zipFile.delete();
-            facesContext.responseComplete();
-
-        } catch (MissingTaskException e) {
-            TaskCatalogBean taskCatalogBean = (TaskCatalogBean) UIBeanHelper.getManagedBean("#{taskCatalogBean}");
-            taskCatalogBean.refilter(e.getMissingLsids());
-            return "task catalog";
-        } catch (Exception e) {
-            log.error(e);
-            throw new RuntimeException(e);
-        }
-        return "success";
+    public List<List<ModuleCategory>> getCategoryColumnsForSuite() {
+	if (currentSuite != null) {
+	    if (categories == null) {
+		categories = (new ModuleHelper()).getTasksByTypeForSuite(currentSuite);
+	    }
+	}
+	return CreateSuiteBean.layoutSuiteCategories(categories);
     }
 
-    public String edit() {
-        loadCurrentSuite();
-        return "edit suite";
+    public Suite getCurrentSuite() {
+	return currentSuite;
     }
 
-    private void loadCurrentSuite() {
-        if (currentSuite == null) {
-            String lsid = UIBeanHelper.getRequest().getParameter("lsid");
-            currentSuite = (new SuiteDAO()).findById(lsid);
-        }
+    public Map<String, Boolean> getEditPermissions() {
+	return editPermissions;
     }
 
-    /**
-     * Delete the selected jobs and files.
-     *
-     * @return
-     */
-    public String delete() {
-        String[] selectedSuites = UIBeanHelper.getRequest().getParameterValues("selectedSuites");
-        deleteSuites(selectedSuites);
-        return "delete suite";
+    public boolean getIncludeDependents() {
+	return includeDependents;
+    }
+
+    public List<Suite> getSuites() {
+	if (suites == null) {
+	    resetSuites();
+	}
+	return suites;
+    }
+
+    public String[] getSupportFiles() {
+	if (currentSuite != null) {
+	    try {
+		String suiteDirPath = DirectoryManager.getSuiteLibDir(currentSuite.getName(), currentSuite.getLsid(),
+			currentSuite.getUserId());
+		File suiteDir = new File(suiteDirPath);
+		return suiteDir.list();
+	    } catch (Exception e) {
+		log.error(e);
+		HibernateUtil.rollbackTransaction();
+	    }
+	}
+	return null;
+    }
+
+    public boolean isCurrentSuiteSet() {
+	return (currentSuite != null);
+    }
+
+    public void setCurrentSuite(Suite currentSuite) {
+	this.currentSuite = currentSuite;
+    }
+
+    public void setIncludeDependents(boolean includeDependents) {
+	this.includeDependents = includeDependents;
+    }
+
+    public String view() {
+	String lsid = UIBeanHelper.getRequest().getParameter("lsid");
+	currentSuite = (new SuiteDAO()).findById(lsid);
+	return (currentSuite != null) ? "view suite" : "failure";
+    }
+
+    public String viewTaskProperty() {
+	String[] selectedSuites = UIBeanHelper.getRequest().getParameterValues("selectedSuites");
+	deleteSuites(selectedSuites);
+	return "delete suite";
     }
 
     /**
      * @param suiteLsids
      */
     private void deleteSuites(String[] suiteLsids) {
-        String user = UIBeanHelper.getUserId();
-        boolean admin = AuthorizationHelper.adminSuites();
-        if (suiteLsids != null) {
-            for (String lsid : suiteLsids) {
-                Suite s = (Suite) HibernateUtil.getSession().get(org.genepattern.server.domain.Suite.class, lsid);
-                if (s.getUserId() == null || s.getUserId().equals(user) || admin) {
-                    (new SuiteDAO()).delete(s);
-                    // Delete supporting files
-                    try {
-                        String suiteDirPath = DirectoryManager.getSuiteLibDir(s.getName(), s.getLsid(), s.getUserId());
-                        File suiteDir = new File(suiteDirPath);
-                        File[] allFiles = suiteDir.listFiles();
-                        for (File file : allFiles) {
-                            file.delete();
-                        }
-                        suiteDir.delete();
-                    } catch (Exception e) {
-                        HibernateUtil.rollbackTransaction();
-                        log.error(e);
-                        throw new RuntimeException(e);
-                    }
-                    // end of deleting supporting files
-                }
-            }
-            resetSuites();
-        }
+	String user = UIBeanHelper.getUserId();
+	boolean admin = AuthorizationHelper.adminSuites();
+	if (suiteLsids != null) {
+	    for (String lsid : suiteLsids) {
+		Suite s = (Suite) HibernateUtil.getSession().get(org.genepattern.server.domain.Suite.class, lsid);
+		if (s.getUserId() == null || s.getUserId().equals(user) || admin) {
+		    (new SuiteDAO()).delete(s);
+		    // Delete supporting files
+		    String suiteDirPath = null;
+		    try {
+			suiteDirPath = DirectoryManager.getSuiteLibDir(s.getName(), s.getLsid(), s.getUserId());
+		    } catch (Exception e) {
+			log.error("Error", e);
+			return;
+		    }
+		    Delete del = new Delete();
+		    del.setDir(new File(suiteDirPath));
+		    del.setIncludeEmptyDirs(true);
+		    del.setProject(new Project());
+		    del.execute();
+
+		}
+	    }
+	    resetSuites();
+	}
     }
 
-    public void deleteSupportFile(ActionEvent event) {
-        if (currentSuite != null) {
-            String key = UIBeanHelper.getRequest().getParameter("supportFileKey");
-            String[] supportFiles = getSupportFiles();
-            if (supportFiles != null) {
-                for (String f : supportFiles) {
-                    if (f.equals(key)) {
-                        try {
-                            String suiteDirPath = DirectoryManager.getSuiteLibDir(currentSuite.getName(), currentSuite
-                                    .getLsid(), currentSuite.getUserId());
-                            new File(suiteDirPath, f).delete();
+    private String export(ZipSuite zs) {
+	String lsid = UIBeanHelper.getRequest().getParameter("lsid");
+	if (lsid == null || lsid.equals("null") || lsid.length() == 0)
+	    return "";
 
-                        } catch (Exception e) {
-                            log.error(e);
-                        }
-                        break;
+	String userID = UIBeanHelper.getUserId();
+	if (userID == null)
+	    return ""; // come back after login
 
-                    }
-                }
-            }
-        }
+	FacesContext facesContext = UIBeanHelper.getFacesContext();
+	try {
+	    File zipFile = zs.packageSuite(lsid, userID);
+
+	    HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+
+	    IAdminClient adminClient = new LocalAdminClient(userID);
+	    SuiteInfo si = adminClient.getSuite(lsid);
+	    String contentType = "application/x-zip-compressed" + "; name=\"" + si.getName() + ".zip" + "\";";
+	    response.addHeader("Content-Disposition", "attachment; filename=\"" + si.getName() + ".zip" + "\";");
+	    response.setContentLength((int) zipFile.length());
+	    response.setContentType(contentType);
+	    OutputStream out = response.getOutputStream();
+
+	    // Copy the contents of the file to the output stream
+	    byte[] buf = new byte[1024];
+	    int count = 0;
+	    FileInputStream in = new FileInputStream(zipFile);
+	    while ((count = in.read(buf)) >= 0) {
+		out.write(buf, 0, count);
+	    }
+	    in.close();
+	    out.flush();
+	    out.close();
+
+	    zipFile.delete();
+	    facesContext.responseComplete();
+
+	} catch (MissingTaskException e) {
+	    TaskCatalogBean taskCatalogBean = (TaskCatalogBean) UIBeanHelper.getManagedBean("#{taskCatalogBean}");
+	    taskCatalogBean.refilter(e.getMissingLsids());
+	    return "task catalog";
+	} catch (Exception e) {
+	    log.error(e);
+	    throw new RuntimeException(e);
+	}
+	return "success";
     }
 
-    public boolean getIncludeDependents() {
-        return includeDependents;
+    private void loadCurrentSuite() {
+	if (currentSuite == null) {
+	    String lsid = UIBeanHelper.getRequest().getParameter("lsid");
+	    currentSuite = (new SuiteDAO()).findById(lsid);
+	}
     }
 
-    public void setIncludeDependents(boolean includeDependents) {
-        this.includeDependents = includeDependents;
-    }
+    /**
+     * Query for suites the current user is authorized to see.
+     * 
+     */
+    private void resetSuites() {
+	if (AuthorizationHelper.adminSuites()) {
+	    suites = (new SuiteDAO()).findAll();
+	} else {
+	    suites = (new SuiteDAO()).findByOwnerOrPublic(UIBeanHelper.getUserId());
+	}
 
-    public String viewTaskProperty() {
-        String[] selectedSuites = UIBeanHelper.getRequest().getParameterValues("selectedSuites");
-        deleteSuites(selectedSuites);
-        return "delete suite";
+	editPermissions = new HashMap<String, Boolean>();
+	for (Suite s : suites) {
+	    boolean canEdit = s.getUserId().equals(UIBeanHelper.getUserId())
+		    && LSIDUtil.getInstance().isAuthorityMine(s.getLsid());
+	    editPermissions.put(s.getLsid(), canEdit);
+
+	}
     }
 
 }
