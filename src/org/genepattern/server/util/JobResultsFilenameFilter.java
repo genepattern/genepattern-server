a650ed141fb0ac3241172acf4e602b1ca250410e
pcarr <pcarr@broadinstitute.org>
2008-05-19 17:13:39 +0000

    GP-2258 - Option to hide certain files from jobResults using a glob pattern set in genepattern.properties
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/v3_1_0@7690 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/util/JobResultsFilenameFilter.java
@@ -0,0 +1,68 @@
+package org.genepattern.server.util;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.apache.oro.io.GlobFilenameFilter;
+
+/**
+ * Filter out files from the jobResults directory based on 
+ * a list of filenames to match exactly and/or 
+ * a pattern specified using GLOB syntax.
+ * 
+ * <p>
+ * Some examples, 
+ * <pre>
+   jobs.FilenameFilter=.*
+   jobs.FilenameFilter=.nfs*
+ * </pre>
+ * 
+ * @author pcarr
+ */
+public class JobResultsFilenameFilter implements FilenameFilter {
+    public static final String KEY = "jobs.FilenameFilter";
+    
+    private Set<String> exactMatches = new TreeSet<String>();
+    private GlobFilenameFilter glob = null;
+    
+    public void addExactMatch(String filename) {
+        exactMatches.add(filename);
+    }
+    
+    public void clearExactMatches() {
+        exactMatches.clear();
+    }
+
+    /**
+     * Null or empty input means don't use glob pattern.
+     * @param pattern
+     */
+    public void setGlob(String pattern) {
+        //null or empty string means ignore glob pattern
+        if (pattern == null || pattern.trim().length() == 0) {
+            glob = null;
+            return;
+        }
+        //init if necessary
+        if (glob == null) {
+            glob =  new GlobFilenameFilter();            
+        }
+        glob.setFilterExpression(pattern);
+    }
+
+    //implement FilenameFilter using GlobFilenameFilter
+    public boolean accept(File dir, String name) {
+        if (exactMatches.contains(name)) {
+            return false;
+        }
+        if (glob != null) {
+            //DO NOT accept the file if the pattern matches the glob
+            if (glob.accept(dir, name)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
