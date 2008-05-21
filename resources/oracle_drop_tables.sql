04a00b0bbb6e3cfc34854fa1d8fdfd0e1379fc66
pcarr <pcarr@broadinstitute.org>
2008-05-21 16:36:43 +0000

    GP-2268 - added CREATE SEQUENCE SYSTEM_MESSAGE_SEQ to the oracle schema script
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/v3_1_0@7709 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/oracle_drop_tables.sql
+++ resources/oracle_drop_tables.sql
@@ -1,3 +1,5 @@
+/* remove all of the data from the tables */
+delete from GPPORTAL.SYSTEM_MESSAGE;
 delete from GPPORTAL.ANALYSIS_JOB;
 delete from GPPORTAL.TASK_MASTER;
 delete from GPPORTAL.SUITE_MODULES;
@@ -6,3 +8,4 @@ delete from GPPORTAL.LSIDS;
 delete from GPPORTAL.JOB_COMPLETION_EVENT;
 delete from GPPORTAL.GP_USER_PROP;
 delete from GPPORTAL.GP_USER;
+
