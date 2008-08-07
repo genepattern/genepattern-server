c0102b79a29d834edea02f97b763780586a565aa
pcarr <pcarr@broadinstitute.org>
2008-08-07 20:15:21 +0000

    GP-2241, GP-2430 Merged r7819 through r7825 from GenePattern_3.1.1_PATCH_01
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@7826 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webservice/server/TaskIntegrator.java
+++ src/org/genepattern/server/webservice/server/TaskIntegrator.java
@@ -674,6 +674,22 @@ public class TaskIntegrator {
 	return (new TaskIntegratorDAO()).saveOrUpdate(suiteInfo);
     }
 
+    /**
+     * Expose <code>IAuthorizationManager.checkPermission</code> to the SOAP interface.
+     * Checks permissions as the current user.
+     * Permissions are configured in <pre>resources/permissionMap.xml</pre>.
+     * 
+     * @param permission
+     * @return true if the current user has the given permission.
+     * 
+     * @see IAuthorizationManager#checkPermission(String, String)
+     */
+    public boolean checkPermission(String permission) {
+        IAuthorizationManager authManager = AuthorizationManagerFactory.getAuthorizationManager();
+        String userName = getUserName();
+        return authManager.checkPermission(permission, userName);
+    }
+
     public int getPermittedAccessId(int access_id) {
 
 	int access = GPConstants.ACCESS_PRIVATE;
@@ -1252,7 +1268,7 @@ public class TaskIntegrator {
 
     protected void isAuthorized(String user, String permission) throws WebServiceException {
 	if (!authManager.checkPermission(permission, user)) {
-	    throw new WebServiceException("You do not have permission to perfom this action.");
+	    throw new WebServiceException("You do not have permission to perfom this action: "+permission);
 	}
     }
 
