08aa6680bfbdef214acec6392c7ad17065cd12a4
pcarr <pcarr@broadinstitute.org>
2008-08-11 23:18:41 +0000

    GP-2440 Merging r7831 from branches/GenePattern_3.1.1_PATCH_01
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@7832 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ test/src/org/genepattern/util/LSIDVersionComparatorTest.java
@@ -0,0 +1,70 @@
+package org.genepattern.util;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for LSIDVersionComparator.
+ * 
+ * @author pcarr
+ */
+public class LSIDVersionComparatorTest extends TestCase {
+    private LSIDVersionComparator comparator = null;
+    
+    public void setUp() throws Exception {
+        super.setUp();
+        comparator = LSIDVersionComparator.INSTANCE;
+    }
+
+    /**
+     * Test cases when one or both input LSID.version is the empty String.
+     */
+    public void testCompareToEmptyString() {
+        assertEquals("compare(empty,empty)", 0, comparator.compare("", ""));
+        assertEquals("empty < 2.1", -1, comparator.compare("", "2.1"));
+        assertEquals("empty < 1", -1, comparator.compare("", "1"));
+        assertEquals("empty < 0", -1, comparator.compare("", "0"));
+        assertEquals("empty > -1", 1, comparator.compare("", "-1"));
+        assertEquals("2.1 > empty", 1, comparator.compare("2.1", ""));
+        assertEquals("1 > empty", 1, comparator.compare("1", ""));
+        assertEquals("0 > empty", 1, comparator.compare("0", ""));
+        assertEquals("-1 < empty", -1, comparator.compare("-1", ""));
+        
+        assertEquals("3. == 3.", 0, comparator.compare("3.", "3."));
+    }
+    
+    /**
+     * Test cases when one or both input LSID.version are null.
+     */
+    public void testCompareToNull() {
+        assertEquals("compare(null,null)", 0, comparator.compare(null, null));
+        assertEquals("compare(\"\",null)", 0, comparator.compare("", null));
+        assertEquals("compare(null,\"\")", 0, comparator.compare(null, ""));
+    }
+    
+    public void testCompare() {
+        doTest(0, "1", "1");
+        doTest(0, "1.2", "1.2");
+        doTest(0, "9999", "9999");
+        doTest(-1,  "1", "10" );
+        doTest(-1,  "1.1", "10" );
+        doTest(-1,  "2", "10" );
+        doTest(-1,  "2.8", "10" );
+        doTest(-1, "1", "1.2");
+        doTest(0, "-1", "-1");
+        doTest(0, "1.-1", "1.-1");
+    }
+ 
+    /**
+     * Test cases where part of the input does not map to an Integer.
+     */
+    public void testExceptions() {
+        doTest(0, "1a", "1a");
+        doTest(0, "1a", "2b");
+        doTest(0, "1.1a", "1.8000b");
+        doTest(-1, "5a", "1");
+    }
+    
+    private void doTest(int expected, String arg1, String arg2) {
+        assertEquals("compare("+arg1+","+arg2+")", expected, comparator.compare(arg1, arg2));
+    }
+}
