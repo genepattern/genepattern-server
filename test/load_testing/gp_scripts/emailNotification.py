bda60d14f17496ff63aa32061fa4453f80d3a480
pcarr <pcarr@broadinstitute.org>
2008-08-19 18:16:04 +0000

    Added example python script to getPermittedAccessId from the SOAP service
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@7845 48a7a3f7-ad46-0410-b969-29950918272e

--- test/load_testing/gp_scripts/emailNotification.py
+++ test/load_testing/gp_scripts/emailNotification.py
@@ -1,10 +1,17 @@
 import urllib
 import mechanize
+
+userID = 'test'
+password = 'test'
+userEmail = 'pcarr@broad.mit.edu'
+gpServer = 'http://127.0.0.1:8080/gp/'
+jobID = '98'
+
 br = mechanize.Browser()
-br.open("http://genepatterntest.broad.mit.edu/gp/")
+br.open(gpServer)
 br.select_form(nr=0)
-br["username"] = "test"
-br["loginForm:password"] = "test"
+br["username"] = userID
+br["loginForm:password"] = password
 r = br.submit()
-data = urllib.urlencode({'jobID':'9044','cmd':'notifyEmailJobCompletion','userID':'test','userEmail':'pcarr@broad.mit.edu'})
-r = br.open("http://genepatterntest.broad.mit.edu/gp/notifyJobCompletion.ajax",data)
+data = urllib.urlencode({'jobID':jobID,'cmd':'notifyEmailJobCompletion','userID':userID,'userEmail':userEmail})
+r = br.open(gpServer + 'notifyJobCompletion.ajax', data)
