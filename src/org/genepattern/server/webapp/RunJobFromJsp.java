6be876c5a5d2b1d38013c511da5b2f11938a67b5
pcarr <pcarr@broadinstitute.org>
2008-12-10 23:34:16 +0000

    GP-2527 Added support for automatically calling the wings module when an 'abstract' pipeline is run.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@7977 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/RunJobFromJsp.java
+++ src/org/genepattern/server/webapp/RunJobFromJsp.java
@@ -1,5 +1,12 @@
 package org.genepattern.server.webapp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.genepattern.server.EncryptionUtil;
+import org.genepattern.server.user.User;
+import org.genepattern.server.user.UserDAO;
+import org.genepattern.server.webservice.server.local.LocalAdminClient;
 import org.genepattern.server.webservice.server.local.LocalAnalysisClient;
 import org.genepattern.webservice.JobInfo;
 import org.genepattern.webservice.ParameterInfo;
@@ -27,8 +34,13 @@ public class RunJobFromJsp {
     }
 
     public String submitJob() throws WebServiceException {
-        return submitDefaultJob();
-    }
+        if (isWingsJob()) {
+            return submitWingsJob();
+        }
+        else {
+            return submitDefaultJob();
+        }
+      }
 
     private String submitDefaultJob() 
         throws WebServiceException
@@ -40,4 +52,52 @@ public class RunJobFromJsp {
         String jobId = "" + job.getJobNumber();
         return jobId;
     }
+    
+    private boolean isWingsJob() {
+        return 
+            task != null &&
+            task.getName() != null &&
+            task.getName().toLowerCase().startsWith("abstract");
+    }
+
+    private String submitWingsJob() throws WebServiceException {
+        LocalAdminClient adminClient = new LocalAdminClient(userId);
+        LocalAnalysisClient analysisClient = new LocalAnalysisClient(userId);
+
+        // step 1: get the task id for the wings module
+        final String wingsTaskName = "wings";
+        final String pipelineLSID = task.getLsid();
+        TaskInfo wingsTaskInfo = adminClient.getTask(wingsTaskName);
+
+        // step 2: build input parameters for the wings module
+        String passwordKey = null;
+        if (userId != null) {
+            User user = new UserDAO().findById(userId);
+            if (user != null) {
+                passwordKey = EncryptionUtil.getInstance().pushPipelineUserKey(user);
+            }
+        }
+
+        Map<String, String> wingsInputs = new HashMap<String, String>();
+        wingsInputs.put("pipelineLsid", pipelineLSID);
+        wingsInputs.put("gp_user_id", userId);
+        wingsInputs.put("gp_password", passwordKey != null ? passwordKey : "");
+
+        for (ParameterInfo pinfo : wingsTaskInfo.getParameterInfoArray()) {
+            if (wingsInputs.containsKey(pinfo.getName())) {
+                pinfo.setValue(wingsInputs.get(pinfo.getName()));
+            }
+        }
+
+        ParameterInfo[] paramInfos = 
+            wingsTaskInfo == null ? null
+                : wingsTaskInfo.getParameterInfoArray();
+        paramInfos = 
+            paramInfos == null ? paramInfos = new ParameterInfo[0]
+                : paramInfos;
+        JobInfo job = analysisClient.submitJob(wingsTaskInfo.getID(), paramInfos);
+        String jobId = "" + job.getJobNumber();
+        return jobId;
+    }
+
 }
