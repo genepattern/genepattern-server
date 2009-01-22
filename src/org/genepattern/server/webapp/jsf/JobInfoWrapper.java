c3d3cede452bd4eaf32fd76526c06a874a6b6039
pcarr <pcarr@broadinstitute.org>
2009-01-22 06:33:57 +0000

    GP-2403 - fixed compilation errors; removed references to RunVisualizer class
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8025 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/JobInfoWrapper.java
+++ src/org/genepattern/server/webapp/jsf/JobInfoWrapper.java
@@ -1,6 +1,5 @@
 package org.genepattern.server.webapp.jsf;
 
-import java.io.StringWriter;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -13,12 +12,10 @@ import org.apache.log4j.Logger;
 import org.genepattern.server.PermissionsHelper;
 import org.genepattern.server.auth.GroupPermission;
 import org.genepattern.server.auth.GroupPermission.Permission;
-import org.genepattern.server.genepattern.RunVisualizer;
 import org.genepattern.server.webapp.jsf.JobInfoBean.InputParameter;
 import org.genepattern.server.webapp.jsf.JobInfoBean.OutputParameter;
 import org.genepattern.webservice.JobInfo;
 import org.genepattern.webservice.TaskInfo;
-import org.genepattern.webservice.TaskInfoAttributes;
 
 public class JobInfoWrapper {
     private static Logger log = Logger.getLogger(JobInfoWrapper.class);
@@ -32,7 +29,6 @@ public class JobInfoWrapper {
     private List<InputParameter> inputParameters;
     private List<OutputParameter> outputFiles;
     private TaskInfo taskInfo = null;
-    private boolean isVisualizer = false;
     
     private PermissionsHelper permissionsHelper;
     
@@ -44,7 +40,7 @@ public class JobInfoWrapper {
         this.dateSubmitted = jobInfo.getDateSubmitted();
         this.dateCompleted = jobInfo.getDateCompleted();
         
-        this.isVisualizer = taskInfo != null && taskInfo.isVisualizer();
+        //this.isVisualizer = taskInfo != null && taskInfo.isVisualizer();
         this.taskInfo = taskInfo;
     }
     
@@ -256,129 +252,5 @@ public class JobInfoWrapper {
         int idx = rval.lastIndexOf(", ");
         return rval.substring(0, idx) + "";
     }
-    
-    public boolean getIsVisualizer() {
-        return isVisualizer;
-    }
-
-    public String getVisualizerAppletTag() {
-        RunVisualizer runVis = new RunVisualizer();
-        runVis.setJobInfo(jobInfo);
-        TaskInfoAttributes taskInfoAttributes = taskInfo.giveTaskInfoAttributes();
-        runVis.setTaskInfoAttributes(taskInfoAttributes);
-        //TODO: lookup context path from servlet context
-        runVis.setContextPath("/gp");
-        StringWriter writer = new StringWriter();
-        try {
-            runVis.writeVisualizer(writer);
-            writer.close();
-        }
-        catch (Exception e) {
-            writer.write("<p>Error in getVisualizerAppletTag: "+e.getLocalizedMessage()+"</p>");
-        }
-        return writer.toString();
-    }
-    
-//    public String getVisualizerAppletTag2() throws MalformedURLException //, UnsupportedEncodingException 
-//    {
-//        String name = this.taskName;
-//        //TaskInfoAttributes tia = taskInfo.giveTaskInfoAttributes();
-//        TaskInfoAttributes tia = taskInfo.getTaskInfoAttributes();
-//        String userID = UIBeanHelper.getUserId();
-//        // unique name so that multiple instances of applet on a single page will not collide
-//        String appletName = "a" + ("" + Math.random()).substring(2); // unique
-//        String javaFlags = new LocalAdminClient(userID).getUserProperty(UserPropKey.VISUALIZER_JAVA_FLAGS);
-//        if(javaFlags==null) {
-//            javaFlags = System.getProperty(RunVisualizerConstants.JAVA_FLAGS_VALUE);
-//        }
-//        //String lsid = (String) tia.get(GPConstants.LSID);
-//        String lsid = taskInfo.getLsid();
-//
-//        //String contextPath = request.getContextPath();
-//        //String contextPath = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
-//        //String pathInfo = FacesContext.getCurrentInstance().getExternalContext().getRequestPathInfo();
-//        String codebase = "http://127.0.0.1:8080/gp/downloads";
-//        
-//        ParameterInfo[] parameterInfoArray = ParameterFormatConverter.getParameterInfoArray(taskInfo.getParameterInfo());
-//        if (parameterInfoArray == null) parameterInfoArray = new ParameterInfo[0];
-//        
-//        StringWriter appletTag = new StringWriter();
-//        appletTag.append("<applet code=\"" + org.genepattern.visualizer.RunVisualizerApplet.class.getName() + "\" archive=\"runVisualizer.jar,commons-httpclient.jar,commons-codec-1.3.jar\" codebase=\"../downloads\" width=\"1\" height=\"1\" alt=\"Your browser refuses to run applets\" name=\""+appletName+"\">");
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.NAME + "\" value=\"" + name + "\">");
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.OS + "\" value=\"" + tia.get(GPConstants.OS) + "\">");
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.CPU_TYPE + "\" value=\"" + tia.get(GPConstants.CPU_TYPE) + "\">");
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.JAVA_FLAGS_VALUE + "\" value=\"" + javaFlags + "\">");
-//        //appletTag.append("<param name=\"" + RunVisualizerConstants.CONTEXT_PATH + "\" value=\"" + URLEncoder.encode(contextPath, "UTF-8") + "\">");
-//        StringBuffer paramValue = new StringBuffer();
-//        for (int i = 0; i < parameterInfoArray.length; i++) {
-//            if (i > 0) paramValue.append(",");
-//            paramValue.append(parameterInfoArray[i].getName());
-//        }
-//
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.PARAM_NAMES + "\" value=\"" + paramValue.toString() + "\">");
-//        for (ParameterInfo pInfo : parameterInfoArray) {
-//            String paramName = pInfo.getName();
-//            String value = pInfo.getValue();
-//            //appletTag.append("<param name=\"" + paramName + "\" value=\"" + URLEncoder.encode(value, "UTF-8") + "\">");
-//            appletTag.append("<param name=\"" + paramName + "\" value=\"" + value + "\">");
-//        }
-//
-//        StringBuffer vis = new StringBuffer();
-//        int numToDownload = 0;
-//        for (ParameterInfo pInfo : parameterInfoArray) {
-//            if (pInfo.isInputFile() && pInfo.getValue() != null) {
-//                try {
-//                    new java.net.URL(pInfo.getValue());
-//                    // note that this parameter is a URL that must be downloaded by adding it to the CSV list for the applet
-//                    if (numToDownload > 0) {
-//                        vis.append(",");
-//                    }
-//                    vis.append(pInfo.getName());
-//                    numToDownload++;
-//                } 
-//                catch (Exception x) {
-//                }
-//            }
-//        }
-//
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.DOWNLOAD_FILES + "\" value=\"" + vis.toString() + "\">");
-//        String commandLine = tia.get(GPConstants.COMMAND_LINE);
-//        commandLine = StringUtils.htmlEncode(commandLine);
-//        //try {
-//        //    commandLine = URLEncoder.encode(commandLine, "UTF-8");
-//        //}
-//        //catch (UnsupportedEncodingException e) {
-//        //    log.error(e.getLocalizedMessage(), e);
-//        //}
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.COMMAND_LINE + "\" value=\"" + commandLine + "\">");
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.DEBUG + "\" value=\"1\">");
-//
-//        StringBuffer fileNamesBuf = new StringBuffer();
-//        String libdir = DirectoryManager.getTaskLibDir(null, lsid, userID);
-//        File[] supportFiles = new File(libdir).listFiles();
-//        for (int i = 0; i < supportFiles.length; i++) {
-//            if (i > 0) fileNamesBuf.append(",");
-//                fileNamesBuf.append(supportFiles[i].getName());
-//        }
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.SUPPORT_FILE_NAMES + "\" value=\"" + fileNamesBuf.toString() + "\" >");
-//
-//        StringBuffer fileDatesBuf = new StringBuffer();
-//        for (int i = 0; i < supportFiles.length; i++) {
-//            if (i > 0) fileDatesBuf.append(",");
-//                fileDatesBuf.append(supportFiles[i].lastModified());
-//        }
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.SUPPORT_FILE_DATES + "\" value=\"" + fileDatesBuf.toString() + "\" >");
-//        appletTag.append("<param name=\"" + RunVisualizerConstants.LSID + "\" value=\"" + lsid + "\">");
-//
-//
-//        StringBuffer rval = new StringBuffer();
-//        //for debugging
-//        rval.append("<SCRIPT LANGUAGE=\"JavaScript\">window.alert(\"Launching visualizer ... codebase="+codebase+"\");</SCRIPT>\n");
-//        //use JavaScript to write the applet tag so that the browserCookie is property set as an applet parameter
-//        rval.append("<SCRIPT LANGUAGE=\"JavaScript\">");
-//        rval.append("document.writeln('"+appletTag.toString()+" <PARAM name=\"browserCookie\" value=\"' + document.cookie + '\"></applet>');</SCRIPT>");
-//        return rval.toString();
-//        
-//    }
 
 }
