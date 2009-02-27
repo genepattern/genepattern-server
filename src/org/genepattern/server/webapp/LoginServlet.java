078b03951cb5301436ea5c5c9999280063b6acc6
pcarr <pcarr@broadinstitute.org>
2009-02-27 23:42:14 +0000

    Update LoginServlet so that automatic redirects can be configured via a request parameter (/gp/login?redirect=false).     Added detailed javadoc documentation.     By default GET does a redirect to the login page, while POST does not do a redirect.     Added detailed messages to HTTP response headers when login does not succeed. GP-2493
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8125 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/LoginServlet.java
+++ src/org/genepattern/server/webapp/LoginServlet.java
@@ -12,10 +12,40 @@ import javax.servlet.http.HttpServletResponse;
 import org.genepattern.server.auth.AuthenticationException;
 
 /**
- * Helper servlet for programmatically logging into a GenePattern server from an HTTP client.
- * This servlet validates the 'username' and 'password' request parameters and adds the userid to the current session.
+ * Servlet for logging an HTTP client into a GenePattern server; 
+ * This is the recommended way to authenticate an HTTP client session from an application or script.
  * 
- * @see org.genepattern.server.webapp.jsf.UIBeanHelper#login(String, boolean, boolean, HttpServletRequest, HttpServletResponse)
+ * <pre>
+ * Example HTTP requests:
+ * 1. POST http://127.0.0.1:8080/gp/login?username=user&password=pw
+ * 2. GET  http://127.0.0.1:8080/gp/login?username=user&password=pw
+ * 3. POST http://127.0.0.1:8080/gp/login?username=user&password=pw&redirect=true
+ * 4. GET  http://127.0.0.1:8080/gp/login?username=user&password=pw&redirect=false
+ * 5. POST http://127.0.0.1:8080/gp/login?username=user&password=pw&redirect=true&origin=/gp/jobResults
+ * 6. GET  http://127.0.0.1:8080/gp/login?username=user&password=pw&redirect=true&origin=/gp/jobResults
+ * 
+ * Request parameters
+ *     username, required, The genepattern username.
+ *     password, optional only of the server does not require passwords for authentication, The unencrypted password.
+ *     redirect, optional, true | false, If true the server will include a redirect in the server response code.
+ *     origin, optional, Sets the location when sending a redirect, otherwise the redirect is to home page.
+ *     
+ * Response codes
+ * 1. 200 OK  
+ *    Indicates successful login.
+ *    
+ * 2. 3xx
+ *    Indicates successful login when redirect=true.
+ *    
+ * 3. 403
+ *    Indicates login error either from invalid username, password, or some other server side error.
+ *    Response headers, when a login error occurs, these response headers give more details:
+ *        X-genepattern-AuthenticationException.Type
+ *        X-genepattern-AuthenticationException.Message
+ * </pre>
+ * 
+ * @see LoginManager#login(HttpServletRequest, HttpServletResponse, boolean)
+ * @see org.genepattern.util.LoginHttpClient#loginLatest(org.apache.commons.httpclient.HttpClient)
  * 
  * @author pcarr
  */
@@ -30,23 +60,28 @@ public class LoginServlet extends HttpServlet implements Servlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) 
     throws IOException, ServletException
     {
-        process(req, resp);
+        process(req, resp, true);
     }
 
     public void doPost(HttpServletRequest req, HttpServletResponse resp) 
     throws IOException, ServletException
     {
-        process(req, resp);
+        process(req, resp, false);
     }
     
-    private void process(HttpServletRequest request, HttpServletResponse resp) 
+    private void process(HttpServletRequest request, HttpServletResponse resp, boolean redirect) 
     throws IOException
     {
+        String redirectParam = request.getParameter("redirect");
+        if (redirectParam != null) {
+            redirect = Boolean.valueOf(redirectParam);
+        }
         try {
-            //TODO: guess whether or not to redirect, based on user agent
-            LoginManager.instance().login(request, resp, true);
+            LoginManager.instance().login(request, resp, redirect);
         }
         catch (AuthenticationException e) {
+            resp.setHeader("X-genepattern-AuthenticationException.Type", e.getType().toString());
+            resp.setHeader("X-genepattern-AuthenticationException.Message", e.getMessage());
             resp.sendError(HttpServletResponse.SC_FORBIDDEN, e.getLocalizedMessage());
         }
     }
