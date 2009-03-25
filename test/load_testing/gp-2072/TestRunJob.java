a857b1bc480686d166a2ae5eca2bd2ff2016addc
pcarr <pcarr@broadinstitute.org>
2009-03-25 21:09:01 +0000

    Updated TestRunJob documentation and added test input files. For testing Java client SOAP interface. GP-2072
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8229 48a7a3f7-ad46-0410-b969-29950918272e

--- test/load_testing/gp-2072/TestRunJob.java
+++ test/load_testing/gp-2072/TestRunJob.java
@@ -5,15 +5,21 @@ import org.genepattern.webservice.Parameter;
 /**
  * Test submitting a GenePattern job from the java API.
  * This is used to verify bug GP-2513, http://jira.broad.mit.edu:8008/browse/GP-2513
+ * 
+ * This test program depends on the 'GenePattern Java Library'.
+ * To run this program,<ol> 
+ *     <li>download and unzip GenePattern.zip, 
+ *     <li>add GenePattern.jar and all the jars in the lib directory to the classpath
+ * </ol>
  *
- * After running this program (which executes two GenePattern jobs on the server),
+ * After running this program (which executes three GenePattern jobs on the server),
  * connect to the server via a web client and verify that input files can be downloaded
  * after you select Info from the job result arrow drop down.
  */
 public class TestRunJob {
 
     public static void main(String[] args) {
-        String server = "http://node256.broad.mit.edu:8080";
+        String server = "http://127.0.0.1:8080";
         String username = "test";
         String password = "test";
 
@@ -29,8 +35,6 @@ public class TestRunJob {
                 password = null;
             }
         }
-        
-        //System.out.println("Connecting to "+server+" as "+username);
 
         GPClient gpClient = null;
         try {
@@ -39,21 +43,23 @@ public class TestRunJob {
             // run ExpressionFileCreator with an uploaded input file
             JobResult efcResult = gpClient.runAnalysis("ExpressionFileCreator",
                     new Parameter[] {
-                            new Parameter("input.file", "2cel_files.zip"),
-                            new Parameter("quantile.normalization", "yes"),
-                            new Parameter("background.correct", "yes") });
+                        new Parameter("input.file", "2cel_files.zip"),
+                        new Parameter("quantile.normalization", "yes"),
+                        new Parameter("background.correct", "yes") 
+                    });
 
             // run PreprocessDataset with a URL input file
             String inputDataset = "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res";
             JobResult preprocess = gpClient.runAnalysis("PreprocessDataset",
-                    new Parameter[] { new Parameter("input.filename",
-                            inputDataset) });
+                    new Parameter[] { 
+                        new Parameter("input.filename", inputDataset) 
+                    });
 
             // run PreprocessDataest with an uploaded input file
             JobResult preprocess2 = gpClient.runAnalysis("PreprocessDataset",
-                    new Parameter[] { new Parameter("input.filename",
-                            "all_aml_test.gct") });
-
+                    new Parameter[] { 
+                        new Parameter("input.filename", "all_aml_test.gct") 
+                    });
         } 
         catch (Exception e) {
             e.printStackTrace();
