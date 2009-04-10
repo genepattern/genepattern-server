bdbc3002f8d957542e7ee08a71f7ced56f52d39f
jnedzel <jnedzel@broadinstitute.org>
2009-04-10 20:04:51 +0000

    refactoring to separate modules and pipelines.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8304 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ test/regression/src/org/genepattern/integration/TestRunPipeline.java
@@ -0,0 +1,40 @@
+package org.genepattern.integration;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.genepattern.client.GPClient;
+import org.genepattern.webservice.JobResult;
+import org.genepattern.webservice.Parameter;
+import org.genepattern.webservice.WebServiceException;
+
+/**
+ * Tests submitting a job to GenePattern using GenePattern client.
+ * @author jnedzel
+ *
+ */
+public class TestSubmitJob extends TestCase {
+	
+	public static final String GOLUB_PIPELINE_NO_VIEWERS_LSID = "urn:lsid:8020.jnedzel.gm94e-69f.broad.mit.edu:genepatternmodules:2:2";
+	public static final String EXECUTION_LOG = "gp_execution_log.gct";
+	
+
+	public void testGolubPipeline() throws WebServiceException, IOException {
+		GenePatternConfig config = new GenePatternConfig();
+		GPClient client = new GPClient(config.getGenePatternUrl(), config.getUsername(), config.getPassword());
+		assertNotNull(client);
+		JobResult jobResult = client.runAnalysis(GOLUB_PIPELINE_NO_VIEWERS_LSID, (Parameter []) null);
+		assertNotNull(jobResult);
+		assertTrue(!jobResult.hasStandardError());
+		
+		File[] files = jobResult.downloadFiles(".");
+		assertNotNull(files);
+		assertEquals(1, files.length);
+		String filename = files[0].getName();
+		assertNotNull(filename);
+		assertTrue(filename.contains("execution_log.html"));
+	}
+
+}
