490513ccd7c8950ca3a2c268b4a54cbd31bcc7a6
jnedzel <jnedzel@broadinstitute.org>
2009-04-10 20:44:53 +0000

    now tests preprocess using broad url for input file.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8308 48a7a3f7-ad46-0410-b969-29950918272e

--- test/regression/src/org/genepattern/integration/TestRunModule.java
+++ test/regression/src/org/genepattern/integration/TestRunModule.java
@@ -21,18 +21,41 @@ public class TestRunModule extends TestCase {
 	public static final String SMALL_GCT = "resources/data/small.gct";
 	public static final String SMALL_RESULT_GCT = "small.preprocessed.gct";	
 	
+	public static final String ALL_AML_RES_URL = "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res";
+	public static final String ALL_AML_RES_RESULT = "all_aml_train.preprocessed.res";
+	
 	/**
-	 * Submits a PreprocessDataset job to GenePattern.
+	 * Submits a PreprocessDataset job to GenePattern.  Input file is uploaded from client.
 	 * @throws WebServiceException 
 	 * @throws IOException 
 	 * @throws SecurityException 
 	 * @throws ParseException 
 	 */
-	public void testPreprocess() throws WebServiceException, SecurityException, IOException, ParseException {
+	public void testPreprocessUploadedFile() throws WebServiceException, SecurityException, IOException, ParseException {
+		File testFile = new File(SMALL_GCT);		
 		Parameter[] params = new Parameter[1];
-		File testFile = new File(SMALL_GCT);
 		Parameter fileParam = new Parameter("input.filename", testFile);
 		params[0] = fileParam;
+		File[] files = runPreprocessHelper(params);
+		if (!SMALL_RESULT_GCT.equals(files[0].getName())) {
+			fail("found unexpected output file: " + files[0].getName());
+		}
+		
+		Dataset dataset = IOUtil.readDataset(files[0].getAbsolutePath());
+		assertNotNull(dataset);
+		assertEquals(2, dataset.getRowCount());
+		assertEquals(3, dataset.getColumnCount());
+	}
+
+	/**
+	 * Actually runs the PreprocessDataset job on GenePattern and downloads the result files.
+	 * @param params Parameters for the run.
+	 * @return Downloaded result files.
+	 * @throws WebServiceException
+	 * @throws IOException
+	 */
+	private File[] runPreprocessHelper(Parameter[] params)
+			throws WebServiceException, IOException {
 		GenePatternConfig config = new GenePatternConfig();
 		GPClient client = new GPClient(config.getGenePatternUrl(), config.getUsername(), config.getPassword());
 		assertNotNull(client);
@@ -43,14 +66,30 @@ public class TestRunModule extends TestCase {
 		File[] files = jobResult.downloadFiles(".");
 		assertNotNull(files);
 		assertEquals(1, files.length);
-		if (!SMALL_RESULT_GCT.equals(files[0].getName())) {
+		return files;
+	}	
+	
+	/**
+	 * Submits a PreprocessDataset job to GenePattern.  Input file is an url to a public
+	 * Broad url.
+	 * @throws IOException 
+	 * @throws SecurityException 
+	 * @throws WebServiceException 
+	 * @throws ParseException 
+	 */
+	public void testPreprocessGpUrl() throws SecurityException, IOException, WebServiceException, ParseException {
+		Parameter[] params = new Parameter[1];
+		Parameter fileParam = new Parameter("input.filename", ALL_AML_RES_URL);
+		params[0] = fileParam;
+		File[] files = runPreprocessHelper(params);
+		if (!ALL_AML_RES_RESULT.equals(files[0].getName())) {
 			fail("found unexpected output file: " + files[0].getName());
 		}
 		
 		Dataset dataset = IOUtil.readDataset(files[0].getAbsolutePath());
 		assertNotNull(dataset);
-		assertEquals(2, dataset.getRowCount());
-		assertEquals(3, dataset.getColumnCount());
-	}	
+		assertEquals(5864, dataset.getRowCount());
+		assertEquals(38, dataset.getColumnCount());		
+	}
 
 }
