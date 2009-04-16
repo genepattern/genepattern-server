6c2c38ed70abeb26395a144b34a2999a676c73a8
jnedzel <jnedzel@broadinstitute.org>
2009-04-16 12:50:38 +0000

    now testing Gene Pattern url and external url as module input.
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8324 48a7a3f7-ad46-0410-b969-29950918272e

--- test/regression/src/org/genepattern/integration/TestRunModule.java
+++ test/regression/src/org/genepattern/integration/TestRunModule.java
@@ -2,6 +2,7 @@ package org.genepattern.integration;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.broadinstitute.io.IOUtil;
 import org.broadinstitute.io.matrix.ParseException;
@@ -13,19 +14,29 @@ import org.genepattern.webservice.WebServiceException;
 
 import junit.framework.TestCase;
 
-
+/**
+ * Tests the ability to run GenePattern Modules.
+ * @author jnedzel
+ *
+ */
 public class TestRunModule extends TestCase {
 	
 	public static final String PREPROCESS_LSID = "urn:lsid:broad.mit.edu:cancer.software.genepattern.module.analysis:00020:3";
-	public static final String GOLUB_PIPELINE_NO_VIEWERS_LSID = "urn:lsid:8020.jnedzel.gm94e-69f.broad.mit.edu:genepatternmodules:2:2";
+	public static final String CONVERT_LSID = "urn:lsid:broad.mit.edu:cancer.software.genepattern.module.analysis:00002:1";
+
 	public static final String SMALL_GCT = "resources/data/small.gct";
-	public static final String SMALL_RESULT_GCT = "small.preprocessed.gct";	
+	public static final String SMALL_PREPROCESS_RESULT_GCT = "small.preprocessed.gct";	
+	public static final String SMALL_CONVERT_RESULT_GCT = "small.cvt.gct";
 	
 	public static final String ALL_AML_RES_URL = "ftp://ftp.broad.mit.edu/pub/genepattern/all_aml/all_aml_train.res";
-	public static final String ALL_AML_RES_RESULT = "all_aml_train.preprocessed.res";
+	public static final String ALL_AML_RES_PREPROCESS_RESULT = "all_aml_train.preprocessed.res";
+	public static final String ALL_AML_RES_CONVERT_RESULT = "all_aml_train.cvt.res";	
 	
 	/**
 	 * Submits a PreprocessDataset job to GenePattern.  Input file is uploaded from client.
+	 * 
+	 * Tests a Java module.
+	 * 
 	 * @throws WebServiceException 
 	 * @throws IOException 
 	 * @throws SecurityException 
@@ -36,60 +47,143 @@ public class TestRunModule extends TestCase {
 		Parameter[] params = new Parameter[1];
 		Parameter fileParam = new Parameter("input.filename", testFile);
 		params[0] = fileParam;
-		File[] files = runPreprocessHelper(params);
-		if (!SMALL_RESULT_GCT.equals(files[0].getName())) {
+		File[] files = runModule(params, PREPROCESS_LSID);
+		validateResultFile(files, SMALL_PREPROCESS_RESULT_GCT, 2, 3);
+	}
+
+	/**
+	 * Validates the result file.  Checks the result filename against the expected filename.
+	 * Opens the dataset and compares the number of rows and columns against the expected values.
+	 * @param files - downloaded GenePattern result files.
+	 * @param expectedFilename Expected name of the result file containing microarray data.
+	 * @param expectedNumRows expected number of rows in result file dataset.
+	 * @param expectedNumColumns expected number of columns in result file dataset.
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	private void validateResultFile(File[] files, String expectedFilename, int expectedNumRows, int expectedNumColumns) throws IOException,
+			ParseException {
+		if (!expectedFilename.equals(files[0].getName())) {
 			fail("found unexpected output file: " + files[0].getName());
 		}
 		
 		Dataset dataset = IOUtil.readDataset(files[0].getAbsolutePath());
 		assertNotNull(dataset);
-		assertEquals(2, dataset.getRowCount());
-		assertEquals(3, dataset.getColumnCount());
+		assertEquals(expectedNumRows, dataset.getRowCount());
+		assertEquals(expectedNumColumns, dataset.getColumnCount());
 	}
 
 	/**
-	 * Actually runs the PreprocessDataset job on GenePattern and downloads the result files.
+	 * Runs the module on GenePattern and downloads the result files.
 	 * @param params Parameters for the run.
+	 * @param lsid Lsid of module to run.
 	 * @return Downloaded result files.
 	 * @throws WebServiceException
 	 * @throws IOException
 	 */
-	private File[] runPreprocessHelper(Parameter[] params)
+	private File[] runModule(Parameter[] params, String lsid)
 			throws WebServiceException, IOException {
-		GenePatternConfig config = new GenePatternConfig();
-		GPClient client = new GPClient(config.getGenePatternUrl(), config.getUsername(), config.getPassword());
-		assertNotNull(client);
-		JobResult jobResult = client.runAnalysis(PREPROCESS_LSID, params);
-		assertNotNull(jobResult);
-		assertTrue(!jobResult.hasStandardError());
-		
+		JobResult jobResult = runModuleHelper(params, lsid);
 		File[] files = jobResult.downloadFiles(".");
 		assertNotNull(files);
 		assertEquals(1, files.length);
 		return files;
+	}
+
+	/**
+	 * Runs the module on GenePattern and returns the jobResult.
+	 * @param params Parameters for the run.
+	 * @param lsid LSID of the module to run on GenePattern.
+	 * @return jobResult for the module
+	 * @throws WebServiceException
+	 */
+	private JobResult runModuleHelper(Parameter[] params, String lsid)
+			throws WebServiceException {
+		GenePatternConfig config = new GenePatternConfig();
+		GPClient client = new GPClient(config.getGenePatternUrl(), config.getUsername(), config.getPassword());
+		assertNotNull(client);
+		JobResult jobResult = client.runAnalysis(lsid, params);
+		assertNotNull(jobResult);
+		assertTrue(!jobResult.hasStandardError());
+		return jobResult;
 	}	
 	
 	/**
 	 * Submits a PreprocessDataset job to GenePattern.  Input file is an url to a public
 	 * Broad url.
+	 * 
+	 * Tests a Java module.
+	 * 
 	 * @throws IOException 
 	 * @throws SecurityException 
 	 * @throws WebServiceException 
 	 * @throws ParseException 
 	 */
-	public void testPreprocessGpUrl() throws SecurityException, IOException, WebServiceException, ParseException {
+	public void testPreprocessExternalUrl() throws SecurityException, IOException, WebServiceException, ParseException {
 		Parameter[] params = new Parameter[1];
 		Parameter fileParam = new Parameter("input.filename", ALL_AML_RES_URL);
 		params[0] = fileParam;
-		File[] files = runPreprocessHelper(params);
-		if (!ALL_AML_RES_RESULT.equals(files[0].getName())) {
-			fail("found unexpected output file: " + files[0].getName());
-		}
-		
-		Dataset dataset = IOUtil.readDataset(files[0].getAbsolutePath());
-		assertNotNull(dataset);
-		assertEquals(5864, dataset.getRowCount());
-		assertEquals(38, dataset.getColumnCount());		
+		File[] files = runModule(params, PREPROCESS_LSID);
+		validateResultFile(files, ALL_AML_RES_PREPROCESS_RESULT, 5864, 38);
 	}
+	
+	public void testPreprocessGpUrl() throws SecurityException, IOException, WebServiceException {
+		File testFile = new File(SMALL_GCT);		
+		Parameter[] params = new Parameter[1];
+		Parameter fileParam = new Parameter("input.filename", testFile);
+		params[0] = fileParam;
+		
+		JobResult jobResult = runModuleHelper(params, PREPROCESS_LSID);
+		File[] files = jobResult.downloadFiles(".");
+		assertNotNull(files);
+		assertEquals(1, files.length);		
+		
+		URL inputUrl = jobResult.getURLForFileName(SMALL_PREPROCESS_RESULT_GCT);
+		assertNotNull(inputUrl);
+		
+		fileParam = new Parameter("input.filename", inputUrl.toString());
+		params[0] = fileParam;
+		
+		files = runModule(params, PREPROCESS_LSID);
+		assertNotNull(files);
+		assertEquals(1, files.length);
+	}
+	
+	/**
+	 * Submits a ConvertLineEndings job to Genepattern.  Input file is an uploaded file.
+	 * 
+	 * Tests a Perl module.
+	 * @throws SecurityException
+	 * @throws IOException
+	 * @throws WebServiceException
+	 * @throws ParseException
+	 */
+	public void testConvertLineEndingsUploadedFile() throws SecurityException, IOException, WebServiceException, ParseException {
+		File testFile = new File(SMALL_GCT);		
+		Parameter[] params = new Parameter[1];
+		Parameter fileParam = new Parameter("input.filename", testFile);
+		params[0] = fileParam;
+		File[] files = runModule(params, CONVERT_LSID);
+		validateResultFile(files, SMALL_CONVERT_RESULT_GCT, 2, 3);
+	}
+	
+	
+	/**
+	 * Submits a ConvertLineEndings job to Genepattern.  Input file is an url to a public
+	 * Broad url.  
+	 * 
+	 * Tests a Perl module.
+	 * @throws SecurityException
+	 * @throws IOException
+	 * @throws WebServiceException
+	 * @throws ParseException
+	 */
+	public void testConvertLineEndingsGpUrl() throws SecurityException, IOException, WebServiceException, ParseException {
+		Parameter[] params = new Parameter[1];
+		Parameter fileParam = new Parameter("input.filename", ALL_AML_RES_URL);
+		params[0] = fileParam;
+		File[] files = runModule(params, CONVERT_LSID);
+		validateResultFile(files, ALL_AML_RES_CONVERT_RESULT, 7129, 38);
+	}	
 
 }
