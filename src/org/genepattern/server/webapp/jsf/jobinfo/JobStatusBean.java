ec036de85596fc3bdd9116be9dbf1720b365bd94
pcarr <pcarr@broadinstitute.org>
2009-05-20 05:49:46 +0000

    Update visualizer code on Job Status Page; Cleanup source code. GP-2712
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8440 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/jobinfo/JobStatusBean.java
+++ src/org/genepattern/server/webapp/jsf/jobinfo/JobStatusBean.java
@@ -14,7 +14,6 @@ package org.genepattern.server.webapp.jsf.jobinfo;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
