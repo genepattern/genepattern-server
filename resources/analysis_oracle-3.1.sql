780249e7bd6778fb3fe6ffde388525ae46130557
pcarr <pcarr@broadinstitute.org>
2009-06-08 16:31:25 +0000

    Merged DB script update from branches/GenePattern_3.2
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8486 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_oracle-3.1.sql
+++ resources/analysis_oracle-3.1.sql
@@ -1,7 +1,5 @@
-alter table GP_USER add registration_date timestamp;
-
 -- update schema version
-UPDATE GPPORTAL.PROPS SET VALUE = '3.1' where KEY = 'schemaVersion';
+UPDATE PROPS SET VALUE = '3.1' where KEY = 'schemaVersion';
 
 COMMIT;
 
