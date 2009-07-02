9669667960b3e2108e796e1c3ccf6e7b885a7ca1
pcarr <pcarr@broadinstitute.org>
2009-07-02 20:47:19 +0000

    Reverse-merging r8547 to revert inadvertent checkin
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8560 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/visualizer/RunVisualizerApplet.java
+++ src/org/genepattern/visualizer/RunVisualizerApplet.java
@@ -13,8 +13,6 @@
 package org.genepattern.visualizer;
 
 import java.applet.Applet;
-import java.awt.Color;
-import java.awt.Graphics;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.HashMap;
@@ -22,7 +20,6 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
 
 public class RunVisualizerApplet extends Applet {
 
@@ -40,60 +37,40 @@ public class RunVisualizerApplet extends Applet {
 	    RunVisualizerConstants.OS, RunVisualizerConstants.CPU_TYPE, RunVisualizerConstants.DOWNLOAD_FILES,
 	    RunVisualizerConstants.LSID };
 
-    private boolean inited = false;
     public void init() {
-        try {
-            // ProcessBuilder is new in java 1.5
-            Class.forName("java.lang.ProcessBuilder");
-        } 
-        catch (Throwable t) {
-            JOptionPane.showMessageDialog(this,
-                "Your browser is not using the correct version of Java for GenePattern. Java version 1.5 or higher is required.");
-            inited = true;
-            return;
-        }
+	try {
+	    Class.forName("java.lang.ProcessBuilder"); // ProcessBuilder is new
+	    // in java 1.5
+	} catch (Throwable t) {
+	    JOptionPane
+		    .showMessageDialog(this,
+			    "Your browser is not using the correct version of Java for GenePattern. Java version 1.5 or higher is required.");
+	    return;
 
-        for (int i = 0; i < wellKnownNames.length; i++) {
-            setParameter(wellKnownNames[i], getParameter(wellKnownNames[i]));
-        }
-        try {
-            setParameter(RunVisualizerConstants.JAVA_FLAGS_NAME, getParameter(RunVisualizerConstants.JAVA_FLAGS_VALUE));
-            StringTokenizer stParameterNames = new StringTokenizer(getParameter(RunVisualizerConstants.PARAM_NAMES), ", ");
-            while (stParameterNames.hasMoreTokens()) {
-                String paramName = stParameterNames.nextToken();
-                String paramValue = getParameter(paramName);
-                setParameter(paramName, paramValue);
-            }
+	}
 
-            setSupportFileNames(getParameter(RunVisualizerConstants.SUPPORT_FILE_NAMES));
-            setSupportFileDates(getParameter(RunVisualizerConstants.SUPPORT_FILE_DATES));
-            exec();
-        } 
-        catch (Throwable t) {
-            t.printStackTrace();
-            JOptionPane.showMessageDialog(this, "An error occurred while launching the visualizer.");
-        }
+	for (int i = 0; i < wellKnownNames.length; i++) {
+	    setParameter(wellKnownNames[i], getParameter(wellKnownNames[i]));
+	}
+	try {
+	    setParameter(RunVisualizerConstants.JAVA_FLAGS_NAME, getParameter(RunVisualizerConstants.JAVA_FLAGS_VALUE));
+	    StringTokenizer stParameterNames = new StringTokenizer(getParameter(RunVisualizerConstants.PARAM_NAMES),
+		    ", ");
+	    while (stParameterNames.hasMoreTokens()) {
+		String paramName = stParameterNames.nextToken();
+		String paramValue = getParameter(paramName);
+		setParameter(paramName, paramValue);
+	    }
+
+	    setSupportFileNames(getParameter(RunVisualizerConstants.SUPPORT_FILE_NAMES));
+	    setSupportFileDates(getParameter(RunVisualizerConstants.SUPPORT_FILE_DATES));
+	    exec();
+	} catch (Throwable t) {
+	    t.printStackTrace();
+	    JOptionPane.showMessageDialog(this, "An error occurred while launching the visualizer.");
+	}
     }
-    
-    //debugging code
-    public void start() {
-        super.start();
-    }
-    
-    public void stop() {
-        super.stop();
-    }
-    
-    public void paint(Graphics g) {
-        int width = this.getSize().width;
-        int height = this.getSize().height;
-        
-        g.setColor(Color.black);
-        g.fillRect(0, 0, getSize().width - 1, getSize().height - 1);
-        g.setColor(Color.white);
-        g.drawString("This is a test!", 5, 15);
-    }
-   
+
     public String getParameter(String name) {
 	String value = super.getParameter(name);
 	if (value == null) {
@@ -131,22 +108,20 @@ public class RunVisualizerApplet extends Applet {
     }
 
     public void exec() throws Exception {
-        validateInputs();
-        RunVisualizer visualizer = new RunVisualizer(params, supportFileNames, supportFileDates, this);
-        visualizer.exec();
+	validateInputs();
+	RunVisualizer visualizer = new RunVisualizer(params, supportFileNames, supportFileDates, this);
+	visualizer.exec();
     }
 
     protected void validateInputs() throws Exception {
-        // make sure that each support file has an associated length
-        if (supportFileNames.length != supportFileDates.length) {
-            throw new Exception("Mismatched number of support file names and dates");
-        }
-        // make sure that all expected mandatory inputs are there
-        for (int i = 0; i < wellKnownNames.length; i++) {
-            if (params.get(wellKnownNames[i]) == null) {
-                throw new Exception("Missing input parameter " + wellKnownNames[i]);
-            }
-        }
+	// make sure that each support file has an associated length
+	if (supportFileNames.length != supportFileDates.length)
+	    throw new Exception("Mismatched number of support file names and dates");
+	// make sure that all expected mandatory inputs are there
+	for (int i = 0; i < wellKnownNames.length; i++) {
+	    if (params.get(wellKnownNames[i]) == null)
+		throw new Exception("Missing input parameter " + wellKnownNames[i]);
+	}
     }
 
 }
