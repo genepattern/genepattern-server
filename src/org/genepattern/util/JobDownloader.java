835013217795f9965c9528be149a9706b6d08a08
pcarr <pcarr@broadinstitute.org>
2009-08-14 21:33:33 +0000

    merging changes from branches/GenePattern_3.2; GP-2811
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8597 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/util/JobDownloader.java
+++ src/org/genepattern/util/JobDownloader.java
@@ -31,78 +31,88 @@ import org.apache.commons.httpclient.methods.GetMethod;
  * 
  */
 public class JobDownloader {
-
     private HttpClient client = new HttpClient();
-
     private LoginHttpClient login = new LoginHttpClient();
-
     private String server;
+    private String contextPath = "/gp";
+    
 
     public JobDownloader(String server, String username, String password) {
-	this.server = server;
-	login = new LoginHttpClient();
-	login.setUsername(username);
-	login.setPassword(password);
-	login.setServerUrl(server);
-	client.setState(new HttpState());
-	client.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
+        if (server != null && server.endsWith("/")) {
+            //remove trailing slash
+            server = server.substring(0, server.length() - 1);
+        }
+        this.server = server;
+        login = new LoginHttpClient();
+        login.setUsername(username);
+        login.setPassword(password);
+        login.setServerUrl(server);
+        client.setState(new HttpState());
+        client.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
     }
 
     public void download(int jobNumber, String filename, File outputFile) throws IOException {
-	login();
-	_download(server + "/gp/jobResults/" + jobNumber + "/" + filename, outputFile);
+        login();
+        _download(server + contextPath + "/jobResults/" + jobNumber + "/" + filename, outputFile);
     }
 
     public void download(String url, File outputFile) throws IOException {
-	URL u = new URL(url);
-	server = u.getProtocol() + "://" + u.getHost() + ":" + u.getPort();
-	login();
-	_download(url, outputFile);
+        URL u = new URL(url);
+        server = u.getProtocol() + "://" + u.getHost() + ":" + u.getPort();
+        login();
+        _download(url, outputFile);
     }
 
     public GetMethod getGetMethod(int jobNumber, String filename) throws IOException {
-	login();
-	GetMethod get = new GetMethod(server + "/gp/jobResults/" + jobNumber + "/" + filename);
-	client.executeMethod(get);
-	return get;
+        login();
+        GetMethod get = new GetMethod(server + contextPath + "/jobResults/" + jobNumber + "/" + filename);
+        client.executeMethod(get);
+        return get;
     }
 
     private void _download(String url, File outputFile) throws IOException {
-	InputStream is = null;
-	BufferedOutputStream os = null;
-	GetMethod get = new GetMethod(url);
-	try {
-	    client.executeMethod(get);
-	    os = new BufferedOutputStream(new FileOutputStream(outputFile));
-	    is = get.getResponseBodyAsStream();
-	    byte[] b = new byte[10000];
-	    int bytesRead = 0;
-	    while ((bytesRead = is.read(b)) != -1) {
-		os.write(b, 0, bytesRead);
-	    }
-	} finally {
-	    if (os != null) {
-		try {
-		    os.close();
-		} catch (IOException x) {
-		}
-	    }
-	    if (is != null) {
-		try {
-		    is.close();
-		} catch (IOException x) {
-		}
-	    }
-
-	    outputFile.setLastModified(System.currentTimeMillis());
-	    get.releaseConnection();
-	}
+        InputStream is = null;
+        BufferedOutputStream os = null;
+        GetMethod get = new GetMethod(url);
+        try {
+            client.executeMethod(get);
+            os = new BufferedOutputStream(new FileOutputStream(outputFile));
+            is = get.getResponseBodyAsStream();
+            byte[] b = new byte[10000];
+            int bytesRead = 0;
+            while ((bytesRead = is.read(b)) != -1) {
+                os.write(b, 0, bytesRead);
+            }
+        } 
+        finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } 
+                catch (IOException x) {
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } 
+                catch (IOException x) {
+                }
+            }
+            outputFile.setLastModified(System.currentTimeMillis());
+            get.releaseConnection();
+        }
     }
 
     private void login() throws IOException {
-	LoginHttpClient.LoginState state = login.login(client);
-	if (state == LoginHttpClient.LoginState.IO_EXCEPTION) {
-	    throw new IOException("Unable to login to GenePattern server");
-	}
+        LoginHttpClient.LoginState state = login.login(client);
+        String serverUrl = login.getServerUrl();
+        if (!this.server.equals(serverUrl)) {
+            this.server = serverUrl;
+            state = login.login(client);
+        }
+        if (state == LoginHttpClient.LoginState.IO_EXCEPTION) {
+            throw new IOException("Unable to login to GenePattern server");
+        }
     }
 }
