f309f33c225b2972104d1c2e8f29e2ed4d549f23
pcarr <pcarr@broadinstitute.org>
2009-09-18 18:06:49 +0000

    Updated custom authentication project, so that it can be posted to the FTP server and used as a template:     ftp://ftp.broadinstitute.org/pub/genepattern/src/gp-custom-auth.zip
        1) clean up ant script, build is based on local installation of GenePattern Server
        2) added 'self.dist' task to package up this project for publication to FTP server
        3) added 'AllAccessAuthentication.java' example implementation
    GP-2835
    
    
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/GenePattern_3.2@8674 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ gp-custom-auth/src/org/genepattern/server/auth/plugin/AllAccessAuthentication.java
@@ -0,0 +1,60 @@
+package org.genepattern.server.auth.plugin;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.genepattern.server.UserAccountManager;
+import org.genepattern.server.auth.AuthenticationException;
+import org.genepattern.server.auth.IAuthenticationPlugin;
+
+/**
+ * Demo Custom Authentication plugin.
+ * 
+ * @author pcarr
+ */
+public class AllAccessAuthentication implements IAuthenticationPlugin {
+    public AllAccessAuthentication() {
+    }
+    
+    /**
+     * Redirect to the gp login page.
+     */
+    public void requestAuthentication(HttpServletRequest request, HttpServletResponse response) 
+    throws IOException
+    {
+        response.sendRedirect(request.getContextPath() + "/pages/login.jsf");       
+    }
+
+    /**
+     * Parse the http request for username and password pair.
+     */
+    public String authenticate(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String gp_username = request.getParameter("username");
+        String passwordString = request.getParameter("password");
+        if (passwordString == null) {
+            passwordString = request.getParameter("loginForm:password");
+        }
+        byte[] password = null;
+        if (passwordString != null) {
+            password = passwordString.getBytes();
+        }
+        boolean authenticated = authenticate(gp_username, password);
+        if (authenticated) {
+            return gp_username;
+        }
+        return null;
+    }
+
+    /**
+     * Authenticate the username:password by lookup into the GP database.
+     */
+    public boolean authenticate(String username, byte[] password) throws AuthenticationException {
+        return true;
+    }
+
+    public void logout(String userid, HttpServletRequest request, HttpServletResponse response) {
+        //ignore: no action required
+    }
+}
