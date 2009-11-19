17b9f87a9f78c6ba5f403c5e8f2467c704e934fb
pcarr <pcarr@broadinstitute.org>
2009-11-19 17:09:55 +0000

    bug fix update to moduleChooseBean; merged from branches/GenePattern_3.2; GP-2886
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8776 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/ModuleChooserBean.java
+++ src/org/genepattern/server/webapp/jsf/ModuleChooserBean.java
@@ -20,132 +20,122 @@ import java.util.List;
 import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 
-import org.apache.log4j.Logger;
-
 public class ModuleChooserBean implements java.io.Serializable {
-
     private static final long serialVersionUID = -9026970426503039995L;
-
-    private static Logger log = Logger.getLogger(ModuleChooserBean.class);
-
     private List<ModuleCategoryGroup> categoryGroups = null;
-
     private List<SelectItem> modes;
-
     private String selectedModule = "";
-
     private CollapsiblePanelState moduleChooserState;
 
     public ModuleChooserBean() {
-	moduleChooserState = (CollapsiblePanelState) UIBeanHelper.getManagedBean("#{moduleChooserState}");
-	boolean showCategoryView = Boolean.valueOf(System.getProperty("module.chooser.show.category.view", "true"));
-	modes = new ArrayList<SelectItem>(3);
-	if (showCategoryView) {
-	    modes.add(new SelectItem("category"));
-	}
-	boolean showSuiteView = Boolean.valueOf(System.getProperty("module.chooser.show.suite.view", "true"));
-	if (showSuiteView) {
-	    modes.add(new SelectItem("suite"));
-	}
-	boolean showAllView = Boolean.valueOf(System.getProperty("module.chooser.show.all.view", "true"));
-	if (showAllView) {
-	    modes.add(new SelectItem("all"));
-	}
-	if (moduleChooserState.getSelectedMode() == null) {
-	    moduleChooserState.setSelectedMode(modes.get(0).getLabel());
-	}
+        moduleChooserState = (CollapsiblePanelState) UIBeanHelper.getManagedBean("#{moduleChooserState}");
+        boolean showCategoryView = Boolean.valueOf(System.getProperty("module.chooser.show.category.view", "true"));
+        modes = new ArrayList<SelectItem>(3);
+        if (showCategoryView) {
+            modes.add(new SelectItem("category"));
+        }
+        boolean showSuiteView = Boolean.valueOf(System.getProperty("module.chooser.show.suite.view", "true"));
+        if (showSuiteView) {
+            modes.add(new SelectItem("suite"));
+        }
+        boolean showAllView = Boolean.valueOf(System.getProperty("module.chooser.show.all.view", "true"));
+        if (showAllView) {
+            modes.add(new SelectItem("all"));
+        }
+        if (moduleChooserState.getSelectedMode() == null) {
+            moduleChooserState.setSelectedMode(modes.get(0).getLabel());
+        }
     }
 
     public boolean isNoModulesInstalled() {
-	return new ModuleHelper(true).getTasks().getModuleCount() == 0;
+        return new ModuleHelper(true).getTasks().getModuleCount() == 0;
     }
 
     public List<ModuleCategoryGroup> getTasks() {
-	if (categoryGroups == null) {
+        if (categoryGroups == null) {
+            categoryGroups = new ArrayList<ModuleCategoryGroup>();
+            ModuleHelper helper = new ModuleHelper();
+            for (SelectItem item : modes) {
+                String mode = item.getLabel();
+                /* Create the "recent" psuedo category */
+                List<ModuleCategory> tmp = new ArrayList<ModuleCategory>();
+                ModuleCategory recent = helper.getRecentlyUsed();
+                recent.setIdPrefix(mode);
+                recent.setExpanded(!moduleChooserState.isClosed(recent.getIdentifier()));
+                tmp.add(recent);
 
-	    categoryGroups = new ArrayList<ModuleCategoryGroup>();
-
-	    ModuleHelper helper = new ModuleHelper();
-	    for (SelectItem item : modes) {
-		String mode = item.getLabel();
-		/* Create the "recent" psuedo category */
-		List<ModuleCategory> tmp = new ArrayList<ModuleCategory>();
-		ModuleCategory recent = helper.getRecentlyUsed();
-		recent.setIdPrefix(mode);
-		recent.setExpanded(!moduleChooserState.isClosed(recent.getIdentifier()));
-		tmp.add(recent);
-
-		if (mode.equals("all")) {
-		    ModuleCategory cat = helper.getTasks();
-		    cat.setExpanded(!moduleChooserState.isClosed(cat.getIdentifier()));
-		    tmp.add(cat);
-		} else if (mode.equals("suite")) {
-		    for (ModuleCategory cat : helper.getTasksBySuite()) {
-			cat.setExpanded(!moduleChooserState.isClosed(cat.getIdentifier()));
-			tmp.add(cat);
-		    }
-		} else if (mode.equals("category")) {
-		    for (ModuleCategory cat : helper.getTasksByType()) {
-			cat.setExpanded(!moduleChooserState.isClosed(cat.getIdentifier()));
-			tmp.add(cat);
-		    }
-		}
-		categoryGroups.add(new ModuleCategoryGroup(mode, tmp));
-	    }
-	}
-	return categoryGroups;
+                if (mode.equals("all")) {
+                    ModuleCategory cat = helper.getTasks();
+                    cat.setExpanded(!moduleChooserState.isClosed(cat.getIdentifier()));
+                    tmp.add(cat);
+                } 
+                else if (mode.equals("suite")) {
+                    for (ModuleCategory cat : helper.getTasksBySuite()) {
+                        cat.setExpanded(!moduleChooserState.isClosed(cat.getIdentifier()));
+                        tmp.add(cat);
+                    }
+                } 
+                else if (mode.equals("category")) {
+                    for (ModuleCategory cat : helper.getTasksByType()) {
+                        cat.setExpanded(!moduleChooserState.isClosed(cat.getIdentifier()));
+                        tmp.add(cat);
+                    }
+                }
+                categoryGroups.add(new ModuleCategoryGroup(mode, tmp));
+            }
+        }
+        return categoryGroups;
     }
 
     public String getSelectedModule() {
-	return selectedModule;
+        return selectedModule;
     }
 
     public void setSelectedModule(String selectedModule) {
-	this.selectedModule = selectedModule;
-	RunTaskBean runTaskBean = (RunTaskBean) UIBeanHelper.getManagedBean("#{runTaskBean}");
-	if (runTaskBean != null) {
-	    runTaskBean.setTask(selectedModule);
-	}
+        this.selectedModule = selectedModule;
+        RunTaskBean runTaskBean = (RunTaskBean) UIBeanHelper.getManagedBean("#{runTaskBean}");
+        if (runTaskBean != null) {
+            runTaskBean.setTask(selectedModule);
+        }
     }
 
     public void moduleClicked(ActionEvent event) {
-	setSelectedModule(getRequest().getParameter("task"));
+        setSelectedModule(getRequest().getParameter("task"));
     }
 
     public String getUserId() {
-	return UIBeanHelper.getUserId();
+        return UIBeanHelper.getUserId();
     }
 
     public List<SelectItem> getModes() {
-	return modes;
+        return modes;
     }
 
     public class ModuleCategoryGroup implements java.io.Serializable {
-	String mode;
+        String mode;
+        List<ModuleCategory> categories;
 
-	List<ModuleCategory> categories;
+        public ModuleCategoryGroup(String mode, List<ModuleCategory> categories) {
+            this.mode = mode;
+            this.categories = categories;
+        }
 
-	public ModuleCategoryGroup(String mode, List<ModuleCategory> categories) {
-	    this.mode = mode;
-	    this.categories = categories;
-	}
+        public List<ModuleCategory> getCategories() {
+            return categories;
+        }
 
-	public List<ModuleCategory> getCategories() {
-	    return categories;
-	}
-
-	public String getMode() {
-	    return mode;
-	}
+        public String getMode() {
+            return mode;
+        }
     }
 
     public String getSelectedMode() {
-	String mode = moduleChooserState.getSelectedMode();
-	return (mode == null ? "category" : mode);
+        String mode = moduleChooserState.getSelectedMode();
+        return (mode == null ? "category" : mode);
     }
 
     public void setSelectedMode(String selectedMode) {
-	moduleChooserState.setSelectedMode(selectedMode);
+        moduleChooserState.setSelectedMode(selectedMode);
     }
 
 }
