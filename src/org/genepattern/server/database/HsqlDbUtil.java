0bb1c7f7a7111df592a898e70538655e1bbd86e8
pcarr <pcarr@broadinstitute.org>
2009-12-03 06:53:54 +0000

    Refactor HsqlDbUtil.java to improve error handling, and hibernate session management; Abort startupservlet when there are HSQL DB errors. GP-2931
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/GenePattern_3.2@8801 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/database/HsqlDbUtil.java
+++ src/org/genepattern/server/database/HsqlDbUtil.java
@@ -17,13 +17,16 @@ import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import org.apache.log4j.Logger;
 import org.genepattern.server.webservice.server.dao.AnalysisDAO;
 import org.genepattern.server.webservice.server.dao.BaseDAO;
+import org.hsqldb.Server;
 
 public class HsqlDbUtil {
     private static Logger log = Logger.getLogger(HsqlDbUtil.class);
@@ -31,27 +34,69 @@ public class HsqlDbUtil {
     /**
      * HSQL.args= -port 9001  -database.0 file:../resources/GenePatternDB -dbname.0 xdb
      */
-    public static void startDatabase() {
+    public static void startDatabase() throws Throwable {
         log.debug("Starting HSQL Database...");
-        // @todo - get from properites file
+        // TODO - get from properties file
         //  String port = System.getProperty("HSQL_port", "9001");
         //  String dbFile = System.getProperty("HSQL.dbfile", "../resources/GenePatternDB");
         //  String dbUrl = "file:" + dbFile;
         //  String dbName = System.getProperty("HSQL.dbName", "xdb");
         //  String[] args = new String[] { "-port", port, "-database.0", dbUrl, "-dbname.0", dbName };
         String args = System.getProperty("HSQL.args", " -port 9001  -database.0 file:../resources/GenePatternDB -dbname.0 xdb");
-        String argsArray[] = new String[6];
+        
+        //TODO: shooting self in foot, this line is here for debugging, make sure to delete it!!!
+        //args = "-port 1 -database.0 file:../resources/GenePatternDB -dbname.0 xdb";
+
         StringTokenizer strTok = new StringTokenizer(args);
-        int i=0;
+        List<String> argsList = new ArrayList<String>();
+        //int i=0;
         while (strTok.hasMoreTokens()){
             String tok = strTok.nextToken();
-            argsArray[i++] = tok;
+            argsList.add(tok);
         }
-        org.hsqldb.Server.main(argsArray);
-
-        HibernateUtil.beginTransaction();
-        updateSchema();
-        HibernateUtil.commitTransaction();
+        //prevent HSQLDB from calling System.exit when errors occur,
+        //    which is the default behavior when starting the DB using Server.main
+        if (!argsList.contains("-no_system_exit")) {
+            argsList.add("-no_system_exit");
+            argsList.add("true");
+        }
+        String[] argsArray = new String[argsList.size()];
+        argsArray = argsList.toArray(argsArray);
+        //TODO: instead of invoking main, start the server by constructing the server
+        //    so that status can be checked and handled here
+        Server.main(argsArray);
+        
+        try {
+            // 1) ...
+            HibernateUtil.beginTransaction();
+            try {
+                // 2) ...
+                updateSchema();
+            }
+            catch (Throwable t) {
+                // ... 2) can't update the schema
+                try {
+                    HibernateUtil.rollbackTransaction();
+                }
+                catch (Throwable ex2) {
+                    log.error("Error rolling back transaction: "+ex2.getLocalizedMessage(), ex2);
+                }
+                throw new Throwable("Error checking or updating db schema: "+t.getLocalizedMessage(), t);
+            }
+        }
+        catch (Throwable t) {
+            // ... 1) can't even begin a transaction
+            throw new Throwable("Database connection error: "+t.getLocalizedMessage(), t);
+        }
+        finally {
+            //in both cases, attempt to close the session
+            try {
+                HibernateUtil.closeCurrentSession();
+            }
+            catch (Exception e) {
+                log.error("Exception thrown closing database connection: "+e.getLocalizedMessage(), e);
+            }
+        }
     }
 
     public static void shutdownDatabase() {
@@ -72,20 +117,16 @@ public class HsqlDbUtil {
         }
     }
 
-    private static void updateSchema() {
+    private static void updateSchema() 
+    throws Exception 
+    {
         log.debug("Updating schema...");
-        try {
-            String resourceDir = new File(System.getProperty("resources")).getCanonicalPath();
-            if (!checkSchema(resourceDir)) {
-                createSchema(resourceDir);
-                if (!checkSchema(resourceDir)) {
-                    log.error("schema didn't have correct version after creating");
-                    throw new IOException("Unable to successfully update database tables.");
-                }
+        if (!checkSchema()) {
+            createSchema();
+            if (!checkSchema()) {
+                log.error("schema didn't have correct version after creating");
+                //throw new IOException("Unable to successfully update database tables.");
             }
-        }
-        catch (IOException e) {
-            log.error(e);
         }
         log.debug("Updating schema...Done!");
     }
@@ -96,16 +137,15 @@ public class HsqlDbUtil {
      * @param props
      * @return
      */
-    private static boolean checkSchema(String resourceDir) {
-        log.debug("checking schema in resourceDir: "+resourceDir);
+    private static boolean checkSchema() throws Exception {
         boolean upToDate = false;
         String dbSchemaVersion = "";
+
+        BaseDAO dao = new BaseDAO();
         String requiredSchemaVersion = System.getProperty("GenePatternVersion");
         // check schemaVersion
-        String sql = "select value from props where key='schemaVersion'";
         try {
-        	BaseDAO dao = new BaseDAO();
-            ResultSet resultSet = dao.executeSQL(sql, false);
+            ResultSet resultSet = dao.executeSQL("select value from props where key='schemaVersion'", false);
             if (resultSet.next()) {
                 dbSchemaVersion = resultSet.getString(1);
                 upToDate = (requiredSchemaVersion.compareTo(dbSchemaVersion) <= 0);
@@ -116,7 +156,6 @@ public class HsqlDbUtil {
             }
         }
         catch (Exception e) {
-            // 	
             log.info("Database tables not found.  Create new database");
             dbSchemaVersion = "";
         }
@@ -127,30 +166,46 @@ public class HsqlDbUtil {
     }
 
     /**
+     * @return the resources directory, or null if there are configuration errors
+     */
+    private static File getResourceDir() {
+        String resourceDirProp = System.getProperty("resources");
+        if (resourceDirProp == null || resourceDirProp.trim().length() == 0) {
+            log.error("Missing required System property, 'resources': "+resourceDirProp);
+            //TODO: throw exception
+            return null;
+        }
+        File resourceDir = new File(resourceDirProp);
+        if (!resourceDir.canRead()) {
+            log.error("Configuration error: Can't read resources directory: "+resourceDir.getPath());
+            //TODO: throw exception
+            return null;
+        }
+        return resourceDir;
+    }
+    
+    /**
      * 
      * @param resourceDir
      * @param props
      * @throws IOException
      */
-    private static void createSchema(String resourceDir) throws IOException {
-        log.debug("createSchema in resourceDir: "+resourceDir);
+    private static void createSchema() {
+        File resourceDir = getResourceDir();
         final String schemaPrefix = System.getProperty("HSQL.schema", "analysis_hypersonic-");
-        File[] schemaFiles = new File(resourceDir).listFiles(new FilenameFilter() {
+        FilenameFilter schemaFilenameFilter = new FilenameFilter() {
             // INNER CLASS !!!
             public boolean accept(File dir, String name) {
                 return name.endsWith(".sql") && name.startsWith(schemaPrefix);
             }
-        });
-        Arrays.sort(schemaFiles, new Comparator() {
-            public int compare(Object o1, Object o2) {
-                File f1 = (File) o1;
-                File f2 = (File) o2;
+        };
+        File[] schemaFiles = resourceDir.listFiles(schemaFilenameFilter);
+        Arrays.sort(schemaFiles, new Comparator<File>() {
+            public int compare(File f1, File f2) {
                 String name1 = f1.getName();
-                String version1 = name1.substring(schemaPrefix.length(), name1.length()
-                        - ".sql".length());
+                String version1 = name1.substring(schemaPrefix.length(), name1.length() - ".sql".length());
                 String name2 = f2.getName();
-                String version2 = name2.substring(schemaPrefix.length(), name2.length()
-                        - ".sql".length());
+                String version2 = name2.substring(schemaPrefix.length(), name2.length() - ".sql".length());
                 return version1.compareToIgnoreCase(version2);
             }
         });
@@ -171,9 +226,16 @@ public class HsqlDbUtil {
         log.debug("createSchema ... Done!");
     }
 
-    protected static void processSchemaFile(File schemaFile) throws IOException {
-        log.info("updating database from schema " + schemaFile.getCanonicalPath());
-        String all = readFile(schemaFile);
+    protected static void processSchemaFile(File schemaFile) {
+        log.info("updating database from schema " + schemaFile.getPath());
+        String all = null;
+        try {
+            all = readFile(schemaFile);
+        }
+        catch (IOException e) {
+            log.error("database not updated from schema, error reading schema " + schemaFile.getPath(), e);
+            return;
+        }
         while (!all.equals("")) {
             all = all.trim();
             int i = all.indexOf('\n');
@@ -194,19 +256,20 @@ public class HsqlDbUtil {
                 all = "";
             }
             sql = sql.trim();
-            try {
-                log.info("apply SQL-> " + sql);
-                (new BaseDAO()).executeUpdate(sql);
-            }
-            catch (Exception se) {
-            	//log.error(sql);
-                log.error(se);
-            }
+            log.info("apply SQL-> " + sql);
+            BaseDAO dao = new BaseDAO();
+            dao.executeUpdate(sql);
         }
         log.debug("updating database from schema ... Done!");
     }
 
-    static String readFile(File file) throws IOException {
+    /**
+     * @param file
+     * @return the contents of the file as a String
+     * @throws IOException from reader.read
+     */
+    private static String readFile(File file) throws IOException 
+    {
         FileReader reader = null;
         StringBuffer b = new StringBuffer();
         try {
@@ -219,8 +282,7 @@ public class HsqlDbUtil {
                 }
                 b.append(buffer, 0, i);
             }
-        }
-        catch (IOException ioe) {
+            return b.toString();
         }
         finally {
             if (reader != null) {
@@ -231,16 +293,15 @@ public class HsqlDbUtil {
                 }
             }
         }
-        return b.toString();
     }
 
     public static void main(String args[]) {
-        startDatabase();
         try {
+            startDatabase();
             System.in.read();
         }
-        catch (IOException e) {
-            e.printStackTrace();
+        catch (Throwable t) {
+            t.printStackTrace();
         }
     }
 }
