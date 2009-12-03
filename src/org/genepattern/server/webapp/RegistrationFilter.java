b8564626e596d8b87de995e88db49bbe9a54b3dd
pcarr <pcarr@broadinstitute.org>
2009-12-03 07:37:59 +0000

    removed bogus call to HibernateUtil.beginTransaction() from the RegistrationFilter; make sure to close connections in RegisterServerBean; GP-2923
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/GenePattern_3.2@8803 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/RegistrationFilter.java
+++ src/org/genepattern/server/webapp/RegistrationFilter.java
@@ -23,46 +23,32 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.genepattern.server.database.HibernateUtil;
-import org.genepattern.server.util.AccessManager;
 import org.genepattern.server.webapp.jsf.RegisterServerBean;
 
 /**
  * @author Liefeld
- * 
  */
 public class RegistrationFilter implements Filter {
-
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
     public void destroy() {
-
     }
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-            ServletException {
-
-    	HibernateUtil.beginTransaction();
-    	HttpServletRequest req = (HttpServletRequest)request;
-    	if (RegisterServerBean.isRegisteredOrDeclined()){
-    		
-    		chain.doFilter(request, response);
-		      
-    	} else {
-    		// deal with server registration
-    		String uri = req.getRequestURI(); 
-    		if (uri.endsWith("registerServer.jsf") || uri.endsWith("unregisteredServer.jsf")){
-    				
-    			chain.doFilter(request, response);
-    		} else {
-    			 ((HttpServletResponse) response).sendRedirect(req.getContextPath()+"/pages/registerServer.jsf");
-    		}
-    		
-  
-    	}
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+        if (RegisterServerBean.isRegisteredOrDeclined()) { 
+            chain.doFilter(request, response);
+        }
+        else {
+            // deal with server registration
+            String uri = req.getRequestURI(); 
+            if (uri.endsWith("registerServer.jsf") || uri.endsWith("unregisteredServer.jsf")) {	
+                chain.doFilter(request, response);
+            } 
+            else {
+                ((HttpServletResponse) response).sendRedirect(req.getContextPath()+"/pages/registerServer.jsf");
+            }
+        }
     }
-
-    
 }
