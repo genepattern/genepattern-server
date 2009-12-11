3124cddd90c728bb4d85ddc352fa3bee18520d48
pcarr <pcarr@broadinstitute.org>
2009-12-11 00:20:22 +0000

    updated AnalysisTask, make sure to close all db connections; use SQLQuery to update analysis_job.job_status in the db, rather than hibernate entities; merged from branches/GenePattern_3.2; GP-2932
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@8819 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/AnalysisTask.java
+++ src/org/genepattern/server/AnalysisTask.java
@@ -12,37 +12,33 @@
 
 package org.genepattern.server;
 
-import java.util.Iterator;
+import java.util.List;
 import java.util.Vector;
 
 import org.apache.log4j.Logger;
 import org.genepattern.server.database.HibernateUtil;
 import org.genepattern.server.domain.AnalysisJob;
-import org.genepattern.server.domain.AnalysisJobDAO;
 import org.genepattern.server.domain.JobStatus;
 import org.genepattern.server.genepattern.GenePatternAnalysisTask;
 import org.genepattern.util.GPConstants;
 import org.genepattern.webservice.JobInfo;
 import org.genepattern.webservice.OmnigeneException;
-import org.hibernate.HibernateException;
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 
 /**
- * Runnable AnalysisTask - Adapts a Runnable to run within a pre-created thread.
- * This object class is used by AnalysisManager.
+ * Runnable AnalysisTask - Adapts a Runnable to run within a pre-created thread. This object class is used by
+ * AnalysisManager.
+ * 
  */
-
 public class AnalysisTask implements Runnable {
-    /**
-     * maximum number of concurrent jobs to run before next one will have to
-     * wait
-     */
-    private static int NUM_THREADS = 20;
-
     private static Logger log = Logger.getLogger(AnalysisTask.class);
 
+    /**
+     * maximum number of concurrent jobs to run before next one will have to wait
+     */
+    private static int NUM_THREADS = 20;
     private final static String TASK_NAME = GenePatternAnalysisTask.TASK_NAME;
-
     private static AnalysisTask instance;
 
     static {
@@ -56,18 +52,24 @@ public class AnalysisTask implements Runnable {
 
     private Object jobQueueWaitObject = new Object();
     private Vector<JobInfo> jobQueue = new Vector<JobInfo>();
-
     // Semaphore to maintain simultaneous job count
     private Semaphore sem;
-
     private GenePatternAnalysisTask genePattern = new GenePatternAnalysisTask();
-    private AnalysisJobDAO dao = new AnalysisJobDAO();
 
     public AnalysisTask(int threadCount) {
         // create semaphore when thread count > 0
         if (threadCount > 0) {
             sem = new Semaphore(threadCount);
         }
+    }
+
+    /**
+     * Add an object to queue
+     * 
+     * @param o, The feature to be added to the JobToQueue attribute
+     */
+    public void addJobToQueue(Object o) {
+        jobQueue.add((JobInfo) o);
     }
 
     /** Clears the AnalysisTask's queue. */
@@ -80,17 +82,14 @@ public class AnalysisTask implements Runnable {
     }
 
     /**
-     * This is a place holder for doing process, which should be done in beginning
-     * and end of each job
+     * This is placeholder for doing process, which should be done in begining and end of each job
      * 
-     * @param o
-     *            The JobInfo object.
+     * @param jobInfo, The JobInfo object.
      */
     public void onJobProcessFrameWork(JobInfo jobInfo) {
         // if max job running, then wait until some thread to finish
         doAcquire();
-        JobThread jobThread = new JobThread(jobInfo);
-        jobThread.start();
+        new JobThread(jobInfo).start();
     }
 
     /** Main AnalysisTask's thread method. */
@@ -102,8 +101,11 @@ public class AnalysisTask implements Runnable {
                 if (jobQueue.isEmpty()) {
                     // Fetch another batch of jobs.
                     jobQueue = this.getWaitingJobs(NUM_THREADS);
-                    jobQueue = this.updateJobStatusToProcessing(jobQueue);
+                    if (jobQueue != null && !jobQueue.isEmpty()) {
+                        jobQueue = this.updateJobStatusToProcessing(jobQueue);
+                    }
                 }
+
                 if (jobQueue.isEmpty()) {
                     try {
                         // jobQueueWaitObject.wait(waitTime);
@@ -118,14 +120,11 @@ public class AnalysisTask implements Runnable {
             if (!jobQueue.isEmpty()) {
                 o = jobQueue.remove(0);
             }
-
             if (o == null) {
                 continue;
             }
-            
             try {
-                JobInfo jobInfo = (JobInfo) o;
-                onJobProcessFrameWork(jobInfo);
+                onJobProcessFrameWork(o);
             } 
             catch (Exception ex) {
                 log.error(ex);
@@ -134,8 +133,7 @@ public class AnalysisTask implements Runnable {
     }
 
     /**
-     * Wake up the job queue thread. The object is synchronized to obtain
-     * ownership of the monitor.
+     * Wake up the job queue thread. The object is synchronized to obtain ownership of the monitor.
      */
     public void wakeupJobQueue() {
         synchronized (jobQueueWaitObject) {
@@ -157,60 +155,68 @@ public class AnalysisTask implements Runnable {
 
     private Vector<JobInfo> getWaitingJobs(int maxJobCount) throws OmnigeneException {
         Vector<JobInfo> jobVector = new Vector<JobInfo>();
-
-        // Validating taskID is not done here bcos.
-        // assuming once job is submitted, it should be executed even if
-        // taskid is removed from task master
-        HibernateUtil.beginTransaction();
-
-        String hql = "from org.genepattern.server.domain.AnalysisJob "
-                + " where jobStatus.statusId = :statusId order by submittedDate ";
-        Query query = HibernateUtil.getSession().createQuery(hql);
-
-        if (maxJobCount > 0) {
-            query.setMaxResults(maxJobCount);
+        try {
+            HibernateUtil.beginTransaction();
+            String hql = "from org.genepattern.server.domain.AnalysisJob where jobStatus.statusId = :statusId order by submittedDate ";
+            Query query = HibernateUtil.getSession().createQuery(hql);
+            if (maxJobCount > 0) {
+                query.setMaxResults(maxJobCount);
+            }
+            query.setInteger("statusId", JobStatus.JOB_PENDING);
+            List<AnalysisJob> jobList = query.list();
+            for(AnalysisJob aJob : jobList) {
+                JobInfo singleJobInfo = new JobInfo(aJob);
+                jobVector.add(singleJobInfo);
+            }
         }
-
-        query.setInteger("statusId", JobStatus.JOB_PENDING);
-
-        for (Iterator<AnalysisJob> iter = query.list().iterator(); iter.hasNext();) {
-            AnalysisJob aJob = iter.next();
-            JobInfo singleJobInfo = new JobInfo(aJob);
-            // Add waiting job info to vector, for AnalysisTask
-            jobVector.add(singleJobInfo);
+        catch (Throwable t) {
+            log.error("Error getting list of pending jobs from queue", t);
         }
-
+        finally {
+            HibernateUtil.closeCurrentSession();
+        }
         return jobVector;
     }
 
     private Vector<JobInfo> updateJobStatusToProcessing(Vector<JobInfo> jobs) {
         Vector<JobInfo> updatedJobs = new Vector<JobInfo>();
-
-        JobStatus newStatus = (JobStatus) HibernateUtil.getSession().get(JobStatus.class, JobStatus.JOB_PROCESSING);
-        Iterator<JobInfo> iter = jobs.iterator();
-
-        while (iter.hasNext()) {
-            HibernateUtil.beginTransaction();
+        for(JobInfo jobInfo : jobs) {
             try {
-                JobInfo jobInfo = iter.next();
-                AnalysisJob aJob = dao.findById(jobInfo.getJobNumber());
-                aJob.setStatus(newStatus);
-                /**
-                 * Commit each time through since any large CLOBs (>4k) will
-                 * fail if they are committed inside a transaction with ANY
-                 * other object
-                 */
-                HibernateUtil.commitTransaction();
+                // Commit each time through since any large CLOBs (>4k) will fail if they are committed inside a
+                // transaction with ANY other object
                 HibernateUtil.beginTransaction();
+                setJobStatus(jobInfo.getJobNumber(), JobStatus.JOB_PROCESSING);
                 updatedJobs.add(jobInfo);
+                HibernateUtil.commitTransaction();
             } 
-            catch (HibernateException he) {
-                HibernateUtil.rollbackTransaction();
+            catch (Throwable t) {
                 // don't add it to updated jobs, record the failure and move on
-                log.error("Error updating job status to processing in AnalysisTask", he);
-            }
+                log.error("Error updating job status to processing in AnalysisTask", t);
+                HibernateUtil.rollbackTransaction();
+            } 
         }
         return updatedJobs;
+    }
+    
+    /**
+     * @param jobNo
+     * @param jobStatus
+     * @return number of rows successfully updated
+     * @throws Exception if the job status was not successfully updated
+     */
+    private int setJobStatus(int jobNo, int jobStatus) throws Exception {
+        //SQL update statement:
+        //    update analysis_job set status_id=statusId where job_no=jobNo
+        String sqlUpdate = "update ANALYSIS_JOB set status_id=:jobStatus where job_no=:jobNo";
+        SQLQuery sqlQuery = HibernateUtil.getSession().createSQLQuery(sqlUpdate);
+        sqlQuery.setInteger("jobStatus", jobStatus);
+        sqlQuery.setInteger("jobNo", jobNo);
+        
+        int rval = sqlQuery.executeUpdate();
+        if (rval != 1) {
+            throw new Exception("Did not update job status for jobNo="+jobNo);
+        }
+        return rval;
     }
 
     private class JobThread extends Thread {
@@ -222,7 +228,12 @@ public class AnalysisTask implements Runnable {
 
         @Override
         public void run() {
-            genePattern.runJob(jobInfo);// run job
+            if (genePattern == null) {
+                log.error("job not run, genePattern == null!");
+            }
+            else {
+                genePattern.runJob(jobInfo);
+            }
             doRelease();// signal completion of thread
         }
     }
