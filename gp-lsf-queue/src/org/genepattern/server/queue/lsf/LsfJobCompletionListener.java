5b19a956757c0c22317dad5cc2df4583e438dd02
pcarr <pcarr@broadinstitute.org>
2010-03-16 03:23:48 +0000

    updated gp-lsf-queue, initial implementation of LSFJobCompletionListener, which calls back to GenePatternAnalysisTask within a new thread; uses the Future and Callable interfaces in the java.util.concurrent package. GP-2915
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/GenePattern_3.2@8906 48a7a3f7-ad46-0410-b969-29950918272e

--- gp-lsf-queue/src/org/genepattern/server/queue/lsf/LsfJobCompletionListener.java
+++ gp-lsf-queue/src/org/genepattern/server/queue/lsf/LsfJobCompletionListener.java
@@ -1,5 +1,10 @@
 package org.genepattern.server.queue.lsf;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
 import org.apache.log4j.Logger;
 import org.genepattern.server.genepattern.GenePatternAnalysisTask;
 
@@ -12,16 +17,37 @@ import edu.mit.broad.core.lsf.LsfJob.JobCompletionListener;
  */
 public class LsfJobCompletionListener implements JobCompletionListener {
     private static Logger log = Logger.getLogger(LsfJobCompletionListener.class);
+    private static ExecutorService executor = Executors.newFixedThreadPool(3);
 
     public void jobCompleted(LsfJob job) throws Exception {
         log.debug("job completed...lsf_id="+job.getLsfJobId()+", gp_job_id="+job.getInternalJobId());
+        
+        final int jobId = job.getInternalJobId().intValue();
+        final String stdoutFilename = job.getOutputFilename();
+        final String stderrFilename = job.getErrorFileName();
+        final int exitCode = 0;
 
-        int jobId = job.getInternalJobId().intValue();
-        String stdoutFilename = job.getOutputFilename();
-        String stderrFilename = job.getErrorFileName();
-                        
-        //TODO: figure out the exit code
-        int exitCode = 0;
-        GenePatternAnalysisTask.handleJobCompletion(jobId, stdoutFilename, stderrFilename, exitCode);
+        //must run this in a new thread because this callback is run from within a hibernate transaction
+        //and the GenePatternAnalyisTask.handleJobCompletion closes that transaction
+        //See the comments in GenePatternAnalysisTask to see why the transaction must be closed, it is
+        //    related to Oracle CLOBs.
+        FutureTask<Integer> future =
+            new FutureTask<Integer>(new Callable<Integer>() {
+              public Integer call() throws Exception {
+                  int rVal = 0;
+                  GenePatternAnalysisTask.handleJobCompletion(jobId, stdoutFilename, stderrFilename, exitCode);
+                  return rVal;
+            }});
+          executor.execute(future);
+
+          //wait for the thread to complete before exiting
+          try {
+              int statusCode = future.get();
+              log.debug("job info saved to GP database, statusCode="+statusCode);
+          }
+          catch (Exception e) {
+              log.error("Error handling job completion for job #"+jobId);
+              throw e;
+          }
     }
 }
