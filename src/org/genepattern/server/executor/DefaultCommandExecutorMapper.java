dfce5af058c8ba8799b0eaaf1b93056f4e275a7b
pcarr <pcarr@broadinstitute.org>
2010-04-05 17:03:40 +0000

    update to DefaultCommandExecutorMapper, use taskName, taskLsidNoVersion, or taskLsid for mapping from jobs to executors GP-2915
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/GenePattern_3.2@8938 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/executor/DefaultCommandExecutorMapper.java
+++ src/org/genepattern/server/executor/DefaultCommandExecutorMapper.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.genepattern.server.domain.Lsid;
 import org.genepattern.webservice.JobInfo;
 
 final class DefaultCommandExecutorMapper implements CommandExecutorMapper {
@@ -11,7 +12,7 @@ final class DefaultCommandExecutorMapper implements CommandExecutorMapper {
 
     private String defaultCmdExecId="executor.default";
     private Map<String,CommandExecutor> mapCmdExecIdToCmdExec = new HashMap<String,CommandExecutor>();
-    private Map<String,String> mapTaskNameToCmdExecId = new HashMap<String,String>();
+    private Map<String,String> mapTaskToCmdExecId = new HashMap<String,String>();
     
     public void setDefaultCmdExecId(String defaultCmdExecId) {
         this.defaultCmdExecId=defaultCmdExecId;
@@ -22,21 +23,40 @@ final class DefaultCommandExecutorMapper implements CommandExecutorMapper {
     }
 
     public void appendTask(String taskName, String cmdExecId) {
-        mapTaskNameToCmdExecId.put(taskName, cmdExecId);
+        mapTaskToCmdExecId.put(taskName, cmdExecId);
     }
 
     public CommandExecutor getCommandExecutor(JobInfo jobInfo) throws CommandExecutorNotFoundException {
-        String taskName = jobInfo.getTaskName();
         CommandExecutor cmdExec = null;
         String cmdExecId = null;
-
-        cmdExecId = mapTaskNameToCmdExecId.get(taskName);
+        
+        if (jobInfo == null) {
+            log.error("null jobInfo");
+        }
+        else {
+            String taskName = jobInfo.getTaskName();
+            //1. map by task name
+            if (taskName != null) {
+                cmdExecId = mapTaskToCmdExecId.get(taskName);
+            }
+            if (cmdExecId == null) {
+                String taskLsid = jobInfo.getTaskLSID();
+                Lsid lsid = new Lsid(jobInfo.getTaskLSID());
+                //2. map by lsid (including version)
+                cmdExecId = mapTaskToCmdExecId.get(lsid.getLsid());
+                if (cmdExecId == null) {
+                    //3. map by lsid (no version)
+                    cmdExecId = mapTaskToCmdExecId.get(lsid.getLsidNoVersion());
+                } 
+            } 
+        }
+        // otherwise, use default 
         if (cmdExecId == null) {
             cmdExecId = this.defaultCmdExecId;
         }
 
         cmdExec = mapCmdExecIdToCmdExec.get(cmdExecId);
-        //TODO: handle null
+
         if (cmdExec == null) {
             String errorMessage = "no CommandExecutor found for job: "+jobInfo.getJobNumber()+". "+jobInfo.getTaskName();
             throw new CommandExecutorNotFoundException(errorMessage);
