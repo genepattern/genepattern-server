5c09a4c3acee7f722e4c4877f4cc539daa0eb915
pcarr <pcarr@broadinstitute.org>
2010-05-11 00:58:21 +0000

    updated RuntimeCommandExecutor to use Java 5 ExecutorService (fixed thread pool) for running jobs; So that GenePatternAnalysisTask.onJob will exit immediately rather than waiting for the job to complete. GP-2914, GP-2996
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/GenePattern_3.2@8992 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/executor/RuntimeCommandExecutor.java
+++ src/org/genepattern/server/executor/RuntimeCommandExecutor.java
@@ -5,6 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.genepattern.server.domain.JobStatus;
@@ -13,35 +19,71 @@ import org.genepattern.webservice.JobInfo;
 
 public class RuntimeCommandExecutor implements CommandExecutor {
     private static Logger log = Logger.getLogger(RuntimeCommandExecutor.class);
+    
+    private ExecutorService executor = null;
+
+    //the total number of jobs which should be executing concurrently
+    //TODO: enable setting this without requiring a server restart;
+    //    at the moment you must restart your GP server to modify this setting
+    private int numThreads = 20;
 
     public void setConfigurationFilename(String filename) {
         log.error("ignoring: setCofigurationFilename("+filename+")");
     }
 
     public void setConfigurationProperties(Properties properties) {
-        log.error("ignoring setConfigurationProperties");
+        setNumThreads(properties.getProperty("num.threads"));
+    }
+    
+    private void setNumThreads(String numThreadsProp) {
+        if (numThreadsProp == null || numThreadsProp.trim().length() == 0) {
+            //expected
+            numThreads = 20;
+            return;
+        }
+        try {
+            numThreads = Integer.parseInt(numThreadsProp);
+        }
+        catch (NumberFormatException e) {
+            numThreads = 20;
+            log.error("Invalid configuration property: num.threads="+numThreadsProp, e);
+        }
     }
     
     //----- keep references to all currently running jobs which were started by this executor
-    private Map<String,RuntimeExecCommand> runningJobs = new HashMap<String,RuntimeExecCommand>();
-
+    private Map<String,CallableRuntimeExecCommand> runningJobs = new HashMap<String,CallableRuntimeExecCommand>();
 
     public void start() {
+        executor = Executors.newFixedThreadPool(numThreads);
     }
 
     public void stop() {
+        executor.shutdown();
         terminateAll("--> Shutting down server");
+        
+        if (executor != null) {
+            log.debug("stopping executor...");
+            executor.shutdown();
+            try {
+                if (!executor.awaitTermination(30, TimeUnit.SECONDS)) {
+                    log.error("executor shutdown timed out after 30 seconds.");
+                    executor.shutdownNow();
+                }
+            }
+            catch (InterruptedException e) {
+                log.error("executor.shutdown was interrupted", e);
+                Thread.currentThread().interrupt();
+            }
+        }
     }
     
     private void terminateAll(String message) {
         //TODO: globally terminate all running pipelines
         //    pipelines used to be terminated here
-        log.debug(message);
-        
-        for(Entry<String,RuntimeExecCommand> entry : runningJobs.entrySet()) {
-            //String jobID = entry.getKey();
-            RuntimeExecCommand cmd = entry.getValue();
-            cmd.terminateProcess();
+        log.debug(message);        
+        for(Entry<String, CallableRuntimeExecCommand> entry : runningJobs.entrySet()) {
+            CallableRuntimeExecCommand cmd = entry.getValue();
+            cmd.cancel();
             Thread.yield();
         }
     }
@@ -51,24 +93,22 @@ public class RuntimeCommandExecutor implements CommandExecutor {
             File stdoutFile, File stderrFile, JobInfo jobInfo, String stdin,
             StringBuffer stderrBuffer) {
         
-        RuntimeExecCommand cmd = new RuntimeExecCommand();
         String jobId = ""+jobInfo.getJobNumber();
-        runningJobs.put(jobId, cmd);
-        cmd.runCommand(commandLine, environmentVariables, runDir, stdoutFile, stderrFile, jobInfo, stdin, stderrBuffer);
-        runningJobs.remove(jobId);
-        int exitValue = cmd.getExitValue();
-        int jobStatus = JobStatus.JOB_FINISHED;
-        if (RuntimeExecCommand.Status.TERMINATED.equals( cmd.getInternalJobStatus() )) {
-            jobStatus = JobStatus.JOB_ERROR;
-        }
-        if (exitValue != 0) {
-            jobStatus = JobStatus.JOB_ERROR;
-        }
+        CallableRuntimeExecCommand task = new CallableRuntimeExecCommand(commandLine, environmentVariables, runDir, stdoutFile, stderrFile, jobInfo, stdin, stderrBuffer);
+        runningJobs.put(jobId, task);
         try {
-            GenePatternAnalysisTask.handleJobCompletion(jobInfo.getJobNumber(), stdoutFile.getName(), stderrFile.getName(), exitValue, jobStatus);
+            Future<?> f = executor.submit(task);
+            task.setFuture(f);
         }
-        catch (Exception e) {
-            log.error("Error handling job completion for job "+jobInfo.getJobNumber(), e);
+        catch (RejectedExecutionException e) {
+            //TODO: when the queue is full, reset the job status back to PENDING
+            runningJobs.remove(jobId);
+            stderrBuffer.append("job #"+jobId+" was not scheduled for execution");
+            throw(e);
+        }
+        catch (Throwable t) {
+            log.error("unexpected error starting job #"+jobId, t);
+            runningJobs.remove(jobId);
         }
     }
 
@@ -78,14 +118,90 @@ public class RuntimeCommandExecutor implements CommandExecutor {
             return;
         }
         String jobId = ""+jobInfo.getJobNumber();
-        
-        RuntimeExecCommand cmd = runningJobs.get(jobId);
+        CallableRuntimeExecCommand cmd = runningJobs.get(jobId);
         if (cmd == null) {
             //terminateJob is called from deleteJob, so quite often terminateJob should have no effect
             log.debug("terminateJob("+jobInfo.getJobNumber()+"): job not running");
             return;
         }
-        cmd.terminateProcess();
+        cmd.cancel();
     }
+    
+    private class CallableRuntimeExecCommand implements Callable<RuntimeExecCommand> 
+    {
+        private String[] commandLine = null;
+        private Map<String, String> environmentVariables = null; 
+        private File runDir = null;
+        private File stdoutFile = null;
+        private File stderrFile = null;
+        private JobInfo jobInfo = null;
+        private String stdin = null;
+        private StringBuffer stderrBuffer = null;
+        
+        private RuntimeExecCommand cmd = null;
+        
+        private Future<?> future = null;
+        public void setFuture(Future<?> f) {
+            this.future = f;
+        }
 
+        public CallableRuntimeExecCommand(
+                String[] commandLine,
+                Map<String, String> environmentVariables, 
+                File runDir,
+                File stdoutFile, 
+                File stderrFile, 
+                JobInfo jobInfo, 
+                String stdin,
+                StringBuffer stderrBuffer) {
+            this.commandLine = commandLine;
+            this.environmentVariables = environmentVariables;
+            this.runDir = runDir;
+            this.stdoutFile = stdoutFile;
+            this.stderrFile = stderrFile;
+            this.jobInfo = jobInfo;
+            this.stdin = stdin;
+            this.stderrBuffer = stderrBuffer;
+        }
+
+        public RuntimeExecCommand call() throws Exception {
+            cmd = new RuntimeExecCommand();
+            cmd.runCommand(commandLine, environmentVariables, runDir, stdoutFile, stderrFile, jobInfo, stdin, stderrBuffer);
+            String jobId = ""+jobInfo.getJobNumber();
+            runningJobs.remove(jobId);
+            int exitValue = cmd.getExitValue();
+            int jobStatus = JobStatus.JOB_FINISHED;
+            if (RuntimeExecCommand.Status.TERMINATED.equals( cmd.getInternalJobStatus() )) {
+                jobStatus = JobStatus.JOB_ERROR;
+            }
+            if (exitValue != 0) {
+                jobStatus = JobStatus.JOB_ERROR;
+            }
+            try {
+                GenePatternAnalysisTask.handleJobCompletion(jobInfo.getJobNumber(), stdoutFile.getName(), stderrFile.getName(), exitValue, jobStatus);
+            }
+            catch (Exception e) {
+                log.error("Error handling job completion for job "+jobInfo.getJobNumber(), e);
+            }
+            return cmd;
+        }
+        
+        public void cancel() {
+            if (cmd != null) {
+                cmd.terminateProcess();
+            }
+            else {
+                //special-case: job has not started
+                if (future != null) {
+                    future.cancel(false);
+                }
+                try {
+                    GenePatternAnalysisTask.handleJobCompletion(jobInfo.getJobNumber(), stdoutFile.getName(), stderrFile.getName(), -1, JobStatus.JOB_ERROR);
+                }
+                catch (Exception e) {
+                    log.error("Error terminating job "+jobInfo.getJobNumber(), e);
+                }
+            }
+        }
+    }
 }
