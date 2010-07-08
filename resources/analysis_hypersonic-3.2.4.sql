9746c0ad38cca7f1c36425f966844fbac7c9f161
pcarr <pcarr@broadinstitute.org>
2010-07-08 00:44:27 +0000

    bumped version number to 3.2.4
    git-svn-id: https://svn.broadinstitute.org/genepattern/branches/GenePattern_3.2@9068 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ resources/analysis_hypersonic-3.2.4.sql
@@ -0,0 +1,2 @@
+-- update schema version
+update props set value='3.2.4' where key='schemaVersion';
---
+++ resources/analysis_oracle-3.2.4.sql
@@ -0,0 +1,13 @@
+-- update schema version
+INSERT INTO PROPS (KEY, VALUE) VALUES ('registeredVersion3.2.4', '3.2.4');
+
+COMMIT;
+
+
+
+
+
+
+
+
+
