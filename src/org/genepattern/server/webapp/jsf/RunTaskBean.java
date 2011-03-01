a637213591d56824cfe04a524ad3f93bde74aab3
pcarr <pcarr@broadinstitute.org>
2011-03-01 03:05:48 +0000

    changed default 'allow.batch.process' from true to false
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9486 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/RunTaskBean.java
+++ src/org/genepattern/server/webapp/jsf/RunTaskBean.java
@@ -113,7 +113,7 @@ public class RunTaskBean {
         Context userContext = Context.getContextForUser(userId);
         allowNewJob = ServerConfiguration.instance().getGPBooleanProperty(userContext, "allow.new.job", true);
         allowInputFilePaths = ServerConfiguration.instance().getAllowInputFilePaths(userContext);
-        allowBatchProcess = ServerConfiguration.instance().getGPBooleanProperty(userContext, "allow.batch.process", true);
+        allowBatchProcess = ServerConfiguration.instance().getGPBooleanProperty(userContext, "allow.batch.process", false);
     }
 
     public void changeVersion(ActionEvent event) {
