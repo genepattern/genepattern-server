4a09555588458c3a6ef971c890382e09f9105f8c
pcarr <pcarr@broadinstitute.org>
2011-05-13 01:11:19 +0000

    added FileLengthConverter, converter="fl", for displaying file length (in bytes) in human readable form GP-3297
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9647 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/webapp/jsf/FileLengthConverter.java
@@ -0,0 +1,48 @@
+package org.genepattern.server.webapp.jsf;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Converter for formatting file size, as File.length in bytes, in human readable form.
+ * E.g. 12.3 GB, 120 MB, 1 KB et cetera
+ * 
+ * @author pcarr
+ */
+public class FileLengthConverter implements Converter {
+    private static Logger log = Logger.getLogger(FileLengthConverter.class);
+
+    //not implemented
+    public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+        log.error("getAsObject not implemented");
+        return null;
+    }
+
+    public String getAsString(FacesContext arg0, UIComponent arg1, Object param) {
+        Long lVal = paramToLong(param);
+        if (lVal == null) {
+            return null;
+        }
+        return JobHelper.getFormattedSize(lVal);
+    }
+    
+    private Long paramToLong(Object param) {
+        if (param == null) {
+            return null;
+        }
+        if (param instanceof Long) {
+            return (Long) param;
+        }
+        try {
+            return Long.parseLong(param.toString());
+        }
+        catch (NumberFormatException e) {
+            
+        }
+        return null;
+    }
+
+}
