5d2567e14ebde126deed6363ea3521e5ffddca2b
tabor <tabor@broadinstitute.org>
2011-05-24 21:36:34 +0000

    Fixed basic math for 20gb
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9685 48a7a3f7-ad46-0410-b969-29950918272e

--- test/load_testing/upload/uploadTest.sh
+++ test/load_testing/upload/uploadTest.sh
@@ -2,8 +2,8 @@
 curl -v --dump-header cookie.txt -u "test:test" -H user-agent:IGV -d javax.faces.ViewState=javax.faces.ViewState http://gpdev.broadinstitute.org/gp/login.jsf
 
 x=0
-while [ $x -lt 20000 ]
+while [ $x -lt 2000 ]
 do
-  curl -v --cookie cookie.txt -F file=@./test.txt -F partitionCount=20000 -F partitionIndex=$x http://gpdev.broadinstitute.org/gp/UploadReceiver
+  curl -v --cookie cookie.txt -F file=@./test.txt -F partitionCount=2000 -F partitionIndex=$x http://gpdev.broadinstitute.org/gp/UploadReceiver
   x=$(( $x + 1 ))
 done
