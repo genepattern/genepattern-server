20042508f6013a1bd5405ab3b2e9452cd01ace22
pcarr <pcarr@broadinstitute.org>
2011-06-03 21:00:02 +0000

    Fixed bug in Oracle SQL for GenomeSpace integration (By default the GP_USERID is the PK, which means it's indexed)
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9731 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_oracle-3.3.3.sql
+++ resources/analysis_oracle-3.3.3.sql
@@ -6,8 +6,6 @@ constraint gsa_pk primary key (GP_USERID),
 constraint gsa_fk foreign key (GP_USERID) references GP_USER(USER_ID)
 );
 
-CREATE INDEX IDX_GSA_GP_USERID ON GS_ACCOUNT (GP_USERID);
-
 -- update schema version
 INSERT INTO PROPS (KEY, VALUE) VALUES ('registeredVersion3.3.3', '3.3.3');
 
