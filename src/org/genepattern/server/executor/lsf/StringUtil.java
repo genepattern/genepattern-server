f56dc63cbdb593f558848bb34dfe882dfa9f0b1c
pcarr <pcarr@broadinstitute.org>
2011-06-15 19:47:51 +0000

    include LSF Executor in default GP install GP-3242
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9769 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/executor/lsf/StringUtil.java
@@ -0,0 +1,67 @@
+package org.genepattern.server.executor.lsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtil {
+    /**
+     * Split the given command line into tokens delimited by space char, or enclosed within quotes.
+     * Make sure to handle escaped quote characters. don't include enclosing quote characters in the returned strings.
+     * 
+     * @param commandLine
+     * @return
+     */
+    public static List<String> splitCommandLine(String commandLine) {
+        List<String> rval = new ArrayList<String>();
+        
+        int idx = 0;
+        while(true) {
+            int startIdx = nextNonWsIdx(idx, commandLine);
+            if (startIdx >= commandLine.length()) {
+                //no more tokens
+                break;
+            }
+            char delim = ' ';
+            if (commandLine.charAt(startIdx) == '\"') {
+                delim = '\"';
+                ++startIdx;
+            }
+            //jump to the end, ignoring escape ('\') characters
+            int endIdx = nextIdx(1+idx, delim, commandLine);
+            String token = commandLine.substring(startIdx, endIdx);
+            rval.add(token);
+            idx = endIdx + 1;
+        }
+        
+        return rval;
+    }
+
+    /** get the next non whitespace character from the string. */
+    private static int nextNonWsIdx(int idx, String commandLine) {
+        while(idx < commandLine.length()) {
+            char c = commandLine.charAt(idx);
+            if (!Character.isWhitespace(c)) {
+                break;
+            }
+            ++idx;
+        }
+        return idx;
+    }
+    
+    /** get the end index of the current token */
+    private static int nextIdx(int idx, char delim, String commandLine) {
+        while(idx < commandLine.length()) {
+            char c = commandLine.charAt(idx);
+            if (c == '\\') {
+                //escape char
+                ++idx;
+            }
+            else if (c == delim) {
+                return idx;
+            }
+            ++idx;
+        }
+        return idx;
+    }
+
+}
---
+++ test/src/org/genepattern/server/executor/lsf/StringUtilTest.java
@@ -0,0 +1,35 @@
+package org.genepattern.server.executor.lsf;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+public class StringUtilTest extends TestCase {
+
+    public void testSplitCommandLineEmptyString() {
+        List<String> rval = StringUtil.splitCommandLine("");
+        assertNotNull("should return non-null", rval);
+        assertEquals("should return zero length list", 0, rval.size());
+    }
+    
+    public void testSplitCommandLineWithQuotedArg() {
+        List<String> rval = StringUtil.splitCommandLine("lsf_wrapper.sh stdout.txt java -cp a.jar:b.jar:c.jar org.broadinstitute.test.MyTest \"Test One\"");
+        
+        //for debugging
+        for(String token : rval) {
+            System.out.println(token);
+        }
+        
+        assertNotNull(rval);
+        assertEquals(7, rval.size());
+        assertEquals("Test One", rval.get(6));
+    }
+    
+    public void testSplitCommandLineWithEscapedQuote() {
+        List<String> rval = StringUtil.splitCommandLine("\"arg1 has an escaped quote (\\\") character\"");
+        assertNotNull(rval);
+        assertEquals(1, rval.size());
+        assertEquals("arg1 has an escaped quote (\\\") character", rval.get(0));
+    }
+
+}
