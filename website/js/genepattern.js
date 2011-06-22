655c15efed9750ecb0b752ce66498ec0317a55e6
tabor <tabor@broadinstitute.org>
2011-06-22 15:29:07 +0000

    Display upload directory name in Jumploader window
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9809 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/genepattern.js
+++ website/js/genepattern.js
@@ -100,4 +100,15 @@ function ajaxEmailResponse( req ) {
         }
         return params;
     }
+    
+    function gup(name) {
+		name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+		var regexS = "[\\?&]"+name+"=([^&#]*)";
+		var regex = new RegExp( regexS );
+		var results = regex.exec( window.location.href );
+		if( results == null )
+			return "";
+		else
+			return results[1];
+	}
    
