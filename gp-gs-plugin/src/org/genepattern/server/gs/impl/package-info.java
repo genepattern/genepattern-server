b18bc01629183995d02b0263f0aee1896cfe74b4
pcarr <pcarr@broadinstitute.org>
2011-08-24 17:04:16 +0000

    added package-info.java for the GenomeSpace implementation in GP (org.genepattern.server.gs.impl) GP-3338
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9929 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ gp-gs-plugin/src/org/genepattern/server/gs/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * implementation package for the GenomeSpace integration library.
+ * 
+ * This package supports GenomeSpace integration in GenePattern, by implementing the
+ * GP interfaces, making use of the GS CDK library and necessary support classes.
+ * 
+ */
+package org.genepattern.server.gs.impl;
