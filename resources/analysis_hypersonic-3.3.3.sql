a9d6f68461d03e530211e30eabbdc24e79aa940b
pcarr <pcarr@broadinstitute.org>
2011-08-29 19:23:00 +0000

    updated SQL script for 3.3.3 update, added indexes to ANALYSIS_JOB table
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9940 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_hypersonic-3.3.3.sql
+++ resources/analysis_hypersonic-3.3.3.sql
@@ -7,5 +7,24 @@ create table GS_ACCOUNT (
 constraint gsa_fk foreign key (GP_USERID) references GP_USER(USER_ID)
 );
 
+-- improve performance by creating indexes on the ANALYSIS_JOB table
+CREATE INDEX IDX_AJ_STATUS ON ANALYSIS_JOB(STATUS_ID);
+CREATE INDEX IDX_AJ_PARENT ON ANALYSIS_JOB(PARENT);
+
+-- from sge_schema_oracle.sql
+CREATE TABLE JOB_SGE
+(
+    GP_JOB_NO INTEGER NOT NULL,
+    SGE_JOB_ID VARCHAR(4000),
+    SGE_SUBMIT_TIME TIMESTAMP,
+    SGE_START_TIME TIMESTAMP,
+    SGE_END_TIME TIMESTAMP,
+    SGE_RETURN_CODE INTEGER,
+    SGE_JOB_COMPLETION_STATUS VARCHAR(4000)
+);
+
+CREATE INDEX IDX_JOB_SGE_GP_JOB_NO ON JOB_SGE (GP_JOB_NO);
+CREATE INDEX IDX_SGE_JOB_ID on JOB_SGE (SGE_JOB_ID);
+
 -- update schema version
 update props set value='3.3.3' where key='schemaVersion';
--- resources/analysis_oracle-3.3.3.sql
+++ resources/analysis_oracle-3.3.3.sql
@@ -6,6 +6,10 @@ constraint gsa_pk primary key (GP_USERID),
 constraint gsa_fk foreign key (GP_USERID) references GP_USER(USER_ID)
 );
 
+-- improve performance by creating indexes on the ANALYSIS_JOB table
+CREATE INDEX IDX_AJ_STATUS ON ANALYSIS_JOB(STATUS_ID);
+CREATE INDEX IDX_AJ_PARENT ON ANALYSIS_JOB(PARENT);
+
 -- update schema version
 INSERT INTO PROPS (KEY, VALUE) VALUES ('registeredVersion3.3.3', '3.3.3');
 
