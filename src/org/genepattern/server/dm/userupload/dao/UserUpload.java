5aa539e7aaef4ad763d35493e6b47027e8fdeaef
tabor <tabor@broadinstitute.org>
2011-09-06 18:43:57 +0000

    Fixed user upload bug
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9953 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/dm/userupload/dao/UserUpload.java
+++ src/org/genepattern/server/dm/userupload/dao/UserUpload.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -26,14 +27,13 @@ public class UserUpload {
         return uf;
     }
     
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    private UserUpload parentDir;
-    
     @Column(name = "user_id")
     private String userId;
     
     @Id
+    @GeneratedValue
+    private long id;
+    
     private String path;
     private String name;
     
@@ -68,13 +68,13 @@ public class UserUpload {
             }
         }
     }
-
-    public UserUpload getParentDir() {
-        return parentDir;
-    }
     
-    public void setParentDir(UserUpload parentDir) {
-        this.parentDir = parentDir;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getUserId() {
