e985e5e44c10c168c9334465a7b6563cd2a4943a
pcarr <pcarr@broadinstitute.org>
2011-09-06 20:54:54 +0000

    removed unnecessary import statements from UserUpload GP-3388
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@9955 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/dm/userupload/dao/UserUpload.java
+++ src/org/genepattern/server/dm/userupload/dao/UserUpload.java
@@ -5,11 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.genepattern.server.config.ServerConfiguration.Context;
