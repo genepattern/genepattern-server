5a8e218632f7940299142188e8c18698f5ffd35b
pcarr <pcarr@broadinstitute.org>
2011-09-20 20:22:50 +0000

    moved hibernate mapping for SGE plugin from the sge project into the core gp project GP-3367
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10019 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/executor/sge/JobSge.java
@@ -0,0 +1,72 @@
+package org.genepattern.server.executor.sge;
+
+import java.util.Date;
+
+/**
+ * Hibernate mapping class.
+ * @author pcarr
+ *
+ */
+public class JobSge {
+    private int gpJobNo = -1;
+    
+    private String sgeJobId = null;
+    private Date sgeSubmitTime = null;
+    private Date sgeStartTime = null;
+    private Date sgeEndTime = null;
+    private int sgeReturnCode = 0;
+    private String sgeJobCompletionStatus = null;
+    
+    public JobSge() {
+    }
+
+    public int getGpJobNo() {
+        return gpJobNo;
+    }
+    public void setGpJobNo(int gpJobNo) {
+        this.gpJobNo = gpJobNo;
+    }
+
+    public String getSgeJobId() {
+        return sgeJobId;
+    }
+    public void setSgeJobId(String sgeJobId) {
+        this.sgeJobId = sgeJobId;
+    }
+
+    public Date getSgeSubmitTime() {
+        return sgeSubmitTime;
+    }
+    public void setSgeSubmitTime(Date sgeSubmitTime) {
+        this.sgeSubmitTime = sgeSubmitTime;
+    }
+
+    public Date getSgeStartTime() {
+        return sgeStartTime;
+    }
+    public void setSgeStartTime(Date sgeStartTime) {
+        this.sgeStartTime = sgeStartTime;
+    }
+
+    public Date getSgeEndTime() {
+        return sgeEndTime;
+    }
+    public void setSgeEndTime(Date sgeEndTime) {
+        this.sgeEndTime = sgeEndTime;
+    }
+
+    public int getSgeReturnCode() {
+        return sgeReturnCode;
+    }
+    public void setSgeReturnCode(int sgeReturnCode) {
+        this.sgeReturnCode = sgeReturnCode;
+    }
+
+    public String getSgeJobCompletionStatus() {
+        return sgeJobCompletionStatus;
+    }
+    public void setSgeJobCompletionStatus(String sgeJobCompletionStatus) {
+        this.sgeJobCompletionStatus = sgeJobCompletionStatus;
+    }
+
+}
---
+++ src/org/genepattern/server/executor/sge/JobSgeDAO.java
@@ -0,0 +1,34 @@
+package org.genepattern.server.executor.sge;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.genepattern.server.database.BaseDAO;
+import org.genepattern.server.database.HibernateUtil;
+import org.genepattern.webservice.JobInfo;
+
+/**
+ * Hibernate queries into the JOB_SGE table.
+ * @author pcarr
+ */
+public class JobSgeDAO extends BaseDAO {
+    public static Logger log = Logger.getLogger(JobSgeDAO.class);
+
+    public void createOrUpdateJobRecord(JobSge jobRecord) {
+        saveOrUpdate( jobRecord );
+    }
+    
+    /**
+     * Can throw an Exception if the record is not in the table.
+     * @param gpJobInfo
+     * @return null if no record found.
+     */
+    public JobSge getJobRecord(JobInfo gpJobInfo) {
+        JobSge jobSge = null;
+        jobSge = (JobSge) HibernateUtil.getSession().load( JobSge.class, gpJobInfo.getJobNumber());
+        if (jobSge == null) {
+            log.error("No record found in JOB_SGE for gpJobNo="+gpJobInfo.getJobNumber());
+        }
+        return jobSge;
+    }
+}
