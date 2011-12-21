a9bd16ee79486400bd1dfb54624fd13f9ee7a9a0
pcarr <pcarr@broadinstitute.org>
2011-12-21 01:30:10 +0000

    For the PathSeq pipeline, added 'BwaLoop' support code into the serverthread package
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10155 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/executor/serverthread/AbstractServerTask.java
@@ -0,0 +1,225 @@
+package org.genepattern.server.executor.serverthread;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+public abstract class AbstractServerTask implements ServerTask {
+    //serverTask properties
+    protected String userId;
+    protected int jobId = -1;
+    protected String[] args = null;
+
+    final public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    final public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    final public void setArgs(String[] args) {
+        this.args = args;
+    }
+    
+
+    /**
+     * Split command line args into groups of parameter names and parameter values.
+     * 
+     * This is to support idiosyncratic usage where a control-flow task wants to use
+     * the same input values for more than one input parameter.
+     * 
+     * @param arg
+     * @param delim
+     * @return
+     * @throws Exception
+     */
+    static public SplitArg splitSplit(String arg, char delim) throws Exception {
+        String[] entry = splitArg(arg, delim);
+        
+        String paramNameStr = entry[0];
+        String valuesStr = entry[1];
+        
+        String paramNameArray[] = paramNameStr.split("(?<!\\\\),");
+        String valuesArray[] = valuesStr.split("(?<!\\\\),");
+       
+        SplitArg splitArg = new SplitArg();
+        for(String paramName : paramNameArray) {
+            splitArg.addName(paramName);
+        }
+        for(String value : valuesArray) {
+            splitArg.addValue(value);
+        }
+        return splitArg;
+    }
+     
+    /**
+     * Split an entry into two tokens, pname and pvalue, using the given delimiter.
+     *     <pname><delimiter><pvalue>; the delimiter char can be escaped with the '\' character. 
+     * 
+     * Examples:
+     *     name=value --> [name, value]
+     *     name\=b=value--> [name=b, value]
+     * 
+     * @param arg
+     * @return
+     * @throws Exception
+     */
+    static public String[] splitArg(String arg, char delim) throws Exception {
+        String array[] = arg.split("(?<!\\\\)"+delim);
+        
+        if (array == null) {
+            throw new Exception("split returned null");
+        }
+        if (array.length != 2) {
+            throw new Exception("split returned "+array.length+" values");
+        }
+        return array;
+    }
+
+    static public class SplitArg {
+        private List<String> names = new ArrayList<String>();
+        private List<String> values = new ArrayList<String>();
+        
+        public void addName(String name) {
+            names.add(name);
+        }
+        public void addValue(String value) {
+            values.add(value);
+        }
+        
+        public List<String> getNames() {
+            return Collections.unmodifiableList(names);
+        }
+        public List<String> getValues() {
+            return Collections.unmodifiableList(values);
+        }
+    }
+    
+    static public class BatchParams {
+        private static Logger log = Logger.getLogger(BatchParams.class);
+
+        private int numJobsToSubmit = 0;
+        private Map<String,List<String>> map = new HashMap<String,List<String>>();
+        private Set<String> loopParams = new HashSet<String>();
+        
+        /**
+         * Add an entry of the form:
+         *     <name0>,<name1>,...,<nameM-1>=<val0>,<val1>,...,<valN-1>
+         * Examples:
+         *     //add three entries for the 'db' parameter
+         *     db=BWA1,BWA1,BWA2
+         *     //add one entry for the 'e' parameter
+         *     e=0.1
+         *     //add three entries for the 'db2' parameter, using the same value for two different parameter names
+         *     param1,param2=BWA1,BWA2,BWA1
+         * 
+         * @param paramName
+         * @param valuesStr
+         */
+        public void addEntries(String paramNameStr, String valuesStr) {
+            String paramNameArray[] = paramNameStr.split("(?<!\\\\),");
+            String valuesArray[] = valuesStr.split("(?<!\\\\),");
+            List<String> paramNameList = new ArrayList<String>();
+            for(String paramName : paramNameArray) {
+                paramNameList.add(paramName);
+            }
+            List<String> valuesList = new ArrayList<String>();
+            for(String value : valuesArray) {
+                valuesList.add(value);
+            }
+            
+            for(String paramName : paramNameList) {
+                //at the moment, ignore empty lists
+                for(String value : valuesList) {
+                    addEntry(paramName, value);
+                }
+            } 
+        }
+        
+        public void addEntry(String paramName, String paramValue) {
+            List<String> values = map.get(paramName);
+            if (values == null) {
+                values = new ArrayList<String>();
+                map.put(paramName, values);
+            }
+            values.add(paramValue);
+        }
+        
+        public void setLoopParam(String paramName) {
+            this.loopParams.add(paramName);
+        }
+        
+        public void validate() throws Exception {
+            //require that each entry has either one value, or the same number of values as the rest of the batch jobs
+            //e.g.
+            // param_A: valA1,valA2,valA3
+            // param_B: valB1
+            // param_C: valC1,valC2,valC3
+            // param_D: valD1
+            // this set means, there are 3 jobs, but param_B and param_D use the same value for each run
+            int numParamValues = 1;
+            for(List<String> valueList : map.values()) {
+                if (valueList.size() == 1) {
+                    //ignore
+                }
+                else {
+                    if (numParamValues == 1) {
+                        //initialize
+                        numParamValues = valueList.size();
+                    }
+                    else {
+                        //validate
+                        if (numParamValues != valueList.size()) {
+                            throw new Exception("each parameter list must contain the same number of comma-delimited values");
+                        }
+                    }
+                }
+            }
+            numJobsToSubmit = numParamValues;
+        }
+        
+        public int getNumJobsToSubmit() {
+            return numJobsToSubmit;
+        }
+
+        public String get(final String paramName, int idx) throws IndexOutOfBoundsException {
+            
+            String key = paramName;
+
+            List<String> values = map.get(key);
+            if (values == null) {
+                // no matching list for paramName
+                return null;
+            }
+            if (values.size() == 1) {
+                //only 1 item in list, use it
+                return values.get(0);
+            }
+            return values.get(idx);
+        }
+    }
+    
+    /**
+     * Flag indicating the parent job to use when creating the newJob.
+     * What is the parent of the newJob being added to the queue?
+     * The 'thisJob' is the thisJob and 'newJob' is the job being added.
+     * 
+     * 1: add_to_parent, newJob.parent = thisJob.parent
+     * 2: add_to_job, newJob.parent = thisJob.jobId
+     * 3: add_to_root, newJob.parent = thisJob.root
+     * 4: add_to_queue, newJob.parent = null (means add directly to the queue).
+     */
+    public enum AddToFlag {
+        add_to_parent, //add the job to the parent of this job (aka as next sibling), newJob.parent = jobId.parent
+        add_to_child, //add the job as the child of this job newJob.parent = jobId
+        add_to_root, //add the job as a child of the root of this job
+        add_to_queue //add directly to the queue, parent==null
+    }
+}
---
+++ src/org/genepattern/server/executor/serverthread/BwaLoop.java
@@ -0,0 +1,220 @@
+package org.genepattern.server.executor.serverthread;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.genepattern.data.pipeline.PipelineModel;
+import org.genepattern.server.JobManager;
+import org.genepattern.server.database.HibernateUtil;
+import org.genepattern.server.executor.CommandManagerFactory;
+import org.genepattern.server.executor.pipeline.PipelineHandler;
+import org.genepattern.server.jobqueue.JobQueue;
+import org.genepattern.server.webservice.server.dao.AdminDAO;
+import org.genepattern.webservice.JobInfo;
+import org.genepattern.webservice.JobStatus;
+import org.genepattern.webservice.ParameterInfo;
+import org.genepattern.webservice.TaskInfo;
+
+/**
+ * For the PathSeq pipeline, part of the subtraction step.
+ * 
+ * Starting with initial input file in fastq format, run BWA over each database.
+ * Use the output of each run as input to the next run.
+ * 
+ * To use the BwaLoop, create a module with the following command line:
+ * org.genepattern.server.executor.serverthread.BwaLoop BWAPipeline fastq <initial.query.fq> <db.list> 
+ * 
+ * @author pcarr
+ */
+public class BwaLoop extends AbstractServerTask {
+    private static Logger log = Logger.getLogger(BwaLoop.class);
+    
+    //local variables
+    private String tasknameOrLsid = "BWAPipeline";
+    private int parentJobId = -1;
+    private BatchParams batchParams = new BatchParams();
+    private String inputFileParamName = "";
+    private String inheritFilename = "1"; //default, "1", means "use 1st output from"
+    //private boolean addToParent = false; //either add to parent or add to child
+    
+    private AddToFlag addToFlag = AddToFlag.add_to_child;
+
+    public String call() throws Exception {
+        parseArgs();
+        doLoop();
+        return JobStatus.PROCESSING;
+    }
+    
+    private void parseArgs() throws Exception {
+        //1) the first arg must be the tasknameOrLsid 
+        this.tasknameOrLsid = args[0]; //default, "BWAPipeline"
+        //2) the second arg must be the 'inheritFilename'
+        this.inheritFilename = args[1];  //default, 'fastq'
+        
+        //3) hard-code the addTo parameter (not parsed from the command line)
+        addToFlag = AddToFlag.add_to_child;
+        
+        //helper field, in case two or more parameters (by name) have the same filelist value
+        Map<String,List<String>> parameterListCache = new HashMap<String,List<String>>();
+        
+        
+        boolean first = true;
+        for(int idx=2; idx<args.length; ++idx) {
+            String arg = args[idx];
+            SplitArg splitArg = splitSplit(arg, '=');
+            for(final String name : splitArg.getNames()) {
+                for(final String value : splitArg.getValues()) {
+                    if (first) {
+                        //HACK: the first entry in the arg list is the 'use output from prev step' param
+                        inputFileParamName = name;
+                        first = false;
+                    } 
+                    if (isParameterList(value)) {
+                        //do something
+                        List<String> parsedParameters = parameterListCache.get(value);
+                        if (parsedParameters == null) {
+                            parsedParameters = parseParameterList(value);
+                            parameterListCache.put(value,  parsedParameters);
+                        } 
+                        for(String parsedParam : parsedParameters) {
+                            batchParams.addEntry(name, parsedParam);
+                        }
+                    }
+                    else {
+                        batchParams.addEntry(name, value);
+                    }
+                }
+            }
+        }
+        batchParams.validate();
+    }
+    
+    private boolean isParameterList(String value) {
+        return value != null && ( value.endsWith("filelist") || value.endsWith("filelist.txt") );
+    }
+
+    private List<String> parseParameterList(String value) throws Exception {
+        File parameterList = new File(value);
+        List<String> rval = PipelineHandler.parseFileList(parameterList);
+        return rval;
+    }
+
+    private void doLoop() throws Exception {
+        try {
+            HibernateUtil.beginTransaction();
+            JobInfo self = PipelineHandler.getJobInfo(jobId);
+            
+            switch (addToFlag) {
+            case add_to_child:
+                parentJobId = self.getJobNumber();
+                break;
+            //big mess, add_to_root and add_to_parent use the same logic, notice there is no 'break'
+            case add_to_root:
+                log.error("Add to root not implemeneted, adding job to parent");
+            case add_to_parent:
+                parentJobId = self._getParentJobNumber();
+                if (parentJobId < 0) {
+                    parentJobId = self.getJobNumber();
+                }
+                break;
+            case add_to_queue:
+                parentJobId = -1;
+                break;
+            }; 
+            int numJobs = batchParams.getNumJobsToSubmit();
+            for(int stepIdx = 0; stepIdx < numJobs; ++stepIdx) {
+                addStep(stepIdx);
+            }
+            HibernateUtil.commitTransaction();
+            CommandManagerFactory.getCommandManager().wakeupJobQueue();
+        }
+        catch (Throwable t) {
+            HibernateUtil.rollbackTransaction();
+            throw new Exception(t);
+        }
+        finally {
+            HibernateUtil.closeCurrentSession();
+        }
+    }
+
+    /**
+     * Add a new step to the pipeline.
+     * 
+     * currentStepIdx, the counter (starting at 0) for each new job added to the parent pipeline.
+     * @throws Exception
+     */
+    private JobInfo addStep(int currentStepIdx) throws Exception {
+        final boolean first = currentStepIdx==0;
+        
+        boolean isInTransaction = HibernateUtil.isInTransaction(); 
+        HibernateUtil.beginTransaction();
+        try {
+            AdminDAO adminDao = new AdminDAO();
+            TaskInfo taskInfo = adminDao.getTask(tasknameOrLsid, userId);
+            ParameterInfo[] parameterInfoArray = taskInfo.getParameterInfoArray();
+            PipelineHandler.substituteLsidInInputFiles(taskInfo.getLsid(), parameterInfoArray);
+            
+            for(ParameterInfo param : parameterInfoArray) {
+                String value = batchParams.get(param.getName(), currentStepIdx); 
+                if (value == null) {
+                    value = param.getDefaultValue();
+                }
+                if (inputFileParamName.equals(param.getName())) {
+                    if (first) {
+                        param.setValue(value);
+                        param.getAttributes().remove(PipelineModel.INHERIT_FILENAME);
+                        param.getAttributes().remove(PipelineModel.INHERIT_TASKNAME);
+                    }
+                    else {
+                        //final String inheritFilename = "1"; //always use 1st output from
+                        final int inheritTaskIdx = (currentStepIdx - 1); //use output from previous step
+                        param.getAttributes().put(PipelineModel.INHERIT_FILENAME, inheritFilename);
+                        param.getAttributes().put(PipelineModel.INHERIT_TASKNAME, ""+inheritTaskIdx);
+                        param.getAttributes().remove("MODE");
+                    }
+                }
+                else if (value != null) {
+                    param.setValue(value);
+                }
+            }
+            JobQueue.Status initialStatus = JobQueue.Status.WAITING;
+            if (first) {
+                initialStatus = JobQueue.Status.PENDING;
+            }
+            
+            if (parameterInfoArray != null) {
+                for (int i = 0; i < parameterInfoArray.length; i++) {
+                    if (parameterInfoArray[i].isInputFile()) {
+                        String file = parameterInfoArray[i].getValue(); // bug 724
+                        if (file != null && file.trim().length() != 0) {
+                            String val = file;
+                            try {
+                                new URL(file);
+                            } 
+                            catch (MalformedURLException e) {
+                                val = new File(file).toURI().toString();
+                            }
+                            parameterInfoArray[i].setValue(val);
+                            parameterInfoArray[i].getAttributes().remove("TYPE");
+                            parameterInfoArray[i].getAttributes().remove("MODE");
+                        }
+                    }
+                }
+            }
+            
+            JobInfo jobInfo = JobManager.addJobToQueue(taskInfo, userId, parameterInfoArray, parentJobId, initialStatus);
+            return jobInfo;
+        }
+        finally {
+            if (!isInTransaction) {
+                HibernateUtil.closeCurrentSession();
+            }
+        } 
+    }
+
+}
