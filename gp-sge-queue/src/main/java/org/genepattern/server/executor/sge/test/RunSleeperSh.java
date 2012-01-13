086f0891b54c922ee309c597b5c2c8dea8f2ba51
pcarr <pcarr@broadinstitute.org>
2012-01-13 18:54:14 +0000

    SGE plugin: added test program, to test DRMAA job submission from the command line
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10186 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ gp-sge-queue/src/main/java/org/genepattern/server/executor/sge/test/RunSleeperSh.java
@@ -0,0 +1,33 @@
+package org.genepattern.server.executor.sge.test;
+
+import java.util.Collections;
+
+import org.ggf.drmaa.DrmaaException;
+import org.ggf.drmaa.JobTemplate;
+import org.ggf.drmaa.Session;
+import org.ggf.drmaa.SessionFactory;
+
+public class RunSleeperSh {
+
+    public static void main(String[] args) {
+        SessionFactory factory = SessionFactory.getFactory();
+        Session session = factory.getSession();
+        try {
+           session.init("");
+           JobTemplate jt = session.createJobTemplate();
+           jt.setRemoteCommand("sleeper.sh");
+           jt.setArgs(Collections.singletonList("5"));
+
+           String id = session.runJob(jt);
+           System.out.println("Your job has been submitted with id " + id);
+
+           session.deleteJobTemplate(jt);
+           session.exit();
+       } 
+       catch (DrmaaException e) {
+           System.err.println("Error: " + e.getMessage());
+           System.exit(-1);
+       }
+    }
+
+}
