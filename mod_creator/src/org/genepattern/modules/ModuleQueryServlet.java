e5d748492a57e9381049e9248869534b29f4c6d0
nazaire <nazaire@broadinstitute.org>
2012-03-13 00:42:31 +0000

    added upload action
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10315 48a7a3f7-ad46-0410-b969-29950918272e

--- mod_creator/src/org/genepattern/modules/ModuleQueryServlet.java
+++ mod_creator/src/org/genepattern/modules/ModuleQueryServlet.java
@@ -2,17 +2,21 @@ package org.genepattern.modules;
 
 import org.genepattern.webservice.TaskInfoCache;
 import org.genepattern.webservice.TaskInfo;
+import org.genepattern.server.config.ServerConfiguration;
 import org.apache.log4j.Logger;
+import org.apache.commons.fileupload.RequestContext;
+import org.apache.commons.fileupload.FileUploadBase;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletRequestContext;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.Comparator;
-import java.util.Collections;
-import java.io.PrintWriter;
-import java.io.IOException;
+import java.util.*;
+import java.io.*;
 
 /**
  * based on PipelineQueryServer class in the org.genepattern.pipelines class
@@ -22,6 +26,8 @@ public class ModuleQueryServlet extends HttpServlet
     public static Logger log = Logger.getLogger(ModuleQueryServlet.class);
 
     public static final String MODULE_CATEGORIES = "/categories";
+    public static final String UPLOAD = "/upload";
+    
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     {
@@ -32,6 +38,10 @@ public class ModuleQueryServlet extends HttpServlet
         {
             getModuleCategories(response);
         }
+        else if (UPLOAD.equals(action))
+        {
+		    uploadFile(request, response);
+		}
         else
         {
 		    sendError(response, "Routing error for " + action);
@@ -121,4 +131,75 @@ public class ModuleQueryServlet extends HttpServlet
         message.addChild("categories", categories.toString());
         this.write(response, message);
     }
+
+    public void uploadFile(HttpServletRequest request, HttpServletResponse response)
+    {
+	    RequestContext reqContext = new ServletRequestContext(request);
+        if (FileUploadBase.isMultipartContent(reqContext)) {
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            try {
+                List<FileItem> postParameters = upload.parseRequest(reqContext);
+
+                for (FileItem i : postParameters) {
+                    // Only read the submitted files
+                    if (!i.isFormField()) {
+                        // Store in a temp directory until the pipeline is saved
+                        String str = System.getProperty("java.io.tmpdir");
+                        File tempDir = new File(str);
+                        File uploadedFile = new File(tempDir, i.getName());
+                        transferUpload(i, uploadedFile);
+
+                        // Return a success response
+                        ResponseJSON message = new ResponseJSON();
+                        message.addChild("location", uploadedFile.getCanonicalPath());
+                        this.write(response, message);
+                    }
+                    else
+                    {
+                        ResponseJSON message = new ResponseJSON();
+                        message.addChild("formfield", "false");
+                        this.write(response, message);
+                    }
+                }
+            }
+            catch (Exception e) {
+                log.error("error", e);
+                sendError(response, "Exception retrieving the uploaded file");
+            }
+        }
+        else {
+            sendError(response, "Unable to find uploaded file");
+        }
+	}
+
+    private void transferUpload(FileItem from, File to) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+
+        try {
+            is = from.getInputStream();
+            os = new BufferedOutputStream(new FileOutputStream(to, true));
+            final int BUFSIZE = 2048;
+            final byte buf[] = new byte[BUFSIZE];
+            int n;
+            while ((n = is.read(buf)) != -1) {
+                os.write(buf, 0, n);
+            }
+        }
+        finally {
+            if(is != null)
+            {
+                is.close();
+            }
+
+
+            if(os != null)
+            {
+                os.close();
+            }
+
+        }
+    }
+
 }
