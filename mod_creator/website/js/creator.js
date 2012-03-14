10108ceb7fb60625a91a3a62acacdb74e97a68f0
nazaire <nazaire@broadinstitute.org>
2012-03-14 01:17:14 +0000

    fixed updating of choice list text field
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10331 48a7a3f7-ad46-0410-b969-29950918272e

--- mod_creator/website/js/creator.js
+++ mod_creator/website/js/creator.js
@@ -90,7 +90,7 @@ function addparameter()
                             $(this).find("tr td").remove();
 
                             var result = choices.split(';');
-                            if(result == null || result == "" || result.length <= 0)
+                            if(result == null || result == "" || result.length < 0)
                             {
                                 //start with two rows of data
                                 var trowdef = $("<tr><td> <input type='text' name='choicen' size='15'/> </td>" +
@@ -145,7 +145,7 @@ function addparameter()
                                         var dvalue = $(this).find("td input[name='choicen']").val();
                                         var value = $(this).find("td input[name='choicev']").val();
 
-
+                                        alert("dvalue: " + dvalue);
                                         if((dvalue == undefined && value == undefined)
                                            || (dvalue == "" && value==""))
                                         {
@@ -161,7 +161,125 @@ function addparameter()
 
                                     $("input[name='choicelist']").each(function()
                                     {
-                                        var editedParameter  =  $(this).parent().next().data('editing');
+                                        var editedParameter = $(this).parent().next().data('editing');
+                                        if(editedParameter)
+                                        {
+                                            $(this).val(choicelist);
+                                            $(this).parent().next().data('editing', false);
+                                        }
+                                    });
+
+                                    $( this ).dialog( "destroy" );
+                                },
+                                "Cancel": function() {
+                                    var choiceListElement = $("input[name='choicelist']");
+                                    choiceListElement.parent().next().data('editing', false);
+                                    $( this ).dialog( "destroy" );
+                                }
+                        },
+                        resizable: true
+                    });
+
+                    $(this).data("editing", true);
+                });
+
+                $(this).parent().after(editChoiceLink);                
+                $(this).parent().after(editChoiceList);
+            }
+        }
+        else if(value == "InputFile")
+        {
+            if($(this).parent().next().next().text().indexOf("edit") == -1)
+            {
+                var editChoiceList = $("<td><input type='text' name='choicelist' size='40'/></td>");
+                var editChoiceLink = $("<td> <a href='#'>edit choice</a></td>");
+
+                editChoiceLink.click(function()
+                {
+                    var choices = $(this).parent().prev().children().val();
+
+                    $( "#editchoicedialog" ).dialog({
+                        autoOpen: true,
+                        height: 500,
+                        width: 360,
+                        create: function()
+                        {
+                            $(this).find("tr td").remove();
+
+                            var result = choices.split(';');
+                            if(result == null || result == "" || result.length <= 0)
+                            {
+                                //start with two rows of data
+                                var trowdef = $("<tr><td> <input type='text' name='choicen' size='15'/> </td>" +
+                                     "<td> <input type='text' name='choicev' size='15'/> </td>" +
+                                     "<td> <button> X </button></td></tr>");
+
+                                trowdef.find("button").button().click(function()
+                                {
+                                        $(this).parent().parent().remove();
+                                });
+
+                                var trowdef2 = $("<tr><td> <input type='text' name='choicen' size='15'/> </td>" +
+                                     "<td> <input type='text' name='choicev' size='15'/> </td>" +
+                                     "<td> <button> X </button></td></tr>");
+
+                                trowdef2.find("button").button().click(function()
+                                {
+                                        $(this).parent().parent().remove();
+                                });
+
+                                $(this).find("table").append(trowdef);
+                                $(this).find("table").append(trowdef2);
+                                return;
+                            }
+
+                            for(i=0;i<result.length;i++)
+                            {
+                                var rowdata = result[i].split("=");
+
+                                if(rowdata.length > 1)
+                                {
+                                    var trow = $("<tr><td> <input type='text' name='choicen' size='15' value='"
+                                                + rowdata[0] +"'/> </td>" +
+                                            "<td> <input type='text' name='choicev' size='15'value='"
+                                             + rowdata[1] + "'/> </td>" +
+                                            "<td> <button> X </button></td></tr>");
+
+                                    trow.find("button").button().click(function()
+                                    {
+                                        $(this).parent().parent().remove();
+                                    });
+
+                                    $(this).find("table").append(trow);
+                                }
+                            }
+                        },
+                        buttons: {
+                                "OK": function() {
+                                    var choicelist = "";
+                                    $(this).find("tr").each(function()
+                                    {
+                                        var dvalue = $(this).find("td input[name='choicen']").val();
+                                        var value = $(this).find("td input[name='choicev']").val();
+
+                                         alert("dvalue: " + dvalue);
+                                        if((dvalue == undefined && value == undefined)
+                                           || (dvalue == "" && value==""))
+                                        {
+                                            return;
+                                        }
+
+                                        if(choicelist != "")
+                                        {
+                                            choicelist += ";";
+                                        }
+                                        choicelist += dvalue + "=" + value;
+                                    });
+
+                                    $("input[name='choicelist']").each(function()
+                                    {
+                                        var editedParameter=$(this).parent().next().data('editing');
+                                        alert("editing: " + editedParameter());
                                         if(editedParameter)
                                         {
                                             $(this).val(choicelist);
@@ -183,9 +301,6 @@ function addparameter()
 
                     $(this).parent().data("editing", true);
                 });
-
-                $(this).parent().after(editChoiceLink);                
-                $(this).parent().after(editChoiceList);
             }
         }
         else
