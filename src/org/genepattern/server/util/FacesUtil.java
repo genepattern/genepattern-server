cdac950e1d5f5c61afbaa62889869d7a4d54c6b0
tabor <tabor@broadinstitute.org>
2012-07-11 19:01:41 +0000

    Forgot to check in file
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10733 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/util/FacesUtil.java
@@ -0,0 +1,49 @@
+package org.genepattern.server.util;
+
+import javax.faces.FactoryFinder;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextFactory;
+import javax.faces.lifecycle.Lifecycle;
+import javax.faces.lifecycle.LifecycleFactory;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Utility class used for getting the current FacesContext from a servlet
+ * @author tabor
+ */
+public class FacesUtil {
+    public static FacesContext getFacesContext(HttpServletRequest request, HttpServletResponse response) {
+        // Get current FacesContext.
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        // Check current FacesContext.
+        if (facesContext == null) {
+
+            // Create new Lifecycle.
+            LifecycleFactory lifecycleFactory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+            Lifecycle lifecycle = lifecycleFactory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
+
+            // Create new FacesContext.
+            FacesContextFactory contextFactory = (FacesContextFactory) FactoryFinder.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
+            facesContext = contextFactory.getFacesContext(request.getSession().getServletContext(), request, response, lifecycle);
+
+            // Create new View.
+            UIViewRoot view = facesContext.getApplication().getViewHandler().createView(facesContext, "");
+            facesContext.setViewRoot(view);
+
+            // Set current FacesContext.
+            FacesContextWrapper.setCurrentInstance(facesContext);
+        }
+
+        return facesContext;
+    }
+
+    // Wrap the protected FacesContext.setCurrentInstance() in a inner class.
+    private static abstract class FacesContextWrapper extends FacesContext {
+        protected static void setCurrentInstance(FacesContext facesContext) {
+            FacesContext.setCurrentInstance(facesContext);
+        }
+    }
+}
