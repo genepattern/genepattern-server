351a2c3af1c2080395c52eb1522c8590037aad32
tabor <tabor@broadinstitute.org>
2012-09-14 15:19:20 +0000

    * Class for handling external URLs to files in the same way that we handle internal files (implements GpFilePath) ** Necessary for GenomeSpace feature request where external URLs can be sent to the GenomeSpace landing page
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10910 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/dm/ExternalFile.java
@@ -0,0 +1,90 @@
+package org.genepattern.server.dm;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+import org.genepattern.server.config.ServerConfiguration.Context;
+
+/**
+ * Implementation for handling external URLs to files in the way we handle other files
+ * @author tabor
+ */
+public class ExternalFile extends GpFilePath {
+    private static Logger log = Logger.getLogger(ExternalFile.class);
+    
+    private URL url = null;
+    private URI uri = null;
+    
+    public ExternalFile(String url) {
+        setUrl(url);
+    }
+    
+    public ExternalFile(URL url) {
+        setUrl(url);
+    }
+    
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+    
+    public void setUrl(String url) {
+        try {
+            setUrl(new URL(url));
+        }
+        catch (MalformedURLException e) {
+            log.error("Unable to convert to URL: " + url);
+        }
+    }
+    
+    @Override
+    public URL getUrl() throws Exception {
+        return url;
+    }
+
+    @Override
+    public URI getRelativeUri() {
+        if (uri == null && url != null) {
+            try {
+                uri = new URI(url.toString());
+            }
+            catch (URISyntaxException e) {
+                log.error("Unable to convert the URL to a URI: " + url);
+            }
+        }
+        return uri;
+    }
+
+    @Override
+    public File getServerFile() {
+        log.error("Not implemented exception: ExternalFile.getServerFile()");
+        return null;
+    }
+
+    @Override
+    public File getRelativeFile() {
+        log.error("Not implemented exception: ExternalFile.getRelativeFile()");
+        return null;
+    }
+
+    @Override
+    public boolean canRead(boolean isAdmin, Context userContext) {
+        log.error("Not implemented exception: ExternalFile.canRead()");
+        return false;
+    }
+
+    @Override
+    public String getFormFieldValue() {
+        return getName();
+    }
+
+    @Override
+    public String getParamInfoValue() {
+        log.error("Not implemented exception: ExternalFile.getParamInfoValue()");
+        return null;
+    }
+
+}
