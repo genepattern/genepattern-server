f56b056b509aa688302dd9900f6352db5c2a338e
pcarr <pcarr@broadinstitute.org>
2012-09-19 21:27:52 +0000

    source code formatting in ZipSuiteWithDependents.java GP-3950
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10923 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/process/ZipSuiteWithDependents.java
+++ src/org/genepattern/server/process/ZipSuiteWithDependents.java
@@ -26,85 +26,90 @@ import org.genepattern.webservice.TaskInfo;
 
 public class ZipSuiteWithDependents extends ZipSuite {
 
-	/* (non-Javadoc)
-	 * @see org.genepattern.server.process.ZipSuite#packageSuite(org.genepattern.webservice.SuiteInfo, java.lang.String)
-	 */
-	public File packageSuite(SuiteInfo suiteInfo, String userID) throws Exception {
-		String name = suiteInfo.getName();
-		
-		// create zip file
-		File zipFile = File.createTempFile(name, ".zip");
-		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile));
-		try {
-			// find dependent tasks (if a pipeline) and add them to the zip
-			// file as zip files
-			zipDependentTasks(zos, suiteInfo, userID);
-			zos.finish();
-			zos.close();
-			return zipFile;
-		} catch (Exception e) {
-			zos.close();
-			zipFile.delete();
-			throw e;
-		}
-	}
+    /* (non-Javadoc)
+     * @see org.genepattern.server.process.ZipSuite#packageSuite(org.genepattern.webservice.SuiteInfo, java.lang.String)
+     */
+    public File packageSuite(SuiteInfo suiteInfo, String userID) throws Exception {
+        String name = suiteInfo.getName();
 
-	/**
-	 * @param zos
-	 * @param suiteInfo
-	 * @param userID
-	 * @throws Exception
-	 */
-	private void zipDependentTasks(ZipOutputStream zos, SuiteInfo suiteInfo,
-			String userID) throws Exception {
-		File tmpDir = new File(System.getProperty("java.io.tmpdir"), suiteInfo.getName());
-		try {
-			tmpDir.mkdir();
-			File parent = super.packageSuite(suiteInfo, userID);
-			zipFile(zos, parent);
-			parent.delete();
+        // create zip file
+        File zipFile = File.createTempFile(name, ".zip");
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile));
+        try {
+            // find dependent tasks (if a pipeline) and add them to the zip
+            // file as zip files
+            zipDependentTasks(zos, suiteInfo, userID);
+            zos.finish();
+            zos.close();
+            return zipFile;
+        }
+        catch (Exception e) {
+            zos.close();
+            zipFile.delete();
+            throw e;
+        }
+    }
 
-			String[] lsids = suiteInfo.getModuleLsids();
-			ZipTask zt = new ZipTaskWithDependents();
-			Map<Integer, MissingTaskError> errors = new HashMap<Integer, MissingTaskError>();
-			int taskNum = 0;
-			for (String lsid:lsids) {
-				if (lsid == null || lsid.length() == 0) {
-					throw new Exception(
-							"Must specify task name as name argument to this page");
-				}
+    /**
+     * @param zos
+     * @param suiteInfo
+     * @param userID
+     * @throws Exception
+     */
+    private void zipDependentTasks(ZipOutputStream zos, SuiteInfo suiteInfo,
+            String userID) throws Exception {
+        File tmpDir = new File(System.getProperty("java.io.tmpdir"), suiteInfo.getName());
+        try {
+            tmpDir.mkdir();
+            File parent = super.packageSuite(suiteInfo, userID);
+            zipFile(zos, parent);
+            parent.delete();
 
-				TaskInfo taskInfo = null;
-				try {
-					taskInfo = GenePatternAnalysisTask.getTaskInfo(lsid, userID);
-				} catch (OmnigeneException e) {
-					//this is a new task, no taskID exists
-					// do nothing
-					throw new Exception("no such task: " + lsid);
-				}
-				
-				if (taskInfo==null) {
-					MissingTaskError error= new MissingTaskError(suiteInfo.getLsid(), lsid, lsid);
-					errors.put(new Integer(taskNum), error);		
-				}else {
-					File packagedTasks;
-					try {
-						packagedTasks = zt.packageTask(taskInfo, userID);
-					}catch (MissingTaskException e) {
-						throw e;
-					}
-					
-					zipFile(zos, packagedTasks);
-					packagedTasks.delete();
-				}
-				taskNum++;
-			}
-			if (errors.size()>0)
-				throw new MissingTaskException(errors);
-		}catch (MissingTaskException e) {
-			throw e;
-		} finally {
-			tmpDir.delete();
-		}
-	}
+            String[] lsids = suiteInfo.getModuleLsids();
+            ZipTask zt = new ZipTaskWithDependents();
+            Map<Integer, MissingTaskError> errors = new HashMap<Integer, MissingTaskError>();
+            int taskNum = 0;
+            for (String lsid : lsids) {
+                if (lsid == null || lsid.length() == 0) {
+                    throw new Exception("Must specify task name as name argument to this page");
+                }
+
+                TaskInfo taskInfo = null;
+                try {
+                    taskInfo = GenePatternAnalysisTask.getTaskInfo(lsid, userID);
+                }
+                catch (OmnigeneException e) {
+                    //this is a new task, no taskID exists
+                    // do nothing
+                    throw new Exception("no such task: " + lsid);
+                }
+
+                if (taskInfo == null) {
+                    MissingTaskError error = new MissingTaskError(suiteInfo.getLsid(), lsid, lsid);
+                    errors.put(new Integer(taskNum), error);
+                }
+                else {
+                    File packagedTasks;
+                    try {
+                        packagedTasks = zt.packageTask(taskInfo, userID);
+                    }
+                    catch (MissingTaskException e) {
+                        throw e;
+                    }
+
+                    zipFile(zos, packagedTasks);
+                    packagedTasks.delete();
+                }
+                taskNum++;
+            }
+            if (errors.size() > 0)
+                throw new MissingTaskException(errors);
+        }
+        catch (MissingTaskException e) {
+            throw e;
+        }
+        finally {
+            tmpDir.delete();
+        }
+    }
 }
