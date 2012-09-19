8c5517d3818d264b9eaf9181b6b034f9cf30cae2
pcarr <pcarr@broadinstitute.org>
2012-09-19 21:57:37 +0000

    source code formatting in ZipSuite.java GP-3950
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10924 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/process/ZipSuite.java
+++ src/org/genepattern/server/process/ZipSuite.java
@@ -1,7 +1,7 @@
 /*
   The Broad Institute
   SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2011) by the
+  This software and its documentation are copyright (2003-2012) by the
   Broad Institute/Massachusetts Institute of Technology. All rights are
   reserved.
 
@@ -29,150 +29,142 @@ import org.genepattern.util.GPConstants;
 import org.genepattern.webservice.SuiteInfo;
 
 public class ZipSuite extends CommandLineAction {
-	public static final String suiteManifestFileName = "suiteManifest.xml";
+    public static final String suiteManifestFileName = "suiteManifest.xml";
 
-	
-	/**
-	 * @param zos
-	 * @param dir
-	 * @throws Exception
-	 */
-	protected void zipFiles(ZipOutputStream zos, File dir) throws Exception {
-		File[] fileList = dir.listFiles(new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return !name.endsWith(".old") && !name.endsWith(".bak");
-			}
-		});
+    /**
+     * @param zos
+     * @param dir
+     * @throws Exception
+     */
+    protected void zipFiles(ZipOutputStream zos, File dir) throws Exception {
+        File[] fileList = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return !name.endsWith(".old") && !name.endsWith(".bak");
+            }
+        });
+        for (int i = 0; i < fileList.length; i++) {
+            zipFile(zos, fileList[i]);
+        }
+    }
 
-		for (int i = 0; i < fileList.length; i++) {
-			zipFile(zos, fileList[i]);
-		}
+    /**
+     * @param zos
+     * @param f
+     * @throws Exception
+     */
+    protected void zipFile(ZipOutputStream zos, File f) throws Exception {
+        zipFile(zos, f, null);
+    }
 
-	}
+    /**
+     * @param zos
+     * @param f
+     * @param comment
+     * @throws Exception
+     */
+    private void zipFile(ZipOutputStream zos, File f, String comment) throws Exception {
+        try {
+            if (f.isDirectory()) {
+                return;
+            }
+            ZipEntry zipEntry = null;
+            FileInputStream is = null;
 
-	/**
-	 * @param zos
-	 * @param f
-	 * @throws Exception
-	 */
-	protected void zipFile(ZipOutputStream zos, File f) throws Exception {
-		zipFile(zos, f, null);
-	}
+            byte[] buf = new byte[100000];
+            zipEntry = new ZipEntry(f.getName());
+            zipEntry.setTime(f.lastModified());
+            zipEntry.setSize(f.length());
+            if (comment != null) {
+                zipEntry.setComment(comment);
+            }
+            zos.putNextEntry(zipEntry);
+            long fileLength = f.length();
+            long numRead = 0;
+            is = new FileInputStream(f);
+            int n;
+            while ((n = is.read(buf, 0, buf.length)) > 0) {
+                zos.write(buf, 0, n);
+                numRead += n;
+            }
+            is.close();
+            if (numRead != fileLength) {
+                throw new Exception("only read " + numRead + " of "
+                        + fileLength + " bytes in " + f.getPath());
+            }
+            zos.closeEntry();
+        }
+        catch (Exception t) {
+            t.printStackTrace();
+            throw t;
+        }
+    }
 
-	/**
-	 * @param zos
-	 * @param f
-	 * @param comment
-	 * @throws Exception
-	 */
-	private void zipFile(ZipOutputStream zos, File f, String comment)
-			throws Exception {
-		try {
-			if (f.isDirectory()) return;
+    private ZipEntry zipSuiteManifest(ZipOutputStream zos, SuiteInfo suite) throws Exception {
+        // insert manifest
+        ZipEntry zipEntry = new ZipEntry(GPConstants.SUITE_MANIFEST_FILENAME);
+        zos.putNextEntry(zipEntry);
 
-			ZipEntry zipEntry = null;
-			FileInputStream is = null;
-			
-			byte[] buf = new byte[100000];
-			zipEntry = new ZipEntry(f.getName());
-			zipEntry.setTime(f.lastModified());
-			zipEntry.setSize(f.length());
-			if (comment != null)
-				zipEntry.setComment(comment);
-			zos.putNextEntry(zipEntry);
-			long fileLength = f.length();
-			long numRead = 0;
-			is = new FileInputStream(f);
-			int n;
-			while ((n = is.read(buf, 0, buf.length)) > 0) {
-				zos.write(buf, 0, n);
-				numRead += n;
-			}
-			is.close();
-			if (numRead != fileLength) {
-				throw new Exception("only read " + numRead + " of "
-						+ fileLength + " bytes in " + f.getPath());
-			}
-			zos.closeEntry();
-		} catch (Exception t) {
-			t.printStackTrace();
-			throw t;
-		}
-	}
+        OutputStreamWriter osw = new OutputStreamWriter(zos);
+        BufferedWriter bout = new BufferedWriter(osw);
 
-	private ZipEntry zipSuiteManifest(ZipOutputStream zos,	SuiteInfo suite) throws Exception {
-		// insert manifest
-		ZipEntry zipEntry = new ZipEntry(GPConstants.SUITE_MANIFEST_FILENAME);
-		zos.putNextEntry(zipEntry);
+        SuiteInfoManifestXMLGenerator.generateXMLFile(suite, bout);
 
-		OutputStreamWriter osw = new OutputStreamWriter(zos);
-		BufferedWriter bout = new BufferedWriter(osw);
-		
-		SuiteInfoManifestXMLGenerator.generateXMLFile(suite, bout);
-		
-		zos.closeEntry();
-		return zipEntry;
-	}
+        zos.closeEntry();
+        return zipEntry;
+    }
 
-	/**
-	 * @param name
-	 * @param userID
-	 * @return
-	 * @throws Exception
-	 */
-	public File packageSuite(String name, String userID) throws Exception {
+    /**
+     * @param name
+     * @param userID
+     * @return
+     * @throws Exception
+     */
+    public File packageSuite(String name, String userID) throws Exception {
+        if (name == null || name.length() == 0) {
+            throw new Exception(
+                    "Must specify task name as name argument to this page");
+        }
 
-		if (name == null || name.length() == 0) {
-			throw new Exception(
-					"Must specify task name as name argument to this page");
-		}
+        SuiteInfo suite = null;
+        IAdminClient adminClient = new LocalAdminClient(userID);
+        SuiteInfoManifestXMLGenerator.userId = userID;
 
-		SuiteInfo suite = null;
-		
-		IAdminClient adminClient = new LocalAdminClient(userID);
-		SuiteInfoManifestXMLGenerator.userId = userID;
+        suite = adminClient.getSuite(name);
+        return packageSuite(suite, userID);
+    }
 
-		suite = adminClient.getSuite(name);
-		return packageSuite(suite, userID);
-	}
+    /**
+     * @param suiteInfo
+     * @param userID
+     * @return
+     * @throws Exception
+     */
+    public File packageSuite(SuiteInfo suiteInfo, String userID) throws Exception {
+        String name = suiteInfo.getName();
 
-	/**
-	 * @param suiteInfo
-	 * @param userID
-	 * @return
-	 * @throws Exception
-	 */
-	public File packageSuite(SuiteInfo suiteInfo, String userID) throws Exception {
-		String name = suiteInfo.getName();
+        // use an LSID-unique name so that different versions of same named task
+        // don't collide within zip file
+        String suffix = "_"
+                + Integer.toString(Math.abs(suiteInfo.getLSID()
+                        .hashCode()), 36); // [a-z,0-9]
 
-		// use an LSID-unique name so that different versions of same named task
-		// don't collide within zip file
-		String suffix = "_"
-				+ Integer.toString(Math.abs(suiteInfo.getLSID()
-						.hashCode()), 36); // [a-z,0-9]
+        // create zip file
+        File zipFile = File.createTempFile(name + suffix, ".zip");
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile));
 
-		// create zip file
-		File zipFile = File.createTempFile(name + suffix, ".zip");
-		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile));
+        // add the manifest
+        zipSuiteManifest(zos, suiteInfo);
 
-		// add the manifest
-		zipSuiteManifest(zos, suiteInfo);
+        // insert attachments
+        // find $OMNIGENE_ANALYSIS_ENGINE/taskLib/<taskName> to locate DLLs,
+        // other support files
 
-		// insert attachments
-		// find $OMNIGENE_ANALYSIS_ENGINE/taskLib/<taskName> to locate DLLs,
-		// other support files
+        File dir = new File(DirectoryManager.getSuiteLibDir(name, suiteInfo.getLSID(), userID));
+        zipFiles(zos, dir);
+        zos.finish();
+        zos.close();
 
-		File dir = new File(DirectoryManager.getSuiteLibDir(name, suiteInfo.getLSID(), userID));
-		zipFiles(zos, dir);
-		zos.finish();
-		zos.close();
+        return zipFile;
+    }
 
-		return zipFile;
-	}
-
-	
-	
-	
 }
-
-
