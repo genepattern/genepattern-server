07b69b808400971bcb34678a5663d9fbd9874063
pcarr <pcarr@broadinstitute.org>
2012-10-01 22:38:09 +0000

    updated DDL for Oracle, for EULA Record table. Need to create sequence eula_record_SEQ. GP-3959, GP-3978
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@10969 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_oracle-3.4.2.sql
+++ resources/analysis_oracle-3.4.2.sql
@@ -8,6 +8,7 @@ create table eula_record (
     constraint user_id_fk foreign key (user_id) references GP_USER(USER_ID),
     unique (user_id, lsid) 
 );
+create sequence eula_record_SEQ;
 create index idx_eula_record_lsid on eula_record (lsid);
 create index idx_eula_record_user_id on eula_record (user_id); 
 
