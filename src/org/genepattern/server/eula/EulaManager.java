6c4483aa7c084d0d0c386c13c806b6ddeb91f421
pcarr <pcarr@broadinstitute.org>
2012-10-18 16:00:19 +0000

    Added helper method, EulaManager#initEulaInfo(TaskInfo,File) GP-3996
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11031 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/eula/EulaManager.java
+++ src/org/genepattern/server/eula/EulaManager.java
@@ -1,7 +1,11 @@
 package org.genepattern.server.eula;
 
+import java.io.File;
+
 import org.genepattern.server.config.ServerConfiguration;
 import org.genepattern.server.config.ServerConfiguration.Context;
+import org.genepattern.server.eula.EulaInfo.EulaInitException;
+import org.genepattern.webservice.TaskInfo;
 
 public class EulaManager {
     static public IEulaManager instance(Context context) {
@@ -22,5 +26,27 @@ public class EulaManager {
     static private class Singleton {
         private static final IEulaManager INSTANCE = new EulaManagerImpl();
         private static final IEulaManager NO_OP = new EulaManagerNoOp();
+    }
+    
+    /**
+     * Helper method for initializing a EulaInfo for the given taskInfo and licenseFile.
+     * 
+     * @param taskInfo, must be non-null and have a valid lsid
+     * @param licenseFile, must be readable from the current working directory. This same exact path is used when reading the content.
+     * @return
+     * @throws EulaInitException if the taskInfo is null, or has an invalid LSID
+     */
+    final static public EulaInfo initEulaInfo(final TaskInfo taskInfo, final File licenseFile) throws EulaInitException {
+        if (taskInfo==null) {
+            throw new EulaInitException("taskInfo==null");
+        }
+        if (licenseFile==null) {
+            throw new EulaInitException("licenseFile==null");
+        }
+        EulaInfo eula = new EulaInfo();
+        eula.setModuleLsid(taskInfo.getLsid());
+        eula.setModuleName(taskInfo.getName());
+        eula.setLicenseFile(licenseFile);
+        return eula;
     }
 }
