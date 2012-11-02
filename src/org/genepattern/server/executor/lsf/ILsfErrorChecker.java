cd680585067381d701104152edebcc7396697e40
pcarr <pcarr@broadinstitute.org>
2012-11-02 17:55:56 +0000

    added ILsfErrorChecker to project
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11116 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/executor/lsf/ILsfErrorChecker.java
@@ -0,0 +1,16 @@
+package org.genepattern.server.executor.lsf;
+
+/**
+ * Implement this interface so that we can provide more meaningful error message and exitCode
+ * when an LSF job failed because of LSF specific problems, as opposed to module errors.
+ * 
+ * For example,
+ *     1) when the job was terminated by LSF because of insufficient memory
+ *     2) when the job was terminated by the bkill command
+ * 
+ * @author pcarr
+ *
+ */
+public interface ILsfErrorChecker {    
+    LsfErrorStatus getStatus();    
+}
