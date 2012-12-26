99547ccc6622cb4b65dd0bf5bb4f845447029412
tabor <tabor@broadinstitute.org>
2012-12-26 16:16:04 +0000

    Removed prototype calls
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11245 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/moduleChooser.js
+++ website/js/moduleChooser.js
@@ -1,126 +1,126 @@
 function toggleVisibility(id) {
-    var element = document.getElementById(id + "_panel");
-    var panelState = "open";  // default   
-    if(element.style.display=="none")
-    {
-        openCategory(id);
-    }
-    else
-    {
-        closeCategory(id);
-    }
+	var element = document.getElementById(id + "_panel");
+	var panelState = "open"; // default
+	if (element.style.display == "none") {
+		openCategory(id);
+	} else {
+		closeCategory(id);
+	}
 
 }
 
-function closeCategory( id ) {
-    var tableElement = document.getElementById(id + "_panel");
-    var downArrow = document.getElementById(id + "_expanded_img");
-    var rightArrow = document.getElementById(id + "_collapsed_img");	
-        
-    tableElement.style.display="none";
-    downArrow.style.display ="none";
-    rightArrow.style.display ="inline"	   
-    
-    updatePanelState(id, "closed"); 
+function closeCategory(id) {
+	var tableElement = document.getElementById(id + "_panel");
+	var downArrow = document.getElementById(id + "_expanded_img");
+	var rightArrow = document.getElementById(id + "_collapsed_img");
+
+	tableElement.style.display = "none";
+	downArrow.style.display = "none";
+	rightArrow.style.display = "inline"
+
+	updatePanelState(id, "closed");
 }
 
-function openCategory( id ) {
-    var tableElement = document.getElementById(id + "_panel");
-    var downArrow = document.getElementById(id + "_expanded_img");
-    var rightArrow = document.getElementById(id + "_collapsed_img");	
-        
-    tableElement.style.display="block";
-    downArrow.style.display ="inline";
-    rightArrow.style.display ="none"
-    
-    updatePanelState(id, "open");
+function openCategory(id) {
+	var tableElement = document.getElementById(id + "_panel");
+	var downArrow = document.getElementById(id + "_expanded_img");
+	var rightArrow = document.getElementById(id + "_collapsed_img");
+
+	tableElement.style.display = "block";
+	downArrow.style.display = "inline";
+	rightArrow.style.display = "none"
+
+	updatePanelState(id, "open");
 }
-	
+
 function openAll() {
-   var parentPanel = getCurrentModePanel();
-   var panels = parentPanel.getElementsByTagName("div");
-   for(var i=0; i<panels.length; i++) {
-     var catId = panels[i].getAttribute('categoryId');
-     if(catId) {
-        openCategory(catId);
-     }
-   }
+	var parentPanel = getCurrentModePanel();
+	var panels = parentPanel.getElementsByTagName("div");
+	for ( var i = 0; i < panels.length; i++) {
+		var catId = panels[i].getAttribute('categoryId');
+		if (catId) {
+			openCategory(catId);
+		}
+	}
 }
 
-	
 function closeAll() {
-   var parentPanel = getCurrentModePanel();
-   var panels = parentPanel.getElementsByTagName("div");
-   for(var i=0; i<panels.length; i++) {
-     var catId = panels[i].getAttribute('categoryId');
-     if(catId) {
-        closeCategory(catId);
-     }
-   }
+	var parentPanel = getCurrentModePanel();
+	var panels = parentPanel.getElementsByTagName("div");
+	for ( var i = 0; i < panels.length; i++) {
+		var catId = panels[i].getAttribute('categoryId');
+		if (catId) {
+			closeCategory(catId);
+		}
+	}
 }
 
 function getCurrentModePanel() {
-   var radioPanel = document.getElementById("viewchoiceRadioPanel");
-   var radioButtons = radioPanel.getElementsByTagName("input");
-   for(var i=0; i<radioButtons.length; i++) {
-     if(radioButtons[i].type == "radio") {
-       var panelId  = "module_table_" + radioButtons[i].value;
-       var panel = document.getElementById(panelId);
-       if(radioButtons[i].checked && panel != null) {
-         return panel;
-       }
-     }
-   }
-   return null;
-}
-     
-function chooserModeChanged() {	  
-   var radioPanel = document.getElementById("viewchoiceRadioPanel");
-   var radioButtons = radioPanel.getElementsByTagName("input");
-   for(var i=0; i<radioButtons.length; i++) {
-     if(radioButtons[i].type == "radio") {
-       var panelId  = "module_table_" + radioButtons[i].value;
-       var panel = document.getElementById(panelId);
-       if(radioButtons[i].checked && panel != null) {
-         Element.show(panel);
-         updateChooserMode(radioButtons[i].value);
-       }
-       else {
-         Element.hide(panel);
-       }
-     }
-   }
+	var radioPanel = document.getElementById("viewchoiceRadioPanel");
+	var radioButtons = radioPanel.getElementsByTagName("input");
+	for ( var i = 0; i < radioButtons.length; i++) {
+		if (radioButtons[i].type == "radio") {
+			var panelId = "module_table_" + radioButtons[i].value;
+			var panel = document.getElementById(panelId);
+			if (radioButtons[i].checked && panel != null) {
+				return panel;
+			}
+		}
+	}
+	return null;
 }
 
-
-function updateChooserMode(mode) {            
-  var opt = {
-    method:    'get',
-    parameters:  'el=moduleChooserState.updateChooserMode&mode=' + mode,
-    onSuccess: receiveModuleResponse,
-    onFailure: function(t) {
-      alert('Error ' + t.status + ' -- ' + t.statusText);
-    }
-  } 
-  new  Ajax.Request(contextRoot + '/anyThingAtAll.ajax',opt);
+function chooserModeChanged() {
+	var radioPanel = document.getElementById("viewchoiceRadioPanel");
+	var radioButtons = radioPanel.getElementsByTagName("input");
+	for ( var i = 0; i < radioButtons.length; i++) {
+		if (radioButtons[i].type == "radio") {
+			var panelId = "module_table_" + radioButtons[i].value;
+			var panel = document.getElementById(panelId);
+			if (radioButtons[i].checked && panel != null) {
+				Element.show(panel);
+				updateChooserMode(radioButtons[i].value);
+			} else {
+				Element.hide(panel);
+			}
+		}
+	}
 }
 
-        
-function updatePanelState(id, state) {            
-  var opt = {
-    method:    'get',
-    parameters:  'el=moduleChooserState.updatePanelState&id=' + id + '&state=' + state,
-    onSuccess: receiveModuleResponse,
-    onFailure: function(t) {
-      alert('Error ' + t.status + ' -- ' + t.statusText);
-    }
-  } 
-  new  Ajax.Request(contextRoot + '/anyThingAtAll.ajax',opt);
+function updateChooserMode(mode) {
+	jq.ajax({
+		type : "GET",
+		url : contextRoot + '/anyThingAtAll.ajax',
+		data : 'el=moduleChooserState.updateChooserMode&mode=' + mode,
+		dataType : "json",
+		success : function(data, textStatus, jqXHR) {
+			receiveModuleResponse(jqXHR);
+		},
+		error : function(data, textStatus, jqXHR) {
+			alert('Error ' + jqXHR.status + ' -- ' + jqXHR.statusText);
+		}
+	});
+}
+
+function updatePanelState(id, state) {
+	jq.ajax({
+		type : "GET",
+		url : contextRoot + '/anyThingAtAll.ajax',
+		data : 'el=moduleChooserState.updatePanelState&id=' + id + '&state='
+				+ state,
+		dataType : "json",
+		success : function(data, textStatus, jqXHR) {
+			receiveModuleResponse(jqXHR);
+		},
+		error : function(data, textStatus, jqXHR) {
+			alert('Error ' + jqXHR.status + ' -- ' + jqXHR.statusText);
+		}
+	});
 }
 
 // The callback function - receive response from server
-function receiveModuleResponse( req ) {
-  	// server response is empty
+function receiveModuleResponse(req) {
+	// server response is empty
 }
 
 // Code for the combo box
@@ -128,8 +128,10 @@ var comboBoxSelect = 0;
 
 function hideComboBox() {
 	var choicesBox = document.getElementById("comboBoxChoices");
-	setTimeout("document.getElementById('comboBoxChoices').style.display = 'none';", 500);
-	
+	setTimeout(
+			"document.getElementById('comboBoxChoices').style.display = 'none';",
+			500);
+
 	// Clear selection
 	// choicesBox.childNodes[comboBoxSelect].style.backgroundColor = "#DFDFB9";
 	comboBoxSelect = 0;
@@ -138,7 +140,7 @@ function hideComboBox() {
 function showComboBox() {
 	var choicesBox = document.getElementById("comboBoxChoices");
 	choicesBox.style.display = "block";
-	
+
 	// Clear selection
 	// choicesBox.childNodes[comboBoxSelect].style.backgroundColor = "#DFDFB9";
 	comboBoxSelect = 0;
@@ -149,15 +151,18 @@ function handleSelect(e) {
 	// keyCodes for important keys - UP: 38 - DOWN: 40 - ENTER: 13
 	// Handle the enter key
 	if (e.keyCode == 13) {
-		if (e.preventDefault) e.preventDefault();
-	    if (e.stopPropagation) e.stopPropagation();
+		if (e.preventDefault)
+			e.preventDefault();
+		if (e.stopPropagation)
+			e.stopPropagation();
 		choicesBox.childNodes[comboBoxSelect].childNodes[0].onclick();
 	}
-	
+
 	// Handle down
 	if (e.keyCode == 40) {
-		for (var i = comboBoxSelect + 1; i < choicesBox.childNodes.length; i++) {
-			if (choicesBox.childNodes[i].tagName == "DIV" && choicesBox.childNodes[i].style.display == "block") {
+		for ( var i = comboBoxSelect + 1; i < choicesBox.childNodes.length; i++) {
+			if (choicesBox.childNodes[i].tagName == "DIV"
+					&& choicesBox.childNodes[i].style.display == "block") {
 				if (comboBoxSelect != 0) {
 					choicesBox.childNodes[comboBoxSelect].style.backgroundColor = "#DFDFB9";
 				}
@@ -167,11 +172,12 @@ function handleSelect(e) {
 			}
 		}
 	}
-	
+
 	// Handle up
 	if (e.keyCode == 38) {
-		for (var i = comboBoxSelect - 1; i > 0; i--) {
-			if (choicesBox.childNodes[i].tagName == "DIV" && choicesBox.childNodes[i].style.display == "block") {
+		for ( var i = comboBoxSelect - 1; i > 0; i--) {
+			if (choicesBox.childNodes[i].tagName == "DIV"
+					&& choicesBox.childNodes[i].style.display == "block") {
 				if (comboBoxSelect != 0) {
 					choicesBox.childNodes[comboBoxSelect].style.backgroundColor = "#DFDFB9";
 				}
@@ -184,14 +190,15 @@ function handleSelect(e) {
 }
 
 function filterType(e) {
-	var filterFor = document.getElementById("moduleComboBox").value.toLowerCase();
+	var filterFor = document.getElementById("moduleComboBox").value
+			.toLowerCase();
 	var choicesBox = document.getElementById("comboBoxChoices");
-	
-	for (var i = 0; i < choicesBox.childNodes.length; i++) {
-		if (choicesBox.childNodes[i].tagName == "DIV" && choicesBox.childNodes[i].id.toLowerCase().indexOf(filterFor) != -1) {
+
+	for ( var i = 0; i < choicesBox.childNodes.length; i++) {
+		if (choicesBox.childNodes[i].tagName == "DIV"
+				&& choicesBox.childNodes[i].id.toLowerCase().indexOf(filterFor) != -1) {
 			choicesBox.childNodes[i].style.display = "block";
-		}
-		else if (choicesBox.childNodes[i].tagName == "DIV") {
+		} else if (choicesBox.childNodes[i].tagName == "DIV") {
 			choicesBox.childNodes[i].style.display = "none";
 		}
 	}
