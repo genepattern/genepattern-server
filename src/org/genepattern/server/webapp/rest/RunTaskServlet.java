6e02a13e73621b50c3dc2acb59d03c8a16eb5433
nazaire <nazaire@broadinstitute.org>
2013-01-17 03:49:21 +0000

    added file upload functionality
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11299 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/rest/RunTaskServlet.java
+++ src/org/genepattern/server/webapp/rest/RunTaskServlet.java
@@ -3,12 +3,14 @@ package org.genepattern.server.webapp.rest;
 import org.genepattern.util.GPConstants;
 import org.genepattern.util.LSID;
 import org.genepattern.server.TaskLSIDNotFoundException;
+import org.genepattern.server.config.ServerConfiguration;
 import org.genepattern.webservice.TaskInfo;
 import org.genepattern.webservice.TaskInfoCache;
 import org.genepattern.webservice.TaskInfoAttributes;
 import org.genepattern.webservice.ParameterInfo;
 import org.genepattern.modules.ModuleJSON;
 import org.genepattern.modules.ParametersJSON;
+import org.genepattern.modules.ResponseJSON;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 import org.json.JSONArray;
@@ -19,9 +21,13 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.Comparator;
 import java.util.ArrayList;
+import java.io.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 /**
  * Created by IntelliJ IDEA.
@@ -46,7 +52,7 @@ public class RunTaskServlet extends HttpServlet
 
     @GET
     //@Path("/load/{lsid}")
-    @Path("/load/")
+    @Path("/load")
     @Produces(MediaType.APPLICATION_JSON)
     public Response loadModule(@QueryParam("lsid") String lsid, @Context HttpServletRequest request)
     {
@@ -92,6 +98,82 @@ public class RunTaskServlet extends HttpServlet
             );
         }
 	}
+
+    @POST
+    @Path("/upload")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+        @FormDataParam("ifile") InputStream uploadedInputStream,
+        @FormDataParam("ifile") FormDataContentDisposition fileDetail,
+        @Context HttpServletRequest request)
+    {
+        try
+        {
+            String username = (String) request.getSession().getAttribute("userid");
+            if (username == null)
+            {
+                throw new Exception("User not logged in");
+            }
+
+            ServerConfiguration.Context userContext = ServerConfiguration.Context.getContextForUser(username);
+            File userUploadDir = ServerConfiguration.instance().getUserUploadDir(userContext);
+            File tempDir = File.createTempFile("run", null, userUploadDir);
+            tempDir.delete();
+            tempDir.mkdir();
+
+            File uploadedFile = new File(tempDir, fileDetail.getFileName());
+            String uploadedFilePath = uploadedFile.getCanonicalPath();
+
+            // save it
+            writeToFile(uploadedInputStream, uploadedFilePath);
+
+            String output = "File uploaded to : " + uploadedFilePath;
+            log.error(output);
+
+            ResponseJSON result = new ResponseJSON();
+            result.addChild("location", uploadedFile.getCanonicalPath());
+            return Response.ok().entity(result.toString()).build();
+        }
+        catch(Exception e)
+        {
+            String message = "An error occurred while uploading the file \"" + fileDetail.getFileName() + "\"";
+            if(e.getMessage() != null)
+            {
+                message = message + ": " + e.getMessage();
+            }
+            log.error(message);
+
+            throw new WebApplicationException(
+                Response.status(Response.Status.BAD_REQUEST)
+                    .entity(message)
+                    .build()
+            );
+        }
+    }
+
+    // save uploaded file to new location
+    private void writeToFile(InputStream uploadedInputStream,
+        String uploadedFileLocation) {
+
+        try {
+            OutputStream out = new FileOutputStream(new File(
+                    uploadedFileLocation));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 
     private JSONArray getParameterList(ParameterInfo[] pArray)
     {
