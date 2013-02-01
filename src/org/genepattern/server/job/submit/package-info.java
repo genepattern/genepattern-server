9e79fb328fb1d336b0963f3922cc16051772e362
pcarr <pcarr@broadinstitute.org>
2013-02-01 17:47:08 +0000

    removing files from job/submit folder, GP-4129
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11335 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/job/submit/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * GenePattern Job Manager package. 
- * This package and related <em>sub-packages</em> are for managing GenePattern job submissions.
- */
-package org.genepattern.server.job.submit;
