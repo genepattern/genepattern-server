ab171b7890f5152602145ce47e2e169f073ac83a
pcarr <pcarr@broadinstitute.org>
2013-02-01 20:53:50 +0000

    added jUnit tests for initExternalUrl GP-4129
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11339 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ test/src/org/genepattern/server/job/input/TestExternalUrl.java
@@ -0,0 +1,75 @@
+package org.genepattern.server.job.input;
+
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * jUnit tests for the the external url methods in the *.job.input.* package.
+ * 
+ * @author pcarr
+ *
+ */
+public class TestExternalUrl {
+    private String gpUrl;    
+    private String jobResultUrlFq;
+    
+    @Before
+    public void before() {
+        if (System.getProperty("GenePatternURL")==null) {
+            System.setProperty("GenePatternURL", "http://genepattern.broadinstitute.org/gp/");
+        }
+        this.gpUrl = System.getProperty("GenePatternURL", "http://genepattern.broadinstitute.org/gp/");
+        
+        final String relPath="jobResults/8805/stdout.txt";
+        jobResultUrlFq=gpUrl;
+        if (!gpUrl.endsWith("/")) {
+            jobResultUrlFq+="/";
+        }
+        jobResultUrlFq+=relPath;
+    }
+
+    @After
+    public void after() {
+    }
+    
+    @Test
+    public void gpUrl() {
+        URL url=ParamListHelper.initExternalUrl(gpUrl);
+        Assert.assertNull("Not an external URL: "+gpUrl, url);
+    }
+    
+    @Test
+    public void jobResultUrl() {
+        URL url=ParamListHelper.initExternalUrl(jobResultUrlFq);
+        Assert.assertNull("Not an external URL: "+jobResultUrlFq, url);
+    }
+    
+    @Test
+    public void externalFtp() {
+        final String value="ftp://ftp.broadinstitute.org/pub/genepattern/datasets/all_aml/all_aml_test.cls";
+        URL url=ParamListHelper.initExternalUrl(value);
+        Assert.assertNotNull(url);
+        Assert.assertEquals(value, url.toExternalForm());
+    }
+    
+    @Test
+    public void externalHttp() {
+        final String value="http://www.broadinstitute.org/cancer/software/genepattern/tutorial/linkedFiles/sample.cdt";
+        URL url=ParamListHelper.initExternalUrl(value);
+        Assert.assertNotNull(url);
+        Assert.assertEquals(value, url.toExternalForm());
+
+    }
+    
+    @Test
+    public void serverFilePath() {
+        final String value="/xchip/sqa/TestFiles/all_aml_test.cls";
+        URL url=ParamListHelper.initExternalUrl(value);
+        Assert.assertNull("Not an external URL: ", url);
+    }
+
+}
