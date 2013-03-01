7b32df1a6e2d4248ca536647766534438db237e0
pcarr <pcarr@broadinstitute.org>
2013-03-01 00:22:24 +0000

    Set the version number to 3.5.2
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11406 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ resources/analysis_oracle-3.5.2.sql
@@ -0,0 +1,37 @@
+create table job_input (
+    id number(10,0) identity not null, 
+    job_id integer,
+    name varchar2 (511),
+    user_value varchar2 (4000), 
+    cmd_value varchar2 (4000),  
+    type varchar2 (255),  
+    unique (job_id, name),
+    CONSTRAINT JI_PK PRIMARY KEY (id),
+);
+create index idx_job_input_job_id on job_input (job_id);
+
+create table job_input_attribute (
+    id number(10,0) identity not null, 
+    input_id number(10,0),
+    name varchar2 (255),
+    val varchar2 (4000), 
+    CONSTRAINT JIA_PK PRIMARY KEY (id),
+);
+create index idx_job_input_attribute_input_id on job_input_attribute (input_id);
+
+create table job_result (
+    id number(10,0) identity not null, 
+    job_id integer,
+    name varchar2 (511),
+    path varchar2 (511), 
+    log number (1,0) default 0 not null,  
+    unique (job_id, name),
+    CONSTRAINT JR_PK PRIMARY KEY (id),
+);
+create index idx_job_result_job_id on job_result (job_id);
+
+-- update schema version
+insert into PROPS (KEY, VALUE) VALUES ('registeredVersion3.5.2', '3.5.2');
+
+commit;
+
