0caf2ee60a99de71232020f4d6424fe152e3ac75
pcarr <pcarr@broadinstitute.org>
2013-03-28 20:06:40 +0000

    updated REST call to GET /gp/rest/v1/tasks/{taskNameOrLsid}; the method for creating the JSON representation of a task is hard-coded into the TaskResource servlet.     (This helps me figure out which input parameters are of type 'java.io.File' before submitting a job from a REST client) GP-4102
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11538 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/rest/api/v1/task/TasksResource.java
+++ src/org/genepattern/server/webapp/rest/api/v1/task/TasksResource.java
@@ -1,41 +1,24 @@
 package org.genepattern.server.webapp.rest.api.v1.task;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.apache.log4j.Logger;
-import org.genepattern.data.pipeline.PipelineDependencyHelper;
-import org.genepattern.modules.ModuleJSON;
-import org.genepattern.modules.ParametersJSON;
 import org.genepattern.server.config.ServerConfiguration;
-import org.genepattern.server.webapp.jsf.AuthorizationHelper;
 import org.genepattern.server.webapp.rest.api.v1.Util;
 import org.genepattern.server.webservice.server.local.LocalAdminClient;
-import org.genepattern.server.webservice.server.local.LocalTaskIntegratorClient;
-import org.genepattern.util.GPConstants;
-import org.genepattern.util.LSID;
-import org.genepattern.util.LSIDUtil;
 import org.genepattern.webservice.ParameterInfo;
 import org.genepattern.webservice.TaskInfo;
-import org.genepattern.webservice.TaskInfoAttributes;
-import org.genepattern.webservice.TaskInfoCache;
 import org.genepattern.webservice.WebServiceException;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.sun.jersey.api.Responses;
@@ -50,176 +33,121 @@ import com.sun.jersey.api.Responses;
    </pre>
    <p>Or by task lsid:
    <pre>
-   curl -u test:test http://127.0.0.1:8080/gp/rest/v1/tasks/ComparativeMarkerSelection
-   </pre>
    curl -u test:test http://127.0.0.1:8080/gp/rest/v1/tasks/urn:lsid:broad.mit.edu:cancer.software.genepattern.module.analysis:00044:9
+   </pre>
    
+   This returns a JSON representation of the task, for example,
+   <pre>
+{
+"self":"http://127.0.0.1:8080/gp/rest/v1/tasks/urn:lsid:broad.mit.edu:cancer.software.genepattern.module.test.analysis:00007:0.1",
+"name":"TestJavaWrapper",
+"lsid":"urn:lsid:broad.mit.edu:cancer.software.genepattern.module.test.analysis:00007:0.1",
+"params":[
+  {"text": {
+     "attributes": {
+       "default_value":"",
+       "optional":"on",
+       "prefix_when_specified":"--text=",
+       "type":"java.lang.String",
+       "fileFormat":""}}},
+       
+   ...
+   
+  {"file":{
+    "attributes":{
+      "default_value":"",
+      "optional":"on",
+      "prefix_when_specified":"--file=",
+      "MODE":"IN",
+      "type":"java.io.File",
+      "TYPE":"FILE",
+      "fileFormat":""}}}
+]
+}
+
+   </pre>
+ *
+ * The value of the lower-case 'type' attribute should be used when testing for the type of input parameter.
+ * Here are some example values:
+   <pre>
+"type":"java.io.File"
+"type":"java.lang.String"
+"type":"java.lang.Integer"
+"type":"java.lang.Float"
+"type":"DIRECTORY"
+"type":"PASSWORD"
+   </pre>
+ *   
  * 
  * @author pcarr
  *
  */
 @Path("/v1/tasks")
 public class TasksResource {
-    final static private Logger log = Logger.getLogger(TasksResource.class);
-    private static class TaskNotFoundException extends Exception {
-        public TaskNotFoundException(String message) {
-            super(message);
-        }
-        public TaskNotFoundException(String message, Throwable t) {
-            super(message, t);
-        }
-    }
 
-    @Context
-    UriInfo uriInfo;
-    @Context
-    Request request;
-    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("{lsid}")
-    public Response getTask(@PathParam("lsid") String lsid, @Context HttpServletRequest request) {
-        //Note: not using the userContext instance, but the method does throw an exception if
-        //    there is not a current user
+    @Path("{taskNameOrLsid}")
+    public Response getTask(
+            final @Context UriInfo uriInfo,
+            final @PathParam("taskNameOrLsid") String taskNameOrLsid,
+            final @Context HttpServletRequest request
+            ) {
         ServerConfiguration.Context userContext=Util.getUserContext(request);
-
-        JSONObject responseObject = null;
-        try {
-            responseObject=initTaskResponse(lsid, userContext.getUserId());
-        }
-        catch (TaskNotFoundException e) {
-            return Responses.notFound().entity(e.getLocalizedMessage()).build();
-        }
-        catch (WebApplicationException e) {
-            //send this along to the calling method, to formulate an HTTP error response
-            throw e;
-        }
-        catch (JSONException e) {
-            return Response.serverError()
-                    .entity("Error formatting JSON response for lsid="+lsid+": "+e.getLocalizedMessage())
-                    .build();
-        }
-        catch (Throwable t) {
-            return Response.serverError()
-                    .entity("Error getting task for lsid="+lsid+": "+t.getLocalizedMessage())
-                    .build();
-        }
-        return Response.ok().entity(responseObject.toString()).build();
-    }
-
-    private JSONObject initTaskResponse(final String lsid, final String username)
-    throws TaskNotFoundException, JSONException
-    {
+        final String userId=userContext.getUserId();
         TaskInfo taskInfo = null;
         try {
-            taskInfo=getTaskInfo(lsid, username);
+            taskInfo=getTaskInfo(taskNameOrLsid, userId);
         }
         catch (Throwable t) {
-            throw new TaskNotFoundException("Error initializing task lsid=" + lsid + " for user " + username, t);
+            return Responses.notFound().entity(t.getLocalizedMessage()).build();
         }
         if(taskInfo == null) {
-            throw new TaskNotFoundException("No task with task id: " + lsid + " found " + "for user " + username);
+            String errorMessage="No task with task id: " + taskNameOrLsid + " found " + "for user " + userId;
+            return Responses.notFound().entity(errorMessage).build();
         }
         
-        List<String> moduleVersions=null;
+        //form a JSON response, from the given taskInfo
+        JSONObject jsonObj=null;
         try {
-            moduleVersions=getModuleVersions(taskInfo);
-        }
-        catch (MalformedURLException e) {
-            throw new WebApplicationException(
-                    Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("Error initializing moduleVersions for task=" + lsid)
-                    .build()
-                );
-        }
-        
-        ModuleJSON moduleObject = new ModuleJSON(taskInfo, null);
-        moduleObject.put("lsidVersions", new JSONArray(moduleVersions));
-
-        //check if user is allowed to edit the module
-        boolean createModuleAllowed = AuthorizationHelper.createModule(username);
-        boolean editable = createModuleAllowed && taskInfo.getUserId().equals(username)
-                && LSIDUtil.getInstance().isAuthorityMine(taskInfo.getLsid());
-        moduleObject.put("editable", editable);
-
-        //check if the user is allowed to view the module
-        boolean isViewable = true;
-
-        //check if the module has documentation
-        boolean hasDoc = true;
-
-        File[] docFiles = null;
-        try {
-            LocalTaskIntegratorClient taskIntegratorClient = new LocalTaskIntegratorClient(username);
-            docFiles = taskIntegratorClient.getDocFiles(taskInfo);
-
-            if(docFiles == null || docFiles.length == 0) {
-                hasDoc = false;
+            jsonObj=new JSONObject();
+            URI baseUri=uriInfo.getBaseUri();
+            String self=baseUri.toURL().toExternalForm();
+            if (!self.endsWith("/")) {
+                self += "/";
             }
+            self += "v1/tasks/" + taskInfo.getLsid();
+            jsonObj.put("self", self);
+            jsonObj.put("name", taskInfo.getName());
+            jsonObj.put("lsid", taskInfo.getLsid());
+            JSONArray paramsJson=new JSONArray();
+            for(ParameterInfo pinfo : taskInfo.getParameterInfoArray()) {
+                final JSONObject attributesJson = new JSONObject();
+                for(final Object key : pinfo.getAttributes().keySet()) {
+                    final Object value = pinfo.getAttributes().get(key);
+                    if (value != null) {
+                        attributesJson.put(key.toString(), value.toString());
+                    }
+                }
+                final JSONObject attrObj = new JSONObject();
+                attrObj.put("attributes", attributesJson);
+                final JSONObject paramJson = new JSONObject();
+                paramJson.put(pinfo.getName(), attrObj);
+                paramsJson.put(paramJson);
+            }
+            jsonObj.put("params", paramsJson);
         }
-        catch (WebServiceException e) {
-            log.error("Error getting doc files.", e);
+        catch (Throwable t) {
+            final String errorMessage="Error constructing json response for task="+taskNameOrLsid+": "+
+                    t.getLocalizedMessage();
+            return Response.serverError().entity(errorMessage).build();
         }
-        moduleObject.put("hasDoc", hasDoc);
-
-        //if this is a pipeline check if there are any missing dependencies
-        TaskInfoAttributes tia = taskInfo.giveTaskInfoAttributes();
-        String taskType = tia.get(GPConstants.TASK_TYPE);
-        boolean isPipeline = "pipeline".equalsIgnoreCase(taskType);
-        if(isPipeline && PipelineDependencyHelper.instance().getMissingDependenciesRecursive(taskInfo).size() != 0)
-        {
-            moduleObject.put("missing_tasks", true);
-        }
-        else
-        {
-            moduleObject.put("missing_tasks", false);                        
-        }
-        JSONObject responseObject = new JSONObject();
-        responseObject.put(ModuleJSON.KEY, moduleObject);
-
-        JSONArray parametersObject = getParameterList(taskInfo.getParameterInfoArray());
-        responseObject.put(ParametersJSON.KEY, parametersObject);
-        
-        return responseObject;
+        return Response.ok().entity(jsonObj.toString()).build();        
     }
-
+    
     private TaskInfo getTaskInfo(final String taskLSID, final String username) 
     throws WebServiceException 
     {
         return new LocalAdminClient(username).getTask(taskLSID);
-    }
-
-    private List<String> getModuleVersions(final TaskInfo taskInfo) throws MalformedURLException
-    {
-        LSID taskLSID = new LSID(taskInfo.getLsid());
-        String taskNoLSIDVersion = taskLSID.toStringNoVersion();
-
-        ArrayList<String> moduleVersions = new ArrayList<String>();
-        TaskInfo[] tasks = TaskInfoCache.instance().getAllTasks();
-        for(int i=0;i<tasks.length;i++)
-        {
-            TaskInfoAttributes tia = tasks[i].giveTaskInfoAttributes();
-            String lsidString = tia.get(GPConstants.LSID);
-            LSID lsid = new LSID(lsidString);
-            String lsidNoVersion = lsid.toStringNoVersion();
-            if(taskNoLSIDVersion.equals(lsidNoVersion))
-            {
-                moduleVersions.add(lsidString);
-            }
-        }
-        return moduleVersions;
-    }
-
-    private JSONArray getParameterList(ParameterInfo[] pArray)
-    {
-        JSONArray parametersObject = new JSONArray();
-
-        for(int i =0;i < pArray.length;i++)
-        {
-            ParametersJSON parameter = new ParametersJSON(pArray[i]);
-            parametersObject.put(parameter);
-        }
-
-        return parametersObject;
     }
 }
