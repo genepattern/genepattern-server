ebaa7c3f8cef3de391da5ce23a2bcb289820dd41
tabor <tabor@broadinstitute.org>
2013-05-14 20:46:03 +0000

    Changes for GP-4330
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11721 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/runtaskform.js
+++ website/js/runtaskform.js
@@ -410,7 +410,7 @@ function loadParameterInfo(parameters, initialValues)
             if (parseInt(parameters[q].maxValue) == 1) {
 	            var rowNum = q + 1;
 	            var modeToggle = $("<div id='modeToggle" + rowNum + "'></div>");
-	            modeToggle.append("<input type='radio' value='normal' name='mode" + rowNum + "' id='singleMode" + rowNum + "' checked='true'><label title='A single job will be started using the given input.' for='singleMode" + (q+1) + "'>Normal</label></input>");
+	            modeToggle.append("<input type='radio' value='normal' name='mode" + rowNum + "' id='singleMode" + rowNum + "' checked='true'><label title='This is GenePattern's default and typical mode of operation. A single job will be started using the given input.' for='singleMode" + (q+1) + "'>Single</label></input>");
 	            modeToggle.append("<input type='radio' value='batch' name='mode" + rowNum + "'id='batchMode" + rowNum + "'><label title='This will launch a job for every file in the directory sent to this parameter, provided the file is of a matching type.' for='batchMode" + rowNum + "'>Batch</label></input>");
 	            fileDiv.append(modeToggle);
 	            modeToggle.buttonset();
