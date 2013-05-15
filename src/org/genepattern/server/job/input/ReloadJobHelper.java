70da214f139e5a11970a4d38ce502ba35995251b
pcarr <pcarr@broadinstitute.org>
2013-05-15 04:17:26 +0000

    removed commented code blocks from ReloadJobHelper GP-4306
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11731 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/job/input/ReloadJobHelper.java
+++ src/org/genepattern/server/job/input/ReloadJobHelper.java
@@ -52,49 +52,6 @@ public class ReloadJobHelper {
         return prefix +suffix;
     }
 
-//    /**
-//     * Get a JobInfo from the DB, for the given jobId.
-//     * (Legacy code copied from the RunTaskBean#setTask method).
-//     * 
-//     * @param userContext, Must be non-null with a valid userId
-//     * @param jobId, Must be non-null
-//     * 
-//     * @return
-//     * 
-//     * @throws Exception for the following,
-//     *     1) if there is no job with jobId in the DB
-//     *     2) if the current user does not have permission to 'read' the job
-//     */
-//    public static JobInfo initJobInfo(final Context userContext, final String jobId) throws Exception {
-//        if (userContext==null) {
-//            throw new IllegalArgumentException("userContext==null");
-//        }
-//        if (userContext.getUserId()==null || userContext.getUserId().length()==0) {
-//            throw new IllegalArgumentException("userContext.userId is not set");
-//        }
-//        if (jobId==null) {
-//            throw new IllegalArgumentException("jobId==null");
-//        }
-//        final int jobNumber;
-//        try {
-//            jobNumber=Integer.parseInt(jobId);
-//        }
-//        catch (Throwable t) {
-//            throw new Exception("Error parsing jobId="+jobId, t);
-//        }
-//        JobInfo jobInfo = new AnalysisDAO().getJobInfo(jobNumber);
-//        if (jobInfo==null) {
-//            throw new Exception("Can't load job, jobId="+jobId);
-//        }
-//
-//        // check permissions
-//        final boolean isAdmin = AuthorizationHelper.adminJobs(userContext.getUserId());
-//        PermissionsHelper perm = new PermissionsHelper(isAdmin, userContext.getUserId(), jobNumber);
-//        if (!perm.canReadJob()) {
-//            throw new Exception("User does not have permission to load job");
-//        }
-//        return jobInfo;
-//    }
 
     final Context userContext;
     final GetTaskStrategy getTaskStrategy;
