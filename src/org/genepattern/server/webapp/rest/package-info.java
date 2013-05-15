5d53d635749227b35a1e5d595e7d16c755f3d1e1
pcarr <pcarr@broadinstitute.org>
2013-05-15 03:00:56 +0000

    added javadoc for the rest api
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11725 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/webapp/rest/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The GenePattern REST API.
+ * 
+ * Resources include:
+ * <ul>
+ * <li>{@link org.genepattern.server.webapp.rest.api.v1.job.JobResource}</li>
+ * <li>@link org.genepattern.server.webapp.rest.api.v1.task.TaskResource}</li>
+ * </ul>
+ */
+package org.genepattern.server.webapp.rest;
