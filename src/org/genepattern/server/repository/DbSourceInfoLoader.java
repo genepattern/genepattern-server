e0a1d218da7ad3533e7a96660fc57d920aafdf79
pcarr <pcarr@broadinstitute.org>
2013-06-24 22:50:29 +0000

    for debugging, added deduceFromLsid flag, default=false, to the DbSourceInfoLoader By default, if there is no entry in the DB, then the installation source is unknown ('N/A') GP-4403
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11929 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/repository/DbSourceInfoLoader.java
+++ src/org/genepattern/server/repository/DbSourceInfoLoader.java
@@ -27,6 +27,11 @@ public class DbSourceInfoLoader implements SourceInfoLoader {
     final static private Logger log = Logger.getLogger(SourceInfoLoader.class);
     
     private Context serverContext=ServerConfiguration.Context.getServerContext();
+    /**
+     * Flag for how to deal with modules when there is no 'install_info' entry in the DB.
+     * Either return unknown, or guess based on the lsid of the task.
+     */
+    private boolean deduceFromLsid=false;
 
     @Override
     public SourceInfo getSourceInfo(final TaskInfo taskInfo) {
@@ -55,7 +60,12 @@ public class DbSourceInfoLoader implements SourceInfoLoader {
         if (taskInstall==null) {
             //no record in db, probably installed before updating to GP 3.6.0+
             //use the lsid to guess the source
-            return fromLsid(taskInfo.getLsid());
+            if (deduceFromLsid) {
+                return fromLsid(taskInfo.getLsid());
+            }
+            else {
+                return new FromUnknown();
+            }
         }
 
         final String source_type=taskInstall.getSourceType();
