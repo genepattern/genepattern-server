f44fc192148935d01c9e36ef9d2d6f044a65dec0
pcarr <pcarr@broadinstitute.org>
2013-06-27 13:30:48 +0000

    added Oracle DDL for 'task_install' table GP-4437
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@11956 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_hypersonic-3.6.1.sql
+++ resources/analysis_hypersonic-3.6.1.sql
@@ -2,12 +2,12 @@
 -- create the task_install_record table
 --
 create table task_install (
-    lsid varchar(255) not null,
+    lsid varchar(512) not null,
     user_id varchar(255),
     date_installed timestamp,
     source_type varchar(255),
     repo_url varchar(255),
-    prev_lsid varchar(255),
+    prev_lsid varchar(512),
     zipfile varchar(255),
     libdir varchar(255),
     primary key (lsid)
