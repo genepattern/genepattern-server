ae5f94c9356eb1de703039a2874a1980346184f6
tabor <tabor@broadinstitute.org>
2013-11-01 14:46:28 +0000

    Forgot to check this file in yesterday
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12413 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/jsf/UIBean.java
+++ src/org/genepattern/server/webapp/jsf/UIBean.java
@@ -4,7 +4,7 @@ import org.genepattern.server.config.ServerConfiguration;
 
 public class UIBean {
     public String getSkin() {
-        String env = ServerConfiguration.instance().getGPProperty(UIBeanHelper.getUserContext(), "display.skin", ".");
+        String env = ServerConfiguration.instance().getGPProperty(UIBeanHelper.getUserContext(), "display.skin", "pbj");
         return env;
     }
     
