d462c43c582e7c8c8845cf5953c1327ac7ce6a68
pcarr <pcarr@broadinstitute.org>
2013-11-05 15:37:36 +0000

    for GP-4561, refactor DirFilter so that it can be initialized directly from a 'choiceDirFilter' string, rather than a ParameterInfo     Plan to use this to FTP LS from a selected directory drop-down value
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12428 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/job/input/choice/DirFilter.java
+++ src/org/genepattern/server/job/input/choice/DirFilter.java
@@ -23,20 +23,36 @@ public class DirFilter {
     protected DirFilter.Type type=Type.file;
     final protected String choiceDirFilter;
     final protected FindFileFilter globs=new FindFileFilter();
-    
-    public DirFilter(final ParameterInfo param) {
+
+    /**
+     * Called from the DirFilter(ParameterInfo param) constructor to extract the
+     * 'choiceDirFilter' value from the parameter.
+     * 
+     * @param param
+     * @return
+     */
+    public static String getChoiceDirFilter(final ParameterInfo param) {
         if (param==null) {
             throw new IllegalArgumentException("param==null");
         }
         if (param.getAttributes()==null) {
             throw new IllegalArgumentException("param.attributes==null");
         }
+        final String choiceDirFilter;
         if ( param.getAttributes().containsKey(ChoiceInfo.PROP_CHOICE_DIR_FILTER) )
             //trim if necessary
             choiceDirFilter = ((String) param.getAttributes().get(ChoiceInfo.PROP_CHOICE_DIR_FILTER)).trim();
         else {
             choiceDirFilter = "";
         }
+        return choiceDirFilter;
+    }
+    
+    public DirFilter(final ParameterInfo param) {
+        this(getChoiceDirFilter(param));
+    }
+    public DirFilter(final String choiceDirFilter) {
+        this.choiceDirFilter=choiceDirFilter;
         if (!ChoiceInfoHelper.isSet(choiceDirFilter)) {
             //by default, ignore '*.md5' and 'readme.*' files
             globs.addGlob("!*.md5");
@@ -44,7 +60,7 @@ public class DirFilter {
         }
         else {
             //parse this as a ';' separated list of patterns
-            String[] patterns=choiceDirFilter.split(Pattern.quote(";"));
+            final String[] patterns=choiceDirFilter.split(Pattern.quote(";"));
             for(final String patternIn : patterns) {
                 //trim if necessary
                 final String pattern=patternIn.trim();
