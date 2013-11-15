1a6d112331adb78e01a4a52076a26323cb8d280c
pcarr <pcarr@broadinstitute.org>
2013-11-15 03:42:42 +0000

    set version number to 3.7.4
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12470 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ resources/analysis_hypersonic-3.7.4.sql
@@ -0,0 +1,3 @@
+-- update schema version
+update props set value='3.7.4' where key='schemaVersion';
+
---
+++ resources/analysis_oracle-3.7.4.sql
@@ -0,0 +1,5 @@
+-- update schema version
+insert into PROPS (KEY, VALUE) VALUES ('registeredVersion3.7.4', '3.7.4');
+
+commit;
+
