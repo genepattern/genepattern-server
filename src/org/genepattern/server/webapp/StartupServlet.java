3c8d4d29f089171353f8dcd8e2c06522c9c7d063
pcarr <pcarr@broadinstitute.org>
2013-11-21 06:49:42 +0000

    for GP-4701, removed unused debugging code from StartupServlet
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12512 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/StartupServlet.java
+++ src/org/genepattern/server/webapp/StartupServlet.java
@@ -158,20 +158,6 @@ public class StartupServlet extends HttpServlet {
         catch (Throwable t) {
             log.error("Error migrating job upload directories: "+t.getLocalizedMessage(), t);
         }
-
-        //TODO: this is for debugging, delete this before deploying to production
-        if (false) {
-        try {
-            log.info("\tloading categories from TaskInfoCache...");
-            SortedSet<String> categories = TaskInfoCache.instance().getAllCategories();
-            for(String category : categories) {
-                log.info("\t\t"+category);
-            }
-        }
-        catch (Throwable t) {
-            log.error("Error listing categories from TaskInfoCache: "+t.getLocalizedMessage());
-        }
-        }
         
         //start the JobPurger
         PurgerFactory.instance().start();
