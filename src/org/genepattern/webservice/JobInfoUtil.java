655365e4fb6d211ddcd3db71b104e633a4f3f5ca
pcarr <pcarr@broadinstitute.org>
2013-11-21 03:02:33 +0000

    for GP-4537, created a JobInfoUtil helper class for querying job status
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12505 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/webservice/JobInfoUtil.java
@@ -0,0 +1,31 @@
+package org.genepattern.webservice;
+
+/**
+ * Utility methods for working with JobInfo instances.
+ * We don't want to make modifications to the JobInfo class because it can break compatibility with the SOAP API.
+ * 
+ * @author pcarr
+ *
+ */
+public class JobInfoUtil {
+    /**
+     * helper method which indicates if the job has completed processing.
+     */
+    public static boolean isFinished(final JobInfo jobInfo) {
+        if ( org.genepattern.server.domain.JobStatus.FINISHED.equals(jobInfo.getStatus()) ||
+                org.genepattern.server.domain.JobStatus.ERROR.equals(jobInfo.getStatus()) ) {
+            return true;
+        }
+        return false;        
+    }
+    
+    /**
+     * helper method which indicates of the job has an error status.
+     * @param jobInfo
+     * @return
+     */
+    public static boolean hasError(final JobInfo jobInfo) {
+        return org.genepattern.server.domain.JobStatus.ERROR.equals(jobInfo.getStatus());
+    }
+
+}
