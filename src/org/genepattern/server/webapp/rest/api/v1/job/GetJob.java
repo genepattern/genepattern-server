cb6519ffec93cdbc0494206d898271b526c25555
pcarr <pcarr@broadinstitute.org>
2013-11-21 03:00:55 +0000

    for GP-4537,  added javadoc comments
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12504 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/rest/api/v1/job/GetJob.java
+++ src/org/genepattern/server/webapp/rest/api/v1/job/GetJob.java
@@ -3,6 +3,12 @@ package org.genepattern.server.webapp.rest.api.v1.job;
 import org.genepattern.server.config.ServerConfiguration;
 import org.json.JSONObject;
 
+/**
+ * Interface for getting the JSON representation for a given job, based on jobId.
+ * Implementation details are left to a particular instance of this interface.
+ * @author pcarr
+ *
+ */
 interface GetJob {
     JSONObject getJob(final ServerConfiguration.Context userContext, final String jobId)
     throws GetJobException;
