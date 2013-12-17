e86a613105e031a493bf3a74c94eecb07b09e04c
pcarr <pcarr@broadinstitute.org>
2013-12-17 18:07:40 +0000

    for GP-4777, created NumValues.PROP_LIST_MODE static field
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12630 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/job/input/NumValues.java
+++ src/org/genepattern/server/job/input/NumValues.java
@@ -9,6 +9,7 @@ package org.genepattern.server.job.input;
  */
 public class NumValues {
     public static final String PROP_NUM_VALUES="numValues";
+    public static final String PROP_LIST_MODE="listMode";
     
     private Integer min=null;
     private Integer max=null;
