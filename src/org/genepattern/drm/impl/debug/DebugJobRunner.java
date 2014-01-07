8c47b43b461a3005f0d8ef44603f363eb010d81d
pcarr <pcarr@broadinstitute.org>
2014-01-07 19:34:13 +0000

    created DebugJobRunner, for debugging the JobRunner integration, sent this to Le-Shin at Indiana U. GP-4762
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12701 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/drm/impl/debug/DebugJobRunner.java
@@ -0,0 +1,97 @@
+package org.genepattern.drm.impl.debug;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.genepattern.drm.DrmJobState;
+import org.genepattern.drm.DrmJobStatus;
+import org.genepattern.drm.DrmJobSubmission;
+import org.genepattern.drm.JobRunner;
+import org.genepattern.server.executor.CommandExecutorException;
+
+/**
+ * For debugging the JobRunner integration with GenePattern.
+ * 
+ * <pre>
+    DebugJobRunner:
+        classname: org.genepattern.server.executor.drm.JobExecutor
+        configuration.properties:
+            jobRunnerClassname: org.genepattern.drm.impl.debug.DebugJobRunner
+            jobRunnerName: LocalJobRunner
+            lookupType: DB
+            #lookupType: HASHMAP
+ * </pre>
+
+ * @author pcarr
+ *
+ */
+public class DebugJobRunner implements JobRunner {
+    private static final Logger log = Logger.getLogger(DebugJobRunner.class);
+
+    /**
+     * Utility method for creating a new file with the given message as it's content.
+     * @param message
+     * @param toFile
+     */
+    private static void writeToFile(final String message, final File toFile) {
+        toFile.getParentFile().mkdirs();
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(toFile));
+            writer.write(message);
+        } 
+        catch (IOException e) {
+            log.error("Error writing file="+toFile, e);
+        } 
+        finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void stop() {
+        log.info("stopping DebugJobRunner");
+    }
+
+    @Override
+    public String startJob(final DrmJobSubmission drmJobSubmission) throws CommandExecutorException {
+        StringBuffer buf=new StringBuffer();
+        buf.append("starting gpJob="+drmJobSubmission.getGpJobNo()+" in "+drmJobSubmission.getWorkingDir()+"\n");
+        buf.append("    "+drmJobSubmission.getJobInfo().getTaskName()+" ( lsid='"+drmJobSubmission.getJobInfo().getTaskLSID()+"' ) \n");
+        buf.append("commandLine="+drmJobSubmission.getCommandLine()+"\n");
+        buf.trimToSize();
+        String message=buf.toString();
+        File out=new File(drmJobSubmission.getWorkingDir(), "debug.txt");
+        writeToFile(message, out);
+        log.debug(message);
+        return ""+drmJobSubmission.getGpJobNo();
+    }
+
+    @Override
+    public DrmJobStatus getStatus(final String drmJobId) {
+        log.info("getStatus, drmJobId="+drmJobId);
+        //I work so hard, I'm always finished
+        return new DrmJobStatus.Builder(drmJobId, DrmJobState.DONE)
+            .exitCode(0)
+            .endTime(new Date())
+            .build();
+    }
+
+    @Override
+    public boolean cancelJob(final String drmJobId, final DrmJobSubmission drmJobSubmission) throws Exception {
+        log.info("cancelJob, drmJobId="+drmJobId);
+        return true;
+    }
+
+}
