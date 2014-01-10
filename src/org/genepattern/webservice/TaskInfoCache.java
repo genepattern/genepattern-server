bef4266559e763c6143dff938cc2de919d90d2b0
pcarr <pcarr@broadinstitute.org>
2014-01-10 21:48:33 +0000

    removed unused import statements
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12735 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/webservice/TaskInfoCache.java
+++ src/org/genepattern/webservice/TaskInfoCache.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
