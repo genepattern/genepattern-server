9eb7ef7937c8988189054df9e6e5d8299e67c9af
pcarr <pcarr@broadinstitute.org>
2014-02-04 16:42:20 +0000

    add FileUtilTest junit
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12849 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ test/src/org/genepattern/server/FileUtilTest.java
@@ -0,0 +1,52 @@
+package org.genepattern.server;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+/**
+ * junit test cases for FileUtil#getRelativePath.
+ * @author pcarr
+ *
+ */
+public class FileUtilTest extends TestCase {
+    final static String usera = new String("user_a");
+    final static String userb = new String("user_b");
+    
+    File userRootDir = new File("users");
+    File aHome = new File(userRootDir, usera);
+    File bHome = new File(userRootDir, userb);
+    File a01 = new File(aHome, "1.txt");
+    File a02 = new File(aHome, "2.txt");
+    File b01 = new File(bHome, "1.txt");
+    File b02 = new File(bHome, "2.txt");
+    File serverFile01 = new File("test1.txt");
+    
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    public void testGetRelativePath() {
+        runTest(aHome, a01, "1.txt");
+        runTest(aHome, a02, "2.txt");
+        runTest(aHome, b01, null);
+        runTest(aHome, b02, null);
+        
+        //more test cases
+        runTest(aHome, userRootDir, null);
+        runTest(aHome, aHome, null);
+        
+        //sub directories
+        runTest(aHome, new File(aHome, "sub/test4.txt"), "sub/test4.txt");
+        
+        //and some corner cases
+        File oddPath = new File( new File("users/"), usera );
+        runTest(oddPath, new File(oddPath, "test3.txt"), "test3.txt");
+    }
+    
+    private void runTest(File parent, File child, String expected) {
+        String rval = FileUtil.getRelativePath(parent, child);
+        assertEquals("", expected, rval);
+    }
+
+}
