bd992277d18b972abbadcbbd0fe34dccfdc7f0cc
pcarr <pcarr@broadinstitute.org>
2014-02-13 02:41:58 +0000

    created InputParamGroup class to assist with JSON serialization of (Advanced) parameter groups GP-4872
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12919 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/job/input/InputParamGroup.java
@@ -0,0 +1,105 @@
+package org.genepattern.server.job.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Use this class to represent a group of input parameters as they should appear
+ * in the job input form, for example to add an 'Advanced' parameters section to the input form.
+ * Not to be confused with the GroupInfo class which is for grouping multiple input files (not parameters)
+ * from the job input form into a table serialized to a file.
+ * 
+ * @author pcarr
+ *
+ */
+public class InputParamGroup {
+    final String name;
+    final String description;
+    final boolean hidden;
+    final List<String> parameters;
+
+    private InputParamGroup(final Builder in) {
+        this.name=in.name;
+        this.description=in.description;
+        this.hidden=in.hidden;
+        this.parameters=ImmutableList.copyOf(in.parameters);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public boolean isHidden() {
+        return hidden;
+    }
+    public List<String> getParameters() {
+        return parameters;
+    }
+    
+    /**
+     * Example json representation:
+     * <pre>
+{
+  name: "Basic/Required", 
+  description: "This contains the required parameters", 
+  hidden: false, 
+  parameters: [
+    "basic.required.parameter.1", 
+    "basic.required.parameter.2"]
+}
+     * </pre>
+     * @return
+     * @throws JSONException
+     */
+    public JSONObject toJson() throws JSONException {
+        JSONObject json=new JSONObject();
+        json.put("name", this.name);
+        json.put("description", this.description);
+        json.put("hidden", hidden);
+        JSONArray params=new JSONArray();
+        for(final String pname : parameters) {
+            params.put(pname);
+        }
+        json.put("parameters", params);
+        return json;
+    }
+
+    public static class Builder {
+        final String name;
+        String description="";
+        boolean hidden=true;
+        final List<String> parameters=new ArrayList<String>();
+        public Builder(final String name) {
+            this.name=name;
+        }
+        public Builder description(final String description) {
+            this.description=description;
+            return this;
+        }
+        public Builder hidden(final boolean hidden) {
+            this.hidden=hidden;
+            return this;
+        }
+        /**
+         * Add the parameters in the order that they should appear in the group.
+         * @param pname
+         * @return
+         */
+        public Builder addParameter(final String pname) {
+            parameters.add(pname);
+            return this;
+        }
+        
+        public InputParamGroup build() {
+            return new InputParamGroup(this);
+        }
+    }
+}
