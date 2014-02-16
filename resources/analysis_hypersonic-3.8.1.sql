bdba98265932c6434799eba0ea8a1981cec07304
pcarr <pcarr@broadinstitute.org>
2014-02-16 04:16:21 +0000

    added oracle ddl for job_input_value table GP-4872
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@12945 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_hypersonic-3.8.1.sql
+++ resources/analysis_hypersonic-3.8.1.sql
@@ -1,10 +1,10 @@
 create table job_input_value (
     gp_job_no integer not null,
     idx integer default 1 not null,
-    pname varchar(255) not null,
+    pname varchar(511) not null,
     pvalue varchar(1023) not null,
-    group_id varchar(255) default '' not null,
-    group_name varchar(255) default '' not null,
+    group_id varchar(511) default '' not null,
+    group_name varchar(511) default '' not null,
     primary key (gp_job_no, idx, pname),
     constraint jiv_gpjn_fk foreign key (GP_JOB_NO) references ANALYSIS_JOB(JOB_NO) on delete cascade
 );
--- resources/analysis_oracle-3.8.1.sql
+++ resources/analysis_oracle-3.8.1.sql
@@ -1,3 +1,14 @@
+create table job_input_value (
+    gp_job_no integer not null,
+    idx integer default 1 not null,
+    pname varchar(511) not null,
+    pvalue varchar(1023) not null,
+    group_id varchar(511) default '' not null,
+    group_name varchar(511) default '' not null,
+    primary key (gp_job_no, idx, pname),
+    constraint jiv_gpjn_fk foreign key (GP_JOB_NO) references ANALYSIS_JOB(JOB_NO) on delete cascade
+);
+
 -- update schema version
 update props set value='3.8.1' where key='schemaVersion';
 
