251a71b9f9cbd5d144d399f9bbf62c7de1a78e6b
pcarr <pcarr@broadinstitute.org>
2014-03-05 01:05:41 +0000

    DDL updates for the job_input_value table, pname must accept null values GP-4872
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13043 48a7a3f7-ad46-0410-b969-29950918272e

--- resources/analysis_hypersonic-3.8.1.sql
+++ resources/analysis_hypersonic-3.8.1.sql
@@ -2,7 +2,7 @@ create table job_input_value (
     gp_job_no integer not null,
     idx integer default 1 not null,
     pname varchar(511) not null,
-    pvalue varchar(1023) not null,
+    pvalue varchar(1023) default '',
     group_id varchar(511) default '',
     group_name varchar(511) default '',
     primary key (gp_job_no, idx, pname),
