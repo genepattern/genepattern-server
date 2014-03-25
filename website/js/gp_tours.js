63d079f19563558c158b48abd2a6862e12b0abe8
nazaire <nazaire@broadinstitute.org>
2014-03-25 19:51:51 +0000

    fix for GP-4979
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13183 48a7a3f7-ad46-0410-b969-29950918272e

--- website/js/gp_tours.js
+++ website/js/gp_tours.js
@@ -1,41 +1,58 @@
 
 $(function()
 {
-    $("#gp_tours").click(function(event)
+    $(".gp_tours").click(function(event)
     {
         event.preventDefault();
         var intro = introJs();
+
+        //create the steps
+        var steps = [
+            {
+                element: "img[src*='GP-logo.gif']",
+                intro: "You can click the GenePattern icon to return to this home page at any time."
+            },
+            {
+                element: '#navband1',
+                intro: "The navigation bar provides access to other pages."
+            },
+            {
+                element: '#left-nav-modules-tab',
+                intro: 'The Modules tab lists the analyses that you can run. Enter the first few characters of a module or pipeline name in the search box to locate that analysis.',
+                position: 'right'
+            },
+            {
+                element: '#left-nav-jobs-tab',
+                intro: 'The Jobs tab lists the most recent analyses that you have run and their results files.',
+                position: 'right'
+            },
+            {
+                element: '#left-nav-files-tab',
+                intro: 'The Files tab lists files that you have uploaded to the GenePattern server.',
+                position: 'right'
+            }];
+
+
+        if($("#protocols").is(":visible"))
+        {
+            steps.push({
+                element: '#protocols',
+                intro: "The center pane is the main display pane, which GenePattern uses to display information and to prompt you for input.",
+                position: 'left'
+            });
+        }
+
+        if($("#submitJob").is(":visible"))
+        {
+            steps.push({
+                element: '#submitJob',
+                intro: "The center pane is the main display pane, which GenePattern uses to display information and to prompt you for input.",
+                position: 'left'
+            });
+        }
+
         intro.setOptions({
-            steps: [
-                {
-                    element: "img[src*='GP-logo.gif']",
-                    intro: "You can click the GenePattern icon to return to this home page at any time."
-                },
-                {
-                    element: '#navband1',
-                    intro: "The navigation bar provides access to other pages."
-                },
-                {
-                    element: '#left-nav-modules-tab',
-                    intro: 'The Modules tab lists the analyses that you can run. Enter the first few characters of a module or pipeline name in the search box to locate that analysis.',
-                    position: 'right'
-                },
-                {
-                    element: '#left-nav-jobs-tab',
-                    intro: 'The Jobs tab lists the most recent analyses that you have run and their results files.',
-                    position: 'right'
-                },
-                {
-                    element: '#left-nav-files-tab',
-                    intro: 'The Files tab lists files that you have uploaded to the GenePattern server.',
-                    position: 'right'
-                },
-                {
-                    element: '#protocols',
-                    intro: "The center pane is the main display pane, which GenePattern uses to display information and to prompt you for input.",
-                    position: 'left'
-                }
-            ],
+            steps: steps,
             showStepNumbers: false,
             skipLabel: "End tour"
         });
@@ -57,6 +74,11 @@ $(function()
             {
                 $( "#left-nav" ).tabs( "option", "active", 2 );
             }
+            else if(targetElement.id == "submitJob")
+            {
+                //hack to not show the hidden native file upload button
+                $("#submitJob").find(".uploadedinputfile").hide();
+            }
         });
 
         intro.onexit(function()
@@ -67,6 +89,7 @@ $(function()
         intro.oncomplete(function()
         {
             $( "#left-nav" ).tabs( "option", "active", $("#gp_tours").data("last-left-nav-tab"));
+
         });
 
         intro.start();
