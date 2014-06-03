77dd639e7070924eab9fffedbaf2594e70f42d6a
pcarr <pcarr@broadinstitute.org>
2014-06-03 23:59:36 +0000

    code cleanup
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13431 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/webapp/rest/api/v1/Util.java
+++ src/org/genepattern/server/webapp/rest/api/v1/Util.java
@@ -1,8 +1,5 @@
 package org.genepattern.server.webapp.rest.api.v1;
 
-import java.util.Date;
-import java.util.TimeZone;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -12,9 +9,6 @@ import org.genepattern.server.config.GpContext;
 import org.genepattern.server.webservice.server.local.LocalAdminClient;
 import org.genepattern.webservice.TaskInfo;
 import org.genepattern.webservice.WebServiceException;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 public class Util {
     private static final Logger log = Logger.getLogger(Util.class);
@@ -55,50 +49,5 @@ public class Util {
     {
         return new LocalAdminClient(username).getTask(taskLSID);
     }
-    
-//    /**
-//     * The default ISO 8601 date formatter returned when getting JSON representations
-//     * of jobs and result files.
-//     * Using Joda Time without the milliseconds
-//     * <pre>
-//       yyyy-MM-dd'T'HH:mm:ssZZ
-//     * </pre>
-//     */
-//    public static final DateTimeFormatter isoNoMillis=DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZZ");
-//    public static final DateTimeFormatter isoNoMillisUtc=DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").withZoneUTC();
-//    
-//    /**
-//     * Outputs the date in 'ECMAScript 5 ISO-8601' format, in the server's timezone.
-//     * 
-//     * @param date
-//     * @return
-//     */
-//    public static String toIso8601(final Date date) {
-//        return toIso8601(date, null);
-//    }
-//
-//    /**
-//     * 
-//     * @param date
-//     * @param TimeZone
-//     * @return
-//     */
-//    public static String toIso8601(final Date date, final TimeZone tz) {
-//        if (date==null) {
-//            log.debug("Unexpected null date");
-//            return "";
-//        }
-//        if (tz==null) {
-//            return isoNoMillis.print(date.getTime());
-//        }
-//        DateTimeZone jodaTz=DateTimeZone.forTimeZone(tz);
-//        if (jodaTz.getOffset(date.getTime())==0L) {
-//            return isoNoMillisUtc.withZone(jodaTz).print(date.getTime());
-//        }
-//        else {
-//            return isoNoMillis.withZone(jodaTz)
-//                .print(date.getTime());
-//        }
-//    }
 
 }
