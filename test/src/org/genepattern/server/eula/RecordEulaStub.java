135bf80735facbe0cf67bf60a0f63fbd5de469ad
pcarr <pcarr@broadinstitute.org>
2014-06-27 00:00:33 +0000

    fix failing junit, TestEulaManagerImpl, No runnable methods
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13545 48a7a3f7-ad46-0410-b969-29950918272e

--- test/src/org/genepattern/server/eula/RecordEulaStub.java
+++ test/src/org/genepattern/server/eula/RecordEulaStub.java
@@ -20,6 +20,7 @@ public class RecordEulaStub implements RecordEula {
     public RecordEulaStub() {
     }
 
+    @Override
     public void recordLicenseAgreement(final String userId, final EulaInfo eula) throws Exception {
         if (eula==null) {
             throw new IllegalArgumentException("eula==null");
@@ -29,25 +30,28 @@ public class RecordEulaStub implements RecordEula {
         acceptedEulas.put(uniq_key, new Date());
     }
 
+    @Override
     public boolean hasUserAgreed(final String userId, final EulaInfo eula) throws Exception {
         final String lsid=eula.getModuleLsid();
         final String uniq_key = lsid+"_"+userId;
         return acceptedEulas.containsKey(uniq_key);
     }
 
+    @Override
     public Date getUserAgreementDate(final String userId, final EulaInfo eula) throws Exception {
         final String lsid=eula.getModuleLsid();
         final String uniq_key = lsid+"_"+userId;
         return acceptedEulas.get(uniq_key);
     }
 
+    @Override
     public void addToRemoteQueue(final String userId, final EulaInfo eula, final String remoteUrl) throws Exception {        
         EulaRemoteQueue.Key key = new EulaRemoteQueue.Key(eula.hashCode(), remoteUrl);
         EulaRemoteQueue val = new EulaRemoteQueue(key);
         remoteRecordQueue.put(key, val);
     }
 
-    //@Override
+    @Override
     public void updateRemoteQueue(String userId, EulaInfo eula, String remoteUrl, boolean success) throws Exception {
         EulaRemoteQueue.Key key = new EulaRemoteQueue.Key(eula.hashCode(), remoteUrl);
         EulaRemoteQueue val = remoteRecordQueue.get(key);
