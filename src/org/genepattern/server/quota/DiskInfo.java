e4e91e4935a703ee8569b104e93a59d6f3c1306e
nazaire <nazaire@broadinstitute.org>
2014-07-18 19:56:44 +0000

    related to GP-5283
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13649 48a7a3f7-ad46-0410-b969-29950918272e

---
+++ src/org/genepattern/server/quota/DiskInfo.java
@@ -0,0 +1,93 @@
+package org.genepattern.server.quota;
+
+import org.apache.log4j.Logger;
+import org.genepattern.drm.Memory;
+import org.genepattern.server.config.GpContext;
+import org.genepattern.server.database.HibernateUtil;
+import org.genepattern.server.dm.userupload.dao.UserUploadDao;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by nazaire on 7/10/14.
+ */
+@XmlRootElement
+public class DiskInfo
+{
+    final static private Logger log = Logger.getLogger(DiskInfo.class);
+
+    private Memory diskUsageTotal;
+    private Memory diskUsageTmp;
+    private Memory diskUsageFilesTab;
+    private Memory diskQuota;
+
+    private DiskInfo()
+    {
+        this.diskUsageTotal = Memory.fromSizeInBytes(-1);
+        this.diskUsageFilesTab = Memory.fromSizeInBytes(-1);
+        this.diskUsageTmp = Memory.fromSizeInBytes(-1);
+    }
+
+    public void setDiskUsageTotal(Memory diskUsageTotal)
+    {
+        this.diskUsageTotal = diskUsageTotal;
+    }
+
+    public Memory getDiskUsageTotal() { return diskUsageTotal;}
+
+    public void setDiskUsageTmp(Memory diskUsageTmp) {
+        this.diskUsageTmp = diskUsageTmp;
+    }
+
+    public Memory getDiskUsageTmp() { return diskUsageTmp;}
+
+    public void setDiskUsageFilesTab(Memory diskUsageFilesTab) {
+        this.diskUsageFilesTab = diskUsageFilesTab;
+    }
+
+    public Memory getDiskUsageFilesTab() { return diskUsageFilesTab;}
+
+    public Memory getDiskQuota() {
+        return diskQuota;
+    }
+
+    public void setDiskQuota(Memory diskQuota) {
+        this.diskQuota = diskQuota;
+    }
+
+    public static DiskInfo createDiskInfo(GpContext context)
+    {
+
+        DiskInfo diskInfo = new DiskInfo();
+
+        final boolean isInTransaction= HibernateUtil.isInTransaction();
+
+        try
+        {
+            HibernateUtil.beginTransaction();
+            UserUploadDao userUploadDao = new UserUploadDao();
+
+            Memory diskUsageTotal = userUploadDao.sizeOfAllUserUploads(context.getUserId(), true);
+            Memory diskUsageFilesTab = userUploadDao.sizeOfAllUserUploads(context.getUserId(), false);
+            Memory diskUsageTmp = Memory.fromSizeInBytes(diskUsageTotal.getNumBytes() - diskUsageFilesTab.getNumBytes());
+
+            diskInfo.setDiskUsageTotal(diskUsageTotal);
+            diskInfo.setDiskUsageFilesTab(diskUsageFilesTab);
+            diskInfo.setDiskUsageTmp(diskUsageTmp);
+        }
+        catch (Throwable t)
+        {
+            log.error(t);
+        }
+        finally
+        {
+            if (!isInTransaction)
+            {
+                HibernateUtil.closeCurrentSession();
+            }
+        }
+
+        return diskInfo;
+    }
+}
