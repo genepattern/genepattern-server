790ac8168bdc6c8622f7ef9924ae2d39195cce7c
pcarr <pcarr@broadinstitute.org>
2014-07-29 14:56:22 +0000

    update CongestionListener to avoid duplicate DB call; calculate queueTimeInSeconds from the Status object
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13718 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/congestion/CongestionListener.java
+++ src/org/genepattern/server/congestion/CongestionListener.java
@@ -1,8 +1,6 @@
 package org.genepattern.server.congestion;
 
-import com.google.common.eventbus.Subscribe;
 import org.apache.log4j.Logger;
-import org.genepattern.server.DbException;
 import org.genepattern.server.domain.AnalysisJob;
 import org.genepattern.server.domain.AnalysisJobDAO;
 import org.genepattern.server.domain.JobStatus;
@@ -10,10 +8,10 @@ import org.genepattern.server.executor.drm.dao.JobRunnerJob;
 import org.genepattern.server.executor.drm.dao.JobRunnerJobDao;
 import org.genepattern.server.executor.events.JobCompletionEvent;
 import org.genepattern.server.executor.events.JobEventBus;
-import org.genepattern.server.executor.events.JobProcessingEvent;
 import org.genepattern.server.executor.events.JobStartedEvent;
+import org.genepattern.server.job.status.Status;
 
-import javax.swing.event.ChangeEvent;
+import com.google.common.eventbus.Subscribe;
 
 /**
  * Listen for job completion events and update the database as appropriate
@@ -36,43 +34,47 @@ public class CongestionListener {
 
     @Subscribe
     public void updateUponProcessing(JobStartedEvent event) {
-        int jobId = event.getJobStatus().getGpJobNo();
-
-        AnalysisJobDAO dao = new AnalysisJobDAO();
-        AnalysisJob job = dao.findById(jobId);
-
-        JobRunnerJobDao jrjDao = new JobRunnerJobDao();
-        JobRunnerJob jrjJob = null;
-
-        try {
-            jrjJob = jrjDao.selectJobRunnerJob(jobId);
+        if (event==null || event.getJobStatus()==null) {
+            log.error("event.jobStatus==null, ignoring");
+            return;
         }
-        catch (Exception e) {
-            log.error("Error with JobRunnerJob for id: " + jobId + ", exiting updateCongestionTable()", e);
-        }
-        finally {
-            if (jrjJob == null) {
-                log.warn("Null JobRunnerJob for id: " + jobId);
-            }
-        }
-
-        // Get the task LSID
-        String lsid = job.getTaskLsid();
-
-        // Find the time difference between submission and start time, then convert to seconds
-        // If JobRunnerJob isn't available, fall back to 0
-        long queuetime = jrjJob != null ? ((jrjJob.getStartTime().getTime() - jrjJob.getSubmitTime().getTime()) / 1000) : 0;
-
-        // Get the queue name
-        String queueName = jrjJob != null ? jrjJob.getQueueId() : null;
+        final String lsid=event.getJobLsid();
+        final long queuetime=getQueueTimeInSeconds(event.getJobStatus());
+        final String queueName=event.getJobStatus().getQueueId();
 
         // Update the database
         try {
             CongestionManager.updateCongestionQueuetime(lsid, queueName, queuetime);
         }
         catch (Exception e) {
-            log.error("Error updating congestion data for job ID: " + jobId);
+            log.error("Error updating congestion data for job ID: " + event.getJobStatus().getGpJobNo());
         }
+    }
+    
+    /**
+     * Find the time difference between submission and start time, then convert to seconds
+     * If JobRunnerJob isn't available, fall back to 0
+     * 
+     * @param jobStatus, the Status instance from the JobStatusEvent.
+     * @return
+     */
+    protected long getQueueTimeInSeconds(final Status jobStatus) {
+        if (jobStatus==null) {
+            log.error("jobStatus==null");
+            return 0;
+        }
+        if (jobStatus.getStartTime()==null) {
+            log.debug("jobStatus.startTime==null");
+            return 0;
+        }
+        if (jobStatus.getSubmitTime()==null) {
+            log.debug("jobStatus.submitTime==null");
+            return 0;
+        }
+
+        // Note: this field can be used if the submitTime is not known jobStatus.getDateSubmittedToGp();
+        long queuetime = (jobStatus.getStartTime().getTime() - jobStatus.getSubmitTime().getTime()) / 1000;
+        return queuetime;
     }
 
     @Subscribe
