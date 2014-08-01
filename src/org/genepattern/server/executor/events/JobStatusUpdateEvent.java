9b86442692da20d3761b1135ed48a30040d6c469
pcarr <pcarr@broadinstitute.org>
2014-08-01 17:52:37 +0000

    Remove unused classes from the events package
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13766 48a7a3f7-ad46-0410-b969-29950918272e

--- src/org/genepattern/server/executor/events/JobStatusUpdateEvent.java
+++
@@ -1,9 +0,0 @@
-package org.genepattern.server.executor.events;
-
-import org.genepattern.server.job.status.Status;
-
-public class JobStatusUpdateEvent extends JobStatusEvent {
-    public JobStatusUpdateEvent(final String lsid, final Status jobStatus) {
-        super(lsid, jobStatus);
-    }
-}
