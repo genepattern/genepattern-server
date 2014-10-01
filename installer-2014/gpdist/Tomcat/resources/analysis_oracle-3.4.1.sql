411034702671c2b716e56c95fa35fd99534636ca
tabor <tabor@broadinstitute.org>
2014-10-01 16:30:15 +0000

    Removed extra resources directory from install
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@13984 48a7a3f7-ad46-0410-b969-29950918272e

--- installer-2014/gpdist/Tomcat/resources/analysis_oracle-3.4.1.sql
+++
@@ -1,3 +0,0 @@
--- update schema version
-insert into PROPS (KEY, VALUE) VALUES ('registeredVersion3.4.1', '3.4.1');
-commit;
--- installer-2014/gpdist/Tomcat/resources/analysis_oracle-3.5.1.sql
+++
@@ -1,5 +0,0 @@
--- update schema version
-insert into PROPS (KEY, VALUE) VALUES ('registeredVersion3.5.1', '3.5.1');
-
-commit;
-
