2fcb3b827f8d43ccf485f2b15b929fabf34079ec
pcarr <pcarr@broadinstitute.org>
2014-10-07 23:00:11 +0000

    added junit tests for parsing disk quota from config_yaml file
    git-svn-id: https://svn.broadinstitute.org/genepattern/trunk@14009 48a7a3f7-ad46-0410-b969-29950918272e

--- test/src/org/genepattern/server/config/TestConfigFileParser.java
+++ test/src/org/genepattern/server/config/TestConfigFileParser.java
@@ -1,9 +1,11 @@
 package org.genepattern.server.config;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 
+import org.genepattern.drm.Memory;
 import org.genepattern.junitutil.FileUtil;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,10 +26,76 @@ public class TestConfigFileParser {
     @Test
     public void listOfValues() throws ConfigurationException {
         Value val=gpConfig.getValue(gpContext, "listOfString");
-        Assert.assertTrue("fromCollection", val.isFromCollection());
-        Assert.assertEquals("listOfString.size", 3, val.getValues().size());
-        Assert.assertEquals("listOfString[0]", "A", val.getValues().get(0));
-        Assert.assertEquals("listOfString[1]", "B", val.getValues().get(1));
-        Assert.assertEquals("listOfString[2]", "C", val.getValues().get(2));
+        assertTrue("fromCollection", val.isFromCollection());
+        assertEquals("listOfString.size", 3, val.getValues().size());
+        assertEquals("listOfString[0]", "A", val.getValues().get(0));
+        assertEquals("listOfString[1]", "B", val.getValues().get(1));
+        assertEquals("listOfString[2]", "C", val.getValues().get(2));
     }
+    
+    @Test
+    public void valueNotSet() {
+        assertEquals("Expecting null value for '    valueNotSet:  ' ", 
+                null, 
+                gpConfig.getValue(gpContext, "valueNotSet").getValue());
+    }
+    
+    @Test
+    public void valueAsNull() {
+        assertEquals("Expecting null value for '    valueAsNull: null'", 
+                null, 
+                gpConfig.getValue(gpContext, "valueAsNull").getValue());
+    }
+    
+    @Test
+    public void valueAsNegativeOne() {
+        assertEquals("Expecting -1 value for '    valueAsNegativeOne: -1'", 
+                new Value("-1"), 
+                gpConfig.getValue(gpContext, "valueAsNegativeOne"));
+    }
+
+    @Test
+    public void valueAsString() {
+        assertEquals("Expecting -1 value for '    valueAsString: 2 Gb'", 
+                new Value("2 Gb"), 
+                gpConfig.getValue(gpContext, "valueAsString"));
+    }
+    
+    @Test
+    public void memoryFromNotSet() {
+        assertEquals("initialize Memory from value: <not set>", 
+                null, 
+                gpConfig.getGPMemoryProperty(gpContext, "valueNotSet"));
+    }
+    
+    @Test
+    public void memoryFromNull() {
+        assertEquals("initialize Memory from value: null", 
+                null, 
+                gpConfig.getGPMemoryProperty(gpContext, "valueAsNull"));
+        
+    }
+    
+    @Test
+    public void memoryFromNegativeOne() {
+        assertEquals("initialize Memory from value: -1", 
+                null, 
+                gpConfig.getGPMemoryProperty(gpContext, "valueAsNegativeOne"));
+    }
+    
+    @Test
+    public void memoryFromZero() {
+        assertEquals("initialize Memory from value: 0", 
+                new Memory(0L), 
+                gpConfig.getGPMemoryProperty(gpContext, "valueAsZero"));
+    }
+    
+    @Test
+    public void memoryFromMemSpec() {
+        assertEquals("initialize Memory from value: 2 Gb", 
+                Memory.fromString("2 Gb"), 
+                gpConfig.getGPMemoryProperty(gpContext, "valueAsString"));
+
+    }
+
 }
