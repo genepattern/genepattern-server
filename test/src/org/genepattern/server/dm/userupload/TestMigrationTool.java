ffb9f4f3f3f903b14f573d6b9ac7fd6cdcdc732f
Peter Carr <pcarr@broadinstitute.org>
2014-10-29 15:49:13 -0400

    fix for broken junit test, by default test with HSQL DB

--- test/src/org/genepattern/server/dm/userupload/TestMigrationTool.java
+++ test/src/org/genepattern/server/dm/userupload/TestMigrationTool.java
@@ -1,36 +1,24 @@
 package org.genepattern.server.dm.userupload;
 
 import org.genepattern.junitutil.DbUtil;
-import org.genepattern.server.database.HibernateSessionManager;
 import org.genepattern.server.dm.userupload.MigrationTool;
+import org.genepattern.server.domain.Props;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestMigrationTool {
     
-//    protected HibernateSessionManager initSessionMySql() {
-//        return new HibernateSessionManager("hibernate.mysql.cfg.xml", "jdbc:mysql://127.0.0.1:3306/genepattern");
-//    }
-//    
-//    protected HibernateSessionManager initSessionHsql() throws Exception {
-//        DbUtil.initDb();
-//        return new HibernateSessionManager("hibernate.junit.cfg.xml", "jdbc:hsqldb:hsql://127.0.0.1:9001/xdb");
-//    }
-
-    @Ignore @Test
-    public void dbQueryHql() throws Exception {
-        //initSessionHsql();
+    @Test
+    public void checkDbForSyncUploadsComplete() throws Exception {
         DbUtil.initDb();
         boolean check=MigrationTool.checkDbForSyncUserUploadsComplete();
-        Assert.assertFalse(check);
+        Assert.assertFalse("before update", check);
+        
+        boolean success=Props.saveProp("sync.user.uploads.complete", "true");
+        if (!success) {
+            Assert.fail("Failed to save property to DB");
+        }
+        Assert.assertTrue("after update", MigrationTool.checkDbForSyncUserUploadsComplete());
     }
     
-    @Test
-    public void dbQueryMySql() {
-        System.setProperty("hibernate.configuration.file", "hibernate.mysql.cfg.xml");
-        boolean check=MigrationTool.checkDbForSyncUserUploadsComplete();
-        Assert.assertFalse(check);
-        
-    }
 }
