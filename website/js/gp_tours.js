eafe438f116f719abef288b6f128efcf669672dd
mnazaire <nazaire@broadinstitute.org>
2015-01-07 16:33:37 -0500

    GP-5478, what's new web tour

--- website/js/gp_tours.js
+++ website/js/gp_tours.js
@@ -139,103 +139,50 @@ $(function()
         //create the steps
         var steps = [
             {
-                element: "#user-box-main",
-                intro: '<div class="tour-header"> User Settings </div>Manage user settings and access system messages by clicking here.',
-                position: 'left',
-                scrollToElement: true
-            },
-            {
-                element: "#quota-box-main",
-                intro: '<div class="tour-header"> Disk Usage </div>Disk usage for the Files tab are displayed.',
-                position: 'left',
-                scrollToElement: true
-            },
-            {
-                element: "#uploads-dir",
-                intro: '<div class="tour-header"> Files Tab </div>There is a new menu option for a directory. First, click on a directory to slide out menu.',
-                position: 'right',
-                scrollToElement: true
-            },
-            {
-                element: ".file-widget",
-                intro: '<div class="tour-header"> Files Tab </div>The slide out menu now has the option <b>Save Directory</b>. This option will save a local copy of the directory.',
-                position: 'right',
-                scrollToElement: true
-            },
-            {
                 element: "#main-pane",
-                intro: '<div class="tour-header"> Pending Jobs </div>There is a new congestion status indicator for pending jobs. Pending jobs are jobs that have been submitted but have not yet started processing. Green indicates low job volume which estimates that the job should enter the processing state soon.',
+                intro: '<div class="tour-header"> Job Comments </div>There is a new commenting feature for jobs. Before running a job, you can now attach comments to it.',
                 position: 'left',
                 scrollToElement: true
             },
             {
                 element: "#main-pane",
-                intro: '<div class="tour-header"> Pending Jobs </div>Yellow indicates medium job volume which estimates that the job may take hours to enter the processing state.',
+                intro: '<div class="tour-header"> Job Comments </div>Comments for a job will be visible on the Job Status page. From there, you or anyone who can view your job can add, edit, or delete a comment.',
                 position: 'left',
                 scrollToElement: true
             },
             {
                 element: "#main-pane",
-                intro: '<div class="tour-header"> Pending Jobs </div>Red indicates high job volume which estimates that the job may take days to enter the processing state..',
+                intro: '<div class="tour-header"> Job Tags </div>There is a new tagging feature for jobs. Before running a job, you can now add tags. Tags are a neat way to label your jobs.',
                 position: 'left',
                 scrollToElement: true
             },
             {
                 element: "#main-pane",
-                intro: '<div class="tour-header"> Job Status Details </div>The Job Status page now provides additional information about a job is now available by clicking the Show Details link.',
-                position: 'left',
-                scrollToElement: true
-            },
-            {
-                element: "#main-pane",
-                intro: '<div class="tour-header"> Job Status Details </div>Additional job details such as time added to GenePattern, run start time, cpu usage, and max memory used are displayed.',
+                intro: '<div class="tour-header"> Job Tags </div>Tags for a job will be visible on the Job Status page. From there, you can also delete or add additional tags.',
                 position: 'left',
                 scrollToElement: true
             },
             {
                 intro: '<div class="tour-header"> The End</div> This is the end of the tour. To learn more about what'
-                    + ' is new, please see the <a href="http://www.broadinstitute.org/cancer/software/genepattern/doc/relnotes/3.9.0" target="_blank">release notes</a>.',
+                    + ' is new, please see the <a href="http://www.broadinstitute.org/cancer/software/genepattern/doc/relnotes/3.9.1" target="_blank">release notes</a>.',
                 position: 'left',
                 scrollToElement: true
             }
            ];
 
-        if($("#quota-space-label").text().indexOf("/") >= 0)
-        {
-            //replace the second step with item that mentions quota information
-            steps[1] = {
-                element: "#quota-box-main",
-                intro: '<div class="tour-header"> Disk Usage and Quota </div>Disk usage for the Files tab are displayed. Indicator bar turns red when user nears quota. Quota only applies to Files tab content and not to linked GenomeSpace or Jobs Tab.',
-                position: 'left',
-                scrollToElement: true
-            };
-
-        }
-            intro.setOptions({
-            steps: steps,
-            showStepNumbers: false,
-            skipLabel: "End Tour",
-            tooltipClass: "tourStyle"
+        intro.setOptions({
+        steps: steps,
+        showStepNumbers: false,
+        skipLabel: "End Tour",
+        tooltipClass: "tourStyle"
         });
 
         intro.onbeforechange(function(targetElement)
         {
             //switch the active left navigation tab to the appropriate one for the step
-            if(intro._currentStep == 2)
+            if(intro._currentStep == 0)
             {
-                $( "#left-nav" ).tabs( "option", "active", 2 );
-                $("#uploadTree").find("a[name='uploads']").attr("id", "uploads-dir");
-                $("#uploads-dir").click();
-
-            }
-            else if(intro._currentStep == 3)
-            {
-                $("#menus-uploads .file-widget-actions").find(".module-listing").last().addClass("tourHighlight");
-            }
-
-            else if(intro._currentStep == 4)
-            {
-                $("#menus-uploads .file-widget-actions").find(".module-listing").last().removeClass("tourHighlight");
+                $(".tour_main_image").remove();
 
                 $("#main-pane").children().each(function()
                 {
@@ -245,29 +192,48 @@ $(function()
                     }
                 });
 
-                $("#main-pane").append('<img class="tour_congestion_image" src="../images/congestion_indicator_green.png" alt="Pending job" width="820" height="500" style="border: none;"></img>');
+                $("#main-pane").append('<img class="tour_main_image" src="../images/gp_comments_jobsubmit.png" alt="Job Comments Job Submit" width="815" height="540" style="border: none;"></img>');
             }
-            else if(intro._currentStep == 5)
+            else if(intro._currentStep == 1)
             {
-                $(".tour_congestion_image").remove();
-                $("#main-pane").append('<img class="tour_congestion_image" src="../images/congestion_indicator_yellow.png" alt="Pending job" width="820" height="500" style="border: none;"></img>');
+                $(".tour_main_image").remove();
+
+                $("#main-pane").children().each(function()
+                {
+                    if($(this).is(":visible"))
+                    {
+                        $(this).addClass("wasVisibleBefore").hide();
+                    }
+                });
+
+                $("#main-pane").append('<img class="tour_main_image" src="../images/gp_comments_jobstatus.png" alt="Job Comments Job Status" width="850" height="490" style="border: none;"></img>');
             }
-            else if(intro._currentStep == 6)
+            else if(intro._currentStep == 2)
             {
-                $(".tour_congestion_image").remove();
-                $("#main-pane").append('<img class="tour_congestion_image" src="../images/congestion_indicator_red.png" alt="Pending job" width="820" height="500" style="border: none;"></img>');
+                $(".tour_main_image").remove();
+                $("#main-pane").children().each(function()
+                {
+                    if($(this).is(":visible"))
+                    {
+                        $(this).addClass("wasVisibleBefore").hide();
+                    }
+                });
+                $("#main-pane").append('<img class="tour_main_image" src="../images/gp_tags_jobsubmit.png" alt="Job Tags Job Submit" width="820" height="535" style="border: none;"></img>');
             }
-            else if(intro._currentStep == 7)
+            else if(intro._currentStep == 3)
             {
-                $(".tour_congestion_image").remove();
-                $("#main-pane").append('<img class="tour_congestion_image" src="../images/job_status_details_show.png" alt="Job Detail" width="820" height="300" style="border: none;"></img>');
+                $(".tour_main_image").remove();
+                $("#main-pane").children().each(function()
+                {
+                    if($(this).is(":visible"))
+                    {
+                        $(this).addClass("wasVisibleBefore").hide();
+                    }
+                });
+
+                $("#main-pane").append('<img class="tour_main_image" src="../images/gp_tags_jobstatus.png" alt="Job Tags Job Status" width="850" height="450" style="border: none;"></img>');
             }
-            else if(intro._currentStep == 8)
-            {
-                $(".tour_congestion_image").remove();
-                $("#main-pane").append('<img class="tour_congestion_image" src="../images/job_status_details_hide.png" alt="Job Details" width="820" height="500" style="border: none;"></img>');
-            }
-            else if(intro._currentStep == 9)
+            else if(intro._currentStep == 4)
             {
                 newTourCleanup();
             }
@@ -290,7 +256,6 @@ $(function()
         intro.oncomplete(function()
         {
             introTourCleanup();
-
             newTourCleanup()
         });
 
