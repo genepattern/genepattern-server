4221c4edf27e1967df75d89af626fa54e23a4846
Peter Carr <pcarr@broadinstitute.org>
2015-03-10 15:20:42 -0400

    mac installer fix for <tomcatCommonLib> substitution

--- gp-macapp/src/org/genepattern/startapp/PropertiesWriter.java
+++ gp-macapp/src/org/genepattern/startapp/PropertiesWriter.java
@@ -172,7 +172,10 @@ public class PropertiesWriter {
                 line = "resources=" + gpHomeDirString + "/resources";
             }
             else if (line.startsWith("tomcatCommonLib=")) {
-                line = "tomcatCommonLib=" + workingDirString + "/GenePatternServer/Tomcat/common/lib";
+                File workingDir=new File(workingDirString);
+                File gpAppDir=workingDir.getParentFile();
+                File tomcatCommonLib=new File(gpAppDir, "Contents/Resources/GenePatternServer/Tomcat/common/lib").getAbsoluteFile();
+                line = "tomcatCommonLib=" +tomcatCommonLib;
             }
             else if (line.startsWith("webappDir=")) {
                 line = "webappDir=" + workingDirString + "/GenePatternServer/Tomcat/webapps/gp";
