28fece40d53a91066a4376a27aeee814e7f22eea
Peter Carr <pcarr@broadinstitute.org>
2015-04-13 15:19:08 -0400

    update junit test for s

--- test/src/org/genepattern/server/database/TestSchemaUpdater.java
+++ test/src/org/genepattern/server/database/TestSchemaUpdater.java
@@ -5,9 +5,10 @@ import java.util.Properties;
 
 import org.genepattern.junitutil.ConfigUtil;
 import org.genepattern.junitutil.DbUtil;
-//import org.junit.Rule;
+import org.genepattern.server.DbException;
+import org.junit.Rule;
 import org.junit.Test;
-//import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * Unit tests for the SchemaUpdater class.
@@ -16,11 +17,11 @@ import org.junit.Test;
  */
 public class TestSchemaUpdater {
 
-    //@Rule
-    //public TemporaryFolder tmp = new TemporaryFolder();
-
+    @Rule
+    public TemporaryFolder tmp = new TemporaryFolder();
+    
     @Test
-    public void initDbSchemaHsqlDb() throws Throwable {
+    public void initDbSchemaHsqlDb() throws DbException, Throwable {
         final File workingDir=new File(System.getProperty("user.dir"));
         final File resourcesDir=new File(workingDir, "resources");
 
@@ -30,13 +31,14 @@ public class TestSchemaUpdater {
         // path to load the schema files (e.g. ./resources/analysis_hypersonic-1.3.1.sql)
         File schemaDir=resourcesDir;
         // path to load the hsql db files (e.g. ./resources/GenePatternDB.script and ./resources/GenePatternDB.properties)
-        final File hsqlDbDir=new File("junitdb");
+        final File hsqlDbDir=tmp.newFolder("junitdb");
         final String hsqlDbName="GenePatternDB";
+        final Integer hsqlDbPort=9001; // default production port is 9001
 
         DbUtil.startDb(hsqlDbDir, hsqlDbName);
         try {
             p.setProperty("database.vendor", "HSQL");
-            p.setProperty("hibernate.connection.url", "jdbc:hsqldb:hsql://127.0.0.1:9001/xdb");
+            p.setProperty("hibernate.connection.url", "jdbc:hsqldb:hsql://127.0.0.1:"+hsqlDbPort+"/xdb");
             HibernateSessionManager mgr=new HibernateSessionManager(p);
             SchemaUpdater.updateSchema(mgr, schemaDir, "analysis_hypersonic-", "3.9.2");
         }
