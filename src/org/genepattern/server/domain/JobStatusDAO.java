938e15fa5b9d006a5e903e9ae4b1ae98d3d183a7
Peter Carr <pcarr@broadinstitute.org>
2015-08-12 14:29:18 -0400

    JobStatusDAO, use HibernateSessionManager

--- src/org/genepattern/server/domain/JobStatusDAO.java
+++ src/org/genepattern/server/domain/JobStatusDAO.java
@@ -7,6 +7,7 @@ package org.genepattern.server.domain;
 // Generated Sep 21, 2006 12:36:06 PM by Hibernate Tools 3.1.0.beta5
 
 import org.apache.log4j.Logger;
+import org.genepattern.server.database.HibernateSessionManager;
 import org.genepattern.server.database.HibernateUtil;
 
 /**
@@ -16,12 +17,17 @@ import org.genepattern.server.database.HibernateUtil;
  * @author Hibernate Tools
  */
 public class JobStatusDAO {
-    private static Logger log = Logger.getLogger(JobStatusDAO.class);
+    private static final Logger log = Logger.getLogger(JobStatusDAO.class);
 
+    /** @deprecated */
     public JobStatus findById(java.lang.Integer id) {
+        return findById(HibernateUtil.instance(), id);
+    }
+    
+    public JobStatus findById(HibernateSessionManager mgr, java.lang.Integer id) {
         log.debug("getting JobStatus instance with id: " + id);
         try {
-            return (JobStatus) HibernateUtil.getSession().get("org.genepattern.server.domain.JobStatus", id);
+            return (JobStatus) mgr.getSession().get("org.genepattern.server.domain.JobStatus", id);
         } 
         catch (RuntimeException re) {
             log.error("get failed", re);
