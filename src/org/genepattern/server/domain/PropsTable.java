41187191270e939f4d18c2993771ff541c0e707d
Peter Carr <pcarr@broadinstitute.org>
2015-08-14 11:32:49 -0400

    deprecated methods in PropsTable; should use HibernateSessionManager arg

--- src/org/genepattern/server/domain/PropsTable.java
+++ src/org/genepattern/server/domain/PropsTable.java
@@ -14,7 +14,6 @@ import javax.persistence.Table;
 import org.apache.log4j.Logger;
 import org.genepattern.server.DbException;
 import org.genepattern.server.database.HibernateSessionManager;
-import org.genepattern.server.database.HibernateUtil;
 import org.hibernate.Query;
 
 /**
@@ -31,16 +30,10 @@ import org.hibernate.Query;
 @Table(name="PROPS")
 public class PropsTable {
     private static final Logger log = Logger.getLogger(PropsTable.class);
-    
-    /**
-     * Get the value for the given key in the PROPS table.
-     * 
-     * @param key
-     * @return
-     * @throws DbException
-     */
+
+    /** @deprecated */
     public static String selectValue(final String key) throws DbException {
-        return selectValue(HibernateUtil.instance(), key);
+        return selectValue(org.genepattern.server.database.HibernateUtil.instance(), key);
     }
     
     /**
@@ -59,17 +52,19 @@ public class PropsTable {
         return row.getValue();
     }
     
+    /** @deprecated */
+    public static List<String> selectKeys(final String matchingKey) {
+        return selectKeys(org.genepattern.server.database.HibernateUtil.instance(), matchingKey);
+    }
+
     /**
      * Get the list of keys from the PROPS table which match the given matchingKey.
      * To get more than one result pass in a '%' wildcard character, e.g.
      *     matchingKey='registeredVersion%'
+     * @param mgr
      * @param matchingKey
      * @return
      */
-    public static List<String> selectKeys(final String matchingKey) {
-        return selectKeys(HibernateUtil.instance(), matchingKey);
-    }
-
     public static List<String> selectKeys(final HibernateSessionManager mgr, final String matchingKey) {
         final boolean isInTransaction=mgr.isInTransaction();
         try {
@@ -91,8 +86,9 @@ public class PropsTable {
         }
     }
 
+    /** @deprecated */
     public static PropsTable selectRow(final String key) throws DbException {
-        return selectRow(HibernateUtil.instance(), key);
+        return selectRow(org.genepattern.server.database.HibernateUtil.instance(), key);
     }
 
     /**
@@ -133,8 +129,9 @@ public class PropsTable {
         }
     }
 
+    /** @deprecated */
     public static List<PropsTable> selectAllProps() throws DbException {
-        return selectAllProps(HibernateUtil.instance());
+        return selectAllProps(org.genepattern.server.database.HibernateUtil.instance());
     }
 
     /**
@@ -163,20 +160,16 @@ public class PropsTable {
         }
     }
 
-    /**
-     * Save a key/value pair to the PROPS table.
-     * @param key
-     * @param value
-     * @return
-     */
+    /** @deprecated */
     public static boolean saveProp(final String key, final String value) 
     throws DbException
     {
-        return saveProp(HibernateUtil.instance(), key, value);
+        return saveProp(org.genepattern.server.database.HibernateUtil.instance(), key, value);
     }
 
     /**
      * Save a key/value pair to the PROPS table.
+     * @param mgr
      * @param key
      * @param value
      * @return
@@ -204,12 +197,9 @@ public class PropsTable {
         }
     }
 
-    /**
-     * Remove an entry from the PROPS table.
-     * @param key
-     */
+    /** @deprecated */
     public static void removeProp(final String key) {
-        removeProp(HibernateUtil.instance(), key);
+        removeProp(org.genepattern.server.database.HibernateUtil.instance(), key);
     }
     
     /**
