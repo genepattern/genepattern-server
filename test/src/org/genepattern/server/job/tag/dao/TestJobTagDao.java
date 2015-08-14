6037aaf46edb648de5ba3442cd88e06ff9698043
Peter Carr <pcarr@broadinstitute.org>
2015-08-14 09:17:39 -0400

    remove unused import

--- test/src/org/genepattern/server/job/tag/dao/TestJobTagDao.java
+++ test/src/org/genepattern/server/job/tag/dao/TestJobTagDao.java
@@ -23,7 +23,6 @@ import org.genepattern.server.job.tag.JobTag;
 import org.genepattern.server.tag.Tag;
 import org.genepattern.webservice.JobInfo;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
