b4e910d4ab5f038c534202529a8bbe9b3d9bad91
Peter Carr <pcarr@broadinstitute.org>
2015-09-08 10:45:26 -0400

    fixed broken junit, substitute_GenePatternVersion

--- test/src/org/genepattern/server/TestTranslateCmdLine.java
+++ test/src/org/genepattern/server/TestTranslateCmdLine.java
@@ -184,7 +184,7 @@ public class TestTranslateCmdLine {
             .webappDir(new File("website"))
         .build();
         assertEquals(
-                Arrays.asList("echo", "genepattern.version=3.9.4", "GenePatternVersion=3.9.4"),
+                Arrays.asList("echo", "genepattern.version=3.9.5", "GenePatternVersion=3.9.5"),
                 CommandLineParser.translateCmdLine(gpConfig, gpContext, "echo genepattern.version=<genepattern.version> GenePatternVersion=<GenePatternVersion>", parameterInfoMap));
     }
 
