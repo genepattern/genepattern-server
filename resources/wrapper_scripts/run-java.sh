2655f9bc94cf76556a8976de2a14569bb2001526
Peter Carr <pcarr@broadinstitute.org>
2015-09-09 16:25:11 -0400

    added run-java.sh wrapper script GP-5827

--- resources/wrapper_scripts/run-java.sh
+++ resources/wrapper_scripts/run-java.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+
 #
-# Wrapper script for running R as a java command
+# Wrapper script for <java> command line module.
+# 
+# Alternate implementations:
+# 1) Skip this script and use a substitution,
+#    java=<run-with-env> -u Java java
 #
-# arg1: the version number of R to run, e.g. '2.15'
-#
+# 2) directly call run-with-env.sh
+#    "${script_dir}/run-with-env.sh" -u Java java "${@}" 
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source "${script_dir}/env-lookup.sh"
+source "$script_dir/env-lookup.sh"
 sourceEnvDefault
 # special-case: check for -c <gp_env_custom> 
 if [ "$1" = "-c" ]; then
@@ -17,16 +22,10 @@ else
     sourceEnvCustom
 fi
 
-r_version="${1}"
-shift;
-
-addEnv "R-${r_version}"
 addEnv "Java"
 for module in "${_runtime_environments[@]}"
 do
   initEnv "${module}"
 done
 
-r=`which R`
-rhome=${r%/*/*}
-java "-DR_HOME=${rhome}" -Dr_flags='--no-save --quiet --slave --no-restore' "$@"
+java "${@}"
