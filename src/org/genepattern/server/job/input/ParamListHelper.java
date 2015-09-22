d4c5be2cc3df79f7860931d267e343f9ae67e88f
Peter Carr <pcarr@broadinstitute.org>
2015-09-22 18:02:26 -0400

    updated ParamListHelper javadoc

--- src/org/genepattern/server/job/input/ParamListHelper.java
+++ src/org/genepattern/server/job/input/ParamListHelper.java
@@ -74,12 +74,28 @@ public class ParamListHelper {
          */
         LIST_INCLUDE_EMPTY,
         /**
-         * When listMode=CMD, the individual values will be listed on the CMD line with a default comma separator
+         * When listMode=CMD, the individual values will be listed on the CMD line as a single arg with a default comma separator.
+         * Set a custom separator with the 'listModeSep' attribute. The prefix_when_specified flag is optionally
+         * added as a command line arg.
+         * 
+         * case 1: no prefix_when_specified results in one arg
+         *     "argA,argB"
+         * case 2: prefix with no trailing space "-i", results in one arg
+         *     "-iargA,argB"
+         * case 3: prefix with trailing space "-i ", results in two args
+         *     "-i", "argA,argB"
          */
         CMD,
         /**
-         * When listMode=CMD_opt, the individual values will be listed on the CMD line with the CMD line prefix
-         * prepended to it
+         * When listMode=CMD_OPT, the individual values will be listed on the CMD line one arg per value.
+         * The prefix_when_speficied optionally is appended to the value.
+         * 
+         * case 1: no prefix_when_specified results in N args
+         *     "argA", "argB"
+         * case 2: prefix with no trailing space results in N args
+         *     "-iargA", "-iargB"
+         * case 3: prefix with trailing space results in 2*N args
+         *     "-i", "argA", "-i", "argB"
          */
         CMD_OPT
     }
