ff8d7adc293b28ac51c1ae7a92996c864002b3f7
Peter Carr <pcarr@broadinstitute.org>
2015-09-23 14:05:52 -0400

    test cases for ValueResolver substitutions for single input parameters - various prefix_when_specified test cases

--- test/src/org/genepattern/server/genepattern/TestValueResolverListMode.java
+++ test/src/org/genepattern/server/genepattern/TestValueResolverListMode.java
@@ -23,102 +23,215 @@ import org.junit.Test;
  *
  */
 public class TestValueResolverListMode {
-    private static final String pname="input.param";
+
     private Map<String,ParameterInfoRecord>  paramInfoMap;
     private JobInput jobInput;
     private GpConfig gpConfig;
     private GpContext jobContext;
-    private Map<String,String> dict=new HashMap<String,String>();
+    private Map<String,String> dict;
     
-    private ParameterInfoBuilder builder;
-    private ParameterInfo pinfo;
+    // text param with numValues=0+
+    private static final String textListName="text.list";
+    private ParameterInfoBuilder textList;
+    private ParameterInfo textListInfo;
+
+    // text param with numValues=0..1
+    private static final String textFieldName="text.field";
+    private ParameterInfoBuilder textField;
+    private ParameterInfo textFieldInfo;
     
     @Before
     public void setUp() throws Exception {
+        dict=new HashMap<String,String>();
+        dict.put(textFieldName, "A VALUE");
+
         jobInput=new JobInput();
-        jobInput.addValue(pname, "A VALUE");
-        jobInput.addValue(pname, "B VALUE");
+        jobInput.addValue(textListName, "A VALUE");
+        jobInput.addValue(textListName, "B VALUE");
         
         gpConfig=new GpConfig.Builder().build();
         jobContext=new GpContext.Builder()
             .jobInput(jobInput)
         .build();
         
-        builder = new ParameterInfoBuilder()
+        textList = new ParameterInfoBuilder()
             .listMode(ListMode.CMD)
-            .name(pname)
+            .name(textListName)
             .numValues("0+")
+            .optional(true)
+            .defaultValue("");
+        
+        textField = new ParameterInfoBuilder()
+            .name(textFieldName)
+            .numValues("0..1")
             .optional(true)
             .defaultValue("");
     }
     
     protected void initPinfo() {
-        pinfo=builder.build();
-        final ParameterInfo[] formalParams = new ParameterInfo[]{pinfo};
-        paramInfoMap=ParameterInfoRecord.initParamInfoMap(formalParams);
+        textListInfo=textList.build();
+        textFieldInfo=textField.build();
+        final ParameterInfo[] formalParams = new ParameterInfo[]{textFieldInfo, textListInfo};
+        paramInfoMap=ParameterInfoRecord.initParamInfoMap(formalParams);        
+    }
+
+    // tests for single valued text param
+    @Test
+    public void singleValue() {
+        initPinfo();
+        final String arg="<"+textFieldName+">";
+
+        assertEquals("substituteValue("+arg+")", 
+                Arrays.asList("A VALUE"),
+                ValueResolver.substituteValue(gpConfig, jobContext, arg, dict, paramInfoMap));
     }
 
     @Test
+    public void singleValue_with_prefix() {
+        textField.prefixWhenSpecified("-i");
+        initPinfo();
+        final String arg="<"+textFieldName+">";
+
+        assertEquals("substituteValue("+arg+")", 
+                Arrays.asList("-iA VALUE"),
+                ValueResolver.substituteValue(gpConfig, jobContext, arg, dict, paramInfoMap));
+    }
+
+    @Test
+    public void singleValue_with_prefix_split() {
+        textField.prefixWhenSpecified("-i ");
+        initPinfo();
+        final String arg="<"+textFieldName+">";
+
+        assertEquals("substituteValue("+arg+")", 
+                Arrays.asList("-i", "A VALUE"),
+                ValueResolver.substituteValue(gpConfig, jobContext, arg, dict, paramInfoMap));
+    }
+    
+    /**
+     * Test a substitution like "-f<text.param>"
+     */
+    @Test
+    public void singleValue_inString() {
+        initPinfo();
+        final String arg="-f<"+textFieldName+">";
+        assertEquals("substituteValue("+arg+")", 
+                Arrays.asList("-fA VALUE"),
+                ValueResolver.substituteValue(gpConfig, jobContext, arg, dict, paramInfoMap));
+    }
+
+    /**
+     * Test a substitution like "BEFORE<text.param>" with prefix_when_specified
+     * Note: this test is descriptive, not prescriptive. We may want to change 
+     * the rules for handling prefixes within complex substitutions.
+     */
+    @Test
+    public void singleValue_inString_with_prefix() {
+        textField.prefixWhenSpecified("-f");
+        initPinfo();
+        
+        final String arg="BEFORE<"+textFieldName+">";
+        assertEquals("substituteValue("+arg+")", 
+                Arrays.asList("BEFORE-fA VALUE"),
+                ValueResolver.substituteValue(gpConfig, jobContext, arg, dict, paramInfoMap));
+    }
+
+    /**
+     * Test a substitution like "BEFORE<text.param>" with prefix_when_specified with trailing space
+     * Note: this test is descriptive, not prescriptive. We may want to change 
+     * the rules for handling prefixes within complex substitutions.
+     * 
+     * Note: this test-case is a BUG, pre-existing in GP.
+     * TODO: fix this bug
+     */
+    @Test
+    public void singleValue_inString_with_prefix_split() {
+        textField.prefixWhenSpecified("-f ");
+        initPinfo();
+        
+        final String arg="BEFORE<"+textFieldName+">";
+        
+        // should be "BEFORE", "-f", "A VALUE"
+        assertEquals("substituteValue("+arg+")", 
+                Arrays.asList("-f", "BEFOREA VALUE"),
+                ValueResolver.substituteValue(gpConfig, jobContext, arg, dict, paramInfoMap));
+    }
+
+    /**
+     * Test a substitution like "BEFORE <text.param> MIDDLE <text.param> AFTER"
+     */
+    @Test
+    public void singleValue_complex_sub_no_prefix() {
+        initPinfo();
+        final String arg="BEFORE <"+textFieldName+"> MIDDLE <"+textFieldName+"> AFTER";
+        assertEquals("substituteValue("+arg+")", 
+                Arrays.asList("BEFORE A VALUE MIDDLE A VALUE AFTER"),
+                ValueResolver.substituteValue(gpConfig, jobContext, arg, dict, paramInfoMap));
+    }
+   
+    // tests for multi valued text param
+    @Test
     public void substitueValue_cmd() {
         initPinfo();
-        assertEquals("substitue(<"+pname+">), listMode=CMD, no prefix", 
+        assertEquals("substitue(<"+textListName+">), listMode=CMD, no prefix", 
                 Arrays.asList("A VALUE,B VALUE"), 
-                ValueResolver.substituteValue(gpConfig, jobContext, "<"+pname+">", dict, paramInfoMap));
+                ValueResolver.substituteValue(gpConfig, jobContext, "<"+textListName+">", dict, paramInfoMap));
     }
 
     @Test
     public void substitueValue_cmd_listModeSep() {
-        builder.listModeSep("&");
+        textList.listModeSep("&");
         initPinfo();
-        assertEquals("substitue(<"+pname+">), custom listModeSep", 
+        assertEquals("substitue(<"+textListName+">), custom listModeSep", 
                 Arrays.asList("A VALUE&B VALUE"), 
-                ValueResolver.substituteValue(gpConfig, jobContext, "<"+pname+">", dict, paramInfoMap));
+                ValueResolver.substituteValue(gpConfig, jobContext, "<"+textListName+">", dict, paramInfoMap));
     }
 
     @Test
     public void substitueValue_cmd_listModeSepAsSpace() {
-        builder.listModeSep(" ");
+        textList.listModeSep(" ");
         initPinfo();
-        assertEquals("substitue(<"+pname+">), custom listModeSep", 
+        assertEquals("substitue(<"+textListName+">), custom listModeSep", 
                 Arrays.asList("A VALUE B VALUE"), 
-                ValueResolver.substituteValue(gpConfig, jobContext, "<"+pname+">", dict, paramInfoMap));
+                ValueResolver.substituteValue(gpConfig, jobContext, "<"+textListName+">", dict, paramInfoMap));
     }
 
     @Test
     public void substitueValue_cmd_prefix() {
-        builder.prefixWhenSpecified("-i");
+        textList.prefixWhenSpecified("-i");
         initPinfo();
-        assertEquals("substitue(<"+pname+">), prefix with no trailing space", 
+        assertEquals("substitue(<"+textListName+">), prefix with no trailing space", 
                 Arrays.asList("-iA VALUE,B VALUE"), 
-                ValueResolver.substituteValue(gpConfig, jobContext, "<"+pname+">", dict, paramInfoMap));
+                ValueResolver.substituteValue(gpConfig, jobContext, "<"+textListName+">", dict, paramInfoMap));
     }
     
     @Test
     public void substitueValue_cmd_prefix_trailing_space() {
-        builder.prefixWhenSpecified("-i ");
+        textList.prefixWhenSpecified("-i ");
         initPinfo();
-        assertEquals("substitue(<"+pname+">), prefix with trailing space", 
+        assertEquals("substitue(<"+textListName+">), prefix with trailing space", 
                 Arrays.asList("-i", "A VALUE,B VALUE"), 
-                ValueResolver.substituteValue(gpConfig, jobContext, "<"+pname+">", dict, paramInfoMap));
+                ValueResolver.substituteValue(gpConfig, jobContext, "<"+textListName+">", dict, paramInfoMap));
     }
 
     @Test
     public void substituteValue_cmdOpt() throws Exception {
-        builder.listMode(ListMode.CMD_OPT);
+        textList.listMode(ListMode.CMD_OPT);
         initPinfo(); 
-        assertEquals("substitue(<"+pname+">), listMode=CMD_OPT, no prefix", 
+        assertEquals("substitue(<"+textListName+">), listMode=CMD_OPT, no prefix", 
                 Arrays.asList("A VALUE", "B VALUE"), 
-                ValueResolver.substituteValue(gpConfig, jobContext, "<"+pname+">", dict, paramInfoMap));
+                ValueResolver.substituteValue(gpConfig, jobContext, "<"+textListName+">", dict, paramInfoMap));
     }
 
     @Test
     public void substituteValue_cmdOpt_prefix() throws Exception {
-        builder.listMode(ListMode.CMD_OPT)
+        textList.listMode(ListMode.CMD_OPT)
             .prefixWhenSpecified("--input-param=");
         initPinfo(); 
-        assertEquals("substitue(<"+pname+">), prefix with no trailing space", 
+        assertEquals("substitue(<"+textListName+">), prefix with no trailing space", 
                 Arrays.asList("--input-param=A VALUE", "--input-param=B VALUE"), 
-                ValueResolver.substituteValue(gpConfig, jobContext, "<"+pname+">", dict, paramInfoMap));
+                ValueResolver.substituteValue(gpConfig, jobContext, "<"+textListName+">", dict, paramInfoMap));
     }
 
+    
 }
