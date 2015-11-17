da9965396e3d996204bf28d4dbed7456b4dadfd4
Peter Carr <pcarr@broadinstitute.org>
2015-11-17 14:42:34 -0500

    removed unused tables from project, pt 2/2

---
+++ website/WEB-INF/schema/analysis_mysql-3.9.6.sql
@@ -0,0 +1,3 @@
+-- drop unused tables from DB
+drop table if exists JOB_INPUT;
+drop table if exists JOB_INPUT_ATTRIBUTE;
---
+++ website/WEB-INF/schema/analysis_oracle-3.9.6.sql
@@ -0,0 +1,7 @@
+-- drop unused tables from DB
+--
+-- Note: must do this as a manual step because I haven't had a
+-- chance to test the Oracle equivalent of 'drop table if exists ...'
+-- 
+-- drop table job_input;
+-- drop table job_input_attribute;
