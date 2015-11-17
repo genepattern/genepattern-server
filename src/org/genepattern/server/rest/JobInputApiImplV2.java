40f8e63c6b0f7f993c3dd0deb766495d3aa3dc24
Peter Carr <pcarr@broadinstitute.org>
2015-11-16 19:38:25 -0500

    removed JobInputApi interface, pt 2

--- src/org/genepattern/server/rest/JobInputApiImplV2.java
+++ src/org/genepattern/server/rest/JobInputApiImplV2.java
@@ -64,7 +64,7 @@ public class JobInputApiImplV2 implements JobInputApi {
     public JobInputApiImplV2(final boolean initDefault) {
         this(null, initDefault);
     }
-    public JobInputApiImplV2(final GetTaskStrategy getTaskStrategy, final boolean initDefault) {
+    private JobInputApiImplV2(final GetTaskStrategy getTaskStrategy, final boolean initDefault) {
         this.getTaskStrategy=getTaskStrategy;
         this.initDefault=initDefault;
     }
@@ -97,7 +97,7 @@ public class JobInputApiImplV2 implements JobInputApi {
     
     // copied implementation from JobInputApiLegacy class
     private static class JobInputHelper {
-        final static private Logger log = Logger.getLogger(JobInputApiLegacy.class);
+        final static private Logger log = Logger.getLogger(JobInputHelper.class);
 
         private final GpConfig gpConfig;
         private final GpContext taskContext;
