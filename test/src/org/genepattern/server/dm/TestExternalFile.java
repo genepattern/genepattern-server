dea35f680739f8b04a639589d3233c01c9580a62
Peter Carr <pcarr@broadinstitute.org>
2015-12-01 15:12:11 -0500

    refactor, move SemanticUtil test cases into new class

--- test/src/org/genepattern/server/dm/TestExternalFile.java
+++ test/src/org/genepattern/server/dm/TestExternalFile.java
@@ -2,27 +2,10 @@ package org.genepattern.server.dm;
 
 import static org.junit.Assert.assertEquals;
 
-import org.genepattern.util.SemanticUtil;
 import org.junit.Test;
 
 public class TestExternalFile {
     public static final String externalHref="http://www.broadinstitute.org:8080";
-    
-    
-    protected void checkExtension(final String filename, final String expected_extension) {
-        assertEquals("getExtension("+filename+")", expected_extension,
-                SemanticUtil.getExtension(filename));
-    }
-    
-    @Test
-    public void semanticUtil_getExtensionFromName() {
-        checkExtension(null, null);
-        checkExtension("filename.txt", "txt");
-        checkExtension("name_no_ext", "");
-        checkExtension("dirname/", "");
-        checkExtension(".hidden.txt", "txt");
-        checkExtension(".hidden_no_ext", "");
-    }
     
     @Test
     public void fromUrlSpec_emptyPath() {
