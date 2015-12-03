9564f01d319fff399b12803005291f555c4aaaa2
Peter Carr <pcarr@broadinstitute.org>
2015-12-02 23:00:49 -0500

    update env-custom-macos.sh; added Java support; setjdk 1.7 and 1.8

--- resources/wrapper_scripts/env-custom-macos.sh
+++ resources/wrapper_scripts/env-custom-macos.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 #
-# declare default implementation for initializing a runtime environment
-# Usage: initEnv <runtime-env-name>
+# Custom runtime environment for MacOS X installation
+#
+
+
+#
+# Initialize runtime environment. 
+#     Usage: initEnv <runtime-env-name>
+# initEnv will be called at module runtime for each required <runtime-env-name>
+#
+# See env-default.sh for canonical runtime environment names
 #
 function initEnv() {
     if ! [ -z ${GP_DEBUG+x} ]; then
@@ -10,9 +18,17 @@ function initEnv() {
         echo "loading $1 ..."
         echo R_LIBS_SITE=${R_LIBS_SITE}
     fi
+    
+    # set path for Java-1.7
+    if [ "$1" = "Java-1.7" ]; then
+        setjdk 1.7
+
+    # set path for Java-1.8
+    elif [ "$1" = "Java-1.8" ]; then
+        setjdk 1.8
 
     # set path for R-3.1
-    if [ "$1" = "R-3.1" ]; then
+    elif [ "$1" = "R-3.1" ]; then
         # add Rscript to path
         R_HOME=/Library/Frameworks/R.framework/Versions/3.1/Resources
         GP_SET_R_PATH=true;
@@ -37,15 +53,25 @@ function initEnv() {
 }
 
 #
-# canonical runtime environment names
+# helper function to switch version of java
 #
+# Usage: setjdk 1.7 
+# Usage: setjdk 1.8 
+#
+# thanks JayWay, http://www.jayway.com/2014/01/15/how-to-switch-jdk-version-on-mac-os-x-maverick/
+#
+function setjdk() {
+    if [ $# -ne 0 ]; then
+        removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+        if [ -n "${JAVA_HOME+x}" ]; then
+            removeFromPath $JAVA_HOME
+        fi
+        export JAVA_HOME=`/usr/libexec/java_home -v $@`
+        export PATH=$JAVA_HOME/bin:$PATH
+    fi
+}
 
-#putValue 'R-2.0'
-#putValue 'R-2.5'
-#putValue 'R-2.7'
-#putValue 'R-2.10'
-#putValue 'R-2.11'
-#putValue 'R-2.13'
-#putValue 'R-2.14'
-#putValue 'R-3.0'
+function removeFromPath() {
+    export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
+}
 
