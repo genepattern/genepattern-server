fea88eea8637216590e6bca0057e990fd1f4c6b8
Peter Carr <pcarr@broadinstitute.org>
2015-12-07 22:45:46 -0500

    removed backup gpbroad wrapper scripts

--- resources/wrapper_scripts/gpbroad/run-use-r-2.7.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-. /broad/tools/scripts/useuse
-use R-2.7
-
-# Needed for FLAME modules
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/xchip/gpdev/shared_libraries"
-
-#debug echo "running cmd: $@"
-"$@"
