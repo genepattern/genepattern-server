72b0f5a11590e06f40d22047998466e1c3cf6ea7
Peter Carr <pcarr@broadinstitute.org>
2016-01-21 22:07:45 -0500

    Mac installer, re-write copyDirectory using ProcessBuilder; remove apache commons-io-2.0.1 dependency GP-5992

--- gp-macapp/src/org/genepattern/startapp/GenePattern.java
+++ gp-macapp/src/org/genepattern/startapp/GenePattern.java
@@ -3,13 +3,10 @@
  *******************************************************************************/
 package org.genepattern.startapp;
 
-import org.apache.commons.io.FileUtils;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Class charged with bootstrapping GenePattern in the Mac app
@@ -62,19 +59,37 @@ public class GenePattern {
         ConfigApp.main(new String[] {args[0]});
     }
 
-    protected static void copyDirectory(final File from, final File to) throws IOException {
-        //FileUtils.copyDirectory(from, to);
-        copyDir_sh(from,to);
-    }
-
-    protected static void copyDirectory_apache_commons_io(final File from, final File to) throws IOException {
-        FileUtils.copyDirectory(from, to);
-    }
-
-    // copy directory as shell exec
-    protected static void copyDir_sh(final File from, final File to) throws IOException {
-        final String[] cmd={"cp", "-Rp", from.getAbsolutePath(), to.getAbsolutePath()};
-        Runtime.getRuntime().exec(cmd);
+    /**
+     * Copy directory with 'cp -Rp' command in a new OS Process, using the ProcessBuilder class.
+     * @param from, the source directory
+     * @param to, the destination directory
+     * @return true on success
+     */
+    protected static boolean copyDirectory(final File from, final File to) { 
+        final List<String> cmd=Arrays.asList("cp", "-Rp", from.getAbsolutePath(), to.getAbsolutePath());
+        final ProcessBuilder pb=new ProcessBuilder()
+            .command( cmd );
+        pb.inheritIO();
+        Process p=null;
+        try {
+            p=pb.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        try {
+            int exitCode=p.waitFor();
+            if (exitCode==0) {
+                return true;
+            }
+            System.err.println("cmd="+cmd);
+            System.err.println("failed with exitCode="+exitCode);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
     
     /**
@@ -155,48 +170,4 @@ public class GenePattern {
         return new File(workingDirString);
     }
 
-    /**
-     * Run the shell script to launch GenePattern
-     * Used after the config app has saved its data
-     *
-     * @param workingDirectory
-     */
-    private static void runShellScript(String workingDirectory) {
-        File workingDir = new File(workingDirectory);
-
-        File catalinaPath = new File(workingDir.getParent(), "MacOS/GenePattern");
-
-        List<String> processList = new ArrayList<String>();
-        processList.add(catalinaPath.getPath());
-
-        ProcessBuilder builder = new ProcessBuilder(processList);
-
-        // This is where you set the root folder for the executable to run in
-        builder.directory(workingDir);
-
-        builder.redirectErrorStream(true);
-        Process process = null;
-        try {
-            process = builder.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Scanner s = new Scanner(process.getInputStream());
-        StringBuilder text = new StringBuilder();
-        while (s.hasNextLine()) {
-            text.append(s.nextLine());
-            text.append("\n");
-        }
-        s.close();
-
-        int result = 0;
-        try {
-            result = process.waitFor();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.print( "Process exited with result " + result + " and output " + text );
-    }
 }
