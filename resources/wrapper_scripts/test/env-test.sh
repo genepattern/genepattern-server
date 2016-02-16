cc001c6079eaa8e1010d7c4b9407ed559402e869
Peter Carr <pcarr@broadinstitute.org>
2016-02-16 18:26:32 -0500

    Move appendPath function into 'env-lookup.sh' GP-6005

--- resources/wrapper_scripts/test/env-test.sh
+++ resources/wrapper_scripts/test/env-test.sh
@@ -87,36 +87,15 @@ testFileExists() {
     assertTrue "fileExists('$prefix$suffix')" "[ -e $prefix$suffix ]"
 }
 
-#
-# Append an element to the end of the path; 
-# Usage: path=$(appendPath "${path}" "${element}")
-#
-appendPath() {
-    local path="${1}";
-    local element="${2}";
-    
-    # Note, to check for a directory: [ -d "$element" ] 
-    # To prepend, path="$element:$path"
-    
-    # if path is not set ... just set it to element
-    # Note:  [ -z "${path+x}" ] checks if the 'path' variable is declared
-    if [ -z "$path" ]; then
-        #echo "2, path not set";
-        path="$element";
-    elif [[ ":$path:" != *":$element:"* ]]; then
-        path="${path:+"$path:"}$element"
-    fi
-    # use echo to return a value
-    echo "$path"
-}
-
 testAppendPath() {
+    source ../env-lookup.sh
     MY_PATH="/opt/dir1";
     MY_PATH=$(appendPath "${MY_PATH}" "/opt/dir2")
     assertEquals "appendPath" "/opt/dir1:/opt/dir2" "${MY_PATH}"
 }
 
 testAppendPath_ignoreDuplicate() {
+    source ../env-lookup.sh
     MY_PATH="/opt/dir1:/opt/dir2:/opt/dir3";
     
     assertEquals "appendPath, ignore dupe in front" "/opt/dir1:/opt/dir2:/opt/dir3" \
@@ -134,6 +113,7 @@ testAppendPath_ignoreDuplicate() {
 }
 
 testAppendPath_toEmpty() {
+    source ../env-lookup.sh
     MY_ARG="/new/pathelement";
     unset MY_PATH;
     MY_PATH=$(appendPath "${MY_PATH}" "${MY_ARG}")
@@ -141,6 +121,7 @@ testAppendPath_toEmpty() {
 }
 
 testMcrAtIndianaU() {
+    source ../env-lookup.sh
     expected_mcr_path="/N/soft/rhel6/matlab/MATLAB_Compiler_Runtime/v81/runtime/glnxa64:\
 /N/soft/rhel6/matlab/MATLAB_Compiler_Runtime/v81/bin/glnxa64:\
 /N/soft/rhel6/matlab/MATLAB_Compiler_Runtime/v81/sys/os/glnxa64:\
