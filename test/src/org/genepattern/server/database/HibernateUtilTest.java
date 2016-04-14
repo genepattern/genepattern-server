c0c0e63e4ef160c99c1e72e9a6b3695bc0ec19ba
Peter Carr <pcarr@broadinstitute.org>
2016-04-14 00:05:13 -0400

    moved createSequence into HibernateUtil class GP-6212, GP-6220

--- test/src/org/genepattern/server/database/HibernateUtilTest.java
+++ test/src/org/genepattern/server/database/HibernateUtilTest.java
@@ -11,8 +11,6 @@ import org.genepattern.junitutil.DbUtil;
 import org.genepattern.server.DbException;
 import org.genepattern.server.config.GpConfig;
 import org.genepattern.server.database.HibernateUtil;
-import org.hibernate.HibernateException;
-import org.hibernate.SQLQuery;
 import org.junit.Before;
 import org.junit.Test;
 
