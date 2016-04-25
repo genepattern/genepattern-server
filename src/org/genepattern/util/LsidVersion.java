9464f048e2b99c122bc02844718985fdf3e606f6
Peter Carr <pcarr@broadinstitute.org>
2016-04-25 03:05:39 -0400

    completed work and tests on LsidVersion class GP-6212, GP-6220

--- src/org/genepattern/util/LsidVersion.java
+++ src/org/genepattern/util/LsidVersion.java
@@ -9,14 +9,16 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 
 /**
- * Help class for managing the LSID version String.
+ * Helper class for managing the LSID version String.
  * 
  * @author pcarr
  *
  */
 public class LsidVersion implements Comparable<LsidVersion> {
+    public static final int initialVersion=1;
+
     /**
-     * Create a new instance.
+     * Create a new instance from a String.
      * Split the version string into a sequence of major and minor version elements.
      * Each element must be an Integer >= 0. Special-case for null or empty input, return the
      * default initial version.
@@ -42,6 +44,12 @@ public class LsidVersion implements Comparable<LsidVersion> {
         return new LsidVersion(versions);
     }
     
+    /**
+     * Helper method for parsing the major or minor version in the sequence.
+     * @param versionElement, expecting an integer >= zero.
+     * @return
+     * @throws IllegalArgumentException
+     */
     protected static Integer parseVersionElement(final String versionElement) throws IllegalArgumentException {
         Integer i=null;
         try {
@@ -63,11 +71,19 @@ public class LsidVersion implements Comparable<LsidVersion> {
     /** 
      * create a new immutable instance, from an array of Integer 
      * E.g.
-     *     LsidVersion(1); // create version="1"
-     *     LsidVersion();  // empty version="", synonym for "0"
+     *     LsidVersion();    // empty version="", synonym for "0"
+     *     LsidVersion(1);   // create version="1"
      *     LsidVersion(1,1); // create version "1.1"
+     *     
+     * @throws IllegalArgumentException if any integer values are less than zero.
      */
-    protected LsidVersion(final Integer...versions) {
+    protected LsidVersion(final Integer...versions) throws IllegalArgumentException {
+        // validate versions
+        for(final Integer i : versions) {
+            if (i<0) {
+                throw new IllegalArgumentException("Invalid versionElement='"+i+"', Must be an integer >= 0");
+            }
+        }
         this.versions=ImmutableList.copyOf(versions);
         this.versionString=asString(this.versions);
         this.hashCode=versions.hashCode();
@@ -78,51 +94,118 @@ public class LsidVersion implements Comparable<LsidVersion> {
      * @param versions
      */
     protected LsidVersion(final List<Integer> versions) {
-        this.versions=ImmutableList.copyOf(versions);
-        this.versionString=asString(this.versions);
-        this.hashCode=versions.hashCode();
+        this(versions.toArray(new Integer[versions.size()]));
     }
     
-    public LsidVersion nextMajor() {
-        return nextSequence(0);
-    }
-    
-    public LsidVersion nextMinor() {
-        return nextSequence(1);
-    }
-    
-    public LsidVersion nextPatch() {
-        return nextSequence(2);
+    /** 
+     * Increment the current version level, for example to create a new major release,
+     *     1 -> 2
+     * Or to create a new minor release,
+     *     3.1 -> 3.2
+     * Other examples,
+     *     3.1.15 -> 3.1.16
+     */
+    public LsidVersion increment() {
+        final int K=versions.size();
+        // special-case: no versions
+        if (K==0) {
+            return incrementVersion(0);
+        }
+        return incrementVersion(K-1);
     }
 
     /**
-     * 
-     * @param sequenceIdx, 
-     *     versions[0], is MAJOR version
-     *     versions[1], is MINOR version
-     *     versions[2], is PATCH version
-     *     versions[N], is Nth additional version
-     * @return
+     * Shift the current version level one to the left, for example 
+     * to create a new major release from a minor release,
+     *     0.48 -> 1
+     *     1.3  -> 2
+     * Other examples,
+     *     3.1.15 -> 3.2
+     * @return a new immutable LsidVersion instance
      */
-    public LsidVersion nextSequence(final int idx) {
-        final List<Integer> next=new ArrayList<Integer>(idx+1);
-        // initialize each element with '0'
-        for (int i = 0; i<=idx; ++i) {
-            next.add(0);
+    public LsidVersion incrementLeft() {
+        final int K=versions.size();
+        return incrementVersion(
+                // handle special cases: zero or 1 version
+                Math.max(0, K-2)
+        );
+    }
+
+    /**
+     * Shift the current version level on to the right, for example
+     * to create a new minor release from a major release,
+     *     1 -> 1.1
+     *     2 -> 2.1
+     * Other examples,
+     *     3.1 -> 3.1.1
+     *     3.1.15 -> 3.1.15.1
+     * @return a new immutable LsidVersion instance
+     */
+    public LsidVersion incrementRight() {
+        final int K=versions.size();
+        if (K==0) {
+            return new LsidVersion(0,1);
         }
-        // copy pre-existing lhs version
-        for (int i = 0; i<idx && i<versions.size(); ++i) {
-            next.set(i, versions.get(i));
+        return incrementVersion(K);
+    }
+
+    /** synonym for {@link #incrementVersion(0)} */
+    public LsidVersion nextMajor() {
+        return incrementVersion(0);
+    }
+    
+    /** synonym for {@link #incrementVersion(1)} */
+    public LsidVersion nextMinor() {
+        return incrementVersion(1);
+    }
+    
+    /** synonym for {@link #incrementVersion(2)} */
+    public LsidVersion nextPatch() {
+        return incrementVersion(2);
+    }
+
+    /**
+     * Create a new LsidVersion by incrementing the version at the request idx level.
+     *     idx=0, {MAJOR}
+     *     idx=1, {MINOR}
+     *     idx=2, {PATCH}
+     *     ...
+     *     idx=N, {additional minor versions}
+     * 
+     * For example, given an existing version "3.1.15" ({major}.{minor}.{patch}),
+     * versions=[3, 1, 15], idx is the index into the versions array.
+     *     increment(0) -> 4
+     *     increment(1) -> 3.2
+     *     increment(2) -> 3.1.16
+     * Fill in additional minor levels as needed, with an initial value of 0 and 
+     * the right-most value incremented to 1.
+     *     increment(5) -> 3.1.15.0.0.1
+     * 
+     * @param idx - the index in the versions[] array
+     * @return a new immutable LsidVersion instance
+     * @throws IllegalArgumentException - idx must be >=0 and <= 100 (arbitrary MAX number of version elements)
+     */
+    protected LsidVersion incrementVersion(final int idx) throws IllegalArgumentException {
+        if (idx<0) {
+            throw new IllegalArgumentException("idx='"+idx+"': Must be >= 0");
         }
-        // increment pre-existing idx version
-        if (idx < versions.size()) {
-            next.set(idx, 1+versions.get(idx));
+        if (idx > 100) {
+            throw new IllegalArgumentException("idx='"+idx+"': Must be <= 100, Usually in the range 0..2");
+        }
+        final int K = versions.size();
+        
+        final ImmutableList.Builder<Integer> b = new ImmutableList.Builder<Integer>();
+        b.addAll( versions.subList(0, Math.min(idx,K)) ); // copy up to idx, exclusive
+        if (idx<K) {
+            b.add(1+versions.get(idx));
         }
         else {
-            // set to initial version
-            next.set(idx, 1);            
+            for(int i=K; i<idx; ++i) {
+                b.add(0);
+            }
+            b.add(1);
         }
-        return new LsidVersion(next);
+        return new LsidVersion(b.build());
     }
 
     protected static String asString(final List<Integer> versions) {
@@ -134,17 +217,6 @@ public class LsidVersion implements Comparable<LsidVersion> {
         return versionString;
     }
     
-    private int getOrZero(int idx) {
-        return getOrDefault(idx, 0);
-    }
-
-    private int getOrDefault(int idx, final Integer defaultValue) {
-        if (versions==null || versions.size()<=idx) {
-            return defaultValue;
-        }
-        return versions.get(idx);
-    }
-
     @Override
     public int compareTo(final LsidVersion o) {
         return compareToLsidMode(o);
@@ -189,15 +261,12 @@ public class LsidVersion implements Comparable<LsidVersion> {
         if (versions.size()==o.versions.size()) {
             return 0;
         }
-        
-        // if we are here, it means o.versions.size > versions.size
-        if (o.versions.size() > versions.size()) {
+        else if (versions.size() < o.versions.size()) {
             return -1;
         }
-        else if (o.versions.size() < versions.size()) {
+        else {
             return 1;
         }
-        return 0;
     }
 
     /**
@@ -226,6 +295,17 @@ public class LsidVersion implements Comparable<LsidVersion> {
         return 0;
     }
     
+    private int getOrZero(int idx) {
+        return getOrDefault(idx, 0);
+    }
+
+    private int getOrDefault(int idx, final Integer defaultValue) {
+        if (versions==null || versions.size()<=idx) {
+            return defaultValue;
+        }
+        return versions.get(idx);
+    }
+
     public boolean equals(final Object obj) {
         if (!(obj instanceof LsidVersion)) {
             return false;
--- test/src/org/genepattern/util/TestLsidVersion.java
+++ test/src/org/genepattern/util/TestLsidVersion.java
@@ -3,24 +3,200 @@ package org.genepattern.util;
 import static org.genepattern.util.LsidVersion.fromString;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class TestLsidVersion {
-    @Before
-    public void setUp() {
+    
+    @Test
+    public void createDefaultVersion() {
+        assertEquals("increment from <init>", fromString("1"),  new LsidVersion().increment());
+    }
+    
+    @Test
+    public void createNewMajorVersion() {
+        assertEquals("1", new LsidVersion().nextMajor().toString());
+    }
+
+    @Test
+    public void createNewMinorVersion() {
+        assertEquals("0.1", new LsidVersion().nextMinor().toString());
+    }
+
+    @Test
+    public void createNewPatchVersion() {
+        assertEquals("0.0.1", new LsidVersion().nextPatch().toString());
+    }
+    
+    protected void incrementTest(final String from, final String to) {
+        assertEquals("'"+from+"'.increment", fromString(to),
+                fromString(from).increment());
+    }
+
+    protected void incrementLeftTest(final String from, final String to) {
+        assertEquals("'"+from+"'.incrementLeft", fromString(to),
+                fromString(from).incrementLeft());
+    }
+    
+    protected void incrementRightTest(final String from, final String to) {
+        assertEquals("'"+from+"'.incrementRight", fromString(to),
+                fromString(from).incrementRight());
+    }
+
+    @Test
+    public void incrementMajor() {
+        incrementTest("1", "2");
+        incrementTest("2", "3");
+    }
+
+    @Test
+    public void incrementMinor() {
+        incrementTest("0.1", "0.2");
+        incrementTest("0.2", "0.3");
+        incrementTest("0.9", "0.10");
+        incrementTest("0.10", "0.11");
+    }
+
+    @Test
+    public void incrementPatch() {
+        incrementTest("0.0.0", "0.0.1");
+        incrementTest("0.0.1", "0.0.2");
+        incrementTest("0.0.9", "0.0.10");
+        incrementTest("0.0.10", "0.0.11");
+        incrementTest("3.1.15", "3.1.16");
+    }
+
+    @Test
+    public void increment_subPatch() {
+        incrementTest("0.0.0.0", "0.0.0.1");
+        incrementTest("0.0.0.1", "0.0.0.2");
+        incrementTest("0.0.0.9", "0.0.0.10");
+        incrementTest("0.0.0.10", "0.0.0.11");
+        incrementTest("3.1.15.1", "3.1.15.2");
+    }
+    
+    @Test
+    public void incrementLeft() {
+        // corner-case: can't shift INITIAL version
+        incrementLeftTest("", "1");
+        // corner-case: can't shift MAJOR version
+        incrementLeftTest("1", "2");
+        incrementLeftTest("0.1", "1");
+        incrementLeftTest("0.48", "1");
+        incrementLeftTest("1.1", "2");
+        incrementLeftTest("3.14159", "4");
+        incrementLeftTest("0.0.23", "0.1");
+        incrementLeftTest("1.3.5", "1.4");
+        incrementLeftTest("3.9.7.135", "3.9.8");
+        
+    }
+    
+    @Test
+    public void incrementRight() {
+        // special-case: '' -> 0.1
+        incrementRightTest("", "0.1");
+        // 0 -> 0.1
+        incrementRightTest("0","0.1");
+        // 1 -> 1.1
+        incrementRightTest("1","1.1");
+        // 1.10 -> 1.10.1
+        incrementRightTest("1.10","1.10.1");
+    }
+    
+    @Test
+    public void example() {
+        // from example, "3.1.15"
+        incrementTest( "3.1.15", "3.1.16");
+        incrementLeftTest("3.1.15", "3.2");
+        incrementLeftTest("3.1", "4");
+        incrementLeftTest("3", "4"); // <---- corner-case
+        // from example, "3.1.15"
+        incrementRightTest("3", "3.1");
+        incrementRightTest("3.1", "3.1.1");
+        incrementRightTest("3.1.15", "3.1.15.1");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void incrementVersion_idxLessThanZero() {
+        // idx < 0, throws exception
+        fromString("3.1.15").incrementVersion(-1);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void incrementVersion_idxExceedsMax() {
+        // idx > 100, throws exception
+        fromString("3.1.15").incrementVersion(101);
     }
     
     @Test(expected=Exception.class)
     public void invalidVersion_mustBeAnInteger() {
-        fromString("1-beta");
+        fromString("my version");
     }
 
     @Test(expected=Exception.class)
-    public void invalidVersion_mustBeAPositiveInteger() {
-        fromString("-1");
+    public void invalidVersion_negativeInteger() {
+        fromString("1.0.-5");
+    }
+
+    /**
+     * Semantic Versioning pre-release version not supported
+     * Examples: 1.0.0-alpha, 1.0.0-alpha.1, 1.0.0-0.3.7, 1.0.0-x.7.z.92
+     */
+    @Test(expected=Exception.class)
+    public void semVer_pre_release_version() {
+        fromString("1.0.0-alpha");
+    }
+
+    @Test(expected=Exception.class)
+    public void semVer_pre_release_version_02() {
+        fromString("1.0.0-alpha.1");
+    }
+
+    @Test(expected=Exception.class)
+    public void semVer_pre_release_version_03() {
+        fromString("1.0.0-0.3.7");
+    }
+
+    @Test(expected=Exception.class)
+    public void semVer_pre_release_version_04() {
+        fromString("1.0.0-x.7.z.92");
     }
     
+    /**
+     * Semantic Versioning build metadata not supported
+     * Examples 1.0.0-alpha+001, 1.0.0+20130313144700, 1.0.0-beta+exp.sha.5114f85
+     */
+    @Test(expected=Exception.class)
+    public void semVer_metadata_01() {
+        fromString("1.0.0-alpha+001");
+    }
+
+    @Test(expected=Exception.class)
+    public void semVer_metadata_02() {
+        fromString("1.0.0+20130313144700");
+    }
+
+    @Test(expected=Exception.class)
+    public void semVer_metadata_03() {
+        fromString("1.0.0-beta+exp.sha.5114f85");
+    }
+
+    @Test(expected=Exception.class)
+    public void invalidVersion_semVer_metaData_example2() {
+        //Note: Semantic Versioning meta data not implemented
+        fromString("1.0.1+beta");
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void invalidIntVersion() {
+        new LsidVersion(3, 1, 15, -5, 0, 0, 1);
+    }
+
+    @Test
+    public void testEquals() {
+        String str="3.1.15";
+        assertEquals(false, fromString(str).equals(str));
+    }
+
     private void doCompareTest(final int expected, final String lhs, final String rhs) {
         final LsidVersion left=fromString(lhs);
         final LsidVersion right=fromString(rhs);
@@ -92,116 +268,47 @@ public class TestLsidVersion {
     }
 
     @Test
-    public void nullVersion() {
-        // this is the version before the first version
-        LsidVersion v=new LsidVersion();
-        assertEquals("<init version>", "", v.toString());
-        
-        assertEquals("<init>.nextMinor", "0.1", v.nextMinor().toString());
-        assertEquals("<init>.nextPatch", "0.0.1", v.nextPatch().toString());
-        
-        assertEquals("<init>.nextMajor as nextSequence(0)", "1", v.nextSequence(0).toString());
-        assertEquals("<init>.nextMinor as nextSequence(1)", "0.1", v.nextSequence(1).toString());
-        assertEquals("<init>.nextPatch as nextSequence(2)", "0.0.1", v.nextSequence(2).toString());
-        assertEquals("<init>.nextSequence(3)", "0.0.0.1", v.nextSequence(3).toString());
+    public void compareToSemVer_basic() {
+        // '' == ''
+        String from="";
+        String to="";        
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                0, fromString(from).compareToSemVer(fromString(to)));
+
+        // '' < 1
+        to="1";        
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                -1, fromString(from).compareToSemVer(fromString(to)));
+
+        from="3.1.15";
+        to="3.1.15";        
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                0, fromString(from).compareToSemVer(fromString(to)));
+        from="3";
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                -1, fromString(from).compareToSemVer(fromString(to)));
+        from="3.1";
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                -1, fromString(from).compareToSemVer(fromString(to)));
+        from="3.1.14";
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                -1, fromString(from).compareToSemVer(fromString(to)));
+        from="3.1.14.502";
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                -1, fromString(from).compareToSemVer(fromString(to)));
+        // Note: not sure if this is good idea, '3.1.15.0' == '3.1.15'
+        from="3.1.15.0";
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                0, fromString(from).compareToSemVer(fromString(to)));
+        from="3.1.15.1";
+        assertEquals("'"+from+"'.compareTo('"+to+"')", 
+                1, fromString(from).compareToSemVer(fromString(to)));
     }
     
     @Test
-    public void initialMajorVersion() {
-        final LsidVersion init=new LsidVersion();
-        assertEquals("<init>.nextMajor", fromString("1"), init.nextMajor());
-        assertEquals("LsidVersion.compare", fromString("1"), init.nextMajor());
-    }
-
-    // 1 -> 2
-    @Test
-    public void incrementMajorVersion() { 
-        String from="1";
-        assertEquals("nextMajor("+from+")", 
-                fromString("2"), 
-                fromString(from).nextMajor());
-    }
-
-    // 1.3 -> 2
-    @Test
-    public void nextMajorVersion_fromMinor() {
-        final String from="1.3";
-        final String expected="2";
-        assertEquals("("+from+").nextMajor", 
-                fromString(expected), fromString(from).nextMajor());
-    }
-
-    // 1.3.2 -> 2
-    @Test
-    public void nextMajorVersion_fromPatch() { 
-        final String from="1.3.2";
-        final String expected="2";
-        assertEquals("("+from+").nextMajor", 
-                fromString(expected), fromString(from).nextMajor());
-    }
-
-    // 1 -> 1.1
-    @Test
-    public void nextMinorVersion_fromMajor() { 
-        String from="1";
-        assertEquals("nextMinor("+from+")", 
-                fromString("1.1"), 
-                fromString(from).nextMinor());
-    }
-
-    // 1.3 -> 1.4
-    @Test
-    public void incrementMinorVersion() {
-        final String from="1.3";
-        final String expected="1.4";
-        assertEquals("("+from+").nextMinor", 
-                fromString(expected), 
-                fromString(from).nextMinor());
-    }
-
-    // 1.3.2 -> 1.4
-    @Test
-    public void nextMinorVersion_fromPatch() { 
-        final String from="1.3.2";
-        final String expected="1.4";
-        assertEquals("("+from+").nextMinor", 
-                fromString(expected), fromString(from).nextMinor());
-    }
-
-    // 1 -> 1.0.1
-    @Test
-    public void nextPatchVersion_fromMajor() { 
-        String from="1";
-        assertEquals("nextPath("+from+")", 
-                fromString("1.0.1"), 
-                fromString(from).nextPatch());
-    }
-
-    // 1.3 -> 1.3.1
-    @Test
-    public void nextPatchVersion_fromMinor() { 
-        String from="1.3";
-        assertEquals("nextPatch("+from+")", 
-                fromString("1.3.1"), 
-                fromString(from).nextPatch());
-    }
-    
-    // 1.3.2 -> 1.3.3
-    @Test
-    public void incrementPatch() { 
-        String from="1.3.2";
-        assertEquals("nextPatch("+from+")", 
-                fromString("1.3.3"), 
-                fromString(from).nextPatch());
-    }
-
-    // 1.3.9 -> 1.3.10
-    @Test
-    public void incrementPatch_digits() { 
-        String from="1.3.9";
-        assertEquals("nextPatch("+from+")", 
-                fromString("1.3.10"), 
-                fromString(from).nextPatch());
+    public void compareToSemVer_self() {
+        LsidVersion self=fromString("3.1.15");
+        assertEquals(0, self.compareToSemVer(self));
     }
 
 }
