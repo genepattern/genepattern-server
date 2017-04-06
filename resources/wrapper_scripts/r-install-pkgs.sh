5cd89a19b4d20ea7ca6a2901daba4a8b71204faa
pcarr <pcarr@wmca4-57b.broadinstitute.org>
2017-04-06 04:54:48 -0400

    Rscript wrapper scripts for custom GP_R_LIBS_SITE, GP-6347

---
+++ resources/wrapper_scripts/r-install-pkgs.sh
@@ -0,0 +1,55 @@
+############################################################
+# r-install-pkgs.sh
+#   Install R packages for a GenePattern module
+# Usage:
+#   ./r-install-pkgs 
+# Input:
+#   r-version
+#   r-site-library
+#     default=<patches>/[<env-arch>/]Library/R/<r-version>
+#   r-package-info-file
+#     default=<libdir>r.package.info
+#   gp-mkdirs
+#     default=true
+#
+# Example:
+#   ../r-install-pkgs.sh \
+#     -c env-custom-macos.sh \
+#     -u R-3.1 \
+#     -e GP_R_VERSION=3.1 \
+#     -e GP_DRY_RUN=TRUE \
+#     -e GP_MKDIRS=TRUE \
+#     -e GP_R_LIBS_SITE=${HOME}/tmp/site-library \
+#     -e GP_R_PACKAGE_INFO=affy_efc_v0.14_r.package.info \
+#   >> stdout.txt
+#
+############################################################
+
+############################################################
+# Function: run_rscript_cmd
+#   Run an 'Rscript' module, installing packages if necessary
+############################################################
+#run_rscript_cmd() {
+  # GP_R_LIBS_SITE, default=<patches>/[<env-arch>/]Library/R/<r-version>
+  # GP_R_PACKAGE_INFO, default=<libdir>r.package.info
+#}
+
+main() {
+  local __dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+  source "${__dir}/gp-common.sh"
+  strict_mode
+  source "${__dir}/r-common.sh"
+  parseArgs "${@}"
+  initModuleEnvs
+  export_r_env
+  # by default, GP_MKDIRS=true
+  [[ -z "${GP_MKDIRS+x}" ]] \
+    && GP_MKDIRS="TRUE";
+  
+  # debug: env | sort
+  if [[ -e "${GP_R_PACKAGE_INFO}" ]]; then
+    install_pkgs
+  fi
+}
+
+main "$@"
---
+++ resources/wrapper_scripts/run-rscript-pkgs.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+############################################################
+# run-rscript-pkgs.sh
+#   Run Rscript command for a GenePattern Module
+#   Automatically install required packages
+# Usage:
+#   ./run-rscript-pkgs.sh \
+#     [--dry-run] \
+#     -v r-version \
+#     -c env-custom \
+#     -e GP_R_LIBS_SITE=gp-r-libs-site \
+#     -e GP_R_PACKAGE_INFO=gp-r-package-info \
+#     -- \
+#     rscript-args
+############################################################
+
+############################################################
+# Function: run_rscript_cmd
+#   Run an 'Rscript' module, installing packages if necessary
+############################################################
+#run_rscript_cmd() {
+  # GP_ROOT_R_LIBS_SITE, default=<patches>/[<env-arch>/]Library/R
+  # GP_R_LIBS_SITE, default=<patches>/[<env-arch>/]Library/R/<r-version>
+  # GP_R_PACKAGE_INFO, default=<libdir>r.package.info
+#}
+
+main() {
+  local __dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+  source "${__dir}/gp-common.sh"
+  strict_mode
+  source "${__dir}/r-common.sh"
+  # debug: echo "args: ${@}"
+  
+  # '--dry-run', shortcut for '-e' 'GP_DRY_RUN=true'
+  if [[ "$1" = "--dry-run" ]]; then
+    export GP_DRY_RUN="true"
+    shift;
+  fi
+  
+  # '-v' r-version, shortcut for '-e' 'GP_R_VERSION=r-version'
+  local r_version="";
+  if [[ "$1" = "-v" ]]; then
+    shift;
+    r_version="$1"
+    shift;
+    export GP_R_VERSION="$r_version"
+  fi
+  # debug: echo "r_version=$r_version"
+  # debug: echo "args: ${@}"
+  
+  parseArgs "${@}"
+  addEnv "r/${r_version}"
+  initModuleEnvs
+  export_r_env
+  # by default, GP_MKDIRS=true
+  [[ -z "${GP_MKDIRS+x}" ]] \
+    && GP_MKDIRS="TRUE";
+  
+  # debug: 
+  env | sort
+
+  if [[ -e "${GP_R_PACKAGE_INFO:-}" ]]; then
+    # debug: echo "installing packages ..." 1>&2 ;
+    install_pkgs
+  fi
+
+  # run the Rscript command
+  $__gp_dry_run Rscript "${__gp_module_cmd[@]}"
+}
+
+main "$@"
