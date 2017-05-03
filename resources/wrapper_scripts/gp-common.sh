3ecef251384fd73eb95cc44b33a6b5f4edeff640
pcarr <pcarr@wmca4-57b.broadinstitute.org>
2017-05-03 17:52:52 -0400

    gp-common.sh, comments

--- resources/wrapper_scripts/gp-common.sh
+++ resources/wrapper_scripts/gp-common.sh
@@ -87,6 +87,7 @@ strict_mode() {
   # Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
   set -o nounset
   # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+  #   A trap on ERR, if set, is executed before the shell exits.
   set -o pipefail
   # Turn on traces, useful while debugging but commented out by default
   #set -o xtrace
@@ -130,7 +131,6 @@ not_empty() {
 # not_set
 ############################################################
 not_set() {
-  # [[ -z "${R_LIBS_SITE+x}" ]]
   if [[ $# -eq 0 ]]; then
     return 1;
   fi
