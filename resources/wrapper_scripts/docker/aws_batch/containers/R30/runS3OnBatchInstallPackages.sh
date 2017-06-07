8fcb2c70fe35e2a124a6b1be7f608cfaab49e14d
ted Liefeld <liefeld@broadinstitute.org>
2017-06-07 10:36:18 -0700

    First working version of container for R3.o tested with Revealer module

--- resources/wrapper_scripts/docker/aws_batch/containers/R30/runS3OnBatchInstallPackages.sh
+++ resources/wrapper_scripts/docker/aws_batch/containers/R30/runS3OnBatchInstallPackages.sh
@@ -7,6 +7,10 @@ S3_ROOT="$(echo -e "${3}" | tr -d '[:space:]')"
 WORKING_DIR="$(echo -e "${4}" | tr -d '[:space:]')"
 EXECUTABLE=$5
 
+: ${R_LIBS_S3=/genepattern-server/Rlibraries/R30/rlibs}
+
+export R_LIBS=/usr/local/lib/R/site-library
+
 #
 # assign filenames for STDOUT and STDERR if not already set
 #
@@ -20,16 +24,17 @@ echo Task dir is -$TASKLIB-
 echo executable is -$5-
 echo S3_ROOT is -$S3_ROOT-
 echo input files location  is -$INPUT_FILES_DIR-
+echo R_LIBS is --$R_LIBS
 
 # copy the source over from tasklib
 mkdir -p $TASKLIB
-aws s3 sync $S3_ROOT$TASKLIB $TASKLIB
+aws s3 sync $S3_ROOT$TASKLIB $TASKLIB --quiet
 ls $TASKLIB
 
  
 # copy the inputs
 mkdir -p $INPUT_FILES_DIR
-aws s3 sync $S3_ROOT$INPUT_FILES_DIR $INPUT_FILES_DIR
+aws s3 sync $S3_ROOT$INPUT_FILES_DIR $INPUT_FILES_DIR --quiet
 ls $INPUT_FILES_DIR
 
 # switch to the working directory
@@ -41,8 +46,9 @@ mkdir .gp_metadata
 ##################################################
 # MODIFICATION FOR R PACKAGE INSTALLATION
 ##################################################
+# mount pre-compiled libs from S3
+aws s3 sync $S3_ROOT$R_LIBS_S3 $R_LIBS --quiet
 
-: ${R_LIBS=/genepattern-server/Rlibraries/R30/rlibs}
 if [ -f "$TASKLIB/r.package.info" ]
 then
 	echo "$TASKLIB/r.package.info found."
@@ -50,9 +56,6 @@ then
 else
 	echo "$TASKLIB/r.package.info not found."
 fi
-
-aws s3 sync $S3_ROOT$R_LIBS $R_LIBS
-
 
 
 # run the module
@@ -63,4 +66,4 @@ echo "{ \"exit_code\": $? }">$EXITCODE_FILENAME
 # send the generated files back
 aws s3 sync . $S3_ROOT$WORKING_DIR 
 aws s3 sync $TASKLIB $S3_ROOT$TASKLIB
-aws s3 sync $R_LIBS $S3_ROOT$R_LIBS
+aws s3 sync $R_LIBS $S3_ROOT$R_LIBS_S3
