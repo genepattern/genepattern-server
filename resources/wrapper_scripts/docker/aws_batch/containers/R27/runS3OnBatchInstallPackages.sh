d737427a0a6ee3d774fb5f40699c2f692f8b8dc5
ted Liefeld <liefeld@broadinstitute.org>
2017-06-19 12:59:39 -0700

    still trying to get survival curve to work.  Remove the default 'java ... RunR' in the scripts inside the container as this will now be part of the command line passed in by GenePattern

--- resources/wrapper_scripts/docker/aws_batch/containers/R27/runS3OnBatchInstallPackages.sh
+++ resources/wrapper_scripts/docker/aws_batch/containers/R27/runS3OnBatchInstallPackages.sh
@@ -7,6 +7,10 @@ S3_ROOT="$(echo -e "${3}" | tr -d '[:space:]')"
 WORKING_DIR="$(echo -e "${4}" | tr -d '[:space:]')"
 EXECUTABLE=$5
 
+: ${R_LIBS_S3=/genepattern-server/Rlibraries/R27/rlibs}
+
+export R_LIBS=/usr/local/lib/R/site-library
+
 #
 # assign filenames for STDOUT and STDERR if not already set
 #
@@ -20,16 +24,17 @@ echo Task dir is -$TASKLIB-
 echo executable is -$5-
 echo S3_ROOT is -$S3_ROOT-
 echo input files location  is -$INPUT_FILES_DIR-
+echo R_LIBS is --$R_LIBS
 
 # copy the source over from tasklib
 mkdir -p $TASKLIB
-aws s3 sync $S3_ROOT$TASKLIB $TASKLIB
+aws s3 sync $S3_ROOT$TASKLIB $TASKLIB --quiet
 ls $TASKLIB
 
  
 # copy the inputs
 mkdir -p $INPUT_FILES_DIR
-aws s3 sync $S3_ROOT$INPUT_FILES_DIR $INPUT_FILES_DIR
+aws s3 sync $S3_ROOT$INPUT_FILES_DIR $INPUT_FILES_DIR --quiet
 ls $INPUT_FILES_DIR
 
 # switch to the working directory
@@ -41,6 +46,9 @@ mkdir .gp_metadata
 ##################################################
 # MODIFICATION FOR R PACKAGE INSTALLATION
 ##################################################
+# mount pre-compiled libs from S3
+aws s3 sync $S3_ROOT$R_LIBS_S3 $R_LIBS --quiet
+
 if [ -f "$TASKLIB/r.package.info" ]
 then
 	echo "$TASKLIB/r.package.info found."
@@ -49,15 +57,13 @@ else
 	echo "$TASKLIB/r.package.info not found."
 fi
 
-export RHOME=/packages/R-2.7.2/
-export RFLAGS='--no-save --quiet --slave --no-restore'
-
 
 # run the module
 echo $5
-java -cp /build -DR_HOME=$RHOME -Dr_flags="$RFLAGS" RunR $5 >$STDOUT_FILENAME 2>$STDERR_FILENAME
+$5 >$STDOUT_FILENAME 2>$STDERR_FILENAME
 echo "{ \"exit_code\": $? }">$EXITCODE_FILENAME
 
 # send the generated files back
 aws s3 sync . $S3_ROOT$WORKING_DIR 
 aws s3 sync $TASKLIB $S3_ROOT$TASKLIB
+aws s3 sync $R_LIBS $S3_ROOT$R_LIBS_S3
--- resources/wrapper_scripts/docker/aws_batch/containers/R27/tests/survival_curve/runLocal.sh
+++ resources/wrapper_scripts/docker/aws_batch/containers/R27/tests/survival_curve/runLocal.sh
@@ -11,7 +11,11 @@ S3_ROOT=s3://moduleiotest
 WORKING_DIR=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/r27/tests/survival_curve/job_1111
 
 
-COMMAND_LINE="runLocal.sh $TASKLIB $INPUT_FILES_DIR $S3_ROOT $WORKING_DIR $TASKLIB/SurvivalCurve.R SurvivalCurve surv.txt -c surv.cls surv time censor -fcls.clinical F automatic -lt -lc 1 1 -m 0 1 log 0 T left-bottom"
+
+java -cp /build -DR_HOME=/packages/R-2.7.2/ -Dr_flags="'--no-save --quiet --slave --no-restore'" RunR $5
+
+
+COMMAND_LINE="runLocal.sh $TASKLIB $INPUT_FILES_DIR $S3_ROOT $WORKING_DIR java -cp /build -DR_HOME=/packages/R-2.7.2/ -Dr_flags="'--no-save --quiet --slave --no-restore'" RunR $TASKLIB/SurvivalCurve.R SurvivalCurve surv.txt -c surv.cls surv time censor -fcls.clinical F automatic -lt -lc 1 1 -m 0 1 log 0 T left-bottom"
 
 #docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld_r27  $COMMAND_LINE
 echo $COMMAND_LINE
