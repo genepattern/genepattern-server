00c7e5b43b75e7fb2d576d86fcbf0ef134eda5af
ted Liefeld <liefeld@broadinstitute.org>
2017-06-20 11:56:16 -0700

    changed the way that we build command lines to address proper escaping of r_flags

--- resources/wrapper_scripts/docker/aws_batch/containers/R28/tests/helloWorld/runLocal.sh
+++ resources/wrapper_scripts/docker/aws_batch/containers/R28/tests/helloWorld/runLocal.sh
@@ -10,8 +10,34 @@ INPUT_FILE_DIRECTORIES=/Users/liefeld/GenePattern/gp_dev/genepattern-server/reso
 S3_ROOT=s3://moduleiotest
 WORKING_DIR=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/r28/tests/helloWorld/job_1111
 
+RHOME=/packages/R-2.8.1/
+#RFLAGS='--no-save --quiet --slave --no-restore'
 
-COMMAND_LINE="runLocal.sh $TASKLIB $INPUT_FILE_DIRECTORIES i$S3_ROOT $WORKING_DIR $TASKLIB/hello.R hello"
 
-docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/r28  $COMMAND_LINE
+# note the crazy escaping of the quote string to get the rflags passed without being broken up by either docker or the script inside the container
+
+COMMAND_LINE="runLocal.sh $TASKLIB $INPUT_FILE_DIRECTORIES $S3_ROOT $WORKING_DIR java -cp /build -DR_HOME=$RHOME "-Dr_flags=--no-save --quiet --slave --no-restore" RunR $TASKLIB/hello.R hello"
+echo $COMMAND_LINE 
+
+#docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/r28 $COMMAND_LINE
+
+#docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/r28  runLocal.sh $TASKLIB $INPUT_FILE_DIRECTORIES $S3_ROOT $WORKING_DIR java -cp /build -DR_HOME=$RHOME -Dr_flags="\"--no-save --quiet --slave --no-restore\"" RunR $TASKLIB/hello.R hello 
+
+echo 'a'
+declare -a arr=("/usr/local/bin/runLocal.sh" 
+                "${TASKLIB}" 
+	        "${INPUT_FILE_DIRECTORIES}" 
+		"${S3_ROOT}" 
+		"${WORKING_DIR}" 
+		"java" 
+		"-cp" 
+		"/build" 
+		"-DR_HOME=${RHOME}" 
+		"-Dr_flags=--no-save --quiet --slave --no-restore" 
+		"RunR" 
+		"${TASKLIB}/hello.R" 
+		"hello")
+docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/r28 "${arr[@]}"
+
+#docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/r28 bash -c "$COMMAND_LINE"
 
