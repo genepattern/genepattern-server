cec54a5f8f9311efdd367fa27a71461bd4bc5326
ted Liefeld <liefeld@broadinstitute.org>
2017-06-23 10:44:26 -0700

    updating R30 to use new script form of passing the command line

--- resources/wrapper_scripts/docker/aws_batch/containers/R30/tests/revealer/runLocal.sh
+++ resources/wrapper_scripts/docker/aws_batch/containers/R30/tests/revealer/runLocal.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 
-#
-# parameters to this come from the JobRunner implementation
-# for the AWS install
-#
-# current arg is just job #
-#
-TEST_ROOT=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/R30/tests/revealer
-TASKLIB=$TEST_ROOT/src
-INPUT_FILE_DIRECTORIES=$TEST_ROOT/data
+TASKLIB=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/R30/tests/revealer/src
+INPUT_FILE_DIRECTORIES=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/R30/tests/revealer/data
 S3_ROOT=s3://moduleiotest
-WORKING_DIR=$TEST_ROOT/job_1111
-RLIB=$TEST_ROOT/rlib
+WORKING_DIR=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/R30/tests/revealer/job_1111
+RLIB=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/R30/tests/revealer/rlib
 
+RHOME=/packages/R-3.0.3/
 
-# /xchip/gpprod/servers/genepattern/gp/resources/wrapper_scripts/run-rscript.sh  -c  env-custom-centos5.sh  -l  /xchip/gpprod/servers/genepattern/taskLib/REVEALER.0.53.18220/  -p  /xchip/gpprod/servers/genepattern/gp/patches  -a  centos5  -v  3.0  --  /xchip/gpprod/servers/genepattern/taskLib/REVEALER.0.53.18220//REVEALER_library.v5C.R  --ds1=/xchip/gpprod/servers/genepattern/jobResults/1499734/CTNBB1_transcriptional_reporter.gct  --target.name=BETA-CATENIN-REPORTER  --target.match=positive  --ds2=/xchip/gpprod/servers/genepattern/jobResults/1499734/CCLE_MUT_CNA_AMP_DEL_0.70_2fold.MC.gct  --seed.names=CTNNB1.MC_MUT  --max.n.iter=2  --n.markers=30  --locs.table.file=/xchip/gpprod/servers/genepattern/jobResults/1499734/hgnc_complete_set.Feb_20_2014.v2.txt  --count.thres.low=3  --count.thres.high=50  --pdf.output.file=BCAT_vs_MUT_CNA.pdf
- 
+# note the crazy escaping of the quote string to get the rflags passed without being broken up by either docker or the script inside the container
+#COMMAND_LINE="java -cp /build -DR_HOME=$RHOME -Dr_flags=\"--no-save --quiet --slave --no-restore\" RunR $TASKLIB/hello.R hello"
+
 COMMAND_LINE="Rscript  $TASKLIB/REVEALER_library.v5C.R  --ds1=$INPUT_FILE_DIRECTORIES/CTNBB1_transcriptional_reporter.gct  --target.name=BETA-CATENIN-REPORTER  --target.match=positive  --ds2=$INPUT_FILE_DIRECTORIES/CCLE_MUT_CNA_AMP_DEL_0.70_2fold.MC.gct  --seed.names=CTNNB1.MC_MUT  --max.n.iter=2  --n.markers=30  --locs.table.file=$INPUT_FILE_DIRECTORIES/hgnc_complete_set.Feb_20_2014.v2.txt  --count.thres.low=3  --count.thres.high=50  --pdf.output.file=BCAT_vs_MUT_CNA.pdf"
 
 
 
-docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR -v $RLIB:/build/rlib -w $WORKING_DIR -e LIBDIR=$TASKLIB -e R_LIBS=/build/rlib -e TASKLIB=$TASKLIB  liefeld/r30_cli runLocal.sh $COMMAND_LINE
+#docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR -v $RLIB:/build/rlib -w $WORKING_DIR -e LIBDIR=$TASKLIB -e R_LIBS=/build/rlib -e TASKLIB=$TASKLIB  liefeld/r30_cli runLocal.sh $COMMAND_LINE
 
+
+
+# ##### NEW PART FOR SCRIPT INSTEAD OF COMMAND LINE ################################
+# Make the input file directory since we need to put the script to execute in it
+mkdir -p $WORKING_DIR
+mkdir -p $WORKING_DIR/.gp_metadata
+EXEC_SHELL=$WORKING_DIR/.gp_metadata/local_exec.sh
+
+echo "#!/bin/bash\n" > $EXEC_SHELL
+echo $COMMAND_LINE >>$EXEC_SHELL
+echo "\n " >>$EXEC_SHELL
+
+chmod a+x $EXEC_SHELL
+
+REMOTE_COMMAND="runLocal.sh $TASKLIB $INPUT_FILE_DIRECTORIES $S3_ROOT $WORKING_DIR $EXEC_SHELL"
+
+echo "Container will execute $REMOTE_COMMAND  <EOM>\n"
+docker run -v $RLIB:/usr/local/lib/R/site-library -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/r30 $REMOTE_COMMAND
 
