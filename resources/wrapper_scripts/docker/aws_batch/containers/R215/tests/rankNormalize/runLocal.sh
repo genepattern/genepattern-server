cf5348f9edef7fe4e99bf5b5d6fd18941ee3c052
ted Liefeld <liefeld@broadinstitute.org>
2017-06-27 12:38:56 -0700

    Update for script version execution

--- resources/wrapper_scripts/docker/aws_batch/containers/R215/tests/rankNormalize/runLocal.sh
+++ resources/wrapper_scripts/docker/aws_batch/containers/R215/tests/rankNormalize/runLocal.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-#
-# parameters to this come from the JobRunner implementation
-# for the AWS install
-#
-# current arg is just job #
-#
 TEST_ROOT=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/R215/tests/rankNormalize
 TASKLIB=$TEST_ROOT/src
 INPUT_FILE_DIRECTORIES=$TEST_ROOT/data
@@ -14,11 +8,23 @@ WORKING_DIR=$TEST_ROOT/job_1111
 RLIB=$TEST_ROOT/rlib
 
 
-# run_rank_normalize.R, /xchip/gpprod/servers/genepattern/taskLib/RankNormalize.1.9.17547/, --input.file=/xchip/gpprod/servers/genepattern/users/ted/uploads/tmp/run6841.file/1/all_aml_train.gct, --output.file.name=all_aml_train.NORM.gct
- 
 COMMAND_LINE="Rscript $TASKLIB/run_rank_normalize.R $TASKLIB --input.file=$INPUT_FILE_DIRECTORIES/all_aml_train.gct --output.file.name=all_aml_train.NORM.gct"
 
+# ##### NEW PART FOR SCRIPT INSTEAD OF COMMAND LINE ################################
+# Make the input file directory since we need to put the script to execute in it
+mkdir -p $WORKING_DIR
+mkdir -p $WORKING_DIR/.gp_metadata
+EXEC_SHELL=$WORKING_DIR/.gp_metadata/local_exec.sh
 
-docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR -v $RLIB:/build/rlib -w $WORKING_DIR -e LIBDIR=$TASKLIB -e R_LIBS=/build/rlib -e TASKLIB=$TASKLIB  liefeld/r215 runLocal.sh $COMMAND_LINE
+echo "#!/bin/bash\n" > $EXEC_SHELL
+echo $COMMAND_LINE >>$EXEC_SHELL
+echo "\n " >>$EXEC_SHELL
 
+chmod a+x $EXEC_SHELL
+
+
+REMOTE_COMMAND="runLocal.sh $TASKLIB $INPUT_FILE_DIRECTORIES $S3_ROOT $WORKING_DIR $EXEC_SHELL"
+
+echo "Container will execute $REMOTE_COMMAND  <EOM>\n"
+docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/r215 $REMOTE_COMMAND
 
