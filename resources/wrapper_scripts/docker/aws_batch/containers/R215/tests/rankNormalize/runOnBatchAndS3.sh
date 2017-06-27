bb706ff4d36571bf0a611991f2d1922140193964
ted Liefeld <liefeld@broadinstitute.org>
2017-06-27 12:39:24 -0700

    Update for script version execution

--- resources/wrapper_scripts/docker/aws_batch/containers/R215/tests/rankNormalize/runOnBatchAndS3.sh
+++ resources/wrapper_scripts/docker/aws_batch/containers/R215/tests/rankNormalize/runOnBatchAndS3.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-#
-# parameters to this come from the JobRunner implementation
-# for the AWS install
-#
-# current arg is just job #
-#
 
 TEST_ROOT=/Users/liefeld/GenePattern/gp_dev/genepattern-server/resources/wrapper_scripts/docker/aws_batch/containers/R215/tests/rankNormalize
 TASKLIB=$TEST_ROOT/src
@@ -20,15 +14,35 @@ RLIB=$TEST_ROOT/rlib
 COMMAND_LINE="Rscript $TASKLIB/run_rank_normalize.R $TASKLIB --input.file=$INPUT_FILE_DIRECTORIES/all_aml_train.gct --output.file.name=all_aml_train.NORM.gct"
 
 JOB_DEFINITION_NAME="R215_Generic"
-JOB_ID=gp_job_REVEALER_R215_$1
+JOB_ID=gp_job_RankNormalize_R215_$1
 JOB_QUEUE=TedTest
 
+# ##### NEW PART FOR SCRIPT INSTEAD OF COMMAND LINE ################################
+# Make the input file directory since we need to put the script to execute in it
+mkdir -p $WORKING_DIR/.gp_metadata
+
+EXEC_SHELL=$WORKING_DIR/.gp_metadata/exec.sh
+
+echo "#!/bin/bash\n" > $EXEC_SHELL
+#echo "echo \"$COMMAND_LINE\"" >>$EXEC_SHELL
+echo $COMMAND_LINE >>$EXEC_SHELL 
+echo "\n " >>$EXEC_SHELL 
+
+chmod a+rwx $EXEC_SHELL
+chmod -R a+rwx $WORKING_DIR
+
+REMOTE_COMMAND="runS3OnBatch.sh $TASKLIB $INPUT_FILE_DIRECTORIES $S3_ROOT $WORKING_DIR $EXEC_SHELL"
+# note the batch submit now uses REMOTE_COMMAND instead of COMMAND_LINE 
 
 #
 # Copy the input files to S3 using the same path
 #
 aws s3 sync $INPUT_FILE_DIRECTORIES $S3_ROOT$INPUT_FILE_DIRECTORIES --profile genepattern
 aws s3 sync $TASKLIB $S3_ROOT$TASKLIB --profile genepattern
+aws s3 sync $WORKING_DIR $S3_ROOT$WORKING_DIR --profile genepattern 
+
+######### end new part for script #################################################
+
 
 #       --container-overrides memory=2000 \
 
@@ -37,23 +51,7 @@ aws batch submit-job \
       --job-queue $JOB_QUEUE \
       --container-overrides 'memory=3600' \
       --job-definition $JOB_DEFINITION_NAME \
-      --parameters taskLib=$TASKLIB,inputFileDirectory=$INPUT_FILE_DIRECTORIES,s3_root=$S3_ROOT,working_dir=$WORKING_DIR,exe1="$COMMAND_LINE"  \
+      --parameters taskLib=$TASKLIB,inputFileDirectory=$INPUT_FILE_DIRECTORIES,s3_root=$S3_ROOT,working_dir=$WORKING_DIR,exe1="$REMOTE_COMMAND"  \
       --profile genepattern
 
-# may want to pipe the submit output through this to extract the job ID for checking status
-# | python -c "import sys, json; print json.load(sys.stdin)['jobId']"
-
-
-
-
-# wait for job completion TBD
-#
-#check status 
-# aws batch describe-jobs --jobs 07f93b66-f6c0-47e5-8481-4a04722b7c91 --profile genepattern
-#
-
-#
-# Copy the output of the job back to our local working dir from S3
-#
-#aws s3 sync $S3_ROOT$WORKING_DIR $WORKING_DIR --profile genepattern
 
