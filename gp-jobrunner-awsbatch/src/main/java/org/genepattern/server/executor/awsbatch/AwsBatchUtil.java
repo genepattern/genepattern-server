3ce8b9c52952c3c76005f07f9c2d372d17d3b42b
pcarr <pcarr@wmca4-57b.broadinstitute.org>
2017-07-25 19:20:03 -0400

    awsbatch, 0.1.4-b9, fix for '<run-with-env>' commandLine
    * copy ./<wrapper-scripts>/run-with-env.sh and related files into the
    docker container
    * this fix is required for ComparativeMarkerSelection

--- gp-jobrunner-awsbatch/src/main/java/org/genepattern/server/executor/awsbatch/AwsBatchUtil.java
+++ gp-jobrunner-awsbatch/src/main/java/org/genepattern/server/executor/awsbatch/AwsBatchUtil.java
@@ -2,7 +2,11 @@ package org.genepattern.server.executor.awsbatch;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
@@ -12,6 +16,7 @@ import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
 import org.genepattern.drm.DrmJobRecord;
 import org.genepattern.drm.DrmJobSubmission;
+import org.genepattern.server.config.GpConfig;
 import org.genepattern.server.config.GpContext;
 import org.genepattern.server.executor.CommandExecutorException;
 import org.genepattern.server.job.input.JobInput;
@@ -34,6 +39,22 @@ import com.google.common.base.Strings;
 public class AwsBatchUtil {
     private static final Logger log = Logger.getLogger(AwsBatchUtil.class);
 
+//    public static final Value getOrDefault(final GpConfig gpConfig, final GpContext jobContext, final String key, final Value defaultValue) {
+//        return gpConfig.getValue(jobContext, key, defaultValue);
+//    }
+
+    protected static File getWrapperScriptsDir(final GpConfig gpConfig, final GpContext jobContext) {
+        return gpConfig.getGPFileProperty(jobContext, GpConfig.PROP_WRAPPER_SCRIPTS_DIR);
+    }
+
+    /**
+     * Get the local path to the '<wrapper-scripts>' directory
+     */
+    protected static File getWrapperScriptsDir(final DrmJobSubmission gpJob) {
+        return getWrapperScriptsDir(gpJob.getGpConfig(), gpJob.getJobContext());
+    }
+
+
     /** helper method because String#replaceAll expects a regex */
     protected static String replaceAll_quoted(final String str, final String literal, final String replacement) {
         if (Strings.isNullOrEmpty(str)) {
@@ -55,6 +76,9 @@ public class AwsBatchUtil {
                 Matcher.quoteReplacement(replacement));
     }
 
+    protected static void makeSymLink(final File linkDir, final File target, final String linkName) throws CommandExecutorException {
+        makeSymLink(linkDir, target.toPath(), linkName);
+    }
     /**
      * Make symbolic link to the target file in the given directory.
      *     ln -s <targetFile> <targetFile.name>
@@ -63,13 +87,14 @@ public class AwsBatchUtil {
      * @param linkName
      * @throws CommandExecutorException
      */
-    protected static void makeSymLink(final File linkDir, final File target, final String linkName) throws CommandExecutorException {
+    protected static void makeSymLink(final File linkDir, final Path target, final String linkName) throws CommandExecutorException {
         try {
             Files.createSymbolicLink(
                 // link
                 linkDir.toPath().resolve(linkName), 
                 // target
-                target.toPath());
+                target
+            );
         }
         catch (Throwable t) {
             final String message="Error creating symlink to local input file='"+target+"' in directory='"+linkDir+"'";
@@ -95,23 +120,40 @@ public class AwsBatchUtil {
         return jobContext;
     }
 
+    /**
+     * Get the list of input and support files to be copied into the docker container
+     * before running the job
+     * 
+     * Note: this returns an unmodifiable set
+     */
     protected static Set<File> getInputFiles(final DrmJobSubmission gpJob) {
+        final Set<File> inputFiles = new LinkedHashSet<File>();
+        final Set<File> jobInputFiles=getJobInputFiles(gpJob);
+        inputFiles.addAll(jobInputFiles);
+        // special-case, include <run-with-env> support files
+        final Set<File> additionalSupportFiles=getWrapperScripts(gpJob);
+        inputFiles.addAll(additionalSupportFiles);
+        return Collections.unmodifiableSet(inputFiles);
+    }
+
+    protected static Set<File> getJobInputFiles(final DrmJobSubmission gpJob) {
         if (log.isDebugEnabled()) {
             log.debug("gpJobNo="+gpJob.getGpJobNo()+", listing local file paths ...");
         }
+
         // linked hash set preserves insertion order
-        final Set<File> inputFiles = new LinkedHashSet<File>();
+        final Set<File> jobInputFiles = new LinkedHashSet<File>();
         for(final String localFilePath : gpJob.getJobContext().getLocalFilePaths()) {
-            final File file=getInputFile(gpJob, localFilePath);
-            if (file != null) {
-                inputFiles.add(file);
+            final File file=initJobInputFile(gpJob, localFilePath);
+            if (file != null && file.exists()) {
+                jobInputFiles.add(file);
             }
         }
-        return inputFiles;
+        return Collections.unmodifiableSet(jobInputFiles);
     }
-
-    // called-by getInputFiles
-    protected static File getInputFile(final DrmJobSubmission gpJob, final String localFilePath) {
+    
+    // called-by getJobInputFiles
+    protected static File initJobInputFile(final DrmJobSubmission gpJob, final String localFilePath) {
         log.debug("    localFilePath="+localFilePath);
         if (Strings.isNullOrEmpty(localFilePath)) {
             return null;
@@ -124,6 +166,38 @@ public class AwsBatchUtil {
             log.error("file doesn't exist, for gpJobNo="+gpJob.getGpJobNo()+", localFilePath="+localFilePath);
             return null;
         }
+    }
+
+    protected static Set<File> getWrapperScripts(final DrmJobSubmission gpJob) {
+        if (log.isDebugEnabled()) {
+            log.debug("gpJobNo="+gpJob.getGpJobNo()+", including <run-with-env> files ...");
+        }
+        final Set<File> wrapperScriptFiles = new LinkedHashSet<File>();
+        final Path wrapperScripts=getWrapperScriptsDir(gpJob).toPath();
+        final List<String> supportFiles=Arrays.asList(
+            "run-with-env.sh",
+            "env-hashmap.sh",
+            "env-lookup.sh",
+            "gp-common.sh",
+            "env-default.sh",
+            "env-custom.sh",
+            gpJob.getGpConfig().getGPProperty(gpJob.getJobContext(), "env-custom", "env-custom.sh")
+        );
+        for(final String path : supportFiles) {
+            if (Strings.isNullOrEmpty(path)) {
+                // skip empty path
+            }
+            else {
+                final File file=wrapperScripts.resolve(path).toFile();
+                if (file != null && file.exists()) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("    <wrapper-script>/"+path);
+                    }
+                    wrapperScriptFiles.add(file);
+                }
+            }
+        }
+        return Collections.unmodifiableSet(wrapperScriptFiles); 
     }
 
     protected static ParameterInfo getFormalParam(final Map<String,ParameterInfoRecord> paramInfoMap, final String pname) {
