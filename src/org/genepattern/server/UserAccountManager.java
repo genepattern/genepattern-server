e5d513c26d30f9d47546b0ba2ecf04452679d2b4
peterjcarr <pcarr@broadinstitute.org>
2018-02-14 20:36:45 -0500

    fix for GP-6889, 'username.regex'
    * improved rules for username validation
    * added 'username.regex' configuration property

--- src/org/genepattern/server/UserAccountManager.java
+++ src/org/genepattern/server/UserAccountManager.java
@@ -6,6 +6,9 @@ package org.genepattern.server;
 import java.io.File;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.log4j.Logger;
 import org.genepattern.server.auth.AuthenticationException;
@@ -24,6 +27,8 @@ import org.genepattern.server.database.HibernateUtil;
 import org.genepattern.server.user.User;
 import org.genepattern.server.user.UserDAO;
 
+import com.google.common.base.Strings;
+
 /**
  * Common interface for managing user accounts and groups, used in the web application and soap server.
  * 
@@ -34,6 +39,29 @@ public class UserAccountManager {
     
     public static final String PROP_AUTHENTICATION_CLASS = "authentication.class";
     public static final String PROP_GROUP_MEMBERSHIP_CLASS = "group.membership.class";
+
+    /**
+     * The 'username.regex' is a regular expression which matches all valid usernames 
+     * and which must not match any invalid usernames. This pattern is used to validate
+     * prospective usernames via the java.util.regex.Pattern class.
+     * 
+     * @see "https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html" 
+     * 
+     * Java code:
+     * <pre>
+     * Pattern pattern = Pattern.compile(usernameRegex);
+       return pattern.matcher(username).matches();
+     * </pre>
+     * 
+     * <p>
+     * Example usage:
+     * <pre>
+     *     # match all alphanumeric characters and the '@' symbol
+     *     username.regex: "[A-Za-z0-9_@.\\- ]+"
+     * </pre>
+     */
+    public static final String PROP_USERNAME_REGEX="username.regex";
+    public static final String DEFAULT_USERNAME_REGEX="[A-Za-z0-9_@.\\- ]+";
 
     /**
      * @return the singleton instance of the UserAccountManager.
@@ -92,12 +120,12 @@ public class UserAccountManager {
         this.userGroups=userGroups;
     }
 
-    /** @deprecated */
+    /** @deprecated should pass in a valid GpConfig and userContext */
     public static void validateNewUsername(final String username) throws AuthenticationException {
         validateNewUsername(HibernateUtil.instance(), username);
     }
 
-    /** @deprecated, should pass in a valid GpConfig and userContext */
+    /** @deprecated should pass in a valid GpConfig and userContext */
     public static void validateNewUsername(final HibernateSessionManager mgr, final String username) throws AuthenticationException {
         final GpConfig gpConfig=ServerConfigurationFactory.instance();
         validateNewUsername(gpConfig, mgr, username);
@@ -113,7 +141,7 @@ public class UserAccountManager {
      */
     public static void validateNewUsername(final GpConfig gpConfig, final HibernateSessionManager mgr, final String username) throws AuthenticationException {
         //1) is it a valid username
-        validateUsername(username);
+        validateUsername(gpConfig, username);
         //2) is it a unique username
         User user = (new UserDAO(mgr)).findByIdIgnoreCase(username);
         if (user != null) {
@@ -133,6 +161,63 @@ public class UserAccountManager {
     }
 
     /**
+     * Validate the username with the {@code username.regex}
+     * Throw an exception if the username  does not match the pattern.
+     * {@link #PROP_USERNAME_REGEX}
+     * 
+     * 
+     * @throws AuthenticationException if the username is not valid
+     * 
+     * @see "https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html"
+     */
+    public static boolean validateUsernameFromRegex(final GpConfig gpConfig, final GpContext serverContext, final String username) 
+    throws AuthenticationException
+    {
+        final String regex;
+        if (gpConfig==null) {
+            regex=DEFAULT_USERNAME_REGEX;
+        }
+        else {
+            final String prop=gpConfig.getGPProperty(serverContext, PROP_USERNAME_REGEX, DEFAULT_USERNAME_REGEX);
+            if (Strings.nullToEmpty(prop).trim().isEmpty()) {
+                regex=DEFAULT_USERNAME_REGEX;
+                log.warn("ignoring empty property: "+PROP_USERNAME_REGEX+"='"+regex+"'");
+            }
+            else {
+                regex=prop;
+            }
+        }
+        try {
+            final Pattern pattern = Pattern.compile(regex);
+            //return pattern.matcher(username).matches();
+            final Matcher m = pattern.matcher(username);
+            final boolean valid=m.matches();
+            if (!valid) {
+                // remove matching characters
+                final String invalidChars=m.replaceAll("");
+                if (!Strings.isNullOrEmpty(invalidChars)) {
+                    throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME, 
+                        "Invalid username: '"+username+"': characters not allowed: '"+invalidChars+"'");
+                }
+                else {
+                    throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME, 
+                        "Invalid username: '"+username+"': does not match 'username.regex'");
+                }
+            }
+            return valid;
+        }
+        catch (PatternSyntaxException e) {
+            log.error("Invalid regex pattern, "+PROP_USERNAME_REGEX+"='"+regex+"'", e);
+            throw new AuthenticationException(AuthenticationException.Type.SERVICE_NOT_AVAILABLE,
+                "Server configuration error, invalid regex pattern");
+        }
+        catch (Throwable t) {
+            throw new AuthenticationException(AuthenticationException.Type.SERVICE_NOT_AVAILABLE,
+                "Unexpected server error: "+t.getLocalizedMessage());
+        }
+    }
+    
+    /**
      * Is the username valid for a GenePattern account. This does not check for similar names in the database.
      * It just enforces any rules on what constitutes a valid name.
      * <ul>
@@ -143,10 +228,14 @@ public class UserAccountManager {
      * @param username
      * @throws AuthenticationException if the username is not valid
      */
-    static public void validateUsername(String username) throws AuthenticationException {
+    static public void validateUsername(final GpConfig gpConfig, final String username) throws AuthenticationException {
         if (username == null) {
             throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
                     "Username is null");
+        }
+        if (username.trim().length()==0) {
+            throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
+                    "Username not set (empty string)");
         }
         if (username.startsWith(" ")) {
             throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME, 
@@ -192,6 +281,10 @@ public class UserAccountManager {
             throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
                     "Invalid username: '"+username+"': Can't contain a TAB character.");
         }
+        if (username.contains("\n")) {
+            throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
+                    "Invalid username: '"+username+"': Can't contain a NEWLINE character.");
+        }
         if (username.contains(";")) {
             throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
                     "Invalid username: '"+username+"': Can't contain a semicolon (';') character.");
@@ -200,6 +293,34 @@ public class UserAccountManager {
             throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
                     "Invalid username: '"+username+"': Can't start with the dot ('.') character.");
         }
+        if (username.startsWith("@")) {
+            throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
+                    "Invalid username: '"+username+"': Can't start with the dot ('@') character.");
+        }
+        // match the allowed username regex
+        validateUsernameFromRegex(gpConfig, GpContext.getServerContext(), username);
+
+        if (contains2(username, '@')) {
+            throw new AuthenticationException(AuthenticationException.Type.INVALID_USERNAME,
+                    "Invalid username: '"+username+"': Can't contain more than one '@' character");            
+        }
+    }
+    
+    /**
+     * Helper method, return true if the given string contains two or more of the given characters.
+     */
+    protected static boolean contains2(final String str, final char c) {
+        if (str==null || str.length()<2) {
+            return false;
+        }
+        int i=str.indexOf(c);
+        if (i>=0) {
+            i=str.indexOf(c,i+1);
+            if (i>=0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /** @deprecated */
@@ -236,14 +357,16 @@ public class UserAccountManager {
      * 
      * @param username
      * @param password
-     * @throws AuthenticationException - if the user is already registered.
+     * @throws AuthenticationException if the user is already registered.
+     * 
+     * @deprecated pass in a valid GpConfig and Hibernate session
      */
     public void createUser(String username, String password) throws AuthenticationException {
         String email = null;
-        createUser(username, password, email);
+        createUser(ServerConfigurationFactory.instance(), HibernateUtil.instance(), username, password, email);
     }
     
-    /** @deprecated */
+    /** @deprecated pass in a valid GpConfig and Hibernate session */
     public static void createUser(final String username, final String passwordOrNull, final String email) throws AuthenticationException {
         createUser(ServerConfigurationFactory.instance(), HibernateUtil.instance(), username, passwordOrNull, email);
     }
