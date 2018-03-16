92b461d322ac07cc73283f67ac7076cb2ffffeb6
peterjcarr <pcarr@broadinstitute.org>
2018-03-16 19:18:32 -0400

    gp-awsbatch-0.8.1-snapshot-03, (part 2) checking in GenericFileFilter

--- src/org/genepattern/server/util/GenericFileFilter.java
+++ src/org/genepattern/server/util/GenericFileFilter.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2003-2018 Regents of the University of California and Broad Institute. All rights reserved.
  *******************************************************************************/
-package org.genepattern.server.executor.awsbatch;
+package org.genepattern.server.util;
 
 import java.io.File;
 import java.io.FileFilter;
