91bcd6c4c88a6f9571094742d022e4d667eef989
peterjcarr <pcarr@broadinstitute.org>
2018-11-15 16:45:23 -0500

    doc update for GP-7141

--- src/org/genepattern/server/webapp/rest/api/v1/job/JobObjectCache.java
+++ src/org/genepattern/server/webapp/rest/api/v1/job/JobObjectCache.java
@@ -9,11 +9,12 @@ import org.genepattern.server.config.GpContext;
  * 
  * Configuration options in the config_custom.yaml file:
  * <pre>
-   # set a default value
-   jobCache.maximumSize: "10000"
-   
-   # to disable the cache, set the max size to 0
-   jobCache.maximumSize: "0"
+   # the max size of the cache, corresponds to number of objects
+   jobObjectCache.maximumSize: "10000"
+   # the number of days to keep the records in the cache
+   jobObjectCache.expireAfterWriteDays: 10
+   # to disable the cache
+   jobObjectCache.enabled: false
  * </pre>
  * 
  * @author pcarr
@@ -24,17 +25,18 @@ public class JobObjectCache {
     public static final String PROP_EXPIRE_AFTER_WRITE_DAYS="jobObjectCache.expireAfterWriteDays";
     
     public static boolean isEnabled(final GpConfig gpConfig, final GpContext serverContext) {
+        // default is false
         boolean enabled = gpConfig.getGPBooleanProperty(serverContext, PROP_ENABLED, false);
         return enabled;
     }
     
     public static long getMaximumSize(final GpConfig gpConfig, final GpContext serverContext) {
-        // default is a cache of size of 10,000 records
+        // default is 10,000 records
         return gpConfig.getGPLongProperty(serverContext, PROP_MAX_SIZE, 10000L);
     }
     
     public static long getExpireAfterWriteDays(final GpConfig gpConfig, final GpContext serverContext) {
-        // dedfault is 10 days
+        // default is 10 days
         return gpConfig.getGPLongProperty(serverContext, PROP_EXPIRE_AFTER_WRITE_DAYS, 10L);        
     }
 }
