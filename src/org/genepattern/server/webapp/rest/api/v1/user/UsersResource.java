439e8efd2cd221fce40c1ba415177b0249dfc49f
peterjcarr <pcarr@broadinstitute.org>
2019-01-25 19:27:01 -0500

    bug fix for stats report, '/rest/v1/users/new', GP-7541

--- src/org/genepattern/server/webapp/rest/api/v1/user/UsersResource.java
+++ src/org/genepattern/server/webapp/rest/api/v1/user/UsersResource.java
@@ -1,16 +1,21 @@
 package org.genepattern.server.webapp.rest.api.v1.user;
 
 /* ******************************************************************************
- * Copyright (c) 2003, 2017 Broad Institute, Inc. and Massachusetts Institute of Technology.  All rights reserved.
+ * Copyright (c) 2003, 2019 Broad Institute, Inc. and Massachusetts Institute of Technology.  All rights reserved.
  * ******************************************************************************/
 
 import org.apache.log4j.Logger;
 import org.genepattern.server.config.GpContext;
+import org.genepattern.server.database.HibernateUtil;
 import org.genepattern.server.user.User;
 import org.genepattern.server.user.UserDAO;
+import org.genepattern.server.webapp.rest.api.v1.DateUtil;
 import org.genepattern.server.webapp.rest.api.v1.Util;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.google.common.base.Strings;
+
 import org.json.JSONArray;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,9 +26,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -35,19 +37,25 @@ import java.util.List;
  */
 @Path("/v1/users")
 public class UsersResource {
-    final static private Logger log = Logger.getLogger(UsersResource.class);
-    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final Logger log = Logger.getLogger(UsersResource.class);
+    private static final long DAY_IN_MS = 1000 * 60 * 60 * 24;
 
     /**
-     * Get the list of new users that registered between the specified start date and end date (in yyyy-MM-dd HH:mm:ss format).
-     *
+     * Get the list of new user registrations between the start and end dates. 
+     * Dates are in ISO 8601 format, {yyyy-MM-dd}T{HH:mm:ss}.
+     * 
      * If no end date is provided, the query will use the present as the end date.
      * If no start date is provided, the query will assume one week ago.
-     *
-     * @param request
-     * @param startDate
-     * @param endDate
-     * @return
+     * 
+     * Template:
+     *   GET {gpUrl}/gp/rest/v1/users/new?start={startDate}&end={endDate}
+     * Example:
+     *   GET http://127.0.0.1:8080/gp/rest/v1/users/new?start=2018-01-01T23:00:00&end=2019-01-01T23:00:00
+     *   
+     * Example querys:
+     *   ?start=2018&end=2019   (dates between Jan 1, 2018 and Jan 1, 2019)
+     *   ?start=2018-01&end=2018-02
+     *   
      */
     @GET
     @Path("/new")
@@ -61,54 +69,104 @@ public class UsersResource {
         if (!admin) {
             return Response.status(401).entity("Only admins can query the user database!").build();
         }
-
+        
         // Parse and assign the start and end dates
-        Date endDateObj = null;
-        Date startDateObj = null;
-
+        final Date endDateObj;
+        final Date startDateObj;
         try {
-            // If no end date is provided, assume the present
-            if (endDate == null) {
-                endDateObj = Calendar.getInstance().getTime();
-            }
-            else { // Otherwise, parse the date string
-                endDateObj = dateFormat.parse(endDate);
-            }
-
-            // If no start date is provided, assume a week ago
-            if (startDate == null) {
-                long DAY_IN_MS = 1000 * 60 * 60 * 24;
-                startDateObj = new Date(System.currentTimeMillis() - (7 * DAY_IN_MS));
-            }
-            else { // Otherwise, parse the date string
-                startDateObj = dateFormat.parse(startDate);
-            }
+            final Date now=new Date();
+            endDateObj = parseEndDate(now, endDate);
+            startDateObj = parseStartDate(now, startDate);
         }
-        catch (ParseException e) {
-            return Response.status(400).entity("{'error': 'Error parsing date or end date'}").build();
+        catch (IllegalArgumentException e) {
+            return Response.status(400).entity("{'error': 'Error parsing date or end date'}").build();            
+        }
+        catch (Throwable t) {
+            log.error("Unexpected error parsing dates", t);
+            return Response.status(400).entity("{'error': 'Error parsing date or end date'}").build();            
         }
 
         // Query the database
-        UserDAO dao = new UserDAO();
-        List<User> users = dao.getNewUsers(startDateObj, endDateObj);
-
+        final List<User> users = getNewUsers(startDateObj, endDateObj);
         // Convert user list to JSON
-        JSONObject returnJSON = new JSONObject();
-        JSONArray usersJSONArray = new JSONArray();
-
+        final JSONObject returnJSON;
+        final String jsonStr;
         try {
-            for (User u : users) usersJSONArray.put(new UserJSON(u));
-            returnJSON.put("users", usersJSONArray);
+            returnJSON=toUsersJson(users);
+            int indentFactor=2;
+            jsonStr=returnJSON.toString(indentFactor);
         }
-        catch (JSONException e) {
-            return Response.status(500).entity("{'error': 'Could not build JSON object to return'}").build();
+        catch (Throwable t) {
+            log.error("Error formatting JSON output", t);
+            return Response.status(500)
+                .entity("{'error': 'Could not build JSON object to return'}")
+            .build();
         }
 
         // Return the user list object
-        return Response.ok().entity(returnJSON.toString()).build();
+        return Response.ok()
+            .entity(jsonStr)
+        .build();
     }
 
-    public class UserJSON extends JSONObject {
+    protected static Date parseEndDate(final String endDate) {
+        if (Strings.isNullOrEmpty(endDate)) {
+            return Calendar.getInstance().getTime();
+        }
+        return DateUtil.parseDate(endDate);
+    }
+
+    protected static Date parseEndDate(final Date now, final String endDate) {
+        // If no end date is provided, assume the present
+        if (Strings.isNullOrEmpty(endDate)) {
+            return now;
+        }
+        return DateUtil.parseDate(endDate);
+    }
+
+    protected static Date sevenDaysBefore(final Date now) {
+        return new Date(now.getTime() - (7 * DAY_IN_MS));
+    }
+
+    protected static Date parseStartDate(final Date now, final String startDate) 
+    {
+        // If no start date is provided, assume a week ago
+        if (Strings.isNullOrEmpty(startDate)) {
+            return sevenDaysBefore(now);
+        }
+        return DateUtil.parseDate(startDate);
+    }
+
+    // Query the database
+    protected static List<User> getNewUsers(final Date startDate, final Date endDate) {
+        final boolean inTransaction=HibernateUtil.isInTransaction();
+        try {
+            // Query the database
+            final UserDAO dao = new UserDAO(HibernateUtil.instance());
+            return dao.getNewUsers(startDate, endDate);
+        }
+        finally {
+            if (!inTransaction) {
+                HibernateUtil.closeCurrentSession();
+            }
+        }
+    }
+
+    // Convert user list to JSON
+    protected static JSONObject toUsersJson(final List<User> users) 
+    throws JSONException
+    {
+        JSONObject returnJSON = new JSONObject();
+        JSONArray usersJSONArray = new JSONArray();
+
+        for (final User u : users) {
+            usersJSONArray.put(new UserJSON(u));
+        }
+        returnJSON.put("users", usersJSONArray);
+        return returnJSON;
+    }
+
+    public static class UserJSON extends JSONObject {
         public UserJSON(User user) throws JSONException {
             this.put("username", user.getUserId());
             this.put("email", user.getEmail());
@@ -117,9 +175,8 @@ public class UsersResource {
             this.put("registered", formatDate(user.getRegistrationDate()));
         }
 
-        private String formatDate(Date date) {
-            if (date == null) return "";
-            return dateFormat.format(date);
+        public static String formatDate(final Date date) {
+            return DateUtil.toIso8601(date);
         }
 
         private String extractDomain(String email) {
