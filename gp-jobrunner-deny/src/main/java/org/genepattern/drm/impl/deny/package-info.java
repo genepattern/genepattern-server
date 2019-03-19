4853add2826403d186f0fc7b46ab083df03c0073
peterjcarr <pcarr@broadinstitute.org>
2019-03-19 13:39:52 -0400

    gp-jobrunner-deny, special-case job runner which rejects all new jobs

---
+++ gp-jobrunner-deny/src/main/java/org/genepattern/drm/impl/deny/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2003, 2018 Broad Institute, Inc. and Massachusetts Institute of Technology.
+ * All rights reserved.
+ */
+/**
+ * @author pcarr
+ *
+ */
+package org.genepattern.drm.impl.deny;
