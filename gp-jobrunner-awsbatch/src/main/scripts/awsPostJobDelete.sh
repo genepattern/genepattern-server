661a952a154d423ac2a99658d5111ceed1c57dbc
ted Liefeld <liefeld@broadinstitute.org>
2019-05-02 10:20:48 -0700

    Update GP-7115 to the postDeleteScript to make it also delete the mete dir from S3

--- gp-jobrunner-awsbatch/src/main/scripts/awsPostJobDelete.sh
+++ gp-jobrunner-awsbatch/src/main/scripts/awsPostJobDelete.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 # used to clear out S3 cache when a job is deleted
 
-#!/usr/bin/env bash
+shift
 
 # initialize aws-cli environment
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
@@ -11,4 +12,8 @@ source "${script_dir}/init-aws-cli-env.sh"
 
 #aws batch terminate-job --job-id $1 --reason "Cancelled from GenePattern"
 aws s3 rm $S3_ROOT$2 --recursive
+
+# also drop the meta dir. 
+META_DIR=${2}.meta
+aws s3 rm $S3_ROOT$META_DIR --recursive
 
