1d5857a3db3d91018c03b1afb5c047d124df9164
ted Liefeld <liefeld@broadinstitute.org>
2021-01-20 14:09:06 -0800

    remove non-generalized bits for deployment on AWS

--- gp-jobrunner-awsbatch/src/main/scripts/completeUpload.sh
+++ gp-jobrunner-awsbatch/src/main/scripts/completeUpload.sh
@@ -2,19 +2,8 @@
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 source "${script_dir}/init-aws-cli-env.sh"
 
-echo "Complete upload: $@ "
 
-# add an AWS profile statement if needed
-if [ "$2" ]; then
-   PROFILE=" --profile $2 "
-   echo "Profile is $PROFILE"
-else  
-   PROFILE=""
-fi
-
-aws s3api complete-multipart-upload --cli-input-json ${1}   --profile genepattern
-
-echo /Users/liefeld/AnacondaProjects/CondaInstall/anaconda3/bin/aws s3api complete-multipart-upload --cli-input-json ${1}   --profile genepattern
+aws s3api complete-multipart-upload --cli-input-json ${1}  
 
 
 # remove the temp input file to avoid wasting space
