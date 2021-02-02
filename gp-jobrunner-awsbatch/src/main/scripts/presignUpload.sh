2c22eedea5df3ab0c00c649aff4d7f9c0707be8a
ted Liefeld <liefeld@broadinstitute.org>
2021-02-02 08:11:57 -0800

    delete stale code

--- gp-jobrunner-awsbatch/src/main/scripts/presignUpload.sh
+++ gp-jobrunner-awsbatch/src/main/scripts/presignUpload.sh
@@ -3,15 +3,6 @@
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 source "${script_dir}/init-aws-cli-env.sh"
 
-
-
-echo "Using Python --> $@"
-#source /Users/liefeld/AnacondaProjects/CondaInstall/anaconda3/bin/activate base
 python ${script_dir}/presignUpload.py $@
-
-#echo "Using lambda" 
-#source  ${DIR}/presignUpload.sh.sh $@
-
-
 
 
