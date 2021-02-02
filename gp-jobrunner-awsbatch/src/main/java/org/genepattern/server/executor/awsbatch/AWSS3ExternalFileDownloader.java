822011b5a291de78f4e825e752ffdd186e8dc6e4
ted Liefeld <liefeld@broadinstitute.org>
2021-02-02 08:11:25 -0800

    Changed to use the JobRunner definition for the bucket and root path

--- gp-jobrunner-awsbatch/src/main/java/org/genepattern/server/executor/awsbatch/AWSS3ExternalFileDownloader.java
+++ gp-jobrunner-awsbatch/src/main/java/org/genepattern/server/executor/awsbatch/AWSS3ExternalFileDownloader.java
@@ -27,13 +27,31 @@ import org.json.JSONObject;
 public class AWSS3ExternalFileDownloader extends ExternalFileDownloader {
     private static Logger log = Logger.getLogger(AWSS3ExternalFileDownloader.class);
     
+    private String getBucketName(final GpConfig gpConfig, GpContext userContext) {
+        String aws_s3_root = gpConfig.getGPProperty(userContext, "aws-s3-root");
+        // pull the bucket name out of something like "s3://moduleiotest/gp-dev-ami"
+        int endIdx = aws_s3_root.indexOf("/", 5);
+        return aws_s3_root.substring(5,endIdx);
+    }
+    private String getBucketRoot(final GpConfig gpConfig, GpContext userContext) {
+        String aws_s3_root = gpConfig.getGPProperty(userContext, "aws-s3-root");
+        // pull the bucket root path out of something like "s3://moduleiotest/gp-dev-ami"
+        int endIdx = aws_s3_root.indexOf("/", 5);
+        return aws_s3_root.substring(endIdx+1);
+    }
+    
+    
     public void downloadFile(GpContext userContext, HttpServletRequest req, HttpServletResponse resp, File file) throws IOException {
         // For S3 file downloads, we want to generate a presigned URL to redirect to
         final GpConfig gpConfig=ServerConfigurationFactory.instance();
            
         
-        String bucket = gpConfig.getGPProperty(userContext, "upload.aws.s3.bucket", "gp-temp-test-bucket");
-        String bucketRoot = gpConfig.getGPProperty(userContext, "upload.aws.s3.bucket.root", "tedslaptop");
+        // backstop with the old params for a release
+        String bucket = getBucketName(gpConfig, userContext);
+        if (bucket == null) bucket = gpConfig.getGPProperty(userContext, "upload.aws.s3.bucket", null);
+        String bucketRoot = getBucketRoot(gpConfig, userContext);
+        if (bucketRoot == null) bucketRoot = gpConfig.getGPProperty(userContext, "upload.aws.s3.bucket.root", null);
+
         String profile = gpConfig.getGPProperty(userContext, "upload.aws.s3.profile", "");
         
         String awsfilepath = gpConfig.getGPProperty(userContext,"aws-batch-script-dir");
@@ -54,23 +72,23 @@ public class AWSS3ExternalFileDownloader extends ExternalFileDownloader {
             BufferedReader stdError = new BufferedReader(new  InputStreamReader(proc.getErrorStream()));
     
            // Read the output from the command
-           System.out.println("Here is the standard output of the command:\n");
+           log.debug("Here is the standard output of the command:\n");
            String s = null;
            String redirectUrl = null;
            while ((s = stdInput.readLine()) != null) {
-               System.out.println(s);
+               log.debug(s);
                redirectUrl = s;
            }
         
            // Read any errors from the attempted command
            System.out.println("Here is the standard error of the command (if any):\n");
            while ((s = stdError.readLine()) != null) {
-               System.out.println(s);
+               log.debug(s);
            }    
            resp.sendRedirect(redirectUrl);
            
         } catch (Exception e){
-            e.printStackTrace();
+            log.debug(e);
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
     
@@ -82,62 +100,6 @@ public class AWSS3ExternalFileDownloader extends ExternalFileDownloader {
     
     
     
-    // @Override
-    public void downloadFile2(GpContext userContext, HttpServletRequest req, HttpServletResponse resp, File file) throws IOException {
-        // For S3 file downloads, we want to generate a presigned URL to redirect to
-        final GpConfig gpConfig=ServerConfigurationFactory.instance();
-        
-        String bucket = gpConfig.getGPProperty(userContext, "upload.aws.s3.bucket", "gp-temp-test-bucket");
-        String bucketRoot = gpConfig.getGPProperty(userContext, "upload.aws.s3.bucket.root", "tedslaptop");
-        String profile = gpConfig.getGPProperty(userContext, "upload.aws.s3.profile", "");
-        String awsScriptDir = gpConfig.getGPProperty(userContext, "aws-batch-script-dir");
-        String signingScript = gpConfig.getGPProperty(userContext, "download.aws.s3.presigning.script");
-
-        
-        StringBuffer execBuff = new StringBuffer();
-        // "/Users/liefeld/AnacondaProjects/CondaInstall/anaconda3/bin/aws lambda invoke --function-name createPresignedPost --payload '{\"input\": { \"name\":\""+path+"\", \"fileType\": \""+mimeType+"\"}}' response.json --profile genepattern";
-        execBuff.append(awsScriptDir);
-        execBuff.append(signingScript);
-        execBuff.append(" ");
-        // need to include the path used for the real user dir
-        
-        execBuff.append(bucketRoot);
-        execBuff.append(file.getAbsolutePath());
-        execBuff.append(" ");
-        execBuff.append(bucket);
-        if (profile.length() > 0){
-            execBuff.append(" ");
-            execBuff.append(profile);
-        }
-        
-        System.out.println(execBuff.toString());
-        
-        Process proc = Runtime.getRuntime().exec(execBuff.toString());
-        try {
-            proc.waitFor();
-        
-            BufferedReader stdInput = new BufferedReader(new     InputStreamReader(proc.getInputStream()));
-            BufferedReader stdError = new BufferedReader(new  InputStreamReader(proc.getErrorStream()));
-    
-           // Read the output from the command
-           System.out.println("Here is the standard output of the command:\n");
-           String s = null;
-           String redirectUrl = null;
-           while ((s = stdInput.readLine()) != null) {
-               System.out.println(s);
-               redirectUrl = s;
-           }
-        
-           // Read any errors from the attempted command
-           System.out.println("Here is the standard error of the command (if any):\n");
-           while ((s = stdError.readLine()) != null) {
-               System.out.println(s);
-           }    
-           resp.sendRedirect(redirectUrl);
-        } catch (Exception e){
-            e.printStackTrace();
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
-    }
+   
 
 }
