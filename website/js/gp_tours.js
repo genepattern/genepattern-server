83aff014532f0cdff46d9acd2213022eccdb5866
bahill <bhill@broadinstitute.org>
2021-06-11 10:36:44 -0400

    add new webtour section for browser notification

--- website/js/gp_tours.js
+++ website/js/gp_tours.js
@@ -115,16 +115,24 @@ $(function()
         var steps = [
 
            {
+               intro:'<p>A browser notification is now be displayed when jobs complete.</p>'
+                   + ' <p><em>Note, that you may need to enable notifications in order to see these</em></p>'
+                   + ' <img src="images/browser_notification_webtourJune2021.jpg" alt="GenePattern Browser Notification" />
+               position: 'fixed';
+               bottom: 0;
+               right: 0;
+           }
+           {
                 element: '#left-nav-notebook-tab',
                 intro:  '<div class="tour-header"> Notebook Tab</div>'
-                   + ' <p>The Notebook tab lists public GenePattern notebooks available in the GenePattern Notebook Workspace.</p>'
+                   + ' <p>The Notebook tab lists GenePattern notebooks available in the GenePattern Notebook Workspace.</p>'
                    + ' <p>Selecting a notebook from this list will open a new browser tab in the GenePattern Notebook Workspace.</p>'
-                   + ' <p>If this is your first time in the GP Notebook Workspace, you will be presented with an intro tour in that space.</p>'
+                   + ' <p>If this is your first time in the <a href="https//notebook.genepattern.org" target="_blank">GP Notebook Workspace</a>, you will be presented with an intro tour in that space.</p>'
                    + ' <p><a href="https//notebook.genepattern.org" target="_blank">More information about GenePattern Notebook can be found here.</a></p>',
                 position: 'right'
             },
             {
-                intro: '<div class="tour-header"> Release Notes </div> For more details about this release '
+                intro: '<div class="tour-header"> Release Notes </div> For more details about the June 2021 release '
                     + ' please see the <a href="https://github.com/genepattern/genepattern-server/releases/latest" target="_blank">release notes</a>.'
             }];
 
@@ -141,7 +149,7 @@ $(function()
         intro.onbeforechange(function(targetElement)
         {
             //switch the active left navigation tab to the appropriate one for the step
-            if(intro._currentStep == 0)
+            if(intro._currentStep == 1)
             {
                 $(this).data("last-left-nav-tab", $("#left-nav").tabs( "option", "active"));
 
