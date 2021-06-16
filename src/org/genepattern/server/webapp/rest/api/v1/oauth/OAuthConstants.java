30f714968220c6c36c9ccbc6eef51e609a128a73
ted Liefeld <liefeld@broadinstitute.org>
2021-06-16 09:32:13 -0700

    First draft globus oauth.  Oauth login working but not integrated

---
+++ src/org/genepattern/server/webapp/rest/api/v1/oauth/OAuthConstants.java
@@ -0,0 +1,9 @@
+package org.genepattern.server.webapp.rest.api.v1.oauth;
+
+public class OAuthConstants {
+    public static final String OAUTH_AUTHORIZE_URL_KEY = "oauth.authorize.url";
+    public static final String OAUTH_CLIENT_ID_KEY = "oauth.client.id";
+    public static final String OAUTH_CLIENT_SECRET_KEY = "oauth.client.secret";
+    public static final String OAUTH_AUTHORIZE_SCOPES_KEY = "oauth.client.secret";
+ 
+}
