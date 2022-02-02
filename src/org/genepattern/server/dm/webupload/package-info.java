4eec06b851b99dcd98a610b172ab6fa508360962
ted Liefeld <liefeld@broadinstitute.org>
2022-02-02 13:17:37 -0800

    Updated copyright to 2022

--- src/org/genepattern/server/dm/webupload/package-info.java
+++ src/org/genepattern/server/dm/webupload/package-info.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003-2021 Regents of the University of California and Broad Institute. All rights reserved.
+ * Copyright (c) 2003-2022 Regents of the University of California and Broad Institute. All rights reserved.
  *******************************************************************************/
 /**
  * Support for web upload files, input files uploaded via the HTML input form as part of job submission.
